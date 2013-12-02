a6e0dd1596c59178cee8ca6408b2cc98e9909609
hirakuni45 <hira@rvf-rc45.net>
2013-12-03 00:13:07 +0900

    モニター機能追加中

--- rx63t_inverter/main.cpp
+++ rx63t_inverter/main.cpp
@@ -6,17 +6,20 @@
 //=====================================================================//
 #include <cstdint>
 #include "main.hpp"
-#include "system.hpp"
-#include "port.hpp"
-#include "cmt_io.hpp"
-#include "sci_io.hpp"
+#include "rx/rx63x/system.hpp"
+#include "rx/rx63x/port.hpp"
+#include "rx/rx63x/mpc.hpp"
+#include "rx/cmt_io.hpp"
+#include "rx/sci_io.hpp"
+#include "inv_monitor.hpp"
 
 namespace root {
 	device::cmt_io<device::CMT0> cmt_;
 	device::sci_io<device::SCI1> sci_;
+	utils::inv_monitor monitor_;
 }
 
-#if 0
+
 //-----------------------------------------------------------------//
 /*!
 	@brief	SCI 文字出力
@@ -63,9 +66,8 @@ uint32_t sci_length(void)
 {
 	return root::sci_.length();
 }
-#endif
 
-#if 0
+
 //-----------------------------------------------------------------//
 /*!
 	@brief	10ms 単位タイマー
@@ -78,7 +80,6 @@ void delay_10ms(uint32_t n)
 		--n;
 	}
 }
-#endif
 
 static volatile uint8_t dummy_;
 
@@ -109,52 +110,54 @@ int main(int argc, char** argv)
 	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
 	wait_();
 
-	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(3)		// 1/8
-						  | device::SYSTEM::SCKCR.ICK.b(1)		// 1/2
-						  | device::SYSTEM::SCKCR.BCK.b(2)		// 1/4
-						  | device::SYSTEM::SCKCR.PCKA.b(1)		// 1/2
-						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/4
-						  | device::SYSTEM::SCKCR.PCKC.b(3)		// 1/8
-						  | device::SYSTEM::SCKCR.PCKD.b(3);	// 1/8
+	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(3)		// 1/8 (192/8=24)
+						  | device::SYSTEM::SCKCR.ICK.b(1)		// 1/2 (192/2=96)
+						  | device::SYSTEM::SCKCR.BCK.b(2)		// 1/4 (192/4=48)
+						  | device::SYSTEM::SCKCR.PCKA.b(1)		// 1/2 (192/2=96)
+						  | device::SYSTEM::SCKCR.PCKB.b(2)		// 1/4 (192/4=48)
+						  | device::SYSTEM::SCKCR.PCKC.b(3)		// 1/8 (192/8=24)
+						  | device::SYSTEM::SCKCR.PCKD.b(3);	// 1/8 (192/8=24)
 	device::SYSTEM::SCKCR3.CKSEL = 4;	///< PLL 回路選択
 
-///	device::SYSTEM::PRCR = 0xa500;	///< クロック、低消費電力、関係書き込み禁止
+	// Signal LED 出力設定
+	device::PORTB::PDR.B7 = 1; // PORTB:B7 output
+	device::PORTB::PODR.B7 = 1; // LED Off
 
-	// SCI の初期化（RXD0, TXD0）
-//	device::PORT2::ICR.B1 = 1;		// PORT2:B1 input (RXD0)
-//	sci_.initialize(256, 512, 3);
-//	sci_.set_baud(115200);
+	// SCI1 の初期化（PD5:RXD1:input, PD3:TXD1:output）
+	device::PORTD::PDR.B3 = 1;
+	device::MPC::PWPR.B0WI = 0;				// PWPR 書き込み許可
+	device::MPC::PWPR.PFSWE = 1;			// PxxPFS 書き込み許可
+	device::MPC::PD3PFS.PSEL = 0b01010;		// TXD1 設定
+	device::MPC::PD5PFS.PSEL = 0b01010;		// RXD1 設定
+	device::MPC::PWPR = device::MPC::PWPR.B0WI.b();	// MPC 書き込み禁止
+	device::PORTD::PMR.B3 = 1;
+	device::PORTD::PMR.B5 = 1;
+	static const uint8_t sci_irq_level = 3;
+	sci_.initialize(sci_irq_level);
+	sci_.start(115200);
 
 	// 100Hz タイマー設定
 	cmt_.set_clock(F_PCKA);
 	static const uint8_t cmt_irq_level = 1;
 	cmt_.initialize(100, cmt_irq_level);
 
-	// 「FatFs のタイマー」コールバックを設定
-//	cmt_.set_task(disk_timerproc);
-//	syscalls_initialize();
-	device::PORTB::PDR.B7 = 1; // PORTB:B7 output
-	device::PORTB::PODR.B7 = 0; // LED On
-
 	cmt_.sync();
-	device::PORTB::PODR.B7 = 1; // LED Off
 
-//	monitor_.initialize();
+	monitor_.initialize();
+
+///	device::SYSTEM::PRCR = 0xa500;	///< クロック、低消費電力、関係書き込み禁止
 
 	uint32_t cnt = 0;
 
 	while(1) {
 		cmt_.sync();
 
-///		syscalls_service();
-
-///		monitor_.service();
+		monitor_.service();
 
         ++cnt;
         if(cnt >= 100) {
             cnt = 0;
         }
-
 		if(cnt < 50) {
 			device::PORTB::PODR.B7 = 1; // LED Off
 		} else {
