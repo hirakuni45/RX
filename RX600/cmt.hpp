c96aa38dfdd987e70a06ec95be9227d2e8020e39
hira <hira@rvf-rc45.net>
2016-10-18 00:18:58 +0900

    update peripheral

--- RX600/cmt.hpp
+++ RX600/cmt.hpp
@@ -7,15 +7,22 @@
 */
 //=====================================================================//
 #include "common/io_utils.hpp"
+#if defined(SIG_RX63T)
+#include "RX63T/peripheral.hpp"
+#elif defined(SIG_RX64M)
+#include "RX64M/peripheral.hpp"
+#endif
 
 namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief  CMT 定義基底クラス
+		@param[in]	base	ベース・アドレス
+		@param[in]	t		ペリフェラル
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <uint32_t base>
+	template <uint32_t base, peripheral t>
 	struct cmt_t {
 
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
@@ -23,15 +30,15 @@ namespace device {
 			@brief  CMSTR0 レジスタ
 		*/
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-		typedef rw16_t<0x00088000> cmstr0_io;
-		struct cmstr0_t : public cmstr0_io {
-			using cmstr0_io::operator =;
-			using cmstr0_io::operator ();
-			using cmstr0_io::operator |=;
-			using cmstr0_io::operator &=;
+		struct cmstr0_t : public rw16_t<0x00088000> {
+			typedef rw16_t<0x00088000> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
 
-			bit_rw_t<cmstr0_io, bitpos::B0>	STR0;
-			bit_rw_t<cmstr0_io, bitpos::B1>	STR1;
+			bit_rw_t<io_, bitpos::B0> STR0;
+			bit_rw_t<io_, bitpos::B1> STR1;
 		};
 		static cmstr0_t	CMSTR0;
 
@@ -41,15 +48,15 @@ namespace device {
 			@brief  CMSTR1 レジスタ
 		*/
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-		typedef rw16_t<0x00088010> cmstr1_io;
-		struct cmstr1_t : public cmstr1_io {
-			using cmstr1_io::operator =;
-			using cmstr1_io::operator ();
-			using cmstr1_io::operator |=;
-			using cmstr1_io::operator &=;
+		struct cmstr1_t : public rw16_t<0x00088010> {
+			typedef rw16_t<0x00088010> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
 
-			bit_rw_t<cmstr1_io, bitpos::B0>	STR2;
-			bit_rw_t<cmstr1_io, bitpos::B1>	STR3;
+			bit_rw_t<io_, bitpos::B0> STR2;
+			bit_rw_t<io_, bitpos::B1> STR3;
 		};
 		static cmstr1_t	CMSTR1;
 
@@ -59,15 +66,15 @@ namespace device {
 			@brief  CMCR レジスタ
 		*/
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-		typedef rw16_t<base + 0x00> cmcr_io;
-		struct cmcr_t : public cmcr_io {
-			using cmcr_io::operator =;
-			using cmcr_io::operator ();
-			using cmcr_io::operator |=;
-			using cmcr_io::operator &=;
+		struct cmcr_t : public rw16_t<base + 0x00> {
+			typedef rw16_t<base + 0x00> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
 
-			bits_rw_t<cmcr_io, bitpos::B0, 2>	CKS;
-			bit_rw_t <cmcr_io, bitpos::B6>	CMIE;
+			bits_rw_t<io_, bitpos::B0, 2> CKS;
+			bit_rw_t <io_, bitpos::B6>    CMIE;
 		};
 		static cmcr_t CMCR;
 
@@ -97,10 +104,19 @@ namespace device {
 		static uint32_t get_chanel() {
 			return (base >> 3) & 3;
 		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ペリフェラル型を返す
+			@return ペリフェラル型
+		*/
+		//-----------------------------------------------------------------//
+		static peripheral get_peripheral() { return t; }
 	};
 
-	typedef cmt_t<0x00088002> CMT0;
-	typedef cmt_t<0x00088008> CMT1;
-	typedef cmt_t<0x00088012> CMT2;
-	typedef cmt_t<0x00088018> CMT3;
+	typedef cmt_t<0x00088002, peripheral::CMT0> CMT0;
+	typedef cmt_t<0x00088008, peripheral::CMT1> CMT1;
+	typedef cmt_t<0x00088012, peripheral::CMT2> CMT2;
+	typedef cmt_t<0x00088018, peripheral::CMT3> CMT3;
 }
