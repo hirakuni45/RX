4be351a2b863dd0aa55e8f0ac7d7db3788051488
hira <hira@rvf-rc45.net>
2016-10-20 17:10:44 +0900

    update kick 80MHz

--- rx24t_first_sample/main.cpp
+++ rx24t_first_sample/main.cpp
@@ -5,7 +5,7 @@
     @author 平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
-// #include "RX24T/system.hpp"
+#include "RX24T/system.hpp"
 #include "RX600/port.hpp"
 
 namespace {
@@ -24,32 +24,33 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-#if 0
 	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
 
-	device::SYSTEM::MOSCWTCR = 9;	// 1ms wait
-	// メインクロック強制発振とドライブ能力設定
-	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV2.b(0b10)
-						  | device::SYSTEM::MOFCR.MOFXIN.b(1);			
-	device::SYSTEM::MOSCCR.MOSTP = 0;		// メインクロック発振器動作
-	wait_delay_(5000);
+	device::SYSTEM::MEMWAIT = 0b10; // 80MHz 動作 wait 設定
 
-	device::SYSTEM::PLLCR.STC = 0b010011;		// PLL 10 倍(120MHz)
+	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
+	device::SYSTEM::OPCCR = 0;  // 高速モード選択
+	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
+
+	// clock osc 10MHz
+	device::SYSTEM::MOSCWTCR = 9;	// 4ms wait
+	// メインクロック・ドライブ能力設定、内部発信
+	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV21.b(1);
+	device::SYSTEM::MOSCCR.MOSTP = 0;  // メインクロック発振器動作
+	while(device::SYSTEM::OSCOVFSR.MOOVF() == 0) asm("nop");
+
+	device::SYSTEM::PLLCR.STC = 0b001111;		// PLL input: 1, PLL 8 倍(80MHz)
 	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
-	wait_delay_(5000);
+	while(device::SYSTEM::OSCOVFSR.PLOVF() == 0) asm("nop");
 
-	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (120/1=120)
-						  | device::SYSTEM::SCKCR.BCK.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (120/1=120)
-						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKC.b(1)		// 1/2 (120/2=60)
+	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(2)		// 1/4 (80/4=20)
+						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (80/1=80)
+						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (80/1=80)
+						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (80/2=40)
 						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
-	device::SYSTEM::SCKCR2.UCK = 0b0100;  // USB Clock: 1/5 (120/5=24)
 	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
-#endif
 
-	uint32_t wait = 10000;
+	uint32_t wait = 1000000;
 	device::PORT0::PDR.B0 = 1; // output
 	while(1) {
 		wait_delay_(wait);
