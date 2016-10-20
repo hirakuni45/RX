38e58676f3355eedd8c9da27624c8965299441e2
hira <hira@rvf-rc45.net>
2016-10-20 21:36:57 +0900

    update tune soft delay loop

--- common/delay.hpp
+++ common/delay.hpp
@@ -23,15 +23,24 @@ namespace utils {
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  マイクロ秒単位の待ち
+			@brief  マイクロ秒単位の待ち @n
+					あまり正確では無く、速度に関連する設定に依存します。
 			@param[in]	us	待ち時間（マイクロ秒）
 		*/
 		//-----------------------------------------------------------------//
 		static void micro_second(uint32_t us) {
 			while(us > 0) {
+#if defined(SIG_RX64M)
 				for(uint32_t n = 0; n < (F_PCKA / 4285714); ++n) {
 					asm("nop");
 				}
+#elif defined(SIG_RX24T)
+				for(uint32_t n = 0; n < (F_PCKA / 5333333); ++n) {
+					asm("nop");
+				}
+#else
+#  error "delay.hpp requires tune dummy operations"
+#endif
 				--us;
 			}
 		}
