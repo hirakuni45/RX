5d498aa90fe3ccc5b0875d9b2f250561817c5276
hira <hira@rvf-rc45.net>
2016-10-28 01:34:23 +0900

    update command

--- rx24t_DS3231_sample/main.cpp
+++ rx24t_DS3231_sample/main.cpp
@@ -10,7 +10,7 @@
 #include "common/fifo.hpp"
 #include "common/format.hpp"
 #include "common/iica_io.hpp"
-
+#include "common/command.hpp"
 #include "chip/DS3231.hpp"
 
 namespace {
@@ -31,6 +31,7 @@ namespace {
 
 	chip::DS3231<I2C> rtc_(i2c_);
 
+	utils::command<128> command_;
 
 	time_t get_time_()
 	{
@@ -40,6 +41,7 @@ namespace {
 		}
 		return t;
 	}
+
 
 	void disp_time_(time_t t)
 	{
@@ -53,12 +55,107 @@ namespace {
 			% static_cast<uint32_t>(m->tm_sec)
 			% static_cast<uint32_t>(m->tm_year + 1900);
 	}
+
+
+	const char* get_dec_(const char* p, char tmch, int& value) {
+		int v = 0;
+		char ch;
+		while((ch = *p) != 0) {
+			++p;
+			if(ch == tmch) {
+				break;
+			} else if(ch >= '0' && ch <= '9') {
+				v *= 10;
+				v += ch - '0';
+			} else {
+				return nullptr;
+			}
+		}
+		value = v;
+		return p;
+	}
+
+
+	void set_time_date_()
+	{
+		time_t t = get_time_();
+		if(t == 0) return;
+
+		struct tm *m = localtime(&t);
+		bool err = false;
+		if(command_.get_words() == 3) {
+			char buff[12];
+			if(command_.get_word(1, sizeof(buff), buff)) {
+				const char* p = buff;
+				int vs[3];
+				uint8_t i;
+				for(i = 0; i < 3; ++i) {
+					p = get_dec_(p, '/', vs[i]);
+					if(p == nullptr) {
+						break;
+					}
+				}
+				if(p != nullptr && p[0] == 0 && i == 3) {
+					if(vs[0] >= 1900 && vs[0] < 2100) m->tm_year = vs[0] - 1900;
+					if(vs[1] >= 1 && vs[1] <= 12) m->tm_mon = vs[1] - 1;
+					if(vs[2] >= 1 && vs[2] <= 31) m->tm_mday = vs[2];		
+				} else {
+					err = true;
+				}
+			}
+
+			if(command_.get_word(2, sizeof(buff), buff)) {
+				const char* p = buff;
+				int vs[3];
+				uint8_t i;
+				for(i = 0; i < 3; ++i) {
+					p = get_dec_(p, ':', vs[i]);
+					if(p == nullptr) {
+						break;
+					}
+				}
+				if(p != nullptr && p[0] == 0 && (i == 2 || i == 3)) {
+					if(vs[0] >= 0 && vs[0] < 24) m->tm_hour = vs[0];
+					if(vs[1] >= 0 && vs[1] < 60) m->tm_min = vs[1];
+					if(i == 3 && vs[2] >= 0 && vs[2] < 60) m->tm_sec = vs[2];
+					else m->tm_sec = 0;
+				} else {
+					err = true;
+				}
+			}
+		}
+
+		if(err) {
+			utils::format("Can't analize Time/Date input.\n");
+			return;
+		}
+
+		time_t tt = mktime(m);
+		if(!rtc_.set_time(tt)) {
+			utils::format("Stall RTC write...\n");
+		}
+	}
 }
 
 extern "C" {
 	void sci_putch(char ch)
 	{
 		sci_.putch(ch);
+	}
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
 	}
 }
 
@@ -92,37 +189,37 @@ int main(int argc, char** argv)
 						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
 	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
 
-	// タイマー設定（６０Ｈｚ）
-	cmt_.set_clock(F_PCKB);
-	uint8_t cmt_irq_level = 4;
-	cmt_.start(60, cmt_irq_level);
+	{  // タイマー設定（６０Ｈｚ）
+		uint8_t intr_level = 4;
+		cmt_.start(60, intr_level);
+	}
 
-	// SCI 設定
-	static const uint8_t sci_level = 2;
-	sci_.start(115200, sci_level);
+	{  // SCI 設定
+		uint8_t intr_level = 2;
+		sci_.start(115200, intr_level);
+	}
 
-	// IICA(I2C) の開始
-	{
+	{  // IICA(I2C) の開始
 		uint8_t intr_level = 0;
 		if(!i2c_.start(I2C::speed::fast, intr_level)) {
-//		if(!i2c_.start(I2C::speed::standard, intr_level)) {
 			utils::format("IICA start error (%d)\n") % static_cast<uint32_t>(i2c_.get_last_error());
 		}
 	}
 
-	utils::format("RX24T start\n");
+	utils::format("RX24T DS3231 sample\n");
+
+	// DS3231(RTC) の開始
+	if(!rtc_.start()) {
+		utils::format("Stall RTC start (%d)\n") % static_cast<uint32_t>(i2c_.get_last_error());
+	}
+
+	command_.set_prompt("# ");
 
 	device::PORT0::PDR.B0 = 1; // output
 
 	uint32_t cnt = 0;
-	uint32_t n = 0;
 	while(1) {
 		cmt_.sync();
-
-		if(sci_.length()) {
-			auto ch = sci_.getch();
-			sci_.putch(ch);
-		}
 
 		++cnt;
 		if(cnt >= 30) {
@@ -130,13 +227,29 @@ int main(int argc, char** argv)
 		}
 		device::PORT0::PODR.B0 = (cnt < 10) ? 0 : 1;
 
-		if((n % 60) == 0) {
-			time_t t = get_time_();
-			if(t != 0) {
-				disp_time_(t);
+		// コマンド入力と、コマンド解析
+		if(command_.service()) {
+			uint8_t cmdn = command_.get_words();
+			if(cmdn >= 1) {
+				if(command_.cmp_word(0, "date")) {
+					if(cmdn == 1) {
+						time_t t = get_time_();
+						if(t != 0) {
+							disp_time_(t);
+						}
+					} else {
+						set_time_date_();
+					}
+				} else if(command_.cmp_word(0, "help")) {
+					utils::format("date\n");
+					utils::format("date yyyy/mm/dd hh:mm[:ss]\n");
+				} else {
+					char buff[128];
+					if(command_.get_word(0, sizeof(buff), buff)) {
+						utils::format("Command error: %s\n") % buff;
+					}
+				}
 			}
-//			utils::format("%d\n") % (n / 60);
 		}
-		++n;
 	}
 }
