826ad1ba3aeed56f4bbd5993601502e08d214bdf
hira <hira@rvf-rc45.net>
2016-11-01 04:09:25 +0900

    new file

---
+++ RX24T/dtc.hpp
@@ -0,0 +1,94 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX24T グループ・データトランスファコントローラ（DTCa）定義 @n
+			Copyright 2016 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/io_utils.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  データトランスファコントローラ（DTCa）
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct dtc_t {
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  DTC コントロールレジスタ（DTCCR）
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct dtccr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B4> RRS;
+		};
+		static dtccr_t<0x00082400> DTCCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  DTC アドレスモードレジスタ（DTCADMOD）
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct dtcadmod_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> SHORT;
+		};
+		static dtcadmod_t<0x00082408> DTCADMOD;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  DTC モジュール起動レジスタ（DTCST）
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct dtcst_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> DTCST;
+		};
+		static dtcst_t<0x0008240C> DTCST;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  DTC ステータスレジスタ（DTCSTS）
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct dtcsts_t : public rw16_t<base> {
+			typedef rw16_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 8> VECN;
+			bit_rw_t <io_, bitpos::B15>   ACT;
+		};
+		static dtcsts_t<0x0008240E> DTCSTS;
+
+	};
+	typedef dtc_t DTC;
+}
