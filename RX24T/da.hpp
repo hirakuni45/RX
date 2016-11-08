8aca35d94f442ad12e93ab320dbb4184bc5f7b8a
hira <hira@rvf-rc45.net>
2016-11-08 15:56:08 +0900

    new file

---
+++ RX24T/da.hpp
@@ -0,0 +1,68 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX24T グループ・D/A 定義 @n
+			Copyright 2016 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/io_utils.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  コンパレータ C 用リファレンス電圧生成専用 D/A コンバータ（DA）
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct da_t {
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A データレジスタ 0（DADR0）
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x000880C0> DADR0;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A 制御レジスタ（DACR）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct dacr_t : public rw8_t<ofs> {
+			typedef rw16_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B6> DAOE0;
+		};
+		static dacr_t<0x000880C4> DACR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  DADR0 フォーマット選択レジスタ（DADPR）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct dadpr_t : public rw8_t<ofs> {
+			typedef rw16_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B7> DPSEL;
+		};
+		static dadpr_t<0x000880C5> DADPR;
+
+	};
+	typedef da_t DA;
+
+}
