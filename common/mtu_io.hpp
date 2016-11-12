6cc9ae87f9fff741d9ed2319ade3ccab3a854514
hira <hira@rvf-rc45.net>
2016-11-13 01:08:48 +0900

    new file

---
+++ common/mtu_io.hpp
@@ -0,0 +1,11 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX グループ MTU 制御 @n
+			Copyright 2016 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#if defined(SIG_RX24T)
+#include "RX24T/mtu_io.hpp"
+#endif
