9b38f961201fa9cf28043773412ef4a30552ab89
hira <hira@rvf-rc45.net>
2016-11-13 18:00:55 +0900

    add DOC

--- common/renesas.hpp
+++ common/renesas.hpp
@@ -29,6 +29,7 @@
 #include "RX24T/s12ad.hpp"
 #include "RX24T/da.hpp"
 #include "RX24T/cmpc.hpp"
+#include "RX24T/doc.hpp"
 #include "RX24T/port_map.hpp"
 #include "RX24T/power_cfg.hpp"
 #include "RX24T/icu_mgr.hpp"
