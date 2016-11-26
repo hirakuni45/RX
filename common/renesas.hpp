262bab2208a2e9979e698fea4f907825d75fe920
hira <hira@rvf-rc45.net>
2016-11-26 22:23:37 +0900

    add bus.hpp

--- common/renesas.hpp
+++ common/renesas.hpp
@@ -49,6 +49,7 @@
 #include "RX600/port.hpp"
 #include "RX600/cmt.hpp"
 #include "RX64M/system.hpp"
+#include "RX64M/bus.hpp"
 #include "RX64M/mpc.hpp"
 #include "RX64M/icu.hpp"
 #include "RX64M/sci.hpp"
