2716ff6fc690f61acf870edfb94fc98188c53513
hira <hira@rvf-rc45.net>
2016-11-26 22:25:48 +0900

    new project

---
+++ rx64m_SDRAM_sample/main.cpp
@@ -0,0 +1,45 @@
+//=====================================================================//
+/*! @file
+    @brief  RX64M ファースト・サンプル @n
+			・P07(176) ピンに赤色LED（VF:1.9V）を吸い込みで接続する
+    @author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
+
+	device::SYSTEM::MOSCWTCR = 9;	// 1ms wait
+	// メインクロック強制発振とドライブ能力設定
+	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV2.b(0b10)
+						  | device::SYSTEM::MOFCR.MOFXIN.b(1);			
+	device::SYSTEM::MOSCCR.MOSTP = 0;		// メインクロック発振器動作
+//	wait_delay_(5000);
+
+	device::SYSTEM::PLLCR.STC = 0b010011;		// PLL 10 倍(120MHz)
+	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
+//	wait_delay_(5000);
+
+	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(1)		// 1/2 (120/2=60)
+						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (120/1=120)
+						  | device::SYSTEM::SCKCR.BCK.b(1)		// 1/2 (120/2=60)
+						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (120/1=120)
+						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (120/2=60)
+						  | device::SYSTEM::SCKCR.PCKC.b(1)		// 1/2 (120/2=60)
+						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
+	device::SYSTEM::SCKCR2.UCK = 0b0100;  // USB Clock: 1/5 (120/5=24)
+	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
+
+
+	device::PORT0::PDR.B7 = 1; // output
+	while(1) {
+//		wait_delay_(wait);
+		device::PORT0::PODR.B7 = 0;
+//		wait_delay_(wait);
+		device::PORT0::PODR.B7 = 1;
+	}
+}
