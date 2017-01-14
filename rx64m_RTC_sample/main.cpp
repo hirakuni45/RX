6ca5605faa8bd6a732cb93497c5c1898e92de201
hira <hira@rvf-rc45.net>
2017-01-14 23:16:05 +0900

    update date/time manage

--- rx64m_RTC_sample/main.cpp
+++ rx64m_RTC_sample/main.cpp
@@ -10,7 +10,7 @@
 #include "rx64m/rtc_io.hpp"
 #include "common/fifo.hpp"
 #include "common/format.hpp"
-#include <random>
+#include "common/command.hpp"
 
 namespace {
 
@@ -26,12 +26,133 @@ namespace {
 	device::sci_io<device::SCI1, buffer, buffer> sci_;
 
 	utils::rtc_io rtc_;
+
+	utils::command<64> command_;
+
 }
 
 extern "C" {
 	void sci_putch(char ch)
 	{
 		sci_.putch(ch);
+	}
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+
+	time_t get_time_()
+	{
+		time_t t = 0;
+		if(!rtc_.get_time(t)) {
+			utils::format("Stall RTC read\n");
+		}
+		return t;
+	}
+
+
+	void disp_time_(time_t t)
+	{
+		struct tm *m = localtime(&t);
+		utils::format("%s %s %d %02d:%02d:%02d  %4d\n")
+			% get_wday(m->tm_wday)
+			% get_mon(m->tm_mon)
+			% static_cast<uint32_t>(m->tm_mday)
+			% static_cast<uint32_t>(m->tm_hour)
+			% static_cast<uint32_t>(m->tm_min)
+			% static_cast<uint32_t>(m->tm_sec)
+			% static_cast<uint32_t>(m->tm_year + 1900);
+	}
+
+
+	const char* get_dec_(const char* p, char tmch, int& value) {
+		int v = 0;
+		char ch;
+		while((ch = *p) != 0) {
+			++p;
+			if(ch == tmch) {
+				break;
+			} else if(ch >= '0' && ch <= '9') {
+				v *= 10;
+				v += ch - '0';
+			} else {
+				return nullptr;
+			}
+		}
+		value = v;
+		return p;
+	}
+
+
+	void set_time_date_()
+	{
+		time_t t = get_time_();
+		if(t == 0) return;
+
+		struct tm *m = localtime(&t);
+		bool err = false;
+		if(command_.get_words() == 3) {
+			char buff[12];
+			if(command_.get_word(1, sizeof(buff), buff)) {
+				const char* p = buff;
+				int vs[3];
+				uint8_t i;
+				for(i = 0; i < 3; ++i) {
+					p = get_dec_(p, '/', vs[i]);
+					if(p == nullptr) {
+						break;
+					}
+				}
+				if(p != nullptr && p[0] == 0 && i == 3) {
+					if(vs[0] >= 1900 && vs[0] < 2100) m->tm_year = vs[0] - 1900;
+					if(vs[1] >= 1 && vs[1] <= 12) m->tm_mon = vs[1] - 1;
+					if(vs[2] >= 1 && vs[2] <= 31) m->tm_mday = vs[2];		
+				} else {
+					err = true;
+				}
+			}
+
+			if(command_.get_word(2, sizeof(buff), buff)) {
+				const char* p = buff;
+				int vs[3];
+				uint8_t i;
+				for(i = 0; i < 3; ++i) {
+					p = get_dec_(p, ':', vs[i]);
+					if(p == nullptr) {
+						break;
+					}
+				}
+				if(p != nullptr && p[0] == 0 && (i == 2 || i == 3)) {
+					if(vs[0] >= 0 && vs[0] < 24) m->tm_hour = vs[0];
+					if(vs[1] >= 0 && vs[1] < 60) m->tm_min = vs[1];
+					if(i == 3 && vs[2] >= 0 && vs[2] < 60) m->tm_sec = vs[2];
+					else m->tm_sec = 0;
+				} else {
+					err = true;
+				}
+			}
+		}
+
+		if(err) {
+			sci_puts("Can't analize Time/Date input.\n");
+			return;
+		}
+
+		time_t tt = mktime(m);
+		if(!rtc_.set_time(tt)) {
+			sci_puts("Stall RTC write...\n");
+		}
 	}
 }
 
@@ -112,18 +233,41 @@ int main(int argc, char** argv)
 		rtc_.start();
 	}
 
-
 	utils::format("RX64M RTC sample\n");
 
 	device::PORT0::PDR.B7 = 1; // output
 
+	command_.set_prompt("# ");
+
 	uint32_t cnt = 0;
+	char buff[16];
 	while(1) {
 		cmt_.sync();
 
-		if(sci_.recv_length()) {
-			auto ch = sci_.getch();
-			sci_.putch(ch);
+		// コマンド入力と、コマンド解析
+		if(command_.service()) {
+			uint8_t cmdn = command_.get_words();
+			if(cmdn >= 1) {
+				if(command_.cmp_word(0, "date")) {
+					if(cmdn == 1) {
+						time_t t = get_time_();
+						if(t != 0) {
+							disp_time_(t);
+						}
+					} else {
+						set_time_date_();
+					}
+				} else if(command_.cmp_word(0, "help")) {
+					sci_puts("date\n");
+					sci_puts("date yyyy/mm/dd hh:mm[:ss]\n");
+				} else {
+					if(command_.get_word(0, sizeof(buff), buff)) {
+						sci_puts("Command error: ");
+						sci_puts(buff);
+						sci_putch('\n');
+					}
+				}
+			}
 		}
 
 		++cnt;
