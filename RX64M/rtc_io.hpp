7deec77ab5e07782ceed3cd47c430a9f1d22ddaf
hira <hira@rvf-rc45.net>
2017-01-14 23:15:43 +0900

    update RTC class

--- RX64M/rtc_io.hpp
+++ RX64M/rtc_io.hpp
@@ -7,6 +7,7 @@
 */
 //=====================================================================//
 #include "RX64M/rtc.hpp"
+#include "RX64M/system.hpp"
 #include "common/time.h"
 
 namespace utils {
@@ -17,6 +18,15 @@ namespace utils {
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	class rtc_io {
+
+		static uint32_t get_() {
+			uint32_t t;
+			t  = static_cast<uint32_t>(device::RTC.BCNT0());
+			t |= static_cast<uint32_t>(device::RTC.BCNT1()) << 8;
+			t |= static_cast<uint32_t>(device::RTC.BCNT2()) << 16;
+			t |= static_cast<uint32_t>(device::RTC.BCNT3()) << 24;
+			return t;
+		}
 
 	public:
 		//-----------------------------------------------------------------//
@@ -34,31 +44,55 @@ namespace utils {
 		 */
 		//-----------------------------------------------------------------//
 		bool start() {
+			device::SYSTEM::SOSCCR.SOSTP = 0;
+
+			device::RTC.RCR3 = device::RTC.RCR3.RTCEN.b(1) | device::RTC.RCR3.RTCDV.b(0b110);
+			device::RTC.RCR4 = 0x00;
+			device::RTC.RCR2 = device::RTC.RCR2.START.b(1)
+							| device::RTC.RCR2.RESET.b(1)
+							| device::RTC.RCR2.CNTMD.b(1);  // binary counter
+			device::RTC.RCR1 = 0x00;
+
 			return true;
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief	DS1371 時間設定
+			@brief	時間設定
 			@param[in]	t	時間
 			@return 成功なら「true」
 		 */
 		//-----------------------------------------------------------------//
 		bool set_time(time_t t) const {
+			device::RTC.RCR2.START = 0;
+			device::RTC.BCNT0 = t & 0xff;
+			device::RTC.BCNT1 = (t >> 8)  & 0xff;
+			device::RTC.BCNT2 = (t >> 16) & 0xff;
+			device::RTC.BCNT3 = (t >> 24) & 0xff;
+			device::RTC.RCR2.START = 1;
 			return true;
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief	DS1371 時間呼び出し
+			@brief	時間呼び出し
 			@param[in]	tp	取得時間
 			@return 成功なら「true」
 		 */
 		//-----------------------------------------------------------------//
 		bool get_time(time_t& tp) const {
-			return true;
+			auto a = get_();
+			for(int i = 0; i < 5; ++i) {
+				auto b = get_();
+				if(a == b) {
+					tp = static_cast<time_t>(a);
+					return true;
+				}
+				a = b;
+			}
+			return false;
 		}
 	};
 }
