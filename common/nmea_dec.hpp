991f80e062e541ff4303f3b36db903dba7aae653
hira <hira@rvf-rc45.net>
2017-01-19 18:25:29 +0900

    update api

--- common/nmea_dec.hpp
+++ common/nmea_dec.hpp
@@ -8,6 +8,7 @@
 //=====================================================================//
 #include <cstdint>
 #include <cstring>
+#include "common/time.h"
 
 namespace utils {
 
@@ -20,66 +21,138 @@ namespace utils {
 	template <class SCI_IO>
 	class nmea_dec {
 
+	public:
+
+		static const uint32_t sinfo_num_ = 12;		///< 衛星情報の最大数
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  衛星パラメーター
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		struct sat_info {
+			char	no_[4];  	///< 衛星番号
+			char	elv_[4];	///< 衛星仰角(Elevation)、０～９０度
+			char	azi_[4];	///< 衛星方位角(Azimuth)、０～３５９度
+			char	cn_[4];		///< キャリア／ノイズ比、０～９９dB
+
+			sat_info() {
+				no_[0] = 0;
+				elv_[0] = 0;
+				azi_[0] = 0;
+				cn_[0] = 0;
+			}
+		};
+
+	private:
 		SCI_IO&		sci_;
 
 		uint16_t	pos_;
 		char		line_[128];
 
-		char		time_[11];  // 時間
-		char		lat_[11];   // 緯度(latitude)
+		char		time_[12];  // 時間
+		char		lat_[12];   // 緯度(latitude)
 		char		ns_[2];     // 北緯、南緯
-		char		lon_[11];   // 経度(longitude)
+		char		lon_[12];   // 経度(longitude)
 		char		ew_[2];     // 東経、西経
 		char		q_[2];      // 品質
-		char		satellite_[3];  // 衛星数
+		char		satellite_[4];  // 衛星数
+		char		hq_[4];		// 水平精度低下率
+		char		alt_[6];	// 海抜高度(Altitude above sea level)
+		char		alt_unit_[2];	// 海抜高度単位
 
-		char		date_[7];   // 日付
+		char		date_[8];   // 日付
+
+		uint16_t	sidx_;
+		sat_info	sinfo_[sinfo_num_];	// 衛星情報
 
 		uint32_t	id_;
+		uint32_t	iid_;
 
-		uint16_t word_(const char* src)
+		static uint16_t word_(const char* src)
 		{
 			const char* top = src;
 			char ch;
 			while((ch = *src++) != 0) {
-				if(ch == ',') return src - top;
+				if(ch == ',' || ch == '*') return src - top;
 			}
 			return 0;
+		}
+
+		static void copy_word_(char* dst, const char* src, uint16_t len)
+		{
+			std::strncpy(dst, src, len);
+			dst[len] = 0;
+		}
+
+		static int32_t get_dec_(const char* t, uint16_t n = 0)
+		{
+			int32_t val = 0;
+			char ch;
+			while((ch = *t++) != 0) {
+				if('0' <= ch && ch <= '9') {
+					val *= 10;
+					val += ch - '0';
+				} else {
+					break;
+				}
+				if(n > 0) {
+					--n;
+					if(n == 0) break;
+				}
+			}
+			return val;
 		}
 
 		bool decode_()
 		{
 			if(line_[0] != '$') return false;
+
 			if(std::strncmp(&line_[1], "GPGGA,", 6) == 0) {
 				const char* p = &line_[7];
 				uint16_t n = 0;
 				uint16_t l;
 				while((l = word_(p)) != 0) {
-					if(n == 0) std::strncpy(time_, p, l - 1);
-					else if(n == 1) std::strncpy(lat_, p, l - 1);
-					else if(n == 2) std::strncpy(ns_, p, l - 1);
-					else if(n == 3) std::strncpy(lon_, p, l - 1);
-					else if(n == 4) std::strncpy(ew_, p, l - 1);
-					else if(n == 5) std::strncpy(q_, p, l - 1);
-					else if(n == 6) std::strncpy(satellite_, p, l - 1);
+					if(n == 0) copy_word_(time_, p, l - 1);
+					else if(n == 1) copy_word_(lat_, p, l - 1);
+					else if(n == 2) copy_word_(ns_, p, l - 1);
+					else if(n == 3) copy_word_(lon_, p, l - 1);
+					else if(n == 4) copy_word_(ew_, p, l - 1);
+					else if(n == 5) copy_word_(q_, p, l - 1);
+					else if(n == 6) copy_word_(satellite_, p, l - 1);
+					else if(n == 7) copy_word_(hq_, p, l - 1);
+					else if(n == 8) copy_word_(alt_, p, l - 1);
+					else if(n == 9) copy_word_(alt_unit_, p, l - 1);
 					else {
-						++id_;
-						return true;
+						break;
 					}
 					p += l;
 					++n;
 				}
-#if 0
 			} else if(std::strncmp(&line_[1], "GPRMC,", 6) == 0) {
 				const char* p = &line_[7];
 				uint16_t n = 0;
 				uint16_t l;
 				while((l = word_(p)) != 0) {
-					if(n == 8) std::strncpy(date_, p, l - 1);
+					if(n == 8) copy_word_(date_, p, l - 1);
 					p += l;
 					++n;
 				}
-#endif
+			} else if(std::strncmp(&line_[1], "GPGSV,", 6) == 0) {
+				const char* p = &line_[7];
+				uint16_t n = 0;
+				uint16_t l;
+				while((l = word_(p)) != 0) {
+					if(n == 0) ;
+					else if(n == 1) ;
+// std::strncpy(date_, p, l - 1);
+					p += l;
+					++n;
+				}
+				++iid_;
+			} else if(std::strncmp(&line_[1], "GPVTG,", 6) == 0) {
+				++id_;
+				return true;
 			}
 			return false;
 		}
@@ -90,7 +163,7 @@ namespace utils {
             @brief  コンストラクター
         */
         //-----------------------------------------------------------------//
-		nmea_dec(SCI_IO& sci) : sci_(sci), pos_(0), id_(0) {
+		nmea_dec(SCI_IO& sci) : sci_(sci), pos_(0), sidx_(0), id_(0), iid_(0) {
 			line_[0] = 0;
 			time_[0] = 0;
 			lat_[0] = 0;
@@ -99,6 +172,9 @@ namespace utils {
 			ew_[0] = 0;
 			q_[0] = 0;
 			satellite_[0] = 0;
+			hq_[0] = 0;
+			alt_[0] = 0;
+			alt_unit_[0] = 0;
 
 			date_[0] = 0;
 		}
@@ -115,11 +191,48 @@ namespace utils {
 
         //-----------------------------------------------------------------//
         /*!
+            @brief  情報処理ＩＤを取得
+			@return 情報処理ＩＤ
+        */
+        //-----------------------------------------------------------------//
+		uint32_t get_iid() const { return iid_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
             @brief  時間を取得
 			@return 時間
         */
         //-----------------------------------------------------------------//
 		const char* get_time() const { return time_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  日付を取得
+			@return 日付
+        */
+        //-----------------------------------------------------------------//
+		const char* get_date() const { return date_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  時間「time_t」を取得
+			@return 時間「time_t」
+        */
+        //-----------------------------------------------------------------//
+		time_t get_tm() const {
+			tm ts;
+			ts.tm_sec  = get_dec_(&time_[4], 2);
+			ts.tm_min  = get_dec_(&time_[2], 2);
+			ts.tm_hour = get_dec_(&time_[0], 2);
+			ts.tm_mday = get_dec_(&date_[0], 2);
+			ts.tm_mon  = get_dec_(&date_[2], 2);
+			ts.tm_year = get_dec_(&date_[4], 2);
+			ts.tm_year += 100;  // 起点１９００年
+			return mktime_gmt(&ts);
+		}
 
 
         //-----------------------------------------------------------------//
@@ -142,11 +255,65 @@ namespace utils {
 
         //-----------------------------------------------------------------//
         /*!
+            @brief  品質を取得
+			@return 品質
+        */
+        //-----------------------------------------------------------------//
+		const char* get_quality() const { return q_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
             @brief  衛星数を取得
 			@return 衛星数
         */
         //-----------------------------------------------------------------//
 		const char* get_satellite() const { return satellite_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  水平品質を取得
+			@return 水平品質
+        */
+        //-----------------------------------------------------------------//
+		const char* get_holizontal_quality() const { return hq_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  海抜高度を取得
+			@return 海抜高度
+        */
+        //-----------------------------------------------------------------//
+		const char* get_altitude() const { return alt_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  海抜高度単位を取得
+			@return 海抜高度単位
+        */
+        //-----------------------------------------------------------------//
+		const char* get_altitude_unit() const { return alt_unit_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  衛星情報の取得
+			@param[in]	idx	衛星インデックス
+			@return 衛星情報
+        */
+        //-----------------------------------------------------------------//
+		const sat_info& get_satellite_info(uint16_t idx) const
+		{
+			if(idx < sinfo_num_) {
+				return sinfo_[idx];
+			} else {
+				static sat_info si;
+				return si;
+			}
+		}
 
 
         //-----------------------------------------------------------------//
@@ -161,7 +328,8 @@ namespace utils {
 
         //-----------------------------------------------------------------//
         /*!
-            @brief  サービス
+            @brief  サービス @n
+					情報量に応じて呼ぶ（通常毎フレーム呼ぶ）
 			@return 更新されたら「true」
         */
         //-----------------------------------------------------------------//
