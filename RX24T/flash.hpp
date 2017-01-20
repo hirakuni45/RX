69f34b30bb1d797ae81dcd73134380aa5a8f4b72
hira <hira@rvf-rc45.net>
2017-01-21 03:12:52 +0900

    update

--- RX24T/flash.hpp
+++ RX24T/flash.hpp
@@ -25,7 +25,7 @@ namespace device {
 		//-----------------------------------------------------------------//
 		template <uint32_t ofs>
 		struct dflctl_t : public rw8_t<ofs> {
-			typedef rw16_t<ofs> io_;
+			typedef rw8_t<ofs> io_;
 			using io_::operator =;
 			using io_::operator ();
 			using io_::operator |=;
@@ -34,6 +34,76 @@ namespace device {
 			bit_rw_t<io_, bitpos::B0> DFLEN;
 		};
 		static dflctl_t<0x007FC090> DFLCTL;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ P/E モードエントリレジスタ (FENTRYR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fentryr_t : public rw16_t<ofs> {
+			typedef rw16_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B0>     FENTRY0;
+			bit_rw_t <io_, bitpos::B7>     FENTRYD;
+			bits_rw_t<io_, bitpos::B8, 8>  FEKEY;
+		};
+		static fentryr_t<0x007FFFB2> FENTRYR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  プロテクト解除レジスタ (FPR)
+		*/
+		//-----------------------------------------------------------------//
+		static rw8_t<0x007FC180> FPR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  プロテクト解除ステータスレジスタ (FPSR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fpsr_t : public ro8_t<ofs> {
+			typedef ro8_t<ofs> io_;
+			using io_::operator ();
+
+			bit_ro_t<io_, bitpos::B0> PERR;
+		};
+		static fpsr_t<0x007FC184> FPSR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ P/E モード制御レジスタ (FPMCR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fpmcr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B1>  FMS0;
+			bit_rw_t<io_, bitpos::B3>  RPDIS;
+			bit_rw_t<io_, bitpos::B4>  FMS1;
+			bit_rw_t<io_, bitpos::B6>  LVPE;
+			bit_rw_t<io_, bitpos::B7>  FMS2;
+		};
+		static fpmcr_t<0x007FFFB2> FPMCR;
+
+
 
 
 
