62c3458ec79a2f993a14bb7b7ab6a1fe22c9e86d
hira <hira@rvf-rc45.net>
2017-01-21 08:21:18 +0900

    update flash

--- RX24T/flash.hpp
+++ RX24T/flash.hpp
@@ -104,8 +104,283 @@ namespace device {
 		static fpmcr_t<0x007FFFB2> FPMCR;
 
 
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ初期設定レジスタ (FISR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fisr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 5>  PCKA;
+			bits_rw_t<io_, bitpos::B6, 2>  SAS;
+		};
+		static fisr_t<0x007FC1D8> FISR;
 
 
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュリセットレジスタ (FRESETR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fresetr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  FRESET;
+		};
+		static fresetr_t<0x007FC124> FRESETR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ領域選択レジスタ (FASR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fasr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  EXT;
+		};
+		static fasr_t<0x007FC104> FASR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ制御レジスタ (FCR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fcr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 4>  CMD;
+			bit_rw_t <io_, bitpos::B6>     STOP;
+			bit_rw_t <io_, bitpos::B7>     OPST;
+		};
+		static fcr_t<0x007FC114> FCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュエクストラ領域制御レジスタ (FEXCR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fexcr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 3>  CMD;
+			bit_rw_t <io_, bitpos::B7>     OPST;
+		};
+		static fexcr_t<0x007FC1DC> FEXCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理開始アドレスレジスタ H (FSARH)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC110> FSARH;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理開始アドレスレジスタ L (FSARL)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC108> FSARL;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理終了アドレスレジスタ H (FEARH)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC120> FEARH;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理終了アドレスレジスタ L (FEARL)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC118> FEARL;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュライトバッファ n レジスタ (FWBn) (n = 0 ～ 3)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC130> FWB0;
+		static rw16_t<0x007FC138> FWB1;
+		static rw16_t<0x007FC140> FWB2;
+		static rw16_t<0x007FC144> FWB3;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュステータスレジスタ 0 (FSTATR0)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fstatr0_t : public ro8_t<ofs> {
+			typedef ro8_t<ofs> io_;
+			using io_::operator ();
+
+			bit_ro_t<io_, bitpos::B0>  ERERR;
+			bit_ro_t<io_, bitpos::B1>  PRGERR;
+			bit_ro_t<io_, bitpos::B3>  BCERR;
+			bit_ro_t<io_, bitpos::B4>  ILGLERR;
+			bit_ro_t<io_, bitpos::B5>  EILGLERR;
+		};
+		static fstatr0_t<0x007FC1F0> FSTATR0;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュステータスレジスタ 1 (FSTATR1)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fstatr1_t : public ro8_t<ofs> {
+			typedef ro8_t<ofs> io_;
+			using io_::operator ();
+
+			bit_ro_t<io_, bitpos::B6>  FRDY;
+			bit_ro_t<io_, bitpos::B7>  EXRDY;
+		};
+		static fstatr1_t<0x007FC12C> FSTATR1;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュエラーアドレスモニタレジスタ H (FEAMH)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1E8> FEAMH;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュエラーアドレスモニタレジスタ L (FEAML)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1E0> FEAML;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュスタートアップ設定モニタレジスタ (FSCMR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fscmr_t : public rw16_t<ofs> {
+			typedef rw16_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B8>  SASMF;
+		};
+		static fscmr_t<0x007FC1C0> FSCMR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュアクセスウィンドウ開始アドレスモニタレジスタ (FAWSMR)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1C8> FAWSMR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュアクセスウィンドウ終了アドレスモニタレジスタ (FAWEMR)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1D0> FAWEMR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ユニーク ID レジスタ n (UIDRn) (n = 0 ～ 3)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC350> UIDR0;
+		static rw16_t<0x007FC354> UIDR1;
+		static rw16_t<0x007FC358> UIDR2;
+		static rw16_t<0x007FC35C> UIDR3;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ROM キャッシュ許可レジスタ (ROMCE)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct romce_t : public rw16_t<ofs> {
+			typedef rw16_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  ROMCEN;
+		};
+		static romce_t<0x00081000> ROMCE;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ROM キャッシュ無効化レジスタ (ROMCIV)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct romciv_t : public rw16_t<ofs> {
+			typedef rw16_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  ROMCIV;
+		};
+		static romciv_t<0x00081004> ROMCIV;
 
 
 	};
