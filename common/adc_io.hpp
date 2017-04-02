ccb0f1872cf59681e908fef51a8ca1162fbb0aa5
hira <hira@rvf-rc45.net>
2017-04-03 04:43:11 +0900

    update comment

--- common/adc_io.hpp
+++ common/adc_io.hpp
@@ -2,10 +2,12 @@
 //=====================================================================//
 /*!	@file
 	@brief	RX グループ A/D 制御 @n
-			Copyright 2016 Kunihito Hiramatsu
+			Copyright 2016, 2017 Kunihito Hiramatsu
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
 #if defined(SIG_RX24T)
 #include "RX24T/adc_io.hpp"
+#elif defined(SIG_RX64M)
+#include "RX64M/adc_io.hpp"
 #endif
