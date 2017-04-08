f724ca78f474fc3861cdee3d84ff3c6afa8e2ff1
hira <hira@rvf-rc45.net>
2017-04-09 04:13:40 +0900

    update

--- rx64m_test/main.cpp
+++ rx64m_test/main.cpp
@@ -11,6 +11,7 @@
 #include "common/fifo.hpp"
 #include "common/sci_io.hpp"
 #include "common/format.hpp"
+#include "common/input.hpp"
 #include "common/command.hpp"
 #include "common/rspi_io.hpp"
 #include "common/spi_io.hpp"
@@ -30,6 +31,8 @@
 #endif
 
 namespace {
+
+	const int seeda_version_ = 5;
 
 	volatile unsigned long millis_ = 0;
 	volatile unsigned long delay_ = 0;
@@ -67,16 +70,14 @@ namespace {
 	typedef device::PORT<device::PORT6, device::bitpos::B7> SW1;
 	typedef device::PORT<device::PORT6, device::bitpos::B6> SW2;
 
-#if 0
-	// SDC 用　SPI 定義（RSPI）
-	typedef device::rspi_io<device::RSPI> SPI;
-#else
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LAN_RESN;
+	typedef device::PORT<device::PORT7, device::bitpos::B3> LAN_PDN;
+
 	// Soft SDC 用　SPI 定義（SPI）
 	typedef device::PORT<device::PORTD, device::bitpos::B6> MISO;
 	typedef device::PORT<device::PORTD, device::bitpos::B4> MOSI;
 	typedef device::PORT<device::PORTD, device::bitpos::B5> SPCK;
 	typedef device::spi_io<MISO, MOSI, SPCK> SPI;
-#endif
 	SPI spi_;
 
 	typedef device::PORT<device::PORTD, device::bitpos::B3> sdc_select;	///< カード選択信号
@@ -85,6 +86,22 @@ namespace {
 
 	typedef utils::sdc_io<SPI, sdc_select, sdc_power, sdc_detect> SDC;
 	SDC sdc_(spi_, 10000000);
+
+	// LTC A/D 定義
+	typedef device::PORT<device::PORT4, device::bitpos::B0> LTC_CSN;   // P40(141)
+	typedef device::PORT<device::PORTC, device::bitpos::B6> LTC_CNV;   // PC6(61)
+	typedef device::PORT<device::PORTD, device::bitpos::B0> LTC_BUSY;  // PD0/IRQ0(126)
+	typedef device::PORT<device::PORT5, device::bitpos::B3> LTC_PD;    // P53(53)
+	typedef device::PORT<device::PORT5, device::bitpos::B6> LTC_SDI;   // P56(50)
+	typedef device::PORT<device::PORT8, device::bitpos::B6> LTC_SCKO;  // P86(41)
+	typedef device::PORT<device::PORT8, device::bitpos::B7> LTC_SCKI;  // P87(39)
+	typedef device::PORT<device::PORT2, device::bitpos::B0> LTC_SDO0;  // P20(37)
+	typedef device::PORT<device::PORT2, device::bitpos::B1> LTC_SDO2;  // P21(36)
+	typedef device::PORT<device::PORT2, device::bitpos::B2> LTC_SDO4;  // P22(35)
+	typedef device::PORT<device::PORT2, device::bitpos::B3> LTC_SDO6;  // P23(34)
+	typedef struct chip::LTC2348_SDO_t<LTC_SCKO, LTC_SDO0, LTC_SDO2, LTC_SDO4, LTC_SDO6> LTC_SDO;
+	typedef chip::LTC2348_16<LTC_CSN, LTC_CNV, LTC_BUSY, LTC_PD, LTC_SDI, LTC_SCKI, LTC_SDO> EADC;
+	EADC eadc_;
 
 	utils::command<256> cmd_;
 
@@ -270,6 +287,7 @@ namespace {
 		}
 	}
 
+
 	bool check_mount_() {
 		auto f = sdc_.get_mount();
 		if(!f) {
@@ -277,6 +295,133 @@ namespace {
 		}
 		return f;
 	}
+
+
+	bool reset_signal_(uint8_t cmdn)
+	{
+		bool f = false;
+		if(cmdn == 1) {
+			bool v = LAN_RESN::P();
+			utils::format("LAN-RESN: %d\n") % static_cast<int>(v);
+			return true;
+		} else if(cmdn > 1) {
+			char tmp[16];
+			if(cmd_.get_word(1, sizeof(tmp), tmp)) {
+				// Reset signal
+				if(strcmp(tmp, "0") == 0) {
+					device::PORT7::PODR.B0 = 0;
+					f = true;
+				} else if(strcmp(tmp, "1") == 0) {
+					device::PORT7::PODR.B0 = 1;
+					f = true;
+				} else {
+					utils::format("reset param error: '%s'\n") % tmp;
+				}
+			}
+		}
+		return f;
+	}
+
+
+	bool eadc_conv_(uint8_t cmdn)
+	{
+		bool f = false;
+		if(cmdn == 1) {
+			if(eadc_.convert()) {
+				for(int i = 0; i < 8; ++i) {
+					uint32_t v = eadc_.get_data(i);
+					utils::format("LTC2348-16(%d): CHID: %d, SPAN: %d, %d\n")
+						% i % ((v >> 3) & 7) % (v & 7) % (v >> 8);
+				}
+			} else {
+				utils::format("LTC2348-16 convert error\n");
+			}
+			return true;
+		} else if(cmdn > 1) {
+			char tmp[16];
+			if(cmd_.get_word(1, sizeof(tmp), tmp)) {
+				if(tmp[0] >= '0' && tmp[0] <= '7' && tmp[1] == 0) {
+					if(eadc_.convert()) {
+						int ch = tmp[0] - '0';
+						uint32_t v = eadc_.get_value(ch);
+						utils::format("LTC2348-16(%d): %d\n") % ch % v;
+					} else {
+						utils::format("LTC2348-16 convert error\n");
+					}
+				} else {
+					utils::format("convert param error: '%s'\n") % tmp;
+				}
+				f = true;
+			}
+		}
+		return f;
+	}
+
+
+	bool eadc_sample_(uint8_t cmdn)
+	{
+		if(cmdn < 8) {
+			utils::format("sample param error...\n"); 
+		} else {
+			int mode = 0;
+			int ch = -1;
+			int rate = -1;
+			int num = -1;
+			char fname[32];
+			fname[0] = 0;
+			for(uint8_t i = 1; i < cmdn; ++i) {
+				char tmp[32];
+				if(cmd_.get_word(i, sizeof(tmp), tmp)) {
+					if(tmp[0] == '-') {
+						if(strcmp(tmp, "-ch") == 0) {
+							mode = 1;
+						} else if(strcmp(tmp, "-rate") == 0) {
+							mode = 2;
+						} else if(strcmp(tmp, "-num") == 0) {
+							mode = 3;
+						} else {
+							utils::format("sample option error: '%s'\n") % tmp;
+							return true;
+						}
+					} else {
+						bool f = false;
+						switch(mode) {
+						case 0:
+							strcpy(fname, tmp);
+							f = true;
+							break;
+						case 1:
+							f = (utils::input("%d", tmp) % ch).status();
+							break;
+						case 2:
+							f = (utils::input("%d", tmp) % rate).status();
+							break;
+						case 3:
+							f = (utils::input("%d", tmp) % num).status();
+							break;
+						}
+						mode = 0;
+						if(!f) {
+							utils::format("sample option value error: '%s'\n") % tmp;
+							return true;
+						}
+					}
+				} else {
+					return false;
+				}
+			}
+			if(ch > 7 || ch < 0) {
+				utils::format("sample chanel out range: %d\n") % ch;
+			}
+
+			utils::format("ch:   %d\n") % ch;
+			utils::format("rate: %d\n") % rate;
+			utils::format("num:  %d\n") % num;
+			utils::format("name: '%s'\n") % fname;
+		}
+		return true;
+	}
+
 
 #ifdef SERVER_TASK
 	void service_server()
@@ -358,6 +503,8 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
+	device::PORT3::PCR.B5 = 1; // P35 pull-up
+
 	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
 
 	device::SYSTEM::MOSCWTCR = 9;	// 1ms wait
@@ -385,6 +532,8 @@ int main(int argc, char** argv)
 	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
 
 	{  // DIP-SW プルアップ
+		SW1::DIR = 0;  // input
+		SW2::DIR = 0;  // input
 		SW1::PU = 1;
 		SW2::PU = 1;
 	}
@@ -407,12 +556,37 @@ int main(int argc, char** argv)
 		sdc_.start();
 	}
 
+	{  // LTC2348ILX-16 初期化（クロック１ＭＨｚ）
+		// 内臓リファレンスと内臓バッファ
+		// VREFIN: 2.024V、VREFBUF: 4.096V、Analog range: 0V to 5.12V
+		if(!eadc_.start(100000, 0b001)) {
+///		if(!eadc_.start(100000, 0b000)) {
+			utils::format("LTC2348_16 start fail...\n");
+		}
+	}
+
+	{  // LAN initialize (PHY reset, PHY POWER-DOWN
+		LAN_PDN::DIR = 1;  // output;
+		LAN_PDN::P = 1;    // Not Power Down Mode..
+		LAN_RESN::DIR = 1; // output;
+
+		LAN_RESN::P = 0;
+		utils::delay::milli_second(200); /// reset time
+		LAN_RESN::P = 1;
+	}
+
 	// タイトル・コール
-	utils::format("\nStart Seeda03, ");
+	utils::format("\nStart Seeda03 Version %d.%02d\n") % (seeda_version_ / 100) % (seeda_version_ % 100);
 	utils::format("Endian: %3b") % static_cast<uint32_t>(device::SYSTEM::MDE.MDE());
 	utils::format(", PCKA: %u [Hz]") % static_cast<uint32_t>(F_PCKA);
 	utils::format(", PCKB: %u [Hz]\n") % static_cast<uint32_t>(F_PCKB);
-	utils::format("Switch: %d\n") % static_cast<int>(get_switch_());
+	utils::format("DIP-Switch: %d\n") % static_cast<int>(get_switch_());
+	if(eadc_.probe()) {
+		utils::format("Device LTC2348-16: Ready\n");
+		utils::format("    CH Span: %024b\n") % eadc_.get_span();
+	} else {
+		utils::format("Device LTC2348-16: Not Ready\n");
+	}
 	{
 		time_t t = get_time_();
 		if(t != 0) {
@@ -420,14 +594,11 @@ int main(int argc, char** argv)
 		}
 	}
 
-	{  // PHY initialize
-		device::PORT7::PDR.B3 = 1; // Power down output
-		device::PORT7::PODR.B3 = 1; // Power assert (Power Active)
-		device::PORT7::PDR.B0 = 1; // PHY RESET output
-		device::PORT7::PODR.B0 = 0; // Reset assert
-		utils::delay::milli_second(200); /// reset time
-		device::PORT7::PODR.B0 = 1; // Reset negate
-	}
+
+//	while(1) {
+//		eadc_.convert();
+//	}
+
 
 #ifdef SERVER_TASK
 	if(get_switch_() == 3) {  // Ethernet 起動
@@ -514,17 +685,11 @@ int main(int argc, char** argv)
 					}
 					f = true;
 				} else if(cmd_.cmp_word(0, "reset")) {
-					char buff[4];
-					if(cmd_.get_word(1, sizeof(buff), buff)) {
-						// Reset signal
-						if(strcmp(buff, "0") == 0) {
-							device::PORT7::PODR.B0 = 0;
-							f = true;
-						} else if(strcmp(buff, "1") == 0) {
-							device::PORT7::PODR.B0 = 1;
-							f = true;
-						}
-					}
+					f = reset_signal_(cmdn);
+				} else if(cmd_.cmp_word(0, "eadc")) {
+					f = eadc_conv_(cmdn);
+				} else if(cmd_.cmp_word(0, "sample")) {
+					f = eadc_sample_(cmdn);
 				} else if(cmd_.cmp_word(0, "help") || cmd_.cmp_word(0, "?")) {
 					utils::format("date\n");
 					utils::format("date yyyy/mm/dd hh:mm[:ss]\n");
@@ -532,6 +697,8 @@ int main(int argc, char** argv)
 					utils::format("cd [directory-name]\n");
 					utils::format("pwd\n");
 					utils::format("reset [01]  (PHY reset signal)\n");
+					utils::format("eadc [0-7]  (LTC2348 A/D conversion)\n");
+					utils::format("sample -ch 0-7 -rate FRQ -num SAMPLE-NUM file-name (LTC2348 A/D sample)\n");
 					f = true;
 				}
 				if(!f) {
