ab44ceaff6d1e5f4ec7e1e68462bce0c46961d3b
hira <hira@rvf-rc45.net>
2017-04-10 21:52:18 +0900

    new file

---
+++ rx64m_test/core.hpp
@@ -0,0 +1,146 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  SEEDA03 (RX64M) コア @n
+			Copyright 2017 Kunihito Hiramatsu
+    @author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "main.hpp"
+
+#include "common/cmt_io.hpp"
+#include "common/fifo.hpp"
+#include "common/sci_io.hpp"
+
+namespace seeda {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  コア・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct core {
+
+		static const int seeda_version_ = 15;
+		static const uint32_t build_id_ = B_ID;
+
+		class cmt_task {
+			volatile unsigned long millis_;
+			volatile unsigned long delay_;
+			volatile uint32_t millis10x_;
+			volatile uint32_t cmtdiv_;
+
+		public:
+			cmt_task() : millis_(0), delay_(0), millis10x_(0), cmtdiv_(0) { }
+
+			void operator() () {
+				++millis_;
+				++cmtdiv_;
+				if(cmtdiv_ >= 10) {
+					cmtdiv_ = 0;
+					++millis10x_;
+				}
+				if(delay_) {
+					--delay_;
+				}
+			}
+
+			void sync_100hz()
+			{
+				volatile uint32_t tmp = millis10x_;
+				while(tmp == millis10x_) ;
+			}
+
+			volatile unsigned long get_millis() const { return millis_; }
+			volatile unsigned long get_delay() const { return delay_; }
+			void set_delay(volatile unsigned long n) { delay_ = n; }
+		};
+
+		typedef device::cmt_io<device::CMT0, cmt_task> CMT0;
+		CMT0	cmt0_;
+
+		typedef utils::fifo<uint8_t, 1024> BUFFER;
+		typedef device::sci_io<device::SCI5, BUFFER, BUFFER> SCI;
+		SCI		sci_;
+
+		typedef device::PORT<device::PORT6, device::bitpos::B7> SW1;
+		typedef device::PORT<device::PORT6, device::bitpos::B6> SW2;
+
+		uint8_t get_switch_()
+		{
+			return static_cast<uint8_t>(!SW1::P()) | (static_cast<uint8_t>(!SW2::P()) << 1);
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+		*/
+		//-----------------------------------------------------------------//
+		core() { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  初期化
+		*/
+		//-----------------------------------------------------------------//
+		void init()
+		{
+			{  // DIP-SW プルアップ
+				SW1::DIR = 0;  // input
+				SW2::DIR = 0;  // input
+				SW1::PU = 1;
+				SW2::PU = 1;
+			}
+
+			{  // タイマー設定、１０００Ｈｚ（１ｍｓ）
+				uint8_t int_level = 1;
+				cmt0_.start(1000, int_level);
+			}
+
+			{  // SCI 設定
+				uint8_t int_level = 2;
+				sci_.start(115200, int_level);
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  初期化
+		*/
+		//-----------------------------------------------------------------//
+		void title()
+		{
+			// タイトル・コール
+			utils::format("\nStart Seeda03 Build: %u Version %d.%02d\n") % build_id_
+				% (seeda_version_ / 100) % (seeda_version_ % 100);
+			utils::format("Endian: %3b") % static_cast<uint32_t>(device::SYSTEM::MDE.MDE());
+			utils::format(", PCKA: %u [Hz]") % static_cast<uint32_t>(F_PCKA);
+			utils::format(", PCKB: %u [Hz]\n") % static_cast<uint32_t>(F_PCKB);
+			utils::format("DIP-Switch: %d\n") % static_cast<int>(get_switch_());
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  サービス同期
+		*/
+		//-----------------------------------------------------------------//
+		void sync()
+		{
+			cmt0_.at_task().sync_100hz();
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  サービス
+		*/
+		//-----------------------------------------------------------------//
+		void service()
+		{
+		}		
+	};
+}
---
+++ rx64m_test/net.hpp
@@ -0,0 +1,214 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  SEEDA03 net クラス @n
+			Copyright 2017 Kunihito Hiramatsu
+    @author 平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+
+#include "common/command.hpp"
+
+#include "GR/core/Ethernet.h"
+#include <cstdlib>
+
+namespace seeda {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  net クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class net {
+
+		typedef device::PORT<device::PORT7, device::bitpos::B0> LAN_RESN;
+		typedef device::PORT<device::PORT7, device::bitpos::B3> LAN_PDN;
+
+		EthernetServer server_;
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクタ
+		*/
+		//-----------------------------------------------------------------//
+		net() : server_(80) { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  初期化
+		*/
+		//-----------------------------------------------------------------//
+		void init()
+		{
+			// LAN initialize (PHY reset, PHY POWER-DOWN
+			LAN_PDN::DIR = 1;  // output;
+			LAN_PDN::P = 1;    // Not Power Down Mode..
+			LAN_RESN::DIR = 1; // output;
+
+			LAN_RESN::P = 0;
+			utils::delay::milli_second(200); /// reset time
+			LAN_RESN::P = 1;
+
+#ifdef SERVER_TASK
+	if(get_switch_() == 3) {  // Ethernet 起動
+		device::power_cfg::turn(device::peripheral::ETHERCA);
+		device::port_map::turn(device::peripheral::ETHERCA);
+		set_interrupt_task(INT_Excep_ICU_GROUPAL1, static_cast<uint32_t>(device::icu_t::VECTOR::GROUPAL1));
+		set_interrupt_task(INT_Excep_CMT1_CMI1, static_cast<uint32_t>(device::icu_t::VECTOR::CMI1));
+
+		Ethernet.maininit();
+
+		static const uint8_t mac[6] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+		if(Ethernet.begin(mac) == 0) {
+			utils::format("Ethernet Fail: begin (DHCP)...\n");
+
+			utils::format("SetIP: ");
+			IPAddress ipa(192, 168, 3, 20);
+			Ethernet.begin(mac, ipa);
+		} else {
+			utils::format("DHCP: ");
+		}
+		Ethernet.localIP().print();
+
+		server_.begin();
+		utils::format("Start server: ");
+		Ethernet.localIP().print();
+	}
+#endif
+
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  タイトル表示（ネット関係）
+		*/
+		//-----------------------------------------------------------------//
+		void title()
+		{
+		}
+
+
+#ifdef SERVER_TASK
+	void service_server()
+	{
+		EthernetClient& client = server_.available();
+		if (client) {
+			utils::format("new client\n");
+
+			while (client.connected()) {
+				if (client.available()) {
+					char tmp[256];
+					int len = client.read(tmp, 255);
+					if(len > 0 && len < 256) {
+						tmp[len] = 0;
+	        			sci_puts(tmp);
+					}
+
+					// if you've gotten to the end of the line (received a newline
+					// character) and the line is blank, the http request has ended,
+					// so you can send a reply
+					if (len > 0 && tmp[len - 1] == '\n') {
+						// send a standard http response header
+						client.println("HTTP/1.1 200 OK");
+          				client.println("Content-Type: text/html");
+						// the connection will be closed after completion of the response
+          				client.println("Connection: close");
+						client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+						client.println();
+						client.println("<!DOCTYPE HTML>");
+						client.println("<html>");
+						client.println("<font size=\"5\">");
+						{  // 時間表示
+							char tmp[128];
+							time_t t = get_time_();
+							disp_time_(t, tmp, sizeof(tmp));
+							client.print(tmp);
+							client.println("<br/ >");
+						}
+						// アナログ入力の表示
+						for (int ach = 0; ach < 4; ++ach) {
+							char tmp[128];
+							int v = rand() & (4096 - 1);
+							utils::format("analog input(%d): %d", tmp, sizeof(tmp)) % ach % v;
+							client.print(tmp);
+							client.println("<br/ >");
+						}
+						client.println("</font>");
+						client.println("</html>");
+						break;
+					}
+				}
+			}
+			// give the web browser time to receive the data
+///			delay(1);
+			// close the connection:
+			client.stop();
+			utils::format("client disconnected\n");
+		}
+	}
+#endif
+
+#if 0
+	void telnet_service()
+	{
+		EthernetClient& client = telnet_.available();
+		if(client) {
+			char tmp[256];
+			int len = client.read(tmp, 256);
+			if(len > 0) {
+				client.write(tmp, len);
+			}
+		}
+	}
+#endif
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  サービス
+		*/
+		//-----------------------------------------------------------------//
+		void service()
+		{
+			Ethernet.mainloop();
+//			service_server();
+
+
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  PHY リセット信号制御
+			@param[in]	cmd		コマンド入力インスタンス
+		*/
+		//-----------------------------------------------------------------//
+		bool reset_signal(COMMAND cmd)
+		{
+			bool f = false;
+			if(cmdn == 1) {
+				bool v = LAN_RESN::P();
+				utils::format("LAN-RESN: %d\n") % static_cast<int>(v);
+				return true;
+			} else if(cmdn > 1) {
+				char tmp[16];
+				if(cmd_.get_word(1, sizeof(tmp), tmp)) {
+					// Reset signal
+					if(strcmp(tmp, "0") == 0) {
+						device::PORT7::PODR.B0 = 0;
+						f = true;
+					} else if(strcmp(tmp, "1") == 0) {
+						device::PORT7::PODR.B0 = 1;
+						f = true;
+					} else {
+						utils::format("reset param error: '%s'\n") % tmp;
+					}
+				}
+			}
+			return f;
+		}
+	};
+}
