0fbc31d42210f291ec0f5a0a4abe498d5105bc70
hira <hira@rvf-rc45.net>
2017-04-13 10:58:04 +0900

    add new

--- common/string_utils.hpp
+++ common/string_utils.hpp
@@ -10,6 +10,13 @@
 #include "ff12b/src/ff.h"
 #include "common/time.h"
 
+/// string, vector が使える環境
+// #define STRING_VECTOR
+#ifdef STRING_VECTOR
+#include <string>
+#include <vector>
+#endif
+
 namespace utils {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
@@ -308,7 +315,7 @@ namespace utils {
 //			t = 0;	/* 1970 01-01 00:00:00 */
 			struct tm *tp = localtime(&t);
 
-#ifdef _DEBUG
+#ifdef DEBUG_
 			format("get_fattime: (source) %4d/%d/%d/ %02d:%02d:%02d\n")
 				% (tp->tm_year + 1900) % (tp->tm_mon + 1) % tp->tm_mday
 				% tp->tm_hour % tp->tm_min % tp->tm_sec;
@@ -340,22 +347,20 @@ namespace utils {
 		static time_t fatfs_time_to(WORD date, WORD time)
 		{
 			struct tm ttm;
-/* ftime
-			ファイルの変更された時刻、またはディレクトリの作成された時刻が格納されます。
-			bit15:11 ---> 時が 0..23 の値で入ります。
-			bit10:5  ---> 分が 0..59 の値で入ります。
-			bit4:0   ---> 秒/2が 0..29 の値で入ります。
-*/
+			// ftime
+			// ファイルの変更された時刻、またはディレクトリの作成された時刻が格納されます。
+			// bit15:11 ---> 時が 0..23 の値で入ります。
+			// bit10:5  ---> 分が 0..59 の値で入ります。
+			// bit4:0   ---> 秒/2が 0..29 の値で入ります。
 			ttm.tm_sec  =  time & 0x1f;
 			ttm.tm_min  = (time >> 5) & 0x3f;
 			ttm.tm_hour = (time >> 11) & 0x1f;
 
-/* fdate
-			ファイルの変更された日付、またはディレクトリの作成された日付が格納されます。
-			bit15:9 ---> 1980年を起点とした年が 0..127 で入ります。
-			bit8:5  ---> 月が 1..12 の値で入ります。
-			bit4:0  ---> 日が 1..31 の値で入ります。
-*/
+			// fdate
+			// ファイルの変更された日付、またはディレクトリの作成された日付が格納されます。
+			// bit15:9 ---> 1980年を起点とした年が 0..127 で入ります。
+			// bit8:5  ---> 月が 1..12 の値で入ります。
+			// bit4:0  ---> 日が 1..31 の値で入ります。
 			ttm.tm_mday =  date & 0x1f;
 			ttm.tm_mon  = ((date >> 5) & 0xf) - 1;
 			ttm.tm_year = ((date >> 9) & 0x7f) + 1980 - 1900;
@@ -363,4 +368,175 @@ namespace utils {
 			return mktime(&ttm);
 		}
 	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  行管理クラス
+		@param[in]	buff_size	バッファの最大サイズ
+		@param[in]	line_size	行の最大サイズ
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <uint32_t buff_size, uint32_t line_size>
+	class line_manage {
+
+		char		spch_;
+		char		back_;
+
+		char		buff_[buff_size];
+		uint32_t	buff_pos_;
+
+		const char* line_[line_size];
+		uint32_t	line_pos_;
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	コンストラクター
+			@param[in]	spch	分離キャラクター
+			@param[in]	noch	無視するキャラクター
+		*/
+		//-----------------------------------------------------------------//
+		line_manage(char spch) : spch_(spch), back_(0),
+			buff_pos_(0), line_pos_(0) { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	クリア
+		*/
+		//-----------------------------------------------------------------//
+		void clear()
+		{
+			back_ = 0;
+			buff_pos_ = 0;
+			line_pos_ = 0;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	行の収集サイズを返す
+			@return 行の収集サイズ
+		*/
+		//-----------------------------------------------------------------//
+		uint32_t size() const { return line_pos_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	行の収集サイズが殻か？
+			@return 行の収集サイズが殻なら「true」
+		*/
+		//-----------------------------------------------------------------//
+		bool empty() const { return line_pos_ == 0; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	追加
+			@param[in]	ch	キャラクター
+			@return 追加出来ない場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		bool add(char ch)
+		{
+			if(back_ == 0 && ch != 0) {
+				line_[line_pos_] = &buff_[buff_pos_];
+			}
+
+			if(back_ != spch_ && ch == spch_) {
+				if(line_pos_ < line_size) {
+					buff_[buff_pos_] = 0;
+					++buff_pos_;
+					++line_pos_;
+					back_ = 0;
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			if(buff_pos_ < (buff_size - 1)) {
+				buff_[buff_pos_] = ch;
+				++buff_pos_;
+				back_ = ch;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	[] オペレーター
+			@return 行ポインター
+		*/
+		//-----------------------------------------------------------------//
+		const char* operator[] (uint32_t idx) const {
+			if(idx >= line_pos_) return nullptr;
+			return line_[idx];
+		}
+	};
+
+
+#ifdef STRING_VECTOR
+	//-----------------------------------------------------------------//
+	/*!
+		@brief	キャラクター・リスト中のコードで分割する
+		@param[in]	src		入力文字列
+		@param[in]	list	分割するキャラクター列
+		@param[in]	inhc	分割を無効にするキャラクター列
+		@param[in]	limit	分割する最大数を設定する場合正の値
+		@return	文字列リスト
+	*/
+	//-----------------------------------------------------------------//
+	template <class SS>
+	SS split_textT(const typename SS::value_type& src,
+				   const typename SS::value_type& list,
+				   const typename SS::value_type& inhc,
+				   int limit = 0) noexcept
+	{
+		SS dst;
+		bool tab_back = true;
+		typename SS::value_type word;
+		typename SS::value_type::value_type ihc = 0;
+		for(auto ch : src) {
+			bool tab = false;
+			if(limit <= 0 || static_cast<int>(dst.size()) < (limit - 1)) {
+				if(ihc == 0 && list.find(ch) != std::string::npos) {
+					tab = true;
+				}
+			}
+			if(tab_back && !tab && !word.empty()) {
+				dst.push_back(word);
+				word.clear();
+				ihc = 0;
+			}
+			if(!tab) {
+				if(!inhc.empty()) {
+					if(word.empty() && inhc.find(ch) != std::string::npos) {
+						ihc = ch;
+					} else if(ch == ihc) {
+						ihc = 0;
+					}
+				}
+				word += ch;
+			}
+			tab_back = tab;
+		}
+		if(!word.empty()) {
+			dst.push_back(word);
+		}
+		return dst;
+	}
+
+	typedef std::vector<std::string> strings;
+
+	inline strings split_text(const std::string& src, const std::string& list, const std::string& inhc = "",
+		int limit = 0) noexcept {
+		return split_textT<strings>(src, list, inhc, limit);
+	}
+#endif
 }
