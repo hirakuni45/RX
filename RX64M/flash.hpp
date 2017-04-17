196f6434ac305b178ce3ae24e2b8368713fab5eb
hira <hira@rvf-rc45.net>
2017-04-18 07:48:34 +0900

    new file

---
+++ RX64M/flash.hpp
@@ -0,0 +1,433 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX64M グループ・フラッシュ 定義 @n
+			Copyright 2017 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/io_utils.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  フラッシュ・メモリー制御クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct flash_t {
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ P/E プロテクトレジスタ（FWEPROR）
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fwepror_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 2> FLWE;
+		};
+		static fwepror_t<0x0008C296> FWEPROR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュアクセスステータスレジスタ（FASTAT）
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fastat_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  ECRCT;
+			bit_rw_t<io_, bitpos::B3>  DFAE;
+			bit_rw_t<io_, bitpos::B4>  CMDLK;
+			bit_rw_t<io_, bitpos::B7>  CFAE;
+
+		};
+		static fastat_t<0x007FE010> FASTAT;
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+		template <uint32_t base>
+		struct dflctl_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> DFLEN;
+		};
+		static dflctl_t<0x007FC090> DFLCTL;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ P/E モードエントリレジスタ (FENTRYR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fentryr_t : public rw16_t<base> {
+			typedef rw16_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B0>     FENTRY0;
+			bit_rw_t <io_, bitpos::B7>     FENTRYD;
+			bits_rw_t<io_, bitpos::B8, 8>  FEKEY;
+		};
+		static fentryr_t<0x007FFFB2> FENTRYR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  プロテクト解除レジスタ (FPR)
+		*/
+		//-----------------------------------------------------------------//
+		static rw8_t<0x007FC180> FPR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  プロテクト解除ステータスレジスタ (FPSR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fpsr_t : public ro8_t<base> {
+			typedef ro8_t<base> io_;
+			using io_::operator ();
+
+			bit_ro_t<io_, bitpos::B0> PERR;
+		};
+		static fpsr_t<0x007FC184> FPSR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ P/E モード制御レジスタ (FPMCR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fpmcr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B1>  FMS0;
+			bit_rw_t<io_, bitpos::B3>  RPDIS;
+			bit_rw_t<io_, bitpos::B4>  FMS1;
+			bit_rw_t<io_, bitpos::B6>  LVPE;
+			bit_rw_t<io_, bitpos::B7>  FMS2;
+		};
+		static fpmcr_t<0x007FC100> FPMCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ初期設定レジスタ (FISR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fisr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 5>  PCKA;
+			bits_rw_t<io_, bitpos::B6, 2>  SAS;
+		};
+		static fisr_t<0x007FC1D8> FISR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュリセットレジスタ (FRESETR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fresetr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  FRESET;
+		};
+		static fresetr_t<0x007FC124> FRESETR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ領域選択レジスタ (FASR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fasr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  EXS;
+		};
+		static fasr_t<0x007FC104> FASR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ制御レジスタ (FCR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fcr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 4>  CMD;
+			bit_rw_t <io_, bitpos::B6>     STOP;
+			bit_rw_t <io_, bitpos::B7>     OPST;
+		};
+		static fcr_t<0x007FC114> FCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュエクストラ領域制御レジスタ (FEXCR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fexcr_t : public rw8_t<base> {
+			typedef rw8_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 3>  CMD;
+			bit_rw_t <io_, bitpos::B7>     OPST;
+		};
+		static fexcr_t<0x007FC1DC> FEXCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理開始アドレスレジスタ H (FSARH)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC110> FSARH;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理開始アドレスレジスタ L (FSARL)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC108> FSARL;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理終了アドレスレジスタ H (FEARH)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC120> FEARH;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュ処理終了アドレスレジスタ L (FEARL)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC118> FEARL;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュライトバッファ n レジスタ (FWBn) (n = 0 ～ 3)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC130> FWB0;
+		static rw16_t<0x007FC138> FWB1;
+		static rw16_t<0x007FC140> FWB2;
+		static rw16_t<0x007FC144> FWB3;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュステータスレジスタ 0 (FSTATR0)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fstatr0_t : public ro8_t<base> {
+			typedef ro8_t<base> io_;
+			using io_::operator ();
+
+			bit_ro_t<io_, bitpos::B0>  ERERR;
+			bit_ro_t<io_, bitpos::B1>  PRGERR;
+			bit_ro_t<io_, bitpos::B3>  BCERR;
+			bit_ro_t<io_, bitpos::B4>  ILGLERR;
+			bit_ro_t<io_, bitpos::B5>  EILGLERR;
+		};
+		static fstatr0_t<0x007FC1F0> FSTATR0;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュステータスレジスタ 1 (FSTATR1)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fstatr1_t : public ro8_t<base> {
+			typedef ro8_t<base> io_;
+			using io_::operator ();
+
+			bit_ro_t<io_, bitpos::B6>  FRDY;
+			bit_ro_t<io_, bitpos::B7>  EXRDY;
+		};
+		static fstatr1_t<0x007FC12C> FSTATR1;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュエラーアドレスモニタレジスタ H (FEAMH)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1E8> FEAMH;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュエラーアドレスモニタレジスタ L (FEAML)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1E0> FEAML;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュスタートアップ設定モニタレジスタ (FSCMR)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct fscmr_t : public rw16_t<base> {
+			typedef rw16_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B8>  SASMF;
+		};
+		static fscmr_t<0x007FC1C0> FSCMR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュアクセスウィンドウ開始アドレスモニタレジスタ (FAWSMR)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1C8> FAWSMR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  フラッシュアクセスウィンドウ終了アドレスモニタレジスタ (FAWEMR)
+		*/
+		//-----------------------------------------------------------------//
+		static rw16_t<0x007FC1D0> FAWEMR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ユニーク ID レジスタ n (UIDRn) (n = 0 ～ 3)
+		*/
+		//-----------------------------------------------------------------//
+		static rw32_t<0x007FC350> UIDR0;
+		static rw32_t<0x007FC354> UIDR1;
+		static rw32_t<0x007FC358> UIDR2;
+		static rw32_t<0x007FC35C> UIDR3;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ROM キャッシュ許可レジスタ (ROMCE)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct romce_t : public rw16_t<base> {
+			typedef rw16_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  ROMCEN;
+		};
+		static romce_t<0x00081000> ROMCE;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ROM キャッシュ無効化レジスタ (ROMCIV)
+			@param[in]	base	ベース
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t base>
+		struct romciv_t : public rw16_t<base> {
+			typedef rw16_t<base> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0>  ROMCIV;
+		};
+		static romciv_t<0x00081004> ROMCIV;
+#endif
+	};
+	typedef flash_t FLASH;
+}
