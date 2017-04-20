5e7810f1995e2694f9446cb863b398753b4fe459
hira <hira@rvf-rc45.net>
2017-04-20 09:31:03 +0900

    update thermistor type

--- chip/NTCTH.hpp
+++ chip/NTCTH.hpp
@@ -12,40 +12,53 @@ namespace chip {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
+		@brief  サーミスタ型
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	enum class thermistor {
+		NT103,	///< THB:4126, TR25:10K
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
 		@brief  NTCTH テンプレートクラス
 		@param[in]	ADNUM	A/D 変換値の量子化最大値
-		@param[in]	REFR	分圧基準抵抗（10K オームなら 10.0f）
-		@param[in]	THB		サーミスタ B 定数
+		@param[in]	THM		サーミスタの型
+		@param[in]	REFR	分圧抵抗値
 		@param[in]	thup	サーミスタが VCC 側の場合「true」、GND 側の場合「false」
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <uint16_t ADNUM, bool thup>
+	template <uint16_t ADNUM, thermistor THM, uint16_t REFR, bool thup>
 	class NTCTH {
+
+		// サーミスタの型に応じたパラメーター
+		// THB:  B 定数
+		// TR25: ２５度における基準抵抗値
+		static void get_para_(float& THB, float& TR25)
+		{
+			switch(THM) {
+			case thermistor::NT103:
+				THB  = 4126.0f;  ///< サーミスタＢ定数
+				TR25 = 10e3;     ///< R25 サーミスタ２５℃基準抵抗値
+				break;
+			default:
+				break;
+			}
+		}
 
 		// サーミスターが VCC 側
 		static float thermistor_upper_(uint16_t raw)
 		{
-			static const float THB  = 3452.0f;   ///< サーミスタＢ定数
-			static const float T0   = 298.15f;   ///< 絶対温度
-			static const float TR25 = 10.0f;     ///< R25 サーミスタ２５℃基準抵抗値
-			static const float REFR = 10.0f;     ///< 分圧抵抗 
-
-			float thr = (REFR * static_cast<float>(ADNUM) * (1.0f / static_cast<float>(raw))) - REFR;
-			float t = 1.0f / (std::log(thr / TR25) / THB + (1.0f / T0));
-			return t - 273.15f;
+			float thr = (static_cast<float>(REFR * ADNUM) / static_cast<float>(raw)) - static_cast<float>(REFR);
+			return thr;
 		}
 
 		// サーミスターが GND 側
 		static float thermistor_lower_(uint16_t raw)
 		{
-			static const float THB  = 3452.0f;   ///< サーミスタＢ定数
-			static const float T0   = 298.15f;   ///< 絶対温度
-			static const float TR25 = 10.0f;     ///< R25 サーミスタ２５℃基準抵抗値
-			static const float REFR = 10.0f;     ///< 分圧抵抗 
-
-			float thr = REFR * static_cast<float>(raw) / static_cast<float>(ADNUM - raw);
-			float t = 1.0f / (std::log(thr / TR25) / THB + (1.0f / T0));
-			return t - 273.15f;
+			float thr = static_cast<float>(REFR * raw) / static_cast<float>(ADNUM - raw);
+			return thr;
 		}
 
 	public:
@@ -55,9 +68,20 @@ namespace chip {
 			@param[in]	adn		A/D 変換値
 		 */
 		//-----------------------------------------------------------------//
-		float operator () (uint16_t adn) const {
-			if(thup) return thermistor_upper_(adn);
-			return thermistor_lower_(adn);
+		float operator () (uint16_t adn) const
+		{
+			float thr;
+			if(thup) {
+				thr = thermistor_upper_(adn);
+			} else {
+				thr = thermistor_lower_(adn);
+			}
+			float THB;
+			float TR25;
+			get_para_(THB, TR25);
+			static const float T0   = 298.15f;   ///< 絶対温度
+			float t = 1.0f / (std::log(thr / TR25) / THB + (1.0f / T0));
+			return t - 273.15f;
 		}
 	};
 }
