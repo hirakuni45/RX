b83960cdd245598050262d6de73378ef133e9bda
hira <hira@rvf-rc45.net>
2017-04-21 13:21:15 +0900

    rename

---
+++ GR/core/ethernet.cpp
@@ -0,0 +1,27 @@
+//=====================================================================//
+/*!	@file
+	@brief	ethernet class @n
+			Copyright 2017 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "ethernet.hpp"
+
+namespace net {
+
+	T_IPV4EP ethernet::remoteIPV4EP_;
+	char ethernet::UDPrecvBuf_[UDP_RCV_DAT_DATAREAD_MAXIMUM];
+
+	uint8_t ethernet::byteq_buf[RING_SIZ];
+	byteq_hdl_t	ethernet::hdl_;
+	byteq_hdl_t	ethernet::hdl_forSize_;
+	uint8_t ethernet::byteq_buf_forSize_[RING_SIZ_forSize];    /* sizeQueBody 1024 >> 2 = 256 */
+}
+
+extern "C" {
+
+	int t4_udp_callback(ID cepid, FN fncd , void *p_parblk) {
+		return net::ethernet::t4_udp_callback(cepid, fncd , p_parblk);
+	}
+
+}
