4dd89a957a767fa07c406e0250a1bb7536ff4db0
hira <hira@rvf-rc45.net>
2017-06-08 09:46:02 +0900

    update PHY device class

--- chip/phy_base.hpp
+++ chip/phy_base.hpp
@@ -7,6 +7,7 @@
 */
 //=====================================================================//
 #include <cstdint>
+#include "common/format.hpp"
 
 namespace chip {
 
@@ -15,7 +16,7 @@ namespace chip {
 		@brief  PHY リンク・ステート
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	enum class phy_link_state {
+	enum class phy_link_state : uint16_t {
 		NO_LINK = 0,	///< リンク無し
 		LINK_10H,		///< 10MBPS / Half
 		LINK_10F,		///< 10MBPS / Full
@@ -26,16 +27,362 @@ namespace chip {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  EUI-48/EUI-64 テンプレートクラス
-		@param[in]	SPI	SPI クラス
-		@param[in]	CS	チップ・セレクト
+		@brief  PHY ベース・テンプレート・クラス
+		@param[in]	ETHERC	 インサーネット・コントローラー・クラス
+		@param[in]	DEV_ADR	PHY アドレス
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class SPI, class CS>
+	template <class ETHERC, uint16_t DEV_ADR = 0>
 	class phy_base {
 
+		static const uint16_t REG_CONTROL          = 0;
+		static const uint16_t REG_STATUS           = 1;
+		static const uint16_t REG_IDENTIFIER1      = 2;
+		static const uint16_t REG_IDENTIFIER2      = 3;
+		static const uint16_t REG_AN_ADVERTISEMENT = 4;
+		static const uint16_t REG_AN_LINK_PARTNER  = 5;
+		static const uint16_t REG_AN_EXPANSION     = 6;
+
+		static const uint16_t MII_ST     = 1;
+		static const uint16_t MII_READ   = 2;
+		static const uint16_t MII_WRITE  = 1;
+
+		static const int MII_WAIT = 2;
+
+		// Basic Mode Control Register Bit Definitions
+		static const uint16_t CONTROL_RESET        = (1 << 15);
+		static const uint16_t CONTROL_LOOPBACK     = (1 << 14);
+		static const uint16_t CONTROL_100_MBPS     = (1 << 13);
+		static const uint16_t CONTROL_AN_ENABLE    = (1 << 12);
+		static const uint16_t CONTROL_POWER_DOWN   = (1 << 11);
+		static const uint16_t CONTROL_ISOLATE      = (1 << 10);
+		static const uint16_t CONTROL_AN_RESTART   = (1 << 9);
+		static const uint16_t CONTROL_FULL_DUPLEX  = (1 << 8);
+		static const uint16_t CONTROL_COLLISION    = (1 << 7);
+
+		// Basic Mode Status Register Bit Definitions
+		static const uint16_t STATUS_100_T4        = (1 << 15);
+		static const uint16_t STATUS_100F          = (1 << 14);
+		static const uint16_t STATUS_100H          = (1 << 13);
+		static const uint16_t STATUS_10F           = (1 << 12);
+		static const uint16_t STATUS_10H           = (1 << 11);
+		static const uint16_t STATUS_AN_COMPLETE   = (1 << 5);
+		static const uint16_t STATUS_RM_FAULT      = (1 << 4);
+		static const uint16_t STATUS_AN_ABILITY    = (1 << 3);
+		static const uint16_t STATUS_LINK_UP       = (1 << 2);
+		static const uint16_t STATUS_JABBER        = (1 << 1);
+		static const uint16_t STATUS_EX_CAPABILITY = (1 << 0);
+
+		// Auto Negotiation Advertisement Bit Definitions
+		static const uint16_t AN_ADVERTISEMENT_NEXT_PAGE = (1 << 15);  
+		static const uint16_t AN_ADVERTISEMENT_RM_FAULT  = (1 << 13);
+		static const uint16_t AN_ADVERTISEMENT_ASM_DIR   = (1 << 11);
+		static const uint16_t AN_ADVERTISEMENT_PAUSE     = (1 << 10);
+		static const uint16_t AN_ADVERTISEMENT_100_T4    = (1 << 9);
+		static const uint16_t AN_ADVERTISEMENT_100F      = (1 << 8);
+		static const uint16_t AN_ADVERTISEMENT_100H      = (1 << 7);
+		static const uint16_t AN_ADVERTISEMENT_10F       = (1 << 6);
+		static const uint16_t AN_ADVERTISEMENT_10H       = (1 << 5);
+		static const uint16_t AN_ADVERTISEMENT_SELECTOR  = (1 << 0);
+
+		// Auto Negostiate Link Partner Ability Bit Definitions
+		static const uint16_t AN_LINK_PARTNER_NEXT_PAGE  = (1 << 15);
+		static const uint16_t AN_LINK_PARTNER_ACK        = (1 << 14);
+		static const uint16_t AN_LINK_PARTNER_RM_FAULT   = (1 << 13);
+		static const uint16_t AN_LINK_PARTNER_ASM_DIR    = (1 << 11);
+		static const uint16_t AN_LINK_PARTNER_PAUSE      = (1 << 10);
+		static const uint16_t AN_LINK_PARTNER_100_T4     = (1 << 9);
+		static const uint16_t AN_LINK_PARTNER_100F       = (1 << 8);
+		static const uint16_t AN_LINK_PARTNER_100H       = (1 << 7);
+		static const uint16_t AN_LINK_PARTNER_10F        = (1 << 6);
+		static const uint16_t AN_LINK_PARTNER_10H        = (1 << 5);
+		static const uint16_t AN_LINK_PARTNER_SELECTOR   = (1 << 0);
+
+		// Delay constants
+		static const uint32_t DELAY_RESET     = 0x00020000L;
+		static const uint32_t DELAY_AN        = 0x00800000L;
+
+		ETHERC&	ethc_;
+
+		uint16_t local_advertise_;
+
+
+#if 0
+#ifdef TI_DP83822
+#define PHY_REG_DP83822_REGCR     (0x0D)
+#define PHY_REG_DP83822_ADDAR     (0x0E)
+#define PHY_REG_DP83822_RCSR      (0x17)
+#endif
+
+/* Vendor Specific PHY Registers */
+#ifdef MICREL_KSZ8041NL
+#define PHY_REG_PHY_CONTROL_1           (0x1E)
+#endif /* MICREL_KSZ8041NL */
+#endif
+
+		/*==============================================================================*/
+		//  DP83822 Boot Strap:															//
+		//..............................................................................//
+		//    COL   (#29):  FX_EN:    0, PHY_AD0:   1									//
+		//    RX_D0 (#30):  AN_1:     1, PHY_AD1:   0									//
+		//    RX_D1 (#31):  EEE_EN:   0, PHY_AD2:   0									//
+		//    RX_D2 (#32):  FLD_EN:   0, PHY_AD3:   0									//
+		//    RX_D3 (# 1):  AN_EN:    1, PHY_AD4:   0									//
+		//    LED_0 (#17):  AN_0:     1													//
+		//    CRS   (#27):  LED_CFG:  1, LED_SPEED: 0									//
+		//    RX_ER (#28):  AMDIX_EN: 1, RGMII_EN:  0									//
+		//    RX_DV (#26):  XI_50:    0, RMII_EN:   0									//
+		//..............................................................................//
+		//  Force Modes:																//
+		//    FX_EN: 0, AN_EN: 0, AN_1: 0, AN_0: 0 --->  10BASE-Te, Half-Duplex			//
+		//    FX_EN: 0, AN_EN: 0, AN_1: 0, AN_0: 1 --->  10BASE-Te, Full-Duplex			//
+		//    FX_EN: 0, AN_EN: 0, AN_1: 1, AN_0: 0 ---> 100BASE-TX, Half-Duplex			//
+		//    FX_EN: 0, AN_EN: 0, AN_1: 1, AN_0: 1 ---> 100BASE-TX, Full-Duplex			//
+		//  Advertised Modes:															//
+		//    FX_EN: 0, AN_EN: 1, AN_1: 0, AN_0: 0 --->  10BASE-Te, Half-Duplex			//
+		//    FX_EN: 0, AN_EN: 1, AN_1: 0, AN_0: 1 --->  10BASE-Te, Half/Full-Duplex	//
+		//    FX_EN: 0, AN_EN: 1, AN_1: 1, AN_0: 0 --->  10BASE-Te, Half-Duplex			//
+		//                                              100BASE-TX, Half-Duplex			//
+		//   *FX_EN: 0, AN_EN: 1, AN_1: 1, AN_0: 1 --->  10BASE-Te, Half/Full-Duplex	//
+		//                                              100BASE-TX, Half/Full-Duplex	//
+		//  Fiber Modes:																//
+		//    FX_EN: 1, AN_EN: X, AN_1: X, AN_0: 0 ---> 100BASE-FX, Half-Duplex			//
+		//    FX_EN: 1, AN_EN: X, AN_1: X, AN_0: 1 ---> 100BASE-FX, Full-Duplex			//
+		/*==============================================================================*/
+
+
+		void mii_write1_()
+		{
+			// The processing of one bit about frame format of MII Management Interface which is 
+			// provided by "Table 22-12" of "22.2.4.5" of "IEEE 802.3-2008_section2". 
+			// The data that 1 is output. 
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000006;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000007;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000007;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000006;
+			}
+		}
+
+
+		void mii_write0_()
+		{
+			// The processing of one bit about frame format of MII Management Interface which is 
+			// provided by "Table 22-12" of "22.2.4.5" of "IEEE 802.3-2008_section2". 
+			// The data that 0 is output. 
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000002;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000003;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000003;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000002;
+			}
+		}
+
+
+		void trans_1_to_0_()
+		{
+	    	// The processing of TA (turnaround) about writing of the frame format of
+			// MII Management Interface which is provided by "Table 22-12" of "22.2.4.5"
+			// of "IEEE 802.3-2008_section2".
+			mii_write1_();
+			mii_write0_();
+		}
+
+
+		void trans_z_to_0_()
+		{
+			// The processing of TA (turnaround) about reading of the frame format of
+			// MII Management Interface which is provided by "Table 22-12" of "22.2.4.5"
+			// of "IEEE 802.3-2008_section2".
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000000;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000001;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000001;
+			}
+			for(int j = MII_WAIT; j > 0; j--) {
+				ethc_.PIR = 0x00000000;
+			}
+		}
+
+
+		void reg_set_(uint16_t reg_addr, int32_t option)
+		{
+			// The processing of ST (start of frame),OP (operation code), PHYAD (PHY Address), and 
+			// REGAD (Register Address)  about the frame format of MII Management Interface which is 
+			// provided by "Table 22-12" of "22.2.4.5" of "IEEE 802.3-2008_section2".
+			uint16_t data = (MII_ST << 14);          /* ST code    */
+
+			if(option == MII_READ ) {
+				data |= (MII_READ << 12);	// OP code(RD)
+			} else {
+				data |= (MII_WRITE << 12);	// OP code(WT)
+			}
+
+			data |= (DEV_ADR << 7);			// PHY Address
+			data |= (reg_addr << 2);		// Reg Address
+
+			int i = 14;
+			while( i > 0 ) {
+				if((data & 0x8000) == 0) {
+					mii_write0_();
+				} else {
+					mii_write1_();
+				}
+				data <<= 1;
+				i--;
+			}
+		}
+
+
+		uint16_t reg_read_()
+		{
+			// The processing of DATA (data) about reading of the frame format of MII Management
+			// Interface which is provided by "Table 22-12" of "22.2.4.5" of "IEEE 802.3-2008_section2".
+			uint16_t data = 0;
+			int i = 16;
+			while( i > 0 ) {
+				for(int j = MII_WAIT; j > 0; j--) {
+					ethc_.PIR = 0x00000000;
+				}
+				for(int j = MII_WAIT; j > 0; j--) {
+					ethc_.PIR = 0x00000001;
+				}
+
+				data <<= 1;
+				// MDI read
+				data |= static_cast<uint16_t>((ethc_.PIR() & 0x00000008) >> 3);
+
+				for(int j = MII_WAIT; j > 0; j--) {
+					ethc_.PIR = 0x00000001;
+				}
+				for(int j = MII_WAIT; j > 0; j--) {
+					ethc_.PIR = 0x00000000;
+				}
+				i--;
+			}
+			return data;
+		}
+
+
+		void reg_write_(uint16_t data)
+		{
+			// The processing of DATA (data) about writing of the frame format of MII Management
+			// Interface which is provided by "Table 22-12" of "22.2.4.5" of "IEEE 802.3-2008_section2".
+			int i = 16;
+			while( i > 0 ) {
+				if((data & 0x8000) == 0) {
+					mii_write0_();
+				} else {
+					mii_write1_();
+				}
+				i--;
+				data <<= 1;
+			}
+		}
+
+
+		void preamble_()
+		{
+			// The processing of PRE (preamble) about the frame format of MII Management
+			// Interface which is provided by "Table 22-12" of "22.2.4.5" of "IEEE 802.3-2008_section2".
+			int i = 32;
+			while(i > 0) {
+				mii_write1_();
+				i--;
+			}
+		}
+
+
+		uint16_t read_sub_(uint16_t reg_addr)
+		{
+			// The value is read from the PHY register by the frame format of MII Management
+			// Interface provided for by Table 22-12 of 22.2.4.5 of IEEE 802.3-2008_section2. 
+			preamble_();
+			reg_set_(reg_addr, MII_READ);
+			trans_z_to_0_();
+			uint16_t data = reg_read_();
+			trans_z_to_0_();
+			return data;
+		}
+
+
+		void write_sub_(uint16_t reg_addr, uint16_t data)
+		{
+			// The value is read from the PHY register by the frame format of MII Management
+			// Interface provided for by Table 22-12 of 22.2.4.5 of IEEE 802.3-2008_section2. 
+			preamble_();
+			reg_set_(reg_addr, MII_WRITE);
+			trans_1_to_0_();
+			reg_write_(data);
+			trans_z_to_0_();
+		}
+
+
+		uint16_t read_(uint16_t addr)
+		{
+			if(addr > 0x1f) {  // 拡張アドレスの場合
+#if 0
+#ifdef TI_DP83822
+			write_sub_(PHY_REG_DP83822_REGCR, 0x001F);  // address command
+			write_sub_(PHY_REG_DP83822_ADDAR, addr);
+			write_sub_(PHY_REG_DP83822_REGCR, 0x401F);  // read/write command
+			return read_sub_(PHY_REG_DP83822_ADDAR);
+#else
+			printf("PHY Address Range Error: %04X\n", reg_addr);
+			return 0;
+#endif
+#endif
+			} else {
+				return read_sub_(addr);
+			}
+		}
+
+
+		void write_(uint16_t reg_addr, uint16_t data)
+		{
+			if(reg_addr > 0x1f) {  // 拡張アドレスの場合
+#if 0
+#ifdef TI_DP83822
+				phy_write_sub_(PHY_REG_DP83822_REGCR, 0x001F);  // address command
+				phy_write_sub_(PHY_REG_DP83822_ADDAR, reg_addr);
+				phy_write_sub_(PHY_REG_DP83822_REGCR, 0x401F);  // read/write command
+				phy_write_sub_(PHY_REG_DP83822_ADDAR, data);
+#else
+				printf("PHY Address Range Error: %04X\n", reg_addr);
+				return;
+#endif
+#endif
+			} else {
+				write_sub_(reg_addr, data);
+			}
+		}
 
 	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+			@param[in]	ethc	インサーネット・コントローラー
+		*/
+		//-----------------------------------------------------------------//
+		phy_base(ETHERC& ethc) : ethc_(ethc), local_advertise_(0) { }
+
+
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  PHY 初期化
@@ -44,47 +391,196 @@ namespace chip {
 		//-----------------------------------------------------------------//
 		bool init(void)
 		{
+			uint16_t reg;
+#ifdef PHY_DEBUG
+			utils::format("Start PHY init: ADR: %d\n") % static_cast<int>(MII_ADDR);
+			reg = read_(REG_IDENTIFIER1);
+			reg = read_(REG_IDENTIFIER1);
+			utils::format("PHY Identifier1: 0x%04X\n") % static_cast<int>(reg);
+			reg = read_(REG_IDENTIFIER2);
+			reg = read_(REG_IDENTIFIER2);
+			utils::format("PHY Identifier2: 0x%04X\n") % static_cast<int>(reg);
+#endif
 
-			return true;
+			// Reset PHY
+			write_(REG_CONTROL, CONTROL_RESET);
+
+			uint32_t count = 0;
+			do {  // Reset completion waiting
+				reg = read_(REG_CONTROL);
+				count++;
+			} while((reg & CONTROL_RESET) && (count < DELAY_RESET));
+#ifdef PHY_DEBUG
+			utils::format("PHY init: reset loop: %d/%d\n")
+				% static_cast<int>(count)
+				% static_cast<int>(DELAY_RESET);
+#endif
+
+			if(count < DELAY_RESET) {
+				// When KSZ8041NL of the Micrel, Inc. is used, 
+				// the pin that outputs the state of LINK is used combinedly with ACTIVITY in default. 
+				// The setting of the pin is changed so that only the state of LINK is output. 
+#ifdef MICREL_KSZ8041NL
+				reg = phy_read_(REG_PHY_CONTROL_1);
+				reg &= ~0x8000;
+				reg |= 0x4000;
+				write_(REG_PHY_CONTROL_1, reg);
+#endif
+
+#ifdef TI_DP83822
+#ifdef PHY_DEBUG
+				reg = phy_read_(0x0462);
+				printf("DP83822 Boot Strap Latch (0x0462): 0x%04X\n", (int)reg);
+				reg = phy_read_(0x0467);
+				printf("DP83822 Boot Strap Latch #1(SOR1): 0x%04X\n", (int)reg);
+				reg = phy_read_(0x0468);
+				printf("DP83822 Boot Strap Latch #2(SOR2): 0x%04X\n", (int)reg);
+#endif
+
+				write_(0x0018, 0b0000010001000000);
+				write_(0x0019, 0b0000000000100001);
+				write_(0x0462, 0b0100001100000000);
+
+				write_(REG_DP83822_RCSR, 0b0000000001100001);
+
+#ifdef PHY_DEBUG
+				reg = read_(0x0462);
+				printf("DP83822 Boot Strap Latch (0x0462): 0x%04X\n", (int)reg);
+#endif
+#endif
+
+#ifdef PHY_DEBUG
+				printf("PHY init OK...\n");
+#endif
+				return true;
+			}
+#ifdef PHY_DEBUG
+			utils::format("PHY Init NG...\n");
+#endif
+			return false;
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  PHY 自動速度調整
+			@param[in]	pause_frame	ETHER_FLAG_OFF なら「true」
 		*/
 		//-----------------------------------------------------------------//
-		void start_autonegotiate()
+		void start_autonegotiate(bool pause_frame)
 		{
+			// Set local ability When pause frame is not used
+			if(pause_frame) {
+				local_advertise_ = (AN_ADVERTISEMENT_100F
+					| AN_ADVERTISEMENT_100H
+					| AN_ADVERTISEMENT_10F
+					| AN_ADVERTISEMENT_10H
+					| AN_ADVERTISEMENT_SELECTOR);
+			} else {  // When pause frame is used
+				local_advertise_ = (AN_ADVERTISEMENT_ASM_DIR
+					| AN_ADVERTISEMENT_PAUSE
+					| AN_ADVERTISEMENT_100F
+					| AN_ADVERTISEMENT_100H
+					| AN_ADVERTISEMENT_10F
+					| AN_ADVERTISEMENT_10H
+					| AN_ADVERTISEMENT_SELECTOR);
+			}
+    
+			// Configure what the PHY and the Ethernet controller on this board supports
+			write_(REG_AN_ADVERTISEMENT, local_advertise_);
+			write_(REG_CONTROL, (CONTROL_AN_ENABLE | CONTROL_AN_RESTART));
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  PHY 自動速度調整結果取得
-			@param[out]	line_speed_duplex	both the line speed and the duplex
-			@param[out]	local_pause			store the local pause bits
-			@param[out]	partner_pause		store the partner pause bits
-			@return		正常なら「true」
+			@param[out]	local_pause		store the local pause bits
+			@param[out]	partner_pause	store the partner pause bits
+			@return		リンク・ステート
 		*/
 		//-----------------------------------------------------------------//
-		bool get_autonegotiate(uint16_t* line_speed_duplex, uint16_t* local_pause, uint16_t* partner_pause)
+		phy_link_state get_autonegotiate(uint16_t& local_pause, uint16_t& partner_pause)
 		{
+			phy_link_state speed_duplex = phy_link_state::NO_LINK;
 
-			return true;
+			// Because reading the first time shows the previous state, the Link status bit is read twice.
+			uint16_t reg = read_(REG_STATUS);
+			reg = read_(REG_STATUS);
+
+			// When the link isn't up, return error
+			if(!(reg & STATUS_LINK_UP)) {
+				asm("nop");
+				return speed_duplex;
+			}
+
+			// Establish local pause capability
+			if(local_advertise_ & AN_ADVERTISEMENT_PAUSE) {
+				local_pause |= (1 << 1);
+			}
+    
+			if(local_advertise_ & AN_ADVERTISEMENT_ASM_DIR) {
+				local_pause |= 1;
+			}
+
+			// When the auto-negotiation isn't completed, return error
+			if(!(reg & STATUS_AN_COMPLETE)) {
+				return speed_duplex;
+			} else {
+				// Get the link partner response
+				reg = read_(REG_AN_LINK_PARTNER);
+
+				// Establish partner pause capability
+				if((reg & AN_LINK_PARTNER_PAUSE) == AN_LINK_PARTNER_PAUSE) {
+					partner_pause = ( 1 << 1 );
+				}
+
+				if((reg & AN_LINK_PARTNER_ASM_DIR) == AN_LINK_PARTNER_ASM_DIR) {
+					partner_pause |= 1;
+				}
+
+				// Establish the line speed and the duplex
+				if(reg & AN_LINK_PARTNER_10H) {
+					speed_duplex = phy_link_state::LINK_10H;
+				}
+
+				if(reg & AN_LINK_PARTNER_10F) {
+					speed_duplex = phy_link_state::LINK_10F;
+				}
+
+				if(reg & AN_LINK_PARTNER_100H) {
+					speed_duplex = phy_link_state::LINK_100H;
+				}
+
+				if(reg & AN_LINK_PARTNER_100F) {
+					speed_duplex = phy_link_state::LINK_100F;
+				}
+		        return speed_duplex;
+    		}
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  PHY リンクステータス取得
-			@return -1 if links is down, 0 otherwise 
+			@return リンク・アップなら「true」 
 		*/
 		//-----------------------------------------------------------------//
 		bool get_link_status()
 		{
+			// Because reading the first time shows the previous state, the Link status bit is read twice.
+			uint16_t reg = read_(REG_STATUS);
+			reg = read_(REG_STATUS);
 
-			return true;
+			// When the link isn't up, return error
+			if(!(reg & STATUS_LINK_UP)) {
+				asm("nop");
+				// Link is down
+				return false;
+			} else {
+				// Link is up
+				return true;
+			}
 		}
 	};
 }
