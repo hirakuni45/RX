896cd60313ea65a79f9ab1dd93a021da0e16675e
hira <hira@rvf-rc45.net>
2017-06-19 12:30:26 +0900

    update def extern

--- net/r_t4_rx/src/config_tcpudp.c
+++ net/r_t4_rx/src/config_tcpudp.c
@@ -36,15 +36,15 @@
 * History       : DD.MM.YYYY Version Description
 *               : 30.08.2011 1.00    First release.
 ******************************************************************************/
+#include "config_tcpudp.h"
 
-#include "r_t4_itcpip.h"
 extern ER t4_callback(ID cepid, FN fncd , VP p_parblk);
 
 /****************************************************************************/
 /**********************     TCP-related definition     **********************/
 /****************************************************************************/
 /* Number of LAN port, Number of Serial port */
-const UB _t4_channel_num = 1;
+const uint8_t _t4_channel_num = 1;
 
 /***  Definition of TCP reception point (only port number needs to be set) ***/
 T_TCP_CREP tcp_crep[6] =
@@ -59,7 +59,7 @@ T_TCP_CREP tcp_crep[6] =
 };
 
 /* Total number of TCP reception points */
-const H __tcprepn = sizeof(tcp_crep) / sizeof(T_TCP_CREP);
+const uint16_t __tcprepn = sizeof(tcp_crep) / sizeof(T_TCP_CREP);
 
 /***  Definition of TCP communication end point
       (only receive window size needs to be set) ***/
@@ -80,10 +80,10 @@ T_TCP_CCEP tcp_ccep[6] =
 };
 
 /* Total number of TCP communication end points */
-const H __tcpcepn = sizeof(tcp_ccep) / sizeof(T_TCP_CCEP);
+const uint16_t __tcpcepn = sizeof(tcp_ccep) / sizeof(T_TCP_CCEP);
 
 /***  TCP MSS  ***/
-const UH _tcp_mss[] =
+const uint16_t _tcp_mss[2] =
 {
     /* MAX:1460 bytes */
     1460,
@@ -91,22 +91,22 @@ const UH _tcp_mss[] =
 };
 
 /***  Initial value of sequence number (Set the value other than 0)  ***/
-UW    _tcp_initial_seqno[] = { 1, 1 };
+uint16_t _tcp_initial_seqno[2] = { 1, 1 };
 
 /***  2MSL wait time (unit:10ms)  ***/
-const UH    _tcp_2msl[] =
+const uint16_t _tcp_2msl[2] =
 {
     (1 * 60 * (1000 / 10)),      /* 1 min */
     (1 * 60 * (1000 / 10)),      /* 1 min */
 };
 /***  Maximum value of retransmission timeout period (unit:10ms)  ***/
-const UH    _tcp_rt_tmo_rst[] =
+const uint16_t _tcp_rt_tmo_rst[2] =
 {
     (10 * 60 * (1000 / 10)),     /* 10 min */
     (10 * 60 * (1000 / 10)),     /* 10 min */
 };
 /***  Transmit for delay ack (ON=1/OFF=0) ***/
-UB    _tcp_dack[] = { 1, 1 };
+uint8_t _tcp_dack[2] = { 1, 1 };
 
 
 /****************************************************************************/
@@ -125,18 +125,18 @@ T_UDP_CCEP udp_ccep[6] =
 
 };
 /* Total number of UDP communication end points */
-const H __udpcepn = (sizeof(udp_ccep) / sizeof(T_UDP_CCEP));
+const uint16_t __udpcepn = (sizeof(udp_ccep) / sizeof(T_UDP_CCEP));
 
 /***  TTL for multicast transmission  ***/
-const UB __multi_TTL[] = { 1, 1 };
+const uint8_t __multi_TTL[2] = { 1, 1 };
 
 /*** Behavior of UDP zero checksum ***/
-const UB _udp_enable_zerochecksum[] = { 0, 0 };           /* 0 = disable, other = enable */
+const uint8_t _udp_enable_zerochecksum[2] = { 0, 0 };           /* 0 = disable, other = enable */
 
 /****************************************************************************/
 /**********************     IP-related definition     ***********************/
 /****************************************************************************/
-const UH _ip_tblcnt[] = { 3, 3 };
+const uint16_t _ip_tblcnt[2] = { 3, 3 };
 
 #define MY_IP_ADDR0     192,168,0,3            /* Local IP address  */
 #define GATEWAY_ADDR0   0,0,0,0                /* Gateway address (invalid if all 0s) */
@@ -146,10 +146,10 @@ const UH _ip_tblcnt[] = { 3, 3 };
 #define GATEWAY_ADDR1   0,0,0,0                /* Gateway address (invalid if all 0s) */
 #define SUBNET_MASK1    255,255,255,0          /* Subnet mask  */
 
-TCPUDP_ENV tcpudp_env[] =
+TCPUDP_ENV tcpudp_env[2] =
 {
-    {{MY_IP_ADDR0}, {SUBNET_MASK0}, {GATEWAY_ADDR0}},
-    {{MY_IP_ADDR1}, {SUBNET_MASK1}, {GATEWAY_ADDR1}},
+    { { MY_IP_ADDR0 }, { SUBNET_MASK0 }, { GATEWAY_ADDR0 } },
+    { { MY_IP_ADDR1 }, { SUBNET_MASK1 }, { GATEWAY_ADDR1 } },
 };
 
 
@@ -163,23 +163,23 @@ TCPUDP_ENV tcpudp_env[] =
 #define MY_MAC_ADDR0    0x74,0x90,0x50,0x00,0x79,0x03
 #define MY_MAC_ADDR1    0x74,0x90,0x50,0x00,0x79,0x04
 
-UB _myethaddr[][6] =
+uint8_t _myethaddr[2][6] =
 {
-    {MY_MAC_ADDR0},
-    {MY_MAC_ADDR1},
+    { MY_MAC_ADDR0 },
+    { MY_MAC_ADDR1 },
 };
 
 /*--------------------------------------------------------------------------*/
 /*    Set of PPP-related                                                    */
 /*--------------------------------------------------------------------------*/
 /* Authentication-related setting */
-const UH ppp_auth  = AUTH_PAP;               /* PAP,No authentication enabled */
-UB user_name[6]    = "abcde";                /* user name */
-UB user_passwd[6]  = "abc00";                /* password  */
+const uint16_t ppp_auth  = AUTH_PAP;               /* PAP,No authentication enabled */
+uint8_t user_name[6]    = "abcde";                /* user name */
+uint8_t user_passwd[6]  = "abc00";                /* password  */
 
 /* Dial up-related setting */
-const UB peer_dial[]      = "0,123";                /* Destination telephone number */
-const UB at_commands[]   = "ATW2S0=2&C0&D0&S0M0S10=255X3";    /* Modem initialization command */
+const uint8_t peer_dial[]      = "0,123";                /* Destination telephone number */
+const uint8_t at_commands[]   = "ATW2S0=2&C0&D0&S0M0S10=255X3";    /* Modem initialization command */
 
 
 /* Copyright 2014, RENESAS ELECTRONICS CORPORATION */
---
+++ net/r_t4_rx/src/config_tcpudp.h
@@ -0,0 +1,50 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	TCP/UDP config @n
+			Copyright 2017 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "r_t4_itcpip.h"
+
+extern const uint8_t _t4_channel_num;
+
+extern T_TCP_CREP tcp_crep[6];
+
+extern const uint16_t __tcprepn;
+
+extern T_TCP_CCEP tcp_ccep[6];
+
+extern const uint16_t __tcpcepn;
+
+extern const uint16_t _tcp_mss[2];
+
+extern uint16_t _tcp_initial_seqno[2];
+
+extern const uint16_t _tcp_2msl[2];
+
+extern const uint16_t _tcp_2msl[2];
+
+extern const uint16_t _tcp_rt_tmo_rst[2];
+
+extern uint8_t _tcp_dack[2];
+
+extern T_UDP_CCEP udp_ccep[6];
+
+extern const uint16_t __udpcepn;
+
+extern const uint8_t __multi_TTL[2];
+
+extern const uint8_t _udp_enable_zerochecksum[2];
+
+extern const uint16_t _ip_tblcnt[2];
+
+extern TCPUDP_ENV tcpudp_env[2];
+
+extern uint8_t _myethaddr[2][6];
+
+
+
+
+extern uint8_t _myethaddr[][6];
