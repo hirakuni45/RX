0e97ab4cad762a5313198e49429e3396613164bd
hira <hira@rvf-rc45.net>
2017-06-19 16:42:11 +0900

    update cpu endian define

--- RX64M/ether_io.hpp
+++ RX64M/ether_io.hpp
@@ -11,8 +11,14 @@
 #include "common/format.hpp"
 #include "chip/phy_base.hpp"
 
-#ifdef BIG_ENDIAN
-#elif LITTLE_ENDIAN
+#if (defined(__GNUC__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__))
+#ifndef BIG_ENDIAN
+#define BIG_ENDIAN
+#endif
+#elif (defined(__GNUC__) && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__))
+#ifndef LITTLE_ENDIAN
+#define LITTLE_ENDIAN
+#endif
 #else
 #error "ether_io.hpp requires BIG_ENDIAN or LITTLE_ENDIAN be defined."
 #endif
