79344fe0b9e87d835d6048aa98c97266d59150ae
hira <hira@rvf-rc45.net>
2017-06-29 12:06:58 +0900

    exclusive control for buffer

--- net2/arp.hpp
+++ net2/arp.hpp
@@ -23,19 +23,25 @@ namespace net {
 		static const uint16_t ARP_REQUEST_WAIT = 200;   ///< 2 sec
 		static const uint16_t ARP_REQUEST_NUM  = 5;     ///< 5 times
 
-		struct arp_t {
+		struct arp_h {
 			uint8_t	head[8];
 			uint8_t	src_mac[6];
 			uint8_t	src_ipa[4];
 			uint8_t dst_mac[6];
 			uint8_t dst_ipa[4];
-		};
+		} __attribute__((__packed__));
 
 		net_info&	info_;
 
 		typedef utils::fixed_fifo<arp_info, 8> ARP_BUFF;
-
 		ARP_BUFF	arp_buff_;
+
+		struct arp_send_t {
+			eth_h	eh_;
+			arp_h	arp_;
+		} __attribute__((__packed__));
+		typedef utils::fixed_fifo<arp_send_t, 4> ARP_SEND;
+		ARP_SEND	arp_send_;
 
 		ip_adrs		req_ipa_;
 		uint16_t	req_wait_;
@@ -48,7 +54,7 @@ namespace net {
 		}
 
 
-		void dump_(const arp_t& arp)
+		void dump_(const arp_h& arp)
 		{
 			utils::format("ARP Header:\n");
 			utils::format("  %02X%02X\n")
@@ -76,115 +82,132 @@ namespace net {
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  パース
+			@brief  プロセス @n
+					※割り込み外から呼ぶ事は禁止
 			@param[in]	eth		イーサーネット・ドライバー
 			@param[in]	h		ヘッダー
 			@param[in]	top		先頭ポインター
 			@param[in]	len		長さ
 		*/
 		//-----------------------------------------------------------------//
-		bool parse(ETHER& eth, const eth_h& h, const void* top, int32_t len)
+		bool process(ETHER& eth, const eth_h& h, const void* top, int32_t len)
 		{
-			if(static_cast<size_t>(len) < sizeof(arp_t)) {
-				return false;
+			bool ret = false;
+			if(static_cast<size_t>(len) < sizeof(arp_h)) {
+				goto process_end;
 			}
 
-			const arp_t& r = *static_cast<const arp_t*>(top);
-			if(std::memcmp(get_arp_head7(), r.head, 7) != 0) {
-				return false;
-			}
-
-			if(tools::check_brodcast_mac(h.get_dst()) && r.head[7] == 0x01) {  // ARP Request
-				if(arp_buff_.length() < (arp_buff_.size() - 1)) {
-					arp_info a;
-					a.ipa.set(r.src_ipa[0], r.src_ipa[1], r.src_ipa[2], r.src_ipa[3]);
-					std::memcpy(a.mac, r.src_mac, 6);
-					arp_buff_.put(a);
+			{
+				const arp_h& r = *static_cast<const arp_h*>(top);
+				if(std::memcmp(get_arp_head7(), r.head, 7) != 0) {
+					goto process_end;
 				}
-			} else if(r.head[7] == 0x02) {  // ARP Response
-				// IP/MAC の収集
-				if(arp_buff_.length() < (arp_buff_.size() - 2)) {
-					arp_info a;
-					a.ipa.set(r.src_ipa[0], r.src_ipa[1], r.src_ipa[2], r.src_ipa[3]);
-					std::memcpy(a.mac, r.src_mac, 6);
-					arp_buff_.put(a);
-					a.ipa.set(r.dst_ipa[0], r.dst_ipa[1], r.dst_ipa[2], r.dst_ipa[3]);
-					std::memcpy(a.mac, r.dst_mac, 6);
-					arp_buff_.put(a);
-				}
-				return false;
-			} else {
-				return false;
-			}
 
-			ip_adrs ipa(r.dst_ipa[0], r.dst_ipa[1], r.dst_ipa[2], r.dst_ipa[3]);
-			if(info_.ip != ipa) {
-				return false;
-			}
+				if(tools::check_brodcast_mac(h.get_dst()) && r.head[7] == 0x01) {  // ARP Request
+					if(arp_buff_.length() < (arp_buff_.size() - 1)) {
+						arp_info& a = arp_buff_.put_at();
+						a.ipa.set(r.src_ipa[0], r.src_ipa[1], r.src_ipa[2], r.src_ipa[3]);
+						std::memcpy(a.mac, r.src_mac, 6);
+						arp_buff_.put_go();
+					}
+				} else if(r.head[7] == 0x02) {  // ARP Response
+					// IP/MAC の収集
+					if(arp_buff_.length() < (arp_buff_.size() - 2)) {
+						{
+							arp_info& a = arp_buff_.put_at();
+							a.ipa.set(r.src_ipa[0], r.src_ipa[1], r.src_ipa[2], r.src_ipa[3]);
+							std::memcpy(a.mac, r.src_mac, 6);
+							arp_buff_.put_go();
+						}
+						{
+							arp_info& a = arp_buff_.put_at();
+							a.ipa.set(r.dst_ipa[0], r.dst_ipa[1], r.dst_ipa[2], r.dst_ipa[3]);
+							std::memcpy(a.mac, r.dst_mac, 6);
+							arp_buff_.put_go();
+						}
+					}
+					goto process_end;
+				} else {
+					goto process_end;
+				}
+
+				ip_adrs ipa(r.dst_ipa[0], r.dst_ipa[1], r.dst_ipa[2], r.dst_ipa[3]);
+				if(info_.ip != ipa) {
+					goto process_end;
+				}
 
 //			utils::format("ARP: src: %s, dst: %s\n")
 //				% tools::ip_str(r.src_ipa)
 //				% tools::ip_str(r.dst_ipa);
-			void* dst;
-			uint16_t dlen;
-			if(eth.send_buff(&dst, dlen) != 0) {
-				utils::format("ARP: ether send_buff error\n");
-				return false;
-			}
+				void* dst;
+				uint16_t dlen;
+				if(eth.send_buff(&dst, dlen) != 0) {
+					utils::format("ARP: ether send_buff error\n");
+					goto process_end;
+				}
 
-			eth_h& eh = *static_cast<eth_h*>(dst);
-			eh.set_dst(h.get_src());
-			eh.set_src(info_.mac);
-			eh.set_type(eth_type::ARP);
+				eth_h& eh = *static_cast<eth_h*>(dst);
+				eh.set_dst(h.get_src());
+				eh.set_src(info_.mac);
+				eh.set_type(eth_type::ARP);
 
-   			arp_t& arp = *static_cast<arp_t*>(eth_h::next_ptr(dst));
-			std::memcpy(arp.head, get_arp_head7(), 7);
-			arp.head[7] = 0x02;
-			std::memcpy(arp.src_mac, info_.mac, 6);
-			std::memcpy(arp.src_ipa, info_.ip.get(), 4);
-			std::memcpy(arp.dst_mac, r.src_mac, 6);
-			std::memcpy(arp.dst_ipa, r.src_ipa, 4);
+   				arp_h& arp = *static_cast<arp_h*>(eth_h::next_ptr(dst));
+				std::memcpy(arp.head, get_arp_head7(), 7);
+				arp.head[7] = 0x02;
+				std::memcpy(arp.src_mac, info_.mac, 6);
+				std::memcpy(arp.src_ipa, info_.ip.get(), 4);
+				std::memcpy(arp.dst_mac, r.src_mac, 6);
+				std::memcpy(arp.dst_ipa, r.src_ipa, 4);
 
 //			dump(eh);
 //			dump_(arp);
 
-			eth.send(sizeof(eth_h) + sizeof(arp_t));
+				eth.send(sizeof(eth_h) + sizeof(arp_h));
+			}
+			ret = true;
 
-			return true;
+		process_end:
+
+			while(arp_send_.length() > 0) {
+				const arp_send_t& t = arp_send_.get();
+
+				void* dst;
+				uint16_t dlen;
+				if(eth.send_buff(&dst, dlen) != 0) {
+					utils::format("ARP: ether send_buff error\n");
+					return false;
+				}
+				std::memcpy(dst, &t, sizeof(arp_send_t));
+				eth.send(sizeof(arp_send_t));
+			}
+
+			return ret;
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  リクエスト
-			@param[in]	eth		イーサーネット・ドライバー
 			@param[in]	ipa		リクエストする IP アドレス
 			@return 正常終了なら「true」
 		*/
 		//-----------------------------------------------------------------//
-		bool request(ETHER& eth, const ip_adrs& ipa)
+		bool request(const ip_adrs& ipa)
 		{
-			void* dst;
-			uint16_t dlen;
-			if(eth.send_buff(&dst, dlen) != 0) {
-				utils::format("ARP: ether send_buff error\n");
-				return false;
-			}
+			arp_send_t& t = arp_send_.put_at();
 
-			eth_h& eh = *static_cast<eth_h*>(dst);
-			eh.set_dst(tools::get_brodcast_mac());
-			eh.set_src(info_.mac);
-			eh.set_type(eth_type::ARP);
+			t.eh_.set_dst(tools::get_brodcast_mac());
+			t.eh_.set_src(info_.mac);
+			t.eh_.set_type(eth_type::ARP);
 
-   			arp_t& arp = *static_cast<arp_t*>(eth_h::next_ptr(dst));
-			std::memcpy(arp.head, get_arp_head7(), 7);
-			arp.head[7] = 0x01;  // request
-			std::memcpy(arp.src_mac, info_.mac, 6);
-			std::memcpy(arp.src_ipa, info_.ip.get(), 4);
-			std::memset(arp.dst_mac, 0x00, 6);
-			std::memcpy(arp.dst_ipa, ipa.get(), 4);
+			std::memcpy(t.arp_.head, get_arp_head7(), 7);
+			t.arp_.head[7] = 0x01;  // request
+			std::memcpy(t.arp_.src_mac, info_.mac, 6);
+			std::memcpy(t.arp_.src_ipa, info_.ip.get(), 4);
+			std::memset(t.arp_.dst_mac, 0x00, 6);
+			std::memcpy(t.arp_.dst_ipa, ipa.get(), 4);
 
-			eth.send(sizeof(eth_h) + sizeof(arp_t));
+			arp_send_.put_go();
 
 			req_ipa_   = ipa;
 			req_wait_  = ARP_REQUEST_WAIT;
@@ -198,14 +221,14 @@ namespace net {
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  サービス
-			@param[in]	eth		イーサーネット・ドライバー
 		*/
 		//-----------------------------------------------------------------//
-		void service(ETHER& eth)
+		void service()
 		{
 			while(arp_buff_.length() > 0) {
-				const auto a = arp_buff_.get();
+				const arp_info& a = arp_buff_.get_at();
 				info_.cash.insert(a.ipa, a.mac);
+				arp_buff_.get_go();
 			}
 
 			if(req_wait_) {
@@ -215,7 +238,7 @@ namespace net {
 				auto n = info_.cash.lookup(req_ipa_);
 				if(n >= info_.cash.capacity()) {
 					req_wait_ = ARP_REQUEST_WAIT;
-					request(eth, req_ipa_);
+					request(req_ipa_);
 				}
 			}
 		}
