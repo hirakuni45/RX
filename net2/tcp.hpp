e92a50907f199c8c9f735e06a9cf48a4e81ce44a
hira <hira@rvf-rc45.net>
2017-07-01 06:29:49 +0900

    rename file

--- net2/tcp.hpp
+++ net2/tcp.hpp
@@ -7,42 +7,78 @@
 */
 //=========================================================================//
 #include "net2/net_st.hpp"
-#include "net2/mac_cash.hpp"
+#include "common/fixed_block.hpp"
 
 namespace net {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief  TCP クラス
+		@param[in]	ETHER	イーサーネット・ドライバー・クラス
+		@param[in]	NMAX	管理最大数
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template<class ETHER, uint32_t NMAX>
 	class tcp {
 
+		ETHER&		eth_;
 
+		net_info&	info_;
 
+		enum class task : uint16_t {
+			idle,
+			open,
+			main,
+			close,
+		};
 
+		struct context {
+			ip_adrs		adrs_;
+			uint16_t	src_port_;
+			uint16_t	dst_port_;
+			uint16_t	time_;
+			task		task_;
+
+			uint8_t*	recv_;
+			uint8_t*	send_;
+		};
+
+		typedef utils::fixed_block<context, NMAX> TCP_BLOCK;
+		TCP_BLOCK	tcps_;
 
 	public:
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  コンストラクター
 			@param[in]	eth		イーサーネット・ドライバー
+			@param[in]	info	ネット情報
 		*/
 		//-----------------------------------------------------------------//
-		tcp() { }
+		tcp(ETHER& eth, net_info& info) : eth_(eth), info_(info)
+		{ }
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  パース
+			@brief  格納可能な最大サイズを返す
+			@return 格納可能な最大サイズ
+		*/
+		//-----------------------------------------------------------------//
+		uint32_t capacity() const noexcept { return NMAX; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  プロセス
+					※割り込み外から呼ぶ事は禁止
 			@param[in]	eh	イーサーネット・ヘッダー
 			@param[in]	ih	IPV4 ヘッダー
-			@param[in]	msg	メッセージ先頭
+			@param[in]	tcp	TCP ヘッダー
 			@param[in]	len	メッセージ長
 			@return エラーが無い場合「true」
 		*/
 		//-----------------------------------------------------------------//
-		bool parse(const eth_h& eh, const ipv4_h& ih, const void* msg, int32_t len)
+		bool process(const eth_h& eh, const ipv4_h& ih, const tcp_h* tcp, int32_t len)
 		{
 			return true;
 		}
@@ -53,14 +89,15 @@ namespace net {
 			@brief  オープン
 			@param[in]	adrs	アドレス
 			@param[in]	port	ポート
+			@return ディスクリプタ
 		*/
 		//-----------------------------------------------------------------//
-		bool open(const ip_adrs& adrs, uint16_t port)
+		int open(const ip_adrs& adrs, uint16_t port)
 		{
+			int ds = -1;
 
 
-
-			return true;
+			return ds;
 		}
 
 
