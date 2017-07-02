6b589811738199267b4fed75de43a5763fc4b6d2
hira <hira@rvf-rc45.net>
2017-07-03 00:32:10 +0900

    update UDP task manage

--- net2/udp.hpp
+++ net2/udp.hpp
@@ -6,8 +6,9 @@
     @author 平松邦仁 (hira@rvf-rc45.net)
 */
 //=========================================================================//
-#include "net2/net_st.hpp"
 #include "common/fixed_block.hpp"
+#include "net2/net_st.hpp"
+#include "net2/memory.hpp"
 
 namespace net {
 
@@ -27,7 +28,6 @@ namespace net {
 
 		net_info&	info_;
 
-
 		enum class task : uint16_t {
 			idle,
 			open,
@@ -35,16 +35,20 @@ namespace net {
 			close,
 		};
 
+		typedef memory<4096>  BUFFER;
 
 		struct context {
 			ip_adrs		adrs_;
+			uint8_t		mac_[6];
+			bool		src_port_match_;
+			bool		dst_port_match_;
 			uint16_t	src_port_;
 			uint16_t	dst_port_;
 			uint16_t	time_;
 			task		task_;
 
-			uint8_t*	recv_;
-			uint8_t*	send_;
+			BUFFER		recv_;
+			BUFFER		send_;
 		};
 
 
@@ -100,15 +104,21 @@ namespace net {
 		{
 			uint32_t idx = udps_.alloc();
 			if(udps_.is_alloc(idx)) {
-				context& con = udps_.at(idx);
-				con.adrs_ = adrs;
-				con.src_port_ = sport;
-				con.dst_port_ = dport;
-				con.time_ = TIME_OUT;
-				con.task_ = task::open;
-
-				con.recv_ = nullptr;
-				con.send_ = nullptr;
+				context& ctx = udps_.at(idx);
+				std::memset(ctx.mac_, 0x00, 6);
+				ctx.adrs_ = adrs;
+				ctx.src_port_ = sport;
+				ctx.src_port_match_ = false;
+				ctx.dst_port_ = dport;
+				ctx.dst_port_match_ = true;
+				ctx.time_ = TIME_OUT;
+				if(adrs.is_any() || adrs.is_brodcast()) {
+					ctx.task_ = task::main;
+				} else {
+					ctx.task_ = task::open;
+				}
+				ctx.recv_.clear();
+				ctx.send_.clear();
 
 				udps_.unlock(idx);
 				return static_cast<int>(idx);
@@ -132,7 +142,15 @@ namespace net {
 			if(!udps_.is_alloc(idx)) return -1;
 			if(udps_.is_lock(idx)) return -1;
 
+			context& ctx = udps_.at(idx);
 
+			frame_t t;
+			t.eh_.set_dst(ctx.mac_);
+			t.eh_.set_src(info_.mac);
+			t.eh_.set_type(eth_type::IPV4);
+			t.ipv4_.ver_hlen_ = 0x45;
+			t.ipv4_.type_ = 0x00;
+//			t.udp_
 
 
 			return 0;
@@ -145,19 +163,26 @@ namespace net {
 			@param[in]	desc	ディスクリプタ
 			@param[in]	dst		ソース
 			@param[in]	len		受信バイト数
-			@param[in]	time	最大待ち時間（１０ｍｓ単位）
 			@return 受信バイト
 		*/
 		//-----------------------------------------------------------------//
-		int recv(int desc, void* dst, uint16_t len, int16_t time)
+		int recv(int desc, void* dst, uint16_t len)
 		{
 			uint32_t idx = static_cast<uint32_t>(desc);
 			if(!udps_.is_alloc(idx)) return -1;
 			if(udps_.is_lock(idx)) return -1;
 
+			context& ctx = udps_.at(idx);
+			int rlen = ctx.recv_.length();
+			if(rlen == 0) {  // 読み込むデータが無い
+				return rlen;
+			}
 
-
-			return 0;
+			if(rlen > len) {
+				rlen = len;
+			}
+			ctx.recv_.get(dst, rlen);
+			return rlen;
 		}
 
 
@@ -190,46 +215,52 @@ namespace net {
 		//-----------------------------------------------------------------//
 		bool process(const eth_h& eh, const ipv4_h& ih, const udp_h* udp, int32_t len)
 		{
-#if 0
 			// 該当するコンテキストを探す
 			uint32_t idx = NMAX;
 			for(uint32_t i = 0; i < NMAX; ++i) {
 				if(!udps_.is_alloc(i)) continue;  // alloc: 有効
 				if(udps_.is_lock(i)) continue;  // lock:  無効
-				context& con = udps_.at(i);
-				if(con.task_ != task::main) continue;  // task: main 以外は無効
+				context& ctx = udps_.at(i);  // コンテキスト取得
+				if(ctx.task_ != task::main) continue;  // task: main 以外は無効
 
-				if(udp->get_src_port() == con.dst_port_ && udp->get_dst_port() == con.src_port_) {
-					idx = i;
-					break;
-				} 
+				// 転送先の確認（ブロードキャスト）
+///				if(ih.get_dst_ipa() 
+				if(info_.ip != ih.get_dst_ipa()) continue;
+				// 転送元の確認
+				if(!ctx.adrs_.is_any() && ctx.adrs_ != ih.get_src_ipa()) continue; 
+
+				// ポート番号の確認
+				if(ctx.src_port_match_) {
+					if(ctx.src_port_ != udp->get_src_port()) {
+						continue;
+					}
+				}
+
+				if(ctx.dst_port_match_) {
+					if(ctx.dst_port_ != udp->get_dst_port()) {
+						continue;
+					}
+				}
+
+				// UDP サムの計算
+				csum_h smh;
+				smh.src_.set(ih.get_src_ipa());
+				smh.dst_.set(ih.get_dst_ipa());
+				smh.fix_ = 0x1100;
+				smh.len_ = udp->get_length_();  // 直接アクセス
+				uint16_t sum = tools::calc_sum(&smh, sizeof(smh));
+				sum = tools::calc_sum(udp, udp->get_length(), ~sum);
+				if(sum != 0) {
+					utils::format("UDP Frame sum error: %04X -> %04X\n") % udp->get_csum() % sum;
+					return false;
+				}
+
+				if(udp->get_data_len() < (ctx.recv_.size() - ctx.recv_.length() - 1)) {
+					ctx.recv_.put(udp->get_data_ptr(udp), udp->get_data_len());
+				}
+				return true;
 			}
-			if(idx == NMAX) return false;
-#endif
-			if(udp->get_dst_port() != 3000) {
-				return false;
-			}
-			// UDP サムの計算
-			csum_h smh;
-			smh.src_.set(ih.get_src_ipa());
-			smh.dst_.set(ih.get_dst_ipa());
-			smh.fix_ = 0x1100;
-			smh.len_ = udp->get_length_();  // 直接アクセス
-			uint16_t sum = tools::calc_sum(&smh, sizeof(smh));
-			sum = tools::calc_sum(udp, udp->get_length(), ~sum);
-			if(sum != 0) {
-				utils::format("UDP Frame sum error: %04X -> %04X\n") % udp->get_csum() % sum;
-				return false;
-			}
-
-			dump(*udp);
-
-			char tmp[16];
-			std::memcpy(tmp, udp->get_data_ptr(udp), udp->get_data_len());
-			tmp[udp->get_data_len()] = 0;
-			utils::format("UDP Data: '%s'\n") % tmp;
-
-			return true;
+			return false;
 		}
 
 
@@ -243,9 +274,18 @@ namespace net {
 			for(uint32_t i = 0; i < NMAX; ++i) {
 				if(!udps_.is_alloc(i)) continue;
 
-				context& con = udps_.at(i);
-				switch(con.task_) {
-				case task::open:					
+				context& ctx = udps_.at(i);
+				switch(ctx.task_) {
+				case task::open:
+					{
+						const auto& cash = info_.get_cash();
+						auto idx = cash.lookup(ctx.adrs_);
+						if(cash.is_valid(idx)) {
+							std::memcpy(ctx.mac_, cash[idx].mac, 6);
+							ctx.task_ = task::main;
+utils::format("UDP MAC lookup: %s\n") % ctx.adrs_.c_str();
+						}
+					}
 					break;
 
 				case task::main:
