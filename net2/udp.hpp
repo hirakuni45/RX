bb5e0bc9ce652d70585a29af099ed36f1fb300f5
hira <hira@rvf-rc45.net>
2017-07-04 11:10:42 +0900

    cleanup, add api

--- net2/udp.hpp
+++ net2/udp.hpp
@@ -36,7 +36,7 @@ namespace net {
 
 		net_info&	info_;
 
-		enum class task : uint16_t {
+		enum class send_task : uint8_t {
 			idle,
 			sync_mac,
 			main,
@@ -50,17 +50,17 @@ namespace net {
 		struct context {
 			ip_adrs		adrs_;
 			uint8_t		mac_[6];
-			uint16_t	src_port_;
+			uint16_t	cn_port_;
 			uint16_t	port_;
-			uint16_t	dst_port_;
-			uint16_t	time_;
+			uint16_t	recv_time_;
+			uint16_t	send_time_;
 
 			// IPV4 関係
 			uint16_t	id_;
 			uint16_t	offset_;
 			uint8_t		life_;
 
-			task		task_;
+			send_task	send_task_;
 
 			RECV_B		recv_;
 			SEND_B		send_;
@@ -146,7 +146,7 @@ namespace net {
 			smh.fix_ = 0x1100;  // UDP 固定値
 			smh.len_ = tools::htons(sizeof(udp_h) + len);
 
-			p->udp_.set_src_port(ctx.src_port_);
+			p->udp_.set_src_port(ctx.cn_port_);
 			if(ctx.port_ == 0) {
 				ctx.port_ = tools::random_port();
 			}
@@ -217,10 +217,10 @@ namespace net {
 			context& ctx = udps_.at(idx);
 			std::memset(ctx.mac_, 0x00, 6);
 			ctx.adrs_ = adrs;
-			ctx.src_port_ = port;
+			ctx.cn_port_ = port;
 			ctx.port_ = 0;  // 初期は「０」
-			ctx.dst_port_ = port;
-			ctx.time_ = TIME_OUT;
+			ctx.recv_time_ = TIME_OUT;
+			ctx.send_time_ = TIME_OUT;
 			ctx.id_ = 0;  // 識別子の初期値
 			ctx.life_ = 255;  // 生存時間初期値（ルーターの通過台数）
 			ctx.offset_ = 0;  // フラグメント・オフセット
@@ -231,12 +231,12 @@ namespace net {
 			udps_.unlock(idx);
 
 			if(adrs.is_any() || adrs.is_brodcast()) {
-				ctx.task_ = task::main;
+				ctx.send_task_ = send_task::main;
 			} else {
 				if(get_mac_(ctx)) {  // 既に MAC が利用可能なら「main」へ
-					ctx.task_ = task::main;
+					ctx.send_task_ = send_task::main;
 				} else {
-					ctx.task_ = task::sync_mac;
+					ctx.send_task_ = send_task::sync_mac;
 				}
 			}
 			return static_cast<int>(idx);
@@ -271,6 +271,24 @@ namespace net {
 
 		//-----------------------------------------------------------------//
 		/*!
+			@brief  送信バッファの残量取得
+			@param[in]	desc	ディスクリプタ
+			@return 送信バッファの残量
+		*/
+		//-----------------------------------------------------------------//
+		int get_send_length(int desc) const
+		{
+			uint32_t idx = static_cast<uint32_t>(desc);
+			if(!udps_.is_alloc(idx)) return -1;
+			if(udps_.is_lock(idx)) return -1;
+
+			context& ctx = udps_.at(idx);
+			return ctx.send_.length();
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
 			@brief  受信
 			@param[in]	desc	ディスクリプタ
 			@param[in]	dst		ソース
@@ -300,6 +318,24 @@ namespace net {
 
 		//-----------------------------------------------------------------//
 		/*!
+			@brief  送信バッファの残量取得
+			@param[in]	desc	ディスクリプタ
+			@return 送信バッファの残量
+		*/
+		//-----------------------------------------------------------------//
+		int get_recv_length(int desc) const
+		{
+			uint32_t idx = static_cast<uint32_t>(desc);
+			if(!udps_.is_alloc(idx)) return -1;
+			if(udps_.is_lock(idx)) return -1;
+
+			context& ctx = udps_.at(idx);
+			return ctx.recv_.length();
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
 			@brief  クローズ
 			@param[in]	desc	ディスクリプタ
 		*/
@@ -310,7 +346,7 @@ namespace net {
 			if(!udps_.is_alloc(idx)) return;
 
 			context& ctx = udps_.at(idx);
-			ctx.task_ = task::sync_close;
+			ctx.send_task_ = send_task::sync_close;
 		}
 
 
@@ -333,7 +369,7 @@ namespace net {
 				if(udps_.is_lock(i)) continue;  // lock:  無効
 				context& ctx = udps_.at(i);  // コンテキスト取得
 
-				// 転送先の確認（ブロードキャスト）
+				// 転送先の確認
 				if(info_.ip != ih.get_dst_ipa()) continue;
 
 				// 転送元の確認
@@ -347,7 +383,7 @@ namespace net {
 				} else {
 					ctx.port_ = udp->get_src_port();
 				}
-				if(ctx.dst_port_ != udp->get_dst_port()) {
+				if(ctx.cn_port_ != udp->get_dst_port()) {
 					continue;
 				}
 
@@ -385,27 +421,27 @@ namespace net {
 				if(!udps_.is_alloc(i)) continue;
 
 				context& ctx = udps_.at(i);
-				switch(ctx.task_) {
-				case task::sync_mac:
+				switch(ctx.send_task_) {
+				case send_task::sync_mac:
 					if(get_mac_(ctx)) {
-						ctx.task_ = task::main;
+						ctx.send_task_ = send_task::main;
 					}
 					break;
 
-				case task::main:
+				case send_task::main:
 					send_(ctx);
 					break;
 
-				case task::sync_close:
+				case send_task::sync_close:
 					if(ctx.send_.length() == 0) {
 						udps_.lock(i);  // ロックする（割り込みで利用不可にする）
-						ctx.task_ = task::close;
+						ctx.send_task_ = send_task::close;
 					} else {
 						send_(ctx);
 					}
 					break;
 
-				case task::close:
+				case send_task::close:
 					udps_.erase(i);
 					break;
 				default:
