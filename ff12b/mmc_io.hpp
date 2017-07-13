abbaf6f9ef21b1726b640d9a7ce1a6a8daa54d46
hira <hira@rvf-rc45.net>
2017-07-14 08:27:01 +0900

    update copyright

--- ff12b/mmc_io.hpp
+++ ff12b/mmc_io.hpp
@@ -1,9 +1,11 @@
 #pragma once
 //=====================================================================//
 /*!	@file
-	@brief	MMC（SD カード）FatFS ドライバー @n
-			Copyright 2016, 2017 Kunihito Hiramatsu
-	@author	平松邦仁 (hira@rvf-rc45.net)
+	@brief	MMC（SD カード）FatFS ドライバー
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2016, 2017 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
 //=====================================================================//
 #include "ff12b/src/diskio.h"
