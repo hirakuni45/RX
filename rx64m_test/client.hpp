b63fa8fc99cc941b65127a4c5c913a37f6856cad
hira <hira@rvf-rc45.net>
2017-07-21 07:52:33 +0900

    update client data buffering(FIFO)

--- rx64m_test/client.hpp
+++ rx64m_test/client.hpp
@@ -34,20 +34,25 @@ namespace seeda {
 
 		typedef utils::basic_format<net::ether_string<net::ethernet::format_id::client0, 2048> > format;
 
-		enum class task {
-			startup,
+		enum class recv_task {
+			idle,
+			sync_time,
+			sample_put,
+		};
+		recv_task	recv_task_;
+
+		enum class send_task {
+			idle,
 			req_connect,
 			wait_req_connect,
 			wait_connect,
-			time_sync,
 			make_form,
 			url_decode,
 			send_data,
 			disconnect,
 			sync_close,
 		};
-
-		task		task_;
+		send_task	send_task_;
 
 		net::ip_address		ip_;
 
@@ -56,9 +61,17 @@ namespace seeda {
 		uint32_t	timeout_;
 		uint32_t	re_connect_cnt_;
 
+		time_t		time_ref_;
 		time_t		time_;
 
+		uint32_t	idle_count_;
+
 		struct tm	tm_;
+
+		static const uint32_t BUF_SIZE = 64;
+
+		typedef utils::fixed_fifo<sample_t, BUF_SIZE> FIFO;
+		FIFO	fifo_;
 
 		char		form_[1024];
 		char		data_[2048];
@@ -69,14 +82,15 @@ namespace seeda {
 			@brief  コンストラクタ
 		*/
 		//-----------------------------------------------------------------//
-		client(net::ethernet& eth) : client_(eth, PORT), task_(task::startup),
+		client(net::ethernet& eth) : client_(eth, PORT),
+			recv_task_(recv_task::idle), send_task_(send_task::idle),
 #ifdef SEEDA
 			ip_(192, 168, 1, 3),
 #else
 			ip_(192, 168, 3, 7),
 #endif
 			port_(PORT),
-			delay_(0), timeout_(0), re_connect_cnt_(0), time_(0) { }
+			delay_(0), timeout_(0), re_connect_cnt_(0), time_ref_(0), time_(0), idle_count_(0) { }
 
 
 		//-----------------------------------------------------------------//
@@ -117,25 +131,13 @@ namespace seeda {
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  再接続
-		*/
-		//-----------------------------------------------------------------//
-		void re_start()
-		{
-			if(task_ == task::wait_connect) {
-				task_ = task::req_connect;
-			}
-		}
-
-
-		//-----------------------------------------------------------------//
-		/*!
 			@brief  接続開始
 		*/
 		//-----------------------------------------------------------------//
 		void start_connect()
 		{
-			task_ = task::req_connect;
+			recv_task_ = recv_task::sync_time;
+			send_task_ = send_task::req_connect;
 		}
 
 
@@ -146,32 +148,61 @@ namespace seeda {
 		//-----------------------------------------------------------------//
 		void service()
 		{
-			time_t t;
-			switch(task_) {
+			++idle_count_;
 
-			case task::startup:
+			switch(recv_task_) {
+			case recv_task::idle:
 				break;
 
-			case task::req_connect:
+			case recv_task::sync_time:
+				{
+					const sample_data& smd = get_sample_data();
+					if(time_ref_ != smd.time_) {
+						time_ref_ = smd.time_;
+						recv_task_ = recv_task::sample_put;
+					}
+				}
+				break;
+
+			case recv_task::sample_put:
+				{
+					if((BUF_SIZE - 1 - fifo_.length()) < 8) { // スペースが無い場合
+						break;
+					}
+					const sample_data& smd = get_sample_data();
+					for(int i = 0; i < 8; ++i) {
+						fifo_.put(smd.smp_[i]);
+					}
+					recv_task_ = recv_task::sync_time;
+				}
+				break;
+			}
+
+			switch(send_task_) {
+
+			case send_task::idle:
+				break;
+
+			case send_task::req_connect:
 				if(client_.connect(ip_, port_, TMO_NBLK)) {
 					re_connect_cnt_ = 0;
 					timeout_ = 5 * 100;  // 再接続待ち時間
-					task_ = task::wait_connect;
+					send_task_ = send_task::wait_connect;
 				} else {
 					timeout_ = 1 * 100;
-					task_ = task::wait_req_connect;
+					send_task_ = send_task::wait_req_connect;
 				}
 				break;
 
-			case task::wait_req_connect:
+			case send_task::wait_req_connect:
 				if(timeout_ > 0) {
 					--timeout_;
 				} else {
-					task_ = task::req_connect;
+					send_task_ = send_task::req_connect;
 				}
 				break;
 
-			case task::wait_connect:
+			case send_task::wait_connect:
 				if(!client_.connected()) {
 					if(timeout_ > 0) {
 						--timeout_;
@@ -180,62 +211,55 @@ namespace seeda {
 						auto st = client_.get_ethernet().get_stat(client_.get_cepid());
 						debug_format("TCP Client re_connect(%d): status: %d, desc(%d)\n")
 							% re_connect_cnt_ % static_cast<int>(st) % client_.get_cepid();
-///						if(st == 0) {
-///							task_ = task::disconnect;
-///						} else {
-							// 接続しないので、「re_connect」要求を出してみる
-							// ※ re_connect では、タイムアウト（１０分）を無効にする。
-							client_.re_connect(ip_, port_);
-							timeout_ = 5 * 100;  // 再接続待ち時間
-///							task_ = task::req_connect;
-///						}
-//						task_ = task::disconnect;
+						// 接続しないので、「re_connect」要求を出してみる
+						// ※ re_connect では、タイムアウト（１０分）を無効にする。
+						client_.re_connect(ip_, port_);
+						timeout_ = 5 * 100;  // 再接続待ち時間
 					}
 				} else {
 					debug_format("Start SEEDA03 Client: %s port(%d), fd(%d)\n")
 						% ip_.c_str() % port_ % client_.get_cepid();
 					format::chaout().set_fd(client_.get_cepid());
-					time_ = get_sample_data().time_;
-					task_ = task::time_sync;
-				}
-				break;
-
-			case task::time_sync:
-				{
-					auto t = get_sample_data().time_;
-					if(time_ == t) break;
-					time_ = t;
-					struct tm* m = localtime(&t);
-					tm_ = *m;
-					task_ = task::make_form;
-				}
-				break;
-
-			case task::make_form:
-				utils::sformat("%04d/%02d/%02d,%02d:%02d:%02d", form_, sizeof(form_))
-					% static_cast<uint32_t>(tm_.tm_year + 1900)
-					% static_cast<uint32_t>(tm_.tm_mon + 1)
-					% static_cast<uint32_t>(tm_.tm_mday)
-					% static_cast<uint32_t>(tm_.tm_hour)
-					% static_cast<uint32_t>(tm_.tm_min)
-					% static_cast<uint32_t>(tm_.tm_sec);
-				{
-					const sample_data& smd = get_sample_data();
-					for(int ch = 0; ch < 8; ++ch) {
-						utils::sformat(",", form_, sizeof(form_), true);
-						smd.smp_[ch].make_csv2(form_, sizeof(form_), true);
+					if(idle_count_ > 150) {  // 1.5 秒以上、の「間」がある場合、設定初期化
+						fifo_.clear();
+						time_ = time_ref_;
 					}
+					send_task_ = send_task::make_form;
+				}
+				break;
+
+			case send_task::make_form:
+				if(fifo_.length() < 8) {
+					break;
+				} 
+				for(int ch = 0; ch < 8; ++ch) {
+					const auto& smd = fifo_.get_at();
+					if(ch == 0) {
+						struct tm* m = localtime(&time_);
+						tm_ = *m;
+						utils::sformat("%04d/%02d/%02d,%02d:%02d:%02d", form_, sizeof(form_))
+							% static_cast<uint32_t>(tm_.tm_year + 1900)
+							% static_cast<uint32_t>(tm_.tm_mon + 1)
+							% static_cast<uint32_t>(tm_.tm_mday)
+							% static_cast<uint32_t>(tm_.tm_hour)
+							% static_cast<uint32_t>(tm_.tm_min)
+							% static_cast<uint32_t>(tm_.tm_sec);
+						++time_;
+					}
+					utils::sformat(",", form_, sizeof(form_), true);
+					smd.make_csv2(form_, sizeof(form_), true);
+					fifo_.get_go();
 				}
 				utils::sformat("\n", form_, sizeof(form_), true);
-				task_ = task::url_decode;
+				send_task_ = send_task::url_decode;
 				break;
 
-			case task::url_decode:
+			case send_task::url_decode:
 				utils::str::url_decode_to_str(form_, data_, sizeof(data_)); 
-				task_ = task::send_data;
+				send_task_ = send_task::send_data;
 				break;
 
-			case task::send_data:
+			case send_task::send_data:
 				format::chaout().clear();
 				format("POST /api/?val=%s HTTP/1.1\n") % data_;
 				format("Host: %d.%d.%d.%d\n")
@@ -247,21 +271,22 @@ namespace seeda {
 				format("User-Agent: SEEDA03 Post Client\n");
 				format("Connection: close\n\n");
 				format::chaout().flush();
-				task_ = task::disconnect;
+				send_task_ = send_task::disconnect;
 				break;
 			
-			case task::disconnect:
+			case send_task::disconnect:
 				client_.stop();
 				timeout_ = 0;
-				task_ = task::sync_close;
+				send_task_ = send_task::sync_close;
 				break;
 
-			case task::sync_close:
+			case send_task::sync_close:
 				if(timeout_ > 0) {
 					--timeout_;
 				} else {
+					idle_count_ = 0;
 					debug_format("Client disconnected: %s\n") % ip_.c_str();
-					task_ = task::req_connect;
+					send_task_ = send_task::req_connect;
 				}
 				break;
 			}
