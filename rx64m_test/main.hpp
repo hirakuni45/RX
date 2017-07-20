516723acc4011053aa3546c66b2026b78bafbb8e
hira <hira@rvf-rc45.net>
2017-07-20 21:12:49 +0900

    update version NO

--- rx64m_test/main.hpp
+++ rx64m_test/main.hpp
@@ -35,7 +35,7 @@
 
 namespace seeda {
 
-	static const int seeda_version_ = 237;
+	static const int seeda_version_ = 238;
 	static const uint32_t build_id_ = B_ID;
 
 	typedef utils::command<256> CMD;
