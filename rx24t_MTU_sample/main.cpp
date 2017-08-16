67f13db70ec8ee6365c88dad91cdd7fc54467b99
hira <hira@rvf-rc45.net>
2017-08-17 05:18:08 +0900

    add output chanel

--- rx24t_MTU_sample/main.cpp
+++ rx24t_MTU_sample/main.cpp
@@ -1,8 +1,9 @@
 //=====================================================================//
 /*! @file
     @brief  RX24T MTU3 サンプル @n
-			・PB2(33) の周波数測定 @n
-			・P00(4) ピンに赤色LED（VF:1.9V）を吸い込みで接続する
+			・PB3/MTIOC0A (32) 周波数測定 @n
+			・PA5/MTIOC1A (36) パルス出力 @n
+			・P00 (4) に赤色LED+1000オーム（VF:1.9V）を吸い込みで接続する（インジケーター）
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
 				Released under the MIT license @n
@@ -26,6 +27,9 @@ namespace {
 	typedef device::mtu_io<MTU0> MTU0_IO;
 	MTU0_IO mtu0_io_;
 
+	typedef device::MTU1 MTU1;
+	typedef device::mtu_io<MTU1> MTU1_IO;
+	MTU1_IO mtu1_io_;
 }
 
 extern "C" {
@@ -84,33 +88,43 @@ int main(int argc, char** argv)
 		sci_.start(115200, intr_level);
 	}
 
-	// MTU 設定
+	// MTU0 設定
 	{
 		uint8_t intr_level = 3;		
-		if(!mtu0_io_.start_capture(MTU0::channel::A, MTU0_IO::edge_type::positive, intr_level)) {
+		if(!mtu0_io_.start_capture(MTU0::channel::A, MTU0_IO::capture_type::positive, intr_level)) {
 			utils::format("MTU0 input capture start fail...\n");
 		}
 	}
 
-	utils::format("RX24T MTU sample (Input Capture)\n");
+	// MTU1 設定
+	{
+		uint32_t frq = 1000;
+		if(!mtu1_io_.start_output(MTU1::channel::A, MTU1_IO::output_type::toggle, frq)) {
+			utils::format("MTU1 output start fail...\n");
+		}
+	}
+
+	utils::format("RX24T MTU3 sample: base clock: %u [Hz]\n") % static_cast<uint32_t>(F_PCLKA);
+	utils::format("  PB3/MTIOC0A (32): Input Capture\n");
+	utils::format("  PA5/MTIOC1A (36): Pluse Output (Compare Match Timer)\n");
 
 	device::PORT0::PDR.B0 = 1; // output
 
 	uint32_t cnt = 0;
-	uint16_t cap = mtu0_io_.get_capture_tick();
-	uint32_t capcnt = 0;
 	while(1) {
 		cmt_.sync();
 
-		auto n = mtu0_io_.get_capture_tick();
-		if(cap != n) {
-			cap = n;
-			capcnt = mtu0_io_.get_capture();
-		}
+//		auto n = mtu0_io_.get_capture_tick();
+//		if(cap != n) {
+//			cap = n;
+//			capcnt = mtu0_io_.get_capture();
+//		}
 
 		++cnt;
 		if(cnt >= 30) {
-			float a = static_cast<float>(mtu0_io_.get_base_clock()) / static_cast<float>(capcnt);
+			const auto& cap = mtu0_io_.get_capture();
+
+			float a = static_cast<float>(mtu0_io_.get_base_clock()) / static_cast<float>(cap.all_count_);
 			utils::format("Capture: %7.2f [Hz]\n") % a;
 			cnt = 0;
 		}
