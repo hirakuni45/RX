8cbc11b55429e99e54cf4d777c16fe084c70ea0f
hira <hira@rvf-rc45.net>
2017-08-18 19:34:20 +0900

    update system clock

--- rx24t_PWM_sample/main.cpp
+++ rx24t_PWM_sample/main.cpp
@@ -1,7 +1,7 @@
 //=====================================================================//
 /*! @file
     @brief  RX24T ファースト・サンプル @n
-			・P00(4) ピンに赤色LED（VF:1.9V）を吸い込みで接続する @n
+			・P00(4) ピンに赤色LED（VF:1.9V）を吸い込みで接続する
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
 				Released under the MIT license @n
@@ -16,19 +16,18 @@
 
 namespace {
 
-	class null_task {
-	public:
-		void operator() () {
-		}
-	};
+	device::cmt_io<device::CMT0>  cmt_;
 
-	device::cmt_io<device::CMT0, null_task>  cmt_;
-
-	typedef utils::fifo<uint8_t, 128> buffer;
-	device::sci_io<device::SCI1, buffer, buffer> sci_;
+	typedef utils::fifo<uint8_t, 128> BUFFER;
+	device::sci_io<device::SCI1, BUFFER, BUFFER> sci_;
 
 	typedef device::MTU0 MTU0;
-	device::mtu_io<MTU0, null_task> mtu_;
+	typedef device::mtu_io<MTU0> MTU0_IO;
+	MTU0_IO mtu0_io_;
+
+	typedef device::MTU1 MTU1;
+	typedef device::mtu_io<MTU1> MTU1_IO;
+	MTU1_IO mtu1_io_;
 }
 
 extern "C" {
@@ -49,31 +48,9 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
-
-	device::SYSTEM::MEMWAIT = 0b10; // 80MHz 動作 wait 設定
-
-	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
-	device::SYSTEM::OPCCR = 0;  // 高速モード選択
-	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
-
-	// clock osc 10MHz
-	device::SYSTEM::MOSCWTCR = 9;	// 4ms wait
-	// メインクロック・ドライブ能力設定、内部発信
-	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV21.b(1);
-	device::SYSTEM::MOSCCR.MOSTP = 0;  // メインクロック発振器動作
-	while(device::SYSTEM::OSCOVFSR.MOOVF() == 0) asm("nop");
-
-	device::SYSTEM::PLLCR.STC = 0b001111;		// PLL input: 1, PLL 8 倍(80MHz)
-	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
-	while(device::SYSTEM::OSCOVFSR.PLOVF() == 0) asm("nop");
-
-	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(2)		// 1/4 (80/4=20)
-						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (80/1=80)
-						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (80/1=80)
-						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (80/2=40)
-						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
-	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
+	// 10MHz X-Tal, 80MHz
+	typedef device::system_io<10000000> SYSTEM_IO;
+	SYSTEM_IO::setup_system_clock(80000000);
 
 	// タイマー設定（６０Ｈｚ）
 	uint8_t cmt_irq_level = 4;
@@ -83,10 +60,28 @@ int main(int argc, char** argv)
 	static const uint8_t sci_level = 2;
 	sci_.start(115200, sci_level);
 
-	// MTU 設定
-//	mtu_.start_pwm1(MTU::clock_source::PCLKA_4);
+#if 0
+	// MTU0 設定
+	{
+		uint32_t frq = 10000;
+		if(!mtu0_io_.start_pwm(MTU0::channel::A, MTU0_IO::pwm_type::low_to_high, frq)) {
+			utils::format("MTU0 input capture start fail...\n");
+		}
+	}
 
-	utils::format("RX24T MTU PWM sample\n");
+	// MTU1 設定
+	{
+		uint32_t frq = 10000;
+		if(!mtu1_io_.start_pwm(MTU1::channel::A, MTU1_IO::pwm_type::high_to_low, frq)) {
+			utils::format("MTU1 output start fail...\n");
+		}
+	}
+#endif
+
+	utils::format("RX24T MTU3 PWM sample: base clock: %4.2f [MHz]\n")
+		% (static_cast<float>(F_PCLKA) * 1e-6);
+	utils::format("  PB3/MTIOC0A (32): Left\n");
+	utils::format("  PA5/MTIOC1A (36): Right\n");
 
 	device::PORT0::PDR.B0 = 1; // output
 
@@ -99,7 +94,5 @@ int main(int argc, char** argv)
 			cnt = 0;
 		}
 		device::PORT0::PODR.B0 = (cnt < 10) ? 0 : 1;
-
-
 	}
 }
