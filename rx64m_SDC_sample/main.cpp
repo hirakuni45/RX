54e67a1e908adbf4fdc58431b42062ff74b2a3eb
hira <hira@rvf-rc45.net>
2017-10-14 12:25:02 +0900

    update SDC manage

--- rx64m_SDC_sample/main.cpp
+++ rx64m_SDC_sample/main.cpp
@@ -17,50 +17,124 @@
 #include "common/command.hpp"
 #include "common/rspi_io.hpp"
 #include "common/spi_io.hpp"
-#include "common/sdc_io.hpp"
-
+#include "common/spi_io2.hpp"
+#include "common/sdc_man.hpp"
 #include "RX64M/sdhi_io.hpp"
-
 #include "common/string_utils.hpp"
 
-// #define SDHI
+#define SDHI_IF
 
 namespace {
 
-	device::cmt_io<device::CMT0>  cmt_;
+	typedef device::cmt_io<device::CMT0> CMT;
+	CMT		cmt_;
 
-	typedef utils::fifo<uint16_t, 256> buffer;
-	device::sci_io<device::SCI1, buffer, buffer> sci_;
+	typedef utils::fifo<uint16_t, 256> BUFFER;
+	typedef device::sci_io<device::SCI1, BUFFER, BUFFER> SCI;
+	SCI		sci_;
 
 	utils::rtc_io rtc_;
 
-//	typedef device::rspi_io<device::RSPI> RSPI;
-//	RSPI rspi_;
+	typedef device::PORT<device::PORT8, device::bitpos::B2> SDC_POWER;	///< カード電源制御
 
-#ifdef SDHI
-	typedef fatfs::sdhi_io<device::SDHI, true> SDC;
-
+#ifdef SDHI_IF
+	// カード電源制御は使わない場合、「device::NULL_PORT」を指定する。
+//	typedef fatfs::sdhi_io<device::SDHI, SDC_POWER> SDHI;  ///< ハードウェアー定義
+	typedef fatfs::sdhi_io<device::SDHI, device::NULL_PORT> SDHI;  ///< ハードウェアー定義
+	SDHI	sdh_;
 #else
 	// Soft SDC 用　SPI 定義（SPI）
 	typedef device::PORT<device::PORTC, device::bitpos::B3> MISO;
 	typedef device::PORT<device::PORT7, device::bitpos::B6> MOSI;
 	typedef device::PORT<device::PORT7, device::bitpos::B7> SPCK;
-	typedef device::spi_io<MISO, MOSI, SPCK, device::soft_spi_mode::CK10> SPI;
-	SPI spi_;
 
-	typedef device::PORT<device::PORTC, device::bitpos::B2> sdc_select;	///< カード選択信号
-	typedef device::PORT<device::PORT8, device::bitpos::B2> sdc_power;	///< カード電源制御
-	typedef device::PORT<device::PORT8, device::bitpos::B1> sdc_detect;	///< カード検出
+	typedef device::spi_io2<MISO, MOSI, SPCK> SPI;  ///< Soft SPI 定義
+//	typedef device::rspi_io<device::RSPI> RSPI;  ///< Hard SPI 定義
+//	RSPI rspi_;
+	SPI		spi_;
 
-	typedef utils::sdc_io<SPI, sdc_select, sdc_power, sdc_detect> SDC;
+	typedef device::PORT<device::PORTC, device::bitpos::B2> SDC_SELECT;  ///< カード選択信号
+	typedef device::PORT<device::PORT8, device::bitpos::B1> SDC_DETECT;  ///< カード検出
+
+	typedef fatfs::mmc_io<SPI, SDC_SELECT, SDC_POWER, SDC_DETECT> MMC;   ///< ハードウェアー定義
+
+	MMC		sdh_(spi_, 20000000);
 #endif
-
-	SDC sdc_(spi_, 20000000);
+	typedef utils::sdc_man SDC;
+	SDC		sdc_;
 
 	utils::command<256> cmd_;
 
 //	utils::SDRAM_128M_32W	sdram_;
+
+	uint8_t v_ = 91;
+	uint8_t m_ = 123;
+	uint8_t rand_()
+	{
+		v_ += v_ << 2;
+		++v_;
+		uint8_t n = 0;
+		if(m_ & 0x02) n = 1;
+		if(m_ & 0x40) n ^= 1;
+		m_ += m_;
+		if(n == 0) ++m_;
+		return v_ ^ m_;
+	}
+
+
+	//-----------------------------------------------------------------//
+	/*!
+		@brief  ファイル作成テスト
+		@param[in]	fname	ファイル名
+		@param[in]	size	作成サイズ
+		@return 成功なら「true」
+	*/
+	//-----------------------------------------------------------------//
+	bool create_test_file_(const char* fname, uint32_t size)
+	{
+		uint8_t buff[512];
+		FIL fp;
+
+		for(uint16_t i = 0; i < sizeof(buff); ++i) {
+			buff[i] = rand_();
+		}
+
+		auto st = cmt_.get_counter();
+		if(!sdc_.open(&fp, fname, FA_WRITE | FA_CREATE_ALWAYS)) {
+			utils::format("Can't create file: '%s'\n") % fname;
+			return false;
+		}
+
+		auto ed = cmt_.get_counter();
+		uint32_t topen = ed - st;
+		st = ed;
+
+		auto rs = size;
+		while(rs > 0) {
+			UINT sz = sizeof(buff);
+			if(sz > rs) sz = rs;
+			UINT bw;
+			f_write(&fp, buff, sz, &bw);
+			rs -= bw;
+		}
+		ed = cmt_.get_counter();
+		uint32_t twrite = ed - st;
+		st = ed;
+
+		f_close(&fp);
+		ed = cmt_.get_counter();
+		uint32_t tclose = ed - st;
+
+		utils::format("Open:  %d [ms]\n") % (topen * 10);
+		auto pbyte = size * 100 / twrite;
+		utils::format("Write: %d Bytes/Sec\n") % pbyte;
+		utils::format("Write: %d KBytes/Sec\n") % (pbyte / 1024);
+		utils::format("Close: %d [ms]\n") % (tclose * 10);
+
+		return true;
+	}
 }
+
 
 extern "C" {
 
@@ -69,40 +143,49 @@ extern "C" {
 		sci_.putch(ch);
 	}
 
+
 	void sci_puts(const char* str)
 	{
 		sci_.puts(str);
 	}
+
 
 	char sci_getch(void)
 	{
 		return sci_.getch();
 	}
 
+
 	uint16_t sci_length()
 	{
 		return sci_.recv_length();
 	}
 
+
 	DSTATUS disk_initialize(BYTE drv) {
-		return sdc_.at_mmc().disk_initialize(drv);
+		return sdh_.disk_initialize(drv);
 	}
+
 
 	DSTATUS disk_status(BYTE drv) {
-		return sdc_.at_mmc().disk_status(drv);
+		return sdh_.disk_status(drv);
 	}
+
 
 	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
-		return sdc_.at_mmc().disk_read(drv, buff, sector, count);
+		return sdh_.disk_read(drv, buff, sector, count);
 	}
+
 
 	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
-		return sdc_.at_mmc().disk_write(drv, buff, sector, count);
+		return sdh_.disk_write(drv, buff, sector, count);
 	}
 
+
 	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
-		return sdc_.at_mmc().disk_ioctl(drv, ctrl, buff);
+		return sdh_.disk_ioctl(drv, ctrl, buff);
 	}
+
 
 	DWORD get_fattime(void) {
 		time_t t = 0;
@@ -110,9 +193,11 @@ extern "C" {
 		return utils::str::get_fattime(t);
 	}
 
+
 	void utf8_to_sjis(const char* src, char* dst) {
 		utils::str::utf8_to_sjis(src, dst);
 	}
+
 
 	time_t get_time_()
 	{
@@ -230,14 +315,17 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
+	SDC_POWER::DIR = 1;
+	SDC_POWER::P = 0;
+
 	device::system_io<12000000>::setup_system_clock();
 
 	// SDRAM 初期化 128M/32bits bus
 //	sdram_();
 
-	{  // タイマー設定（６０Ｈｚ）
+	{  // タイマー設定（１００Ｈｚ）
 		uint8_t cmt_irq_level = 4;
-		cmt_.start(60, cmt_irq_level);
+		cmt_.start(100, cmt_irq_level);
 	}
 
 	{  // SCI 設定
@@ -250,11 +338,15 @@ int main(int argc, char** argv)
 		rtc_.start();
 	}
 
+#ifdef SDHI_IF
+	utils::format("RX64M SDHI SD-Card sample\n");
+#else
+	utils::format("RX64M Soft SPI SD-Card sample\n");
+#endif
 	{  // SD カード・クラスの初期化
+		sdh_.start();
 		sdc_.start();
 	}
-
-	utils::format("RX64M SDC sample\n");
 
 	device::PORT0::PDR.B7 = 1; // output
 
@@ -265,7 +357,7 @@ int main(int argc, char** argv)
 	while(1) {
 		cmt_.sync();
 
-		sdc_.service();
+		sdc_.service(sdh_.service());
 
 		// コマンド入力と、コマンド解析
 		if(cmd_.service()) {
@@ -307,12 +399,16 @@ int main(int argc, char** argv)
 						set_time_date_();
 					}
 					f = true;
+				} else if(cmd_.cmp_word(0, "test")) {
+					create_test_file_("write_test.bin", 1024 * 1024);
+					f = true;
 				} else if(cmd_.cmp_word(0, "help") || cmd_.cmp_word(0, "?")) {
 					utils::format("date\n");
 					utils::format("date yyyy/mm/dd hh:mm[:ss]\n");
 					utils::format("dir [name]\n");
 					utils::format("cd [directory-name]\n");
 					utils::format("pwd\n");
+					utils::format("test    test speed for write file\n");
 					f = true;
 				}
 				if(!f) {
@@ -325,9 +421,9 @@ int main(int argc, char** argv)
 		}
 
 		++cnt;
-		if(cnt >= 30) {
+		if(cnt >= 50) {
 			cnt = 0;
 		}
-		device::PORT0::PODR.B7 = (cnt < 10) ? 0 : 1;
+		device::PORT0::PODR.B7 = (cnt < 15) ? 0 : 1;
 	}
 }
