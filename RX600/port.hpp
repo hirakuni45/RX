cbf96ea2db5ebc3f26bb848f83396e5a6e7ca7a1
hira <hira@rvf-rc45.net>
2017-10-15 09:05:40 +0900

    update RX63T

--- RX600/port.hpp
+++ RX600/port.hpp
@@ -141,7 +141,9 @@ namespace device {
 			bit_rw_t<io_, bitpos::B0> B0;
 			bit_rw_t<io_, bitpos::B2> B2;
 			bit_rw_t<io_, bitpos::B4> B4;
+#if defined(SIG_RX64M) || defined(SIG_RX71M)
 			bit_rw_t<io_, bitpos::B6> B6;
+#endif
 		};
 		static odr1_t<base + 1> ODR1;
 	};
@@ -200,6 +202,24 @@ namespace device {
 	typedef port_t<0x0008C00F, odr_oo_t<0x0008C09E> > PORTF;
 	typedef port_t<0x0008C010, odr_oo_t<0x0008C0A0> > PORTG;
 	typedef port_t<0x0008C012, odr_oo_t<0x0008C0A4> > PORTJ;
+#elif defined(SIG_RX63T)
+	typedef port_t<0x0008C000, odr_oo_t<0x0008C080> > PORT0;
+	typedef port_t<0x0008C001, odr_oo_t<0x0008C082> > PORT1;
+	typedef port_t<0x0008C002, odr_oo_t<0x0008C084> > PORT2;
+	typedef port_t<0x0008C003, odr_oo_t<0x0008C086> > PORT3;
+	typedef port_t<0x0008C004, odr_oo_t<0x0008C088> > PORT4;
+	typedef port_t<0x0008C005, odr_oo_t<0x0008C08A> > PORT5;
+	typedef port_t<0x0008C006, odr_oo_t<0x0008C08C> > PORT6;
+	typedef port_t<0x0008C007, odr_oo_t<0x0008C08E> > PORT7;
+	typedef port_t<0x0008C008, odr_oo_t<0x0008C090> > PORT8;
+	typedef port_t<0x0008C009, odr_oo_t<0x0008C092> > PORT9;
+	typedef port_t<0x0008C00A, odr_oo_t<0x0008C094> > PORTA;
+	typedef port_t<0x0008C00B, odr_oo_t<0x0008C096> > PORTB;
+	typedef port_t<0x0008C00C, odr_oo_t<0x0008C098> > PORTC;
+	typedef port_t<0x0008C00D, odr_oo_t<0x0008C09A> > PORTD;
+	typedef port_t<0x0008C00E, odr_oo_t<0x0008C09C> > PORTE;
+	typedef port_t<0x0008C00F, odr_oo_t<0x0008C09E> > PORTF;
+	typedef port_t<0x0008C010, odr_oo_t<0x0008C0A0> > PORTG;
 #elif defined(SIG_RX24T)
 //  ODR0  o  o  o  o  x  x  x  o  o  o  o  o  x  o  o  -  -  -
 //  ODR1  x  x  o  x  x  x  x  o  x  o  o  o  x  o  o  -  -  -
@@ -250,6 +270,8 @@ namespace device {
 		PORTF::PCR = dir;	// (6) PF0,PF1,PF2,PF3,PF4,PF5
 		PORTG::PCR = dir;	// (8) PG0 to PG7
 		PORTJ::PCR = dir;	// (2) PJ3,PJ5
+#elif defined(SIG_RX63T)
+		// RX63T は PCR レジスターをサポートしていない
 #elif defined(SIG_RX24T)
 		// RX24T/LFQFP:100
 		PORT0::PCR = dir;	// (3) P00,P01,P02
