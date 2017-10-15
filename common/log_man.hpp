2f7ed5ad65e55c48703f84a211bace153afffeeb
hira <hira@rvf-rc45.net>
2017-10-15 18:41:54 +0900

    fix message position

--- common/log_man.hpp
+++ common/log_man.hpp
@@ -44,6 +44,20 @@ namespace utils {
 
         //-----------------------------------------------------------------//
         /*!
+            @brief  消去
+        */
+        //-----------------------------------------------------------------//
+		void clear() noexcept
+		{
+			area_.id_ = uniq_id_;
+			area_.pos_ = 0;
+			area_.len_ = 0;
+			MEMIO::copy(&area_, sizeof(area_t), 0x0000);
+		}
+
+
+        //-----------------------------------------------------------------//
+        /*!
             @brief  開始 @n
 					※ワークメモリーが無効状態なら初期化する。
 			@return ワークメモリーが有効なら「true」
@@ -56,23 +70,8 @@ namespace utils {
 			if(area_.id_ == uniq_id_) {
 				return true;
 			}
-			area_.id_ = uniq_id_;
-			area_.pos_ = 0;
-			area_.len_ = 0;
-			MEMIO::copy(&area_, sizeof(area_t), 0x0000);
+			clear();
 			return false;
-		}
-
-
-        //-----------------------------------------------------------------//
-        /*!
-            @brief  文字列追加
-			@param[in]	s	文字列
-        */
-        //-----------------------------------------------------------------//
-		void puts(const char* s) noexcept
-		{
-
 		}
 
 
@@ -84,12 +83,62 @@ namespace utils {
         //-----------------------------------------------------------------//
 		void putch(char ch) noexcept
 		{
-			uint32_t pos = sizeof(area_t) + area_.pos_;
-			MEMIO::put(pos, ch);
-			++pos;
-			if(pos >= (MEMIO::SIZE - sizeof(area_t))) pos = 0;
-			area_.pos_ = pos;
+			MEMIO::put(sizeof(area_t) + area_.pos_, ch);
+			++area_.pos_;
+			uint16_t limit = MEMIO::SIZE - sizeof(area_t);
+			if(area_.pos_ >= limit) area_.pos_ = 0;
+			++area_.len_;
+			if(area_.len_ > limit) area_.len_ = limit;
+			MEMIO::copy(&area_, sizeof(area_t), 0x0000);
+		}
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  文字列追加
+			@param[in]	s	文字列
+        */
+        //-----------------------------------------------------------------//
+		void puts(const char* s) noexcept
+		{
+			if(s == nullptr) return;
+
+			char ch;
+			while((ch = *s) != 0) {
+				putch(ch);
+				++s;
+			}
+		}
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  記録長の取得
+			@return 記録長
+        */
+        //-----------------------------------------------------------------//
+		uint16_t get_length() const noexcept { return area_.len_; }
+
+
+        //-----------------------------------------------------------------//
+        /*!
+            @brief  文字の取得
+			@param[in]	pos	取得位置
+			@return 文字
+        */
+        //-----------------------------------------------------------------//
+		char getch(uint16_t pos) const noexcept
+		{
+			if(area_.len_ == 0) return 0;
+
+			pos += area_.pos_ - area_.len_;
+			pos %= MEMIO::SIZE - sizeof(area_t);
+			uint8_t data;
+			if(MEMIO::get(pos + sizeof(area_t), data)) {
+				return static_cast<char>(data);
+			} else {
+				return 0;
+			}
 		}
 	};
 }
-
