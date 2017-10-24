a2f2dfe3d34ac5aed5e099393b808b7ac22cf431
hira <hira@rvf-rc45.net>
2017-10-24 22:10:26 +0900

    update ETH buffer num, interrupt level

--- r_net/driver/r_ether_local.h
+++ r_net/driver/r_ether_local.h
@@ -52,9 +52,9 @@ Macro definitions
    because the driver is single-frame/single-buffer processing.  */
 #define EMAC_BUFSIZE                (1536)  /* Must be 32-byte aligned */
 /* The number of Rx descriptors. */
-#define EMAC_NUM_RX_DESCRIPTORS     (4)
+#define EMAC_NUM_RX_DESCRIPTORS     (8)
 /* The number of Tx descriptors. */
-#define EMAC_NUM_TX_DESCRIPTORS     (4)
+#define EMAC_NUM_TX_DESCRIPTORS     (8)
 
 /**
  * The total number of EMAC buffers to allocate. The number of
@@ -129,7 +129,7 @@ Macro definitions
 #define USE_MAGIC_PACKET_DETECT    (1)
 
 /* Ethernet interrupt level definition */
-#define ETHER_INTERRUPT_LEVEL      (2)  /* Please define the interruption level within the range of 1-15. */
+#define ETHER_INTERRUPT_LEVEL      (3)  /* Please define the interruption level within the range of 1-15. */
 
 /***********************************************************************************************************************
 Typedef definitions
