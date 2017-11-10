21542c1fc8d92d7524838d7cae3294e6a2e3b830
hira <hira@rvf-rc45.net>
2017-11-10 16:19:40 +0900

    new file

---
+++ igniter_tester/main.hpp
@@ -0,0 +1,19 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  RX71M イグナイター・テスター・コントローラー
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+
+#include "common/sci_io.hpp"
+#include "common/cmt_io.hpp"
+#include "common/fifo.hpp"
+#include "common/format.hpp"
+#include "common/delay.hpp"
+#include "common/command.hpp"
+
