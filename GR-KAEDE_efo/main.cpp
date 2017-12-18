bd272f2bd85e037fdd2b78aa796d900cfe115b89
hira <hira@rvf-rc45.net>
2017-12-19 01:50:18 +0900

    update DAC test

--- GR-KAEDE_efo/main.cpp
+++ GR-KAEDE_efo/main.cpp
@@ -6,8 +6,23 @@
 */
 //=====================================================================//
 #include "common/renesas.hpp"
+#include "common/cmt_io.hpp"
+#include "common/fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/format.hpp"
 
 namespace {
+
+	typedef device::cmt_io<device::CMT0, utils::null_task> CMT0;
+	CMT0	cmt_;
+
+	typedef utils::fifo<uint8_t, 2048> BUFFER;
+	typedef device::sci_io<device::SCI7, BUFFER, BUFFER> SCI;
+
+	SCI		sci_;
+
+	typedef device::dac_out	DAC;
+	DAC		dac_;
 
 }
 
@@ -29,9 +44,9 @@ extern "C" {
 			return;
 		}
 		putch_lock_ = true;
-		core_.sci_.putch(ch);
-		putch_lock_ = false;
 #endif
+		sci_.putch(ch);
+//		putch_lock_ = false;
 	}
 
 
@@ -43,7 +58,7 @@ extern "C" {
 	//-----------------------------------------------------------------//
 	void sci_puts(const char* s)
 	{
-//		core_.sci_.puts(s);
+		sci_.puts(s);
 	}
 
 
@@ -55,8 +70,7 @@ extern "C" {
 	//-----------------------------------------------------------------//
 	char sci_getch(void)
 	{
-//		return core_.sci_.getch();
-		return 0;
+		return sci_.getch();
 	}
 
 
@@ -68,8 +82,7 @@ extern "C" {
 	//-----------------------------------------------------------------//
 	uint16_t sci_length(void)
 	{
-//		return core_.sci_.recv_length();
-		return 0;
+		return sci_.recv_length();
 	}
 }
 
@@ -77,13 +90,10 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-#if 0
-	using namespace seeda;
-
 	device::PORTC::PDR.B0 = 1; // output
 	device::PORTC::PDR.B1 = 1; // output
 	device::PORT0::PDR.B2 = 1; // output
-	device::PORT0::PDR.B3 = 1; // output
+//	device::PORT0::PDR.B3 = 1; // output
 
 	device::PORT3::PCR.B5 = 1; // P35(NMI) pull-up
 
@@ -111,51 +121,40 @@ int main(int argc, char** argv)
 	device::SYSTEM::SCKCR2.UCK = 0b0100;  // USB Clock: 1/5 (120/5=24)
 	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
 
-	main_init_();
 
-	sample_count_ = 0;
+	{  // タイマー設定、１０００Ｈｚ（１ｍｓ）
+		uint8_t int_level = 1;
+		cmt_.start(1000, int_level);
+	}
 
-	core_.init();
+	{  // SCI 設定
+		uint8_t int_level = 2;
+		sci_.start(115200, int_level);
+	}
 
-	// SD カード・クラスの初期化
-	sdc_.start();
+	{  // D/A CH0, AMP enable
+		dac_.start(DAC::output::CH0, true);
+	}
 
-	// 設定ファイルの確認
-///	config_ = sdc_.probe("seeda.cfg");
-	core_.title();
-
-	tools_.init();
-	tools_.title();
-
-	nets_.init();
-	nets_.title();
-
-	enable_eadc_server();
+	utils::format("\nStart KAEDE for EFO\n");
 
 	uint32_t cnt = 0;
+	uint16_t da = 0;
 	while(1) {
-		core_.sync();
-
-		sample_data_.time_ = get_time();
-
-		core_.service();
-		tools_.service();
-
-		sdc_.service();
-
-		service_putch_tmp_();
-
-		nets_.service();
+		cmt_.sync();
 
 		++cnt;
 		if(cnt >= 32) {
 			cnt = 0;
 		}
 
-		device::PORTC::PODR.B0 = (((cnt + 0)  & 31) < 8) ? 1 : 0;
-		device::PORTC::PODR.B1 = (((cnt + 8)  & 31) < 8) ? 1 : 0;
-		device::PORT0::PODR.B2 = (((cnt + 16) & 31) < 8) ? 1 : 0;
-		device::PORT0::PODR.B3 = (((cnt + 24) & 31) < 8) ? 1 : 0;
+		da += 1024;
+		dac_.out0(da);
+
+		auto n = cnt / 10;
+		device::PORTC::PODR.B0 = (((n + 0)  & 31) < 8) ? 1 : 0;
+		device::PORTC::PODR.B1 = (((n + 8)  & 31) < 8) ? 1 : 0;
+		device::PORT0::PODR.B2 = (((n + 16) & 31) < 8) ? 1 : 0;
+//		device::PORT0::PODR.B3 = (((cnt + 24) & 31) < 8) ? 1 : 0;
 	}
-#endif
 }
