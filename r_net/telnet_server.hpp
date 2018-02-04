5e21d17e659ca46f8b2345a3e7334790d39905d4
hira <hira@rvf-rc45.net>
2018-02-05 02:18:13 +0900

    update: telnet data flow manage

--- r_net/telnet_server.hpp
+++ r_net/telnet_server.hpp
@@ -1,9 +1,33 @@
 #pragma once
 //=====================================================================//
 /*!	@file
-	@brief	TELNET サーバー・クラス
+	@brief	TELNET サーバー・クラス @n
+		0xF0	[Sub-negotiaon End] ２次交渉終了  ２次交渉パラメータの終了 @n
+		0xF1	[No Operation] オペレーションなし オペレーションなし。受信した側はこれを無視する @n
+		0xF2	[Data Mark] データマーク          データ削除・リセット @n
+		0xF3	[Break] ブレーク	              ブレーク @n
+		0xF4	[Interrupt Process] プロセス中断  操作の一時中断・割り込み・停止 @n
+		0xF5	[Abort Output] 出力中止	          出力を抑止する @n
+		0xF6	[Are You There] 相手確認          相手が動作しているかどうか確認する @n
+		0xF7	[Erase Character] 文字消去        最後の文字を消去する @n
+		0xF8	[Erase Line] 行消去               最後の行をすべて消去する @n
+		0xF9	[Go Ahead] 送信勧誘               送信するように受信側にうながす @n
+		0xFA	[Sub-negotiation Begin] ２次交渉開始  ２次交渉の開始 @n
+		0xFB	WILL	オプション希望 @n
+		0xFC	WON'T	オプション拒絶 @n
+		0xFD	DO	オプション実行要求 @n
+		0xFE	DON'T	オプション使用中止 @n
+		0xFF	[Interpret as Command] コマンドとして解釈	telnetエスケープシーケンス @n
+		オプション・コード @n
+		0x00	[Transmit Binary] バイナリ転送	IAC以外の文字列はバイナリデータと解釈される @n
+		0x01	[Echo] エコー	データのエコー @n
+		0x03	[Suppress Go Ahead] Go Ahead抑止    Go Aheadコマンドを使わないようにする @n
+		0x05	[Telnet Status Option] telnet状態オプション	相手側のtelnetオプションの状態を見られるようにする @n
+		0x06	[Telnet Timing Mark] telnetタイミングマーク	受信したデータが処理済ならばタイミングマークというデータを送る @n
+		0x18	[Terminal Type] ターミナルタイプ    ターミナルタイプを交換可能にし、適切なターミナルタイプを設定する @n
+		0x22	[Telnet Line Mode] telnetラインモード    行単位で転送できるようにする
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -52,7 +76,6 @@ namespace net {
 		char		pass_[32];
 		uint32_t	count_;
 		uint32_t	disconnect_loop_;
-		uint32_t	recv_lost_;
 
 		typedef utils::fixed_fifo<char, SEND_SIZE> SEND_FIFO;
 		typedef utils::fixed_fifo<char, RECV_SIZE> RECV_FIFO;
@@ -61,6 +84,15 @@ namespace net {
 		RECV_FIFO	recv_;
 
 		bool		crlf_;
+
+		enum class esc_task : uint8_t {
+			none,
+			entry,
+			option,
+			second,
+			second_end,
+		};
+		esc_task	esc_task_;
 
 		void write_()
 		{
@@ -80,6 +112,48 @@ namespace net {
 			}
 		}
 
+
+		bool service_options_(char ch)
+		{
+			bool ret = true;
+			switch(esc_task_) {
+			case esc_task::entry:
+				if(ch == 0xFB || ch == 0xFC || ch == 0xFD || ch == 0xFE) {
+					esc_task_ = esc_task::option;
+					ret = false;
+				} else if(ch == 0xFA) {  // 二次交渉開始
+					esc_task_ = esc_task::second;
+					ret = false;
+				}
+				break;
+			case esc_task::option:
+				esc_task_ = esc_task::none;
+				ret = false;
+				break;
+			case esc_task::second:
+				if(ch == 0xFF) {
+					esc_task_ = esc_task::second_end;
+				}
+				ret = false;
+				break;
+			case esc_task::second_end:
+				if(ch == 0xF0) {
+					esc_task_ = esc_task::none;
+				}
+				ret = false;
+				break;
+
+			case esc_task::none:
+			default:
+				if(ch == 0xFF) {  // TELNET ESC シーケンス開始
+					esc_task_ = esc_task::entry;
+					ret = false;
+				}
+				break;
+			}
+			return ret;
+		}
+
 	public:
 		//-----------------------------------------------------------------//
 		/*!
@@ -90,7 +164,9 @@ namespace net {
 		//-----------------------------------------------------------------//
 		telnet_server(ethernet& e, bool crlf = true) : eth_(e), telnet_(e), task_(task::none),
 			server_name_{ 0 }, user_{ 0 }, pass_{ 0 },
-			count_(0), disconnect_loop_(0), recv_lost_(0), crlf_(crlf) { }
+			count_(0), disconnect_loop_(0), crlf_(crlf),
+			esc_task_(esc_task::none)
+		{ }
 
 
 		//-----------------------------------------------------------------//
@@ -109,7 +185,7 @@ namespace net {
 
 			count_ = 0;
 			disconnect_loop_ = 0;
-			recv_lost_ = 0;
+			esc_task_ = esc_task::none;
 
 			task_ = task::begin;
 
@@ -160,14 +236,19 @@ namespace net {
 					if(telnet_.available() > 0) {  // リードデータがあるか？
 						char tmp[256];
 						int len = telnet_.read(tmp, sizeof(tmp));
-						if(len > 0) {  
+						if(len > 0) {
 							int l = 0;
+// utils::format("Len: %d\n") % len;
 							while((recv_.size() - recv_.length()) > 2) {
-								recv_.put(tmp[l]);
+								char ch = tmp[l];
 								++l;
+								if(service_options_(ch)) {
+									putch(ch);  // local echo
+///	utils::format("%d: %02X\n") % l % static_cast<uint16_t>(ch);
+									recv_.put(ch);
+								}
 								if(l >= len) break;
 							}
-							recv_lost_ += len - l;
 						}
 					}
 					write_();
@@ -258,6 +339,7 @@ namespace net {
 			if(recv_.length() > 0) {
 				return recv_.get();
 			} else {
+utils::format("!!!!\n");
 				return 0;
 			}
 		}
