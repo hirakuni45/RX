88a7b1096c6288637873020705175f39a339d0d2
hira <hira@rvf-rc45.net>
2018-03-27 11:34:40 +0900

    update: delay loop for utils::delay

--- rx64m_first_sample/main.cpp
+++ rx64m_first_sample/main.cpp
@@ -3,7 +3,7 @@
     @brief  RX64M ファースト・サンプル @n
 			・P07(176) ピンに赤色LED（VF:1.9V）を吸い込みで接続する @n
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -12,13 +12,9 @@
 
 namespace {
 
-	void wait_delay_(uint32_t n)
-	{
-		// とりあえず無駄ループ
-		for(uint32_t i = 0; i < n; ++i) {
-			asm("nop");
-		}
-	}
+	typedef device::system_io<12000000> SYSTEM_IO;
+
+	typedef device::PORT<device::PORT0, device::bitpos::B7> LED;
 
 }
 
@@ -26,35 +22,13 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
+	SYSTEM_IO::setup_system_clock();
 
-	device::SYSTEM::MOSCWTCR = 9;	// 1ms wait
-	// メインクロック強制発振とドライブ能力設定
-	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV2.b(0b10)
-						  | device::SYSTEM::MOFCR.MOFXIN.b(1);			
-	device::SYSTEM::MOSCCR.MOSTP = 0;		// メインクロック発振器動作
-	wait_delay_(5000);
-
-	device::SYSTEM::PLLCR.STC = 0b010011;		// PLL 10 倍(120MHz)
-	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
-	wait_delay_(5000);
-
-	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (120/1=120)
-						  | device::SYSTEM::SCKCR.BCK.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (120/1=120)
-						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKC.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
-	device::SYSTEM::SCKCR2.UCK = 0b0100;  // USB Clock: 1/5 (120/5=24)
-	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
-
-	uint32_t wait = 10000000;
-	device::PORT0::PDR.B7 = 1; // output
+	LED::DIR = 1;
 	while(1) {
-		wait_delay_(wait);
-		device::PORT0::PODR.B7 = 0;
-		wait_delay_(wait);
-		device::PORT0::PODR.B7 = 1;
+		utils::delay::milli_second(250);
+		LED::P = 0;
+		utils::delay::milli_second(250);
+		LED::P = 1;
 	}
 }
