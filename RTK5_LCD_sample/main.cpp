6a4e861a149079b1ed6d3380c9b13f0530403c0f
hira <hira@rvf-rc45.net>
2018-05-04 21:20:07 +0900

    update: SD manage

--- RTK5_LCD_sample/main.cpp
+++ RTK5_LCD_sample/main.cpp
@@ -1,6 +1,6 @@
 //=====================================================================//
 /*! @file
-    @brief  RX56N ファースト・サンプル
+    @brief  RX65N ＬＣＤサンプル
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
@@ -9,21 +9,132 @@
 //=====================================================================//
 #include "common/renesas.hpp"
 #include "common/cmt_io.hpp"
-
-// #include "common/format.hpp"
+#include "common/fixed_fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/format.hpp"
+#include "common/command.hpp"
+#include "common/spi_io2.hpp"
+#include "common/sdc_man.hpp"
 
 namespace {
+
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+	typedef device::PORT<device::PORT0, device::bitpos::B5> SW2;
 
 	typedef device::system_io<12000000> SYSTEM_IO;
 
 	device::cmt_io<device::CMT0, utils::null_task>  cmt_;
 
-	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+	typedef utils::fixed_fifo<char, 512>  RECV_BUFF;
+	typedef utils::fixed_fifo<char, 1024> SEND_BUFF;
+	typedef device::sci_io<device::SCI9, RECV_BUFF, SEND_BUFF> SCI;
+	SCI		sci_;
 
 	typedef device::PORT<device::PORT6, device::bitpos::B3> LCD_DISP;
 	typedef device::PORT<device::PORT6, device::bitpos::B6> LCD_LIGHT;
 	typedef device::glcdc_io<device::GLCDC> GLCDC_IO;
 	GLCDC_IO	glcdc_io_;
+
+	// カード電源制御は使わない場合、「device::NULL_PORT」を指定する。
+//	typedef device::PORT<device::PORT6, device::bitpos::B4> SDC_POWER;
+	typedef device::NULL_PORT SDC_POWER;
+
+#ifdef SDHI_IF
+	typedef fatfs::sdhi_io<device::SDHI, SDC_POWER> SDHI;
+	SDHI	sdh_;
+#else
+	// Soft SDC 用　SPI 定義（SPI）
+	typedef device::PORT<device::PORT2, device::bitpos::B2> MISO;  // DAT0
+	typedef device::PORT<device::PORT2, device::bitpos::B0> MOSI;  // CMD
+	typedef device::PORT<device::PORT2, device::bitpos::B1> SPCK;  // CLK
+
+	typedef device::spi_io2<MISO, MOSI, SPCK> SPI;  ///< Soft SPI 定義
+
+	SPI		spi_;
+
+	typedef device::PORT<device::PORT1, device::bitpos::B7> SDC_SELECT;  // DAT3 カード選択信号
+	typedef device::PORT<device::PORT2, device::bitpos::B5> SDC_DETECT;  // CD   カード検出
+
+	typedef fatfs::mmc_io<SPI, SDC_SELECT, SDC_POWER, SDC_DETECT> MMC;   // ハードウェアー定義
+
+	MMC		sdh_(spi_, 20000000);
+#endif
+	typedef utils::sdc_man SDC;
+	SDC		sdc_;
+
+	utils::command<256> cmd_;
+}
+
+extern "C" {
+
+	void sci_putch(char ch)
+	{
+		sci_.putch(ch);
+	}
+
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+
+
+	DSTATUS disk_initialize(BYTE drv) {
+		return sdh_.disk_initialize(drv);
+	}
+
+
+	DSTATUS disk_status(BYTE drv) {
+		return sdh_.disk_status(drv);
+	}
+
+
+	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_read(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_write(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
+		return sdh_.disk_ioctl(drv, ctrl, buff);
+	}
+
+
+	DWORD get_fattime(void) {
+		time_t t = 0;
+///		rtc_.get_time(t);
+		return utils::str::get_fattime(t);
+	}
+
+
+	void utf8_to_sjis(const char* src, char* dst, uint32_t len) {
+		utils::str::utf8_to_sjis(src, dst, len);
+	}
+
+
+	bool check_mount_() {
+		auto f = sdc_.get_mount();
+		if(!f) {
+			utils::format("SD card not mount.\n");
+		}
+		return f;
+	}
 }
 
 int main(int argc, char** argv);
@@ -36,6 +147,20 @@ int main(int argc, char** argv)
 		uint8_t cmt_irq_level = 4;
 		cmt_.start(60, cmt_irq_level);
 	}
+
+	{  // SCI 設定
+		static const uint8_t sci_level = 2;
+		sci_.start(115200, sci_level);
+	}
+
+	{  // SD カード・クラスの初期化
+		sdh_.start();
+		sdc_.start();
+	}
+
+	utils::format("RTK5RX65N Start for LCD sample\n");
+
+	cmd_.set_prompt("# ");
 
 	if(0) {  // GLCDC 初期化
 		LCD_DISP::DIR  = 1;
@@ -54,9 +179,69 @@ int main(int argc, char** argv)
 
 
 	LED::DIR = 1;
+	SW2::DIR = 0;
+
 	uint8_t n = 0;
+	bool sw2 = SW2::P();
 	while(1) {
 		cmt_.sync();
+
+		{  // SW2 の検出
+			auto f = SW2::P();
+			if(sw2 && !f) {
+				utils::format("SW2: Positive\n");
+			}
+			if(!sw2 && f) {
+				utils::format("SW2: Negative\n");
+			}
+			sw2 = f;
+		}
+
+		sdc_.service(sdh_.service());
+
+		if(cmd_.service()) {
+			uint8_t cmdn = cmd_.get_words();
+			if(cmdn >= 1) {
+				bool f = false;
+				if(cmd_.cmp_word(0, "dir")) {  // dir [xxx]
+					if(check_mount_()) {
+						if(cmdn >= 2) {
+							char tmp[128];
+							cmd_.get_word(1, sizeof(tmp), tmp);
+							sdc_.dir(tmp);
+						} else {
+							sdc_.dir("");
+						}
+					}
+					f = true;
+				} else if(cmd_.cmp_word(0, "cd")) {  // cd [xxx]
+					if(check_mount_()) {
+						if(cmdn >= 2) {
+							char tmp[128];
+							cmd_.get_word(1, sizeof(tmp), tmp);
+							sdc_.cd(tmp);						
+						} else {
+							sdc_.cd("/");
+						}
+					}
+					f = true;
+				} else if(cmd_.cmp_word(0, "pwd")) { // pwd
+					utils::format("%s\n") % sdc_.get_current();
+					f = true;
+				} else if(cmd_.cmp_word(0, "help")) {
+					utils::format("    dir [path]\n");
+					utils::format("    cd [path]\n");
+					utils::format("    pwd\n");
+					f = true;
+				}
+				if(!f) {
+					char tmp[128];
+					if(cmd_.get_word(0, sizeof(tmp), tmp)) {
+						utils::format("Command error: '%s'\n") % tmp;
+					}
+				}
+			}
+		}
 
 		++n;
 		if(n >= 30) {
