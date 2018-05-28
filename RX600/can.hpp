b1ec41d329be587c5de413b6d395aabbe748c764
hira <hira@rvf-rc45.net>
2018-05-28 09:47:04 +0900

    update: CAN register

--- RX600/can.hpp
+++ RX600/can.hpp
@@ -48,8 +48,278 @@ namespace device {
 			bits_rw_t<io_, bitpos::B11, 2>  BOM;
 			bit_rw_t <io_, bitpos::B13>     RBOC;
 		};
-		static ctlr_t<base + 0x00> CTLR;
+		static ctlr_t<base + 0x0640> CTLR;
 
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ビットコンフィグレーションレジスタ（ BCR ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct bcr_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B0>      CCLKS;
+			bits_rw_t<io_, bitpos::B8,  3>  TSEG2;
+			bits_rw_t<io_, bitpos::B12, 2>  SJW;
+			bits_rw_t<io_, bitpos::B16, 10> BRP;
+			bits_rw_t<io_, bitpos::B28, 4>  TSEG1;
+		};
+		static bcr_t<base + 0x0644> BCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  マスクレジスタ k （ MKRk ）（ k = 0 ～ 7 ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct mkrk_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0,  18>  EID;
+			bits_rw_t<io_, bitpos::B18, 11>  SID;
+		};
+		static mkrk_t<base + 0x0200> MKR0;
+		static mkrk_t<base + 0x0204> MKR1;
+		static mkrk_t<base + 0x0208> MKR2;
+		static mkrk_t<base + 0x020C> MKR3;
+		static mkrk_t<base + 0x0210> MKR4;
+		static mkrk_t<base + 0x0214> MKR5;
+		static mkrk_t<base + 0x0218> MKR6;
+		static mkrk_t<base + 0x021C> MKR7;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  FIFO 受信 ID 比較レジスタ 0 、 1 （ FIDCR0 、 FIDCR1 ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct fidcrx_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0,  18>  EID;
+			bits_rw_t<io_, bitpos::B18, 11>  SID;
+			bit_rw_t <io_, bitpos::B30>      RTR;
+			bit_rw_t <io_, bitpos::B31>      IDE;
+		};
+		static fidcrx_t<base + 0x0220> FIDCR0;
+		static fidcrx_t<base + 0x0224> FIDCR1;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  マスク無効レジスタ（ MKIVLR ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct mbn_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B0>   MB0;
+			bit_rw_t <io_, bitpos::B1>   MB1;
+			bit_rw_t <io_, bitpos::B2>   MB2;
+			bit_rw_t <io_, bitpos::B3>   MB3;
+			bit_rw_t <io_, bitpos::B4>   MB4;
+			bit_rw_t <io_, bitpos::B5>   MB5;
+			bit_rw_t <io_, bitpos::B6>   MB6;
+			bit_rw_t <io_, bitpos::B7>   MB7;
+			bit_rw_t <io_, bitpos::B8>   MB8;
+			bit_rw_t <io_, bitpos::B9>   MB9;
+			bit_rw_t <io_, bitpos::B10>  MB10;
+			bit_rw_t <io_, bitpos::B11>  MB11;
+			bit_rw_t <io_, bitpos::B12>  MB12;
+			bit_rw_t <io_, bitpos::B13>  MB13;
+			bit_rw_t <io_, bitpos::B14>  MB14;
+			bit_rw_t <io_, bitpos::B15>  MB15;
+			bit_rw_t <io_, bitpos::B16>  MB16;
+			bit_rw_t <io_, bitpos::B17>  MB17;
+			bit_rw_t <io_, bitpos::B18>  MB18;
+			bit_rw_t <io_, bitpos::B19>  MB19;
+			bit_rw_t <io_, bitpos::B20>  MB20;
+			bit_rw_t <io_, bitpos::B21>  MB21;
+			bit_rw_t <io_, bitpos::B22>  MB22;
+			bit_rw_t <io_, bitpos::B23>  MB23;
+			bit_rw_t <io_, bitpos::B24>  MB24;
+			bit_rw_t <io_, bitpos::B25>  MB25;
+			bit_rw_t <io_, bitpos::B26>  MB26;
+			bit_rw_t <io_, bitpos::B27>  MB27;
+			bit_rw_t <io_, bitpos::B28>  MB28;
+			bit_rw_t <io_, bitpos::B29>  MB29;
+			bit_rw_t <io_, bitpos::B30>  MB30;
+			bit_rw_t <io_, bitpos::B31>  MB31;
+		};
+		static mbn_t<base + 0x0228> MKIVLR;
+
+/// 構成を検討中・・・
+#if 0
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  メールボックスレジスタ j （ MBj ）（ j = 0 ～ 31 ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct mbx_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0,  18>  EID;
+			bits_rw_t<io_, bitpos::B18, 11>  SID;
+			bit_rw_t <io_, bitpos::B30>      RTR;
+			bit_rw_t <io_, bitpos::B31>      IDE;
+		};
+///		static mbx_t<base + 0x0000> MB0;
+#endif
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  メールボックス割り込み許可レジスタ（ MIER ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		static mbn_t<base + 0x022C> MIER;
+
+#if 0
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  メッセージ制御レジスタ j （ MCTLj ）（ j ＝ 0 ～ 31 ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct mctln_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0,  18>  EID;
+			bits_rw_t<io_, bitpos::B18, 11>  SID;
+			bit_rw_t <io_, bitpos::B30>      RTR;
+			bit_rw_t <io_, bitpos::B31>      IDE;
+		};
+		static mctln_t<base + 0x0620> MCTL0;
+#endif
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  受信 FIFO 制御レジスタ（ RFCR ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct rfcr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B0>       RFE;
+			bits_ro_t<io_, bitpos::B0,  3>   RFUST;
+			bit_rw_t <io_, bitpos::B4>       RFMLF;
+			bit_ro_t <io_, bitpos::B5>       RFFST;
+			bit_ro_t <io_, bitpos::B6>       RFWST;
+			bit_ro_t <io_, bitpos::B7>       RFEST;
+		};
+		static rfcr_t<base + 0x0648> RFCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  受信 FIFO ポインタ制御レジスタ（ RFPCR ）
+		*/
+		//-----------------------------------------------------------------//
+		static wo8_t<base + 0x0649>  RFPCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  送信 FIFO 制御レジスタ（ TFCR ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct tfcr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t <io_, bitpos::B0>       TFE;
+			bits_rw_t<io_, bitpos::B0,  3>   TFUST;
+			bit_ro_t <io_, bitpos::B6>       TFFST;
+			bit_ro_t <io_, bitpos::B7>       TFEST;
+		};
+		static tfcr_t<base + 0x064A> TFCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  送信 FIFO ポインタ制御レジスタ（ TFPCR ）
+		*/
+		//-----------------------------------------------------------------//
+		static wo8_t<base + 0x064B>  TFPCR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ステータスレジスタ（ STR ）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct str_t : public ro16_t<ofs> {
+			typedef ro16_t<ofs> in_;
+			using in_::operator ();
+
+			bit_ro_t <in_, bitpos::B0>   NDST;
+			bit_ro_t <in_, bitpos::B1>   SDST;
+			bit_ro_t <in_, bitpos::B2>   RFST;
+			bit_ro_t <in_, bitpos::B3>   TFST;
+			bit_ro_t <in_, bitpos::B4>   NMLST;
+			bit_ro_t <in_, bitpos::B5>   FMLST;
+			bit_ro_t <in_, bitpos::B6>   TABST;
+			bit_ro_t <in_, bitpos::B7>   EST;
+			bit_ro_t <in_, bitpos::B8>   RSTST;
+			bit_ro_t <in_, bitpos::B9>   HLTST;
+			bit_ro_t <in_, bitpos::B10>  SLPST;
+			bit_ro_t <in_, bitpos::B11>  EPST;
+			bit_ro_t <in_, bitpos::B12>  BOST;
+			bit_ro_t <in_, bitpos::B13>  TRMST;
+			bit_ro_t <in_, bitpos::B14>  RECST;
+		};
+		static str_t<base + 0x0642> STR;
 
 
 
@@ -68,10 +338,10 @@ namespace device {
 
 	};
 
-	typedef can_t<0x00090840, peripheral::CAN0> CAN0;
-	typedef can_t<0x00091840, peripheral::CAN1> CAN1;
+	typedef can_t<0x00090200, peripheral::CAN0> CAN0;
+	typedef can_t<0x00091200, peripheral::CAN1> CAN1;
 #if defined(SIG_RX64M) || defined(SIG_RX71M)
-	typedef can_t<0x00092840, peripheral::CAN2> CAN2;
+	typedef can_t<0x00092200, peripheral::CAN2> CAN2;
 #endif
 // note: RX65x CAN0, CAN1
 }
