4bd22705bdd49f0bca7c1eb25784e706f523789d
hira <hira@rvf-rc45.net>
2018-05-30 16:59:01 +0900

    update: LCD manage for RX65N

--- RX65x/glcdc_io.hpp
+++ RX65x/glcdc_io.hpp
@@ -11,6 +11,7 @@
 #include "RX65x/glcdc.hpp"
 #include "common/delay.hpp"
 #include "glcdc_def.hpp"
+#include "common/dmac_man.hpp"
 
 #define BITS_PER_PIXEL 16  // Allowed values: 1, 4, 8, 16, 32
 
