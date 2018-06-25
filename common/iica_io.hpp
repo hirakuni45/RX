7ca606f0f579bc60695681e0638b160227f87975
hira <hira@rvf-rc45.net>
2018-06-26 06:46:47 +0900

    update: cleanup

--- common/iica_io.hpp
+++ common/iica_io.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX グループ・IICA 制御
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2016, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
