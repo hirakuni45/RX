d8e17a9d5dde9025b778bcc519744a1a66f36262
hira <hira@rvf-rc45.net>
2018-06-26 09:13:24 +0900

    update: a new file

---
+++ graphics/filer.hpp
@@ -0,0 +1,27 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	ファイラー
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/sdc_man.hpp"
+
+namespace graphics {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief	ファイラー
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class filer {
+
+
+	public:
+
+
+	};
+}
