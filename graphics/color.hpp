e38cc14f695a911e34457f1c828e7e3f32735ac3
hira <hira@rvf-rc45.net>
2018-06-28 12:21:01 +0900

    add: a new file

---
+++ graphics/color.hpp
@@ -0,0 +1,69 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	グラフィックス・カラー定義
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include <cstdint>
+
+namespace graphics {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief	基本カラー定数
+		@param[in]	T		ピクセル型（１６、３２）
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <typename T>
+	class base_color {
+
+		static constexpr uint32_t rgb888_(uint8_t r, uint8_t g, uint8_t b)
+		{
+			return
+				(static_cast<uint32_t>(r) << 16)
+			  | (static_cast<uint32_t>(g) << 8)
+			  | static_cast<uint32_t>(b);
+		}
+
+
+		static constexpr uint16_t rgb565_(uint8_t r, uint8_t g, uint8_t b)
+		{
+			return
+				(static_cast<uint16_t>(r & 0xf8) << 8)
+			  | (static_cast<uint16_t>(g & 0xfc) << 3)
+			  | (static_cast<uint16_t>(b & 0xf8) >> 3);
+		}
+
+
+		static constexpr T rgb_(uint8_t r, uint8_t g, uint8_t b)
+		{
+			if(sizeof(T) == 2) return rgb565_(r, g, b);
+			else if(sizeof(T) == 4) return rgb888_(r, g, b);
+			else return 0;
+		}
+
+
+	public:
+		// https://jonasjacek.github.io/colors/
+		static constexpr T Black   = rgb_(  0,   0,   0);
+		static constexpr T Maroon  = rgb_(128,   0,   0);
+		static constexpr T Green   = rgb_(  0, 128,   0);
+		static constexpr T Olive   = rgb_(128, 128,   0);
+		static constexpr T Navy    = rgb_(  0,   0, 128);
+		static constexpr T Purple  = rgb_(128,   0, 128);
+		static constexpr T Teal    = rgb_(  0, 128, 128);
+		static constexpr T Silver  = rgb_(192, 192, 192);
+		static constexpr T Grey    = rgb_(128, 128, 128);
+		static constexpr T Red     = rgb_(255,   0,   0);
+		static constexpr T Lime    = rgb_(  0, 255,   0);
+		static constexpr T Yellow  = rgb_(255, 255,   0);
+		static constexpr T Blue    = rgb_(  0,   0, 255);
+		static constexpr T Fuchsi  = rgb_(255,   0, 255);
+		static constexpr T Aqua    = rgb_(  0, 255, 255);
+		static constexpr T White   = rgb_(255, 255, 255);
+	};
+}
