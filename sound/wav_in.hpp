cb28b6af6fd547d58d4e968e384004eea58ecfb0
hira <hira@rvf-rc45.net>
2018-07-06 20:17:40 +0900

    update: cleanup

--- sound/wav_in.hpp
+++ sound/wav_in.hpp
@@ -11,11 +11,8 @@
 #include <cstring>
 #include "common/file_io.hpp"
 #include "common/format.hpp"
-
-extern "C" {
-	void bmp_putch(char ch);
-	void bmp_locate(int8_t idx);
-};
+#include "sound/audio_out.hpp"
+#include "sound/tag.hpp"
 
 namespace audio {
 
@@ -52,27 +49,31 @@ namespace audio {
 
 		uint32_t	data_top_;
 		uint32_t	data_size_;
+		uint32_t	data_pos_;
 
 		uint32_t	rate_;
 		uint8_t		channel_;
 		uint8_t		bits_;
 
-		bool list_tag_(FIL* fp, uint16_t size, int8_t idx) {
-			if(idx >= 0) bmp_locate(idx);
+		bool		pause_;
+
+
+		bool list_tag_(utils::file_io& fi, uint16_t size, char* dst, uint32_t dstlen) noexcept
+		{
 			while(size > 0) {
 				char ch = 0;
-				UINT br;
-				if(f_read(fp, &ch, 1, &br) != FR_OK) {
+				if(fi.read(&ch, 1) != 1) {
 					return false;
 				}
 				--size;
 				if(ch != 0) {
-					utils::format("%c") % ch;
-					if(idx >= 0) {
-						bmp_putch(ch);
+					if(dst != nullptr && dstlen > 1) {
+						*dst++ = ch;
+						dstlen--;
 					}
 				}
 			}
+			*dst = 0;
 			return true;
 		}
 
@@ -82,46 +83,43 @@ namespace audio {
 			@brief	コンストラクター
 		*/
 		//-------------------------------------------------------------//
-		wav_in() : data_top_(0), data_size_(0), rate_(0), channel_(0), bits_(0) { }
+		wav_in() noexcept : data_top_(0), data_size_(0), data_pos_(0),
+			rate_(0), channel_(0), bits_(0), pause_(false) { }
 
 
 		//-------------------------------------------------------------//
 		/*!
 			@brief	ヘッダーをロードして、フォーマット、サイズを取得する
 			@param[in]	path	ファイル・パス
-			@param[in]	lcd		LCD 向け表示の場合「true」
+			@param[in]	tag		タグの参照
 		*/
 		//-------------------------------------------------------------//
-		bool load_header(FIL* fil, bool lcd = false)
+		bool load_header(utils::file_io& fi, tag_t& tag) noexcept
 		{
 			uint32_t ofs = 0;
 			{
 				WAVEFILEHEADER wh;
-				UINT br;
-				if(f_read(fil, &wh, sizeof(wh), &br) != FR_OK) {
+				if(fi.read(&wh, sizeof(wh)) != sizeof(wh)) {
 					return false;
 				}
-				if(br != sizeof(wh)) return false;
-				if(std::strncmp(wh.szRIFF, "RIFF", 4) == 0 && std::strncmp(wh.szWAVE, "WAVE", 4) == 0) ;
+				if(std::strncmp(wh.szRIFF, "RIFF", 4) == 0 &&
+				   std::strncmp(wh.szWAVE, "WAVE", 4) == 0) ;
 				else return false;
 				ofs += sizeof(wh);
 			}
 
 			while(1) {
 				RIFFCHUNK rc;
-				UINT br;
-				if(f_read(fil, &rc, sizeof(rc), &br) != FR_OK) {
+				if(fi.read(&rc, sizeof(rc)) != sizeof(rc)) {
 					return false;
 				}
-				if(br != sizeof(rc)) return false;
 				ofs += sizeof(rc);
 
 				if(std::strncmp(rc.szChunkName, "fmt ", 4) == 0) {
 					WAVEFMT wf;
-					if(f_read(fil, &wf, sizeof(wf), &br) != FR_OK) {
+					if(fi.read(&wf, sizeof(wf)) != sizeof(wf)) {
 						return false;
 					}
-					if(br != sizeof(wf)) return false;
 					rate_ = wf.ulSamplesPerSec;
 					channel_ = wf.usChannels;
 					bits_ = wf.usBitsPerSample;
@@ -131,41 +129,140 @@ namespace audio {
 				} else if(std::strncmp(rc.szChunkName, "LIST", 4) == 0) {
 					uint16_t sz = rc.ulChunkSize;
 					char key[4];
-					if(f_read(fil, &key, sizeof(key), &br) != FR_OK) {
+					if(fi.read(&key, sizeof(key)) != sizeof(key)) {
 						return false;
 					}
 					if(std::strncmp(key, "INFO", 4) == 0) {
 						sz -= 4;
 						while(sz > 0) {
-							RIFFCHUNK tag;
-							UINT br;
-							if(f_read(fil, &tag, sizeof(tag), &br) != FR_OK) {
+							RIFFCHUNK rch;
+							if(fi.read(&rch, sizeof(rch)) != sizeof(rch)) {
 								return false;
 							}
-							sz -= sizeof(tag);
-							utils::format("%c%c%c%c: ") % tag.szChunkName[0] % tag.szChunkName[1]
-								% tag.szChunkName[2] % tag.szChunkName[3];
-							int8_t idx = -1;
-							if(lcd) {
-								if(std::strncmp(tag.szChunkName, "IART", 4) == 0) idx = 0;
-								else if(std::strncmp(tag.szChunkName, "INAM", 4) == 0) idx = 1;
-								else if(std::strncmp(tag.szChunkName, "IPRD", 4) == 0) idx = 2;
+							sz -= sizeof(rch);
+							char* dst = nullptr;
+							uint32_t dstlen = 0;
+							if(std::strncmp(rch.szChunkName, "IART", 4) == 0) {
+								dst = tag.album_;
+								dstlen = sizeof(tag.album_);
+							} else if(std::strncmp(rch.szChunkName, "INAM", 4) == 0) {
+								dst = tag.title_;
+								dstlen = sizeof(tag.title_);
+							} else if(std::strncmp(rch.szChunkName, "IPRD", 4) == 0) {
+								dst = tag.artist_;
+								dstlen = sizeof(tag.artist_);
 							}
-							uint16_t n = tag.ulChunkSize;
+							uint16_t n = rch.ulChunkSize;
 							if(n & 1) ++n;
-							if(!list_tag_(fil, n, idx)) {
+							if(!list_tag_(fi, n, dst, dstlen)) {
 								return false;
 							}
-							utils::format("\n");
 							sz -= n;
 						}
 					}
 				}
 				ofs += rc.ulChunkSize;
-				f_lseek(fil, ofs);
+				fi.seek(utils::file_io::SEEK::SET, ofs);
 			}
-			data_top_ = f_tell(fil);
+			data_top_ = fi.tell();
 
+			data_pos_ = 0;
+
+			return true;
+		}
+
+
+		//-------------------------------------------------------------//
+		/*!
+			@brief	デコード
+			@param[in]	fi	ファイルＩ／Ｏ
+		*/
+		//-------------------------------------------------------------//
+		template <class AUDIO_OUT>
+		bool decode(utils::file_io& fin, AUDIO_OUT& out) noexcept
+		{
+			while(data_pos_ < data_size_) {
+#if 0
+
+				if(!pause_) {
+					while(((wpos ^ pos) & 128) == 0) {
+						pos = get_wave_pos_();
+					}
+					uint32_t unit = (wav_in_.get_bits() / 8) * wav_in_.get_channel();
+					uint8_t tmp[512];
+					if(fi.read(tmp, unit * 128) != (unit * 128)) {
+						utils::format("read fail abort: '%s'\n") % fname;
+						out.mute();
+						break;
+					}
+					audio::wave_t* dst = out.get_wave((nnn + 512) & 0x3ff);
+					if(wav_in_.get_bits() == 16) {
+						const uint16_t* src = reinterpret_cast<const uint16_t*>(tmp);
+						for(uint32_t i = 0; i < 128; ++i) {
+							if(get_channel() == 2) {
+								dst[i].l_ch = src[0] ^ 0x8000;
+								dst[i].r_ch = src[1] ^ 0x8000;
+								src += 2;
+							} else {
+								dst[i].l_ch = src[0] ^ 0x8000;
+								dst[i].r_ch = dst[i].l_ch;
+								++src;
+							}
+						}
+					} else {  // 8 bits
+						const uint8_t* src = reinterpret_cast<const uint8_t*>(tmp);
+						for(uint32_t i = 0; i < 128; ++i) {
+							if(get_channel() == 2) {
+								dst[i].l_ch = static_cast<uint16_t>(src[0] ^ 0x80) << 8;
+								dst[i].l_ch |= (src[0] & 0x7f) << 1;
+								dst[i].r_ch = static_cast<uint16_t>(src[1] ^ 0x80) << 8;
+								dst[i].l_ch |= (src[1] & 0x7f) << 1;
+								src += 2;
+							} else {
+								dst[i].l_ch = static_cast<uint16_t>(src[0] ^ 0x80) << 8;
+								dst[i].l_ch |= (src[0] & 0x7f) << 1;
+								dst[i].r_ch = dst[i].l_ch;
+								++src;
+							}
+						}
+					}
+					nnn += 128;
+					fpos += unit * 128;  // file position
+					wpos = pos;   // wave memory position
+
+
+
+                // LED モニターの点滅
+                if(n >= 20) {  // play 時
+                    n = 0;
+                    LED::P = !LED::P();
+                }
+                ++n;
+            } else {  // pause 時
+                // LED モニターの点滅
+                if(n >= 20) {  // play 時
+                    n = 0;
+                    LED::P = !LED::P();
+                }
+                ++n;
+            } else {  // pause 時
+                // LED モニターの点滅
+                if(n >= 20) {  // play 時
+                    n = 0;
+                    LED::P = !LED::P();
+                }
+                ++n;
+            } else {  // pause 時
+                if(n < 192) {
+                    LED::P = (n >> 5) & 1;
+                } else {
+                    LED::P = 1;
+                }
+                utils::delay::milli_second(2);
+                ++n;
+                nnn = get_wave_pos_() & 0x380;
+#endif
+			}
 			return true;
 		}
 
@@ -176,7 +273,7 @@ namespace audio {
 			@return ファイル位置
 		*/
 		//-------------------------------------------------------------//
-		uint32_t get_top() const { return data_top_; }
+		uint32_t get_top() const noexcept { return data_top_; }
 
 
 		//-------------------------------------------------------------//
@@ -185,7 +282,7 @@ namespace audio {
 			@return データサイズ
 		*/
 		//-------------------------------------------------------------//
-		uint32_t get_size() const { return data_size_; }
+		uint32_t get_size() const noexcept { return data_size_; }
 
 
 		//-------------------------------------------------------------//
@@ -194,7 +291,7 @@ namespace audio {
 			@return サンプルレート
 		*/
 		//-------------------------------------------------------------//
-		uint32_t get_rate() const { return rate_; }
+		uint32_t get_rate() const noexcept { return rate_; }
 
 
 		//-------------------------------------------------------------//
@@ -203,7 +300,7 @@ namespace audio {
 			@return チャネル数
 		*/
 		//-------------------------------------------------------------//
-		uint8_t get_channel() const { return channel_; }
+		uint8_t get_channel() const noexcept { return channel_; }
 
 
 		//-------------------------------------------------------------//
@@ -212,7 +309,7 @@ namespace audio {
 			@return 量子化数
 		*/
 		//-------------------------------------------------------------//
-		uint8_t get_bits() const { return  bits_; }
+		uint8_t get_bits() const noexcept { return  bits_; }
 
 
 		//-------------------------------------------------------------//
@@ -221,6 +318,6 @@ namespace audio {
 			@return 時間
 		*/
 		//-------------------------------------------------------------//
-		uint32_t get_time() const { return data_size_ / (channel_ * bits_ / 8) / rate_; }
+		uint32_t get_time() const noexcept { return data_size_ / (channel_ * bits_ / 8) / rate_; }
 	};
 }
