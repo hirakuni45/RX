a38fd9b93d99a70dff4d01c308d0a5ce1eeef23e
hira <hira@rvf-rc45.net>
2018-07-28 00:46:58 +0900

    add: new project for FIRST_sample (led_sample)

---
+++ FIRST_sample/main.cpp
@@ -0,0 +1,46 @@
+//=====================================================================//
+/*! @file
+    @brief  ファースト・サンプル（LED 点滅） @n
+			RX64M, RX71M: @n
+			　　　　P07 ピンにLEDを接続する @n
+			RX65N (Renesas Envision kit RX65N): @n
+			　　　　P70 に接続された LED を利用する @n
+			RX24T: @n
+			　　　　P00 ピンにLEDを接続する
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+
+namespace {
+
+#if defined(SIG_RX64M) | defined(SIG_RX71M)
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B7> LED;
+#elif defined(SIG_RX65N)
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+#elif defined(SIG_RX24T)
+	typedef device::system_io<10000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B0> LED;
+#endif
+
+}
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	SYSTEM_IO::setup_system_clock();
+
+	LED::DIR = 1;
+	while(1) {
+		utils::delay::milli_second(250);
+		LED::P = 0;
+		utils::delay::milli_second(250);
+		LED::P = 1;
+	}
+}
