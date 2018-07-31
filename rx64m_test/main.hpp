3ecd2d0878e913b6c853a19ee0db268c8a5e7f86
hira <hira@rvf-rc45.net>
2018-07-31 18:23:09 +0900

    fix: version no

--- rx64m_test/main.hpp
+++ rx64m_test/main.hpp
@@ -42,7 +42,7 @@
 namespace seeda {
 
 //	static const int seeda_version_ = 549;
-	static const int seeda_version_ = 601;
+	static const int seeda_version_ = 602;
 	static const uint32_t build_id_ = B_ID;
 
 	typedef device::PORT<device::PORTE, device::bitpos::B3> LED;
