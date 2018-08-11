af9cd525adee15af8cf8166699b97122981989fb
hira <hira@rvf-rc45.net>
2018-08-12 01:13:58 +0900

    update: cleanup

--- RX65x/glcdc_def.hpp
+++ RX65x/glcdc_def.hpp
@@ -50,129 +50,191 @@ namespace device {
 		};
 
 
-	/** Output format select ( Don't change this value, because this value is set to the register ) */
-	enum class OUT_FORMAT {
-		RGB888 = 0,    // Output format RGB888, 24 bits.
-		RGB666 = 1,    // Output format RGB666, 18 bits.
-		RGB565 = 2,    // Output format RGB565, 16 bits.
-		SERIAL = 3     // Output format SERIAL,  8 bits (this function is not supported).
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Output format select ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class OUT_FORMAT {
+			RGB888 = 0,    // Output format RGB888, 24 bits.
+			RGB666 = 1,    // Output format RGB666, 18 bits.
+			RGB565 = 2,    // Output format RGB565, 16 bits.
+			SERIAL = 3     // Output format SERIAL,  8 bits (this function is not supported).
+		};
 
 
-	/** Data endian select ( Don't change this value, because this value is set to the register ) */
-	enum class ENDIAN
-	{
-		LITTLE = 0,     // Little endian.
-		BIG    = 1,     // Big endian.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Data endian select ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class ENDIAN
+		{
+			LITTLE = 0,     // Little endian.
+			BIG    = 1,     // Big endian.
+		};
 
 
-	/** RGB color order select ( Don't change this value, because this value is set to the register ) */
-	enum class COLOR_ORDER
-	{
-		RGB = 0,         // Color order RGB.
-		BGR = 1          // Color order BGR.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  RGB color order select ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class COLOR_ORDER
+		{
+			RGB = 0,         // Color order RGB.
+			BGR = 1          // Color order BGR.
+		};
 
 
-	/** Polarity of a signal select ( Don't change this value, because this value is set to the register ) */
-	enum class SIGNAL_POLARITY
-	{
-		HIACTIVE = 0,    // High active signal.
-		LOACTIVE = 1     // Low active signal.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Polarity of a signal select ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class SIGNAL_POLARITY
+		{
+			HIACTIVE = 0,    // High active signal.
+			LOACTIVE = 1     // Low active signal.
+		};
 
 
-	/** Signal synchronization edge select ( Don't change this value, because this value is set to the register ) */
-	enum class SIGNAL_SYNC_EDGE
-	{
-		RISING = 0,     // Signal is synchronized to rising edge.
-		FALLING = 1     // Signal is synchronized to falling edge.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Signal synchronization edge select ( Don't change @n
+					this value, because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class SIGNAL_SYNC_EDGE
+		{
+			RISING = 0,     // Signal is synchronized to rising edge.
+			FALLING = 1     // Signal is synchronized to falling edge.
+		};
 
 
-	/** Blending control select */
-	enum class BLEND_CONTROL
-	{
-		NONE    = 0,     // Applying no fading control.
-		FADEIN  = 1,     // Applying fade-in control.
-		FADEOUT = 2,     // Applying fade-out control.
-		FIXED   = 3,     // Applying fixed blend control.
-		PIXEL   = 4      // Applying pixel blend control.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Blending control select
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class BLEND_CONTROL
+		{
+			NONE    = 0,     // Applying no fading control.
+			FADEIN  = 1,     // Applying fade-in control.
+			FADEOUT = 2,     // Applying fade-out control.
+			FIXED   = 3,     // Applying fixed blend control.
+			PIXEL   = 4      // Applying pixel blend control.
+		};
 
 
-	/** Fading status */
-	enum class FADE_STATUS
-	{
-		NOT_UNDERWAY,    // Fade-in/fade-out is not in progress.
-		FADING_UNDERWAY, // Fade-in or fade-out is in progress.
-		UNCERTAIN        // Fade-in/fade-out status is uncertain just before hardware working.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Fading status
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class FADE_STATUS
+		{
+			NOT_UNDERWAY,    // Fade-in/fade-out is not in progress.
+			FADING_UNDERWAY, // Fade-in or fade-out is in progress.
+			UNCERTAIN        // Fade-in/fade-out status is uncertain just before hardware working.
+		};
 
 
-	/** Clock source select ( Don't change this value, because this value is set to the register ) */
-	enum class CLK_SRC
-	{
-		EXTERNAL = 0,    // External clock (LCD_EXTCLK) (this function is not supported).
-		INTERNAL = 1     // Internal clock (PLL).
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Clock source select ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class CLK_SRC
+		{
+			EXTERNAL = 0,    // External clock (LCD_EXTCLK) (this function is not supported).
+			INTERNAL = 1     // Internal clock (PLL).
+		};
 
 
-/** Clock frequency division ratio ( Don't change this value, because this value is set to the register ) */
-	enum class PANEL_CLK_DIVISOR
-	{
-		_1  = 1,        // Division Ratio 1/1.
-		_2  = 2,        // Division Ratio 1/2.
-		_3  = 3,        // Division Ratio 1/3.
-		_4  = 4,        // Division Ratio 1/4.
-		_5  = 5,        // Division Ratio 1/5.
-		_6  = 6,        // Division Ratio 1/6.
-		_7  = 7,        // Division Ratio 1/7.
-		_8  = 8,        // Division Ratio 1/8.
-		_9  = 9,        // Division Ratio 1/9.
-		_12 = 12,       // Division Ratio 1/12.
-		_16 = 16,       // Division Ratio 1/16.
-		_24 = 24,       // Division Ratio 1/24.
-		_32 = 32,       // Division Ratio 1/32.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Clock frequency division ratio ( Don't change this @n
+					value, because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class PANEL_CLK_DIVISOR
+		{
+			_1  = 1,        // Division Ratio 1/1.
+			_2  = 2,        // Division Ratio 1/2.
+			_3  = 3,        // Division Ratio 1/3.
+			_4  = 4,        // Division Ratio 1/4.
+			_5  = 5,        // Division Ratio 1/5.
+			_6  = 6,        // Division Ratio 1/6.
+			_7  = 7,        // Division Ratio 1/7.
+			_8  = 8,        // Division Ratio 1/8.
+			_9  = 9,        // Division Ratio 1/9.
+			_12 = 12,       // Division Ratio 1/12.
+			_16 = 16,       // Division Ratio 1/16.
+			_24 = 24,       // Division Ratio 1/24.
+			_32 = 32,       // Division Ratio 1/32.
+		};
 
 
-	/** LCD TCON output pin select */
-	enum class TCON_PIN
-	{
-		_0 = 0,         // LCD_TCON0.
-		_1 = 1,         // LCD_TCON1.
-		_2 = 2,         // LCD_TCON2.
-		_3 = 3          // LCD_TCON3.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  LCD TCON output pin select
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class TCON_PIN
+		{
+			_0 = 0,         // LCD_TCON0.
+			_1 = 1,         // LCD_TCON1.
+			_2 = 2,         // LCD_TCON2.
+			_3 = 3          // LCD_TCON3.
+		};
 
 
-	/** Correction circuit sequence control select ( Don't change this value, because this value is set to the register ) */
-	enum class CORRECTION_PROC_ORDER
-	{
-		BRIGHTNESS_CONTRAST_TO_GAMMA = 0, // Brightness, contrast -> gamma correction.
-		GAMMA_TO_BRIGHTNESS_CONTRAST = 1  // Gamma correction -> brightness, contrast.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Correction circuit sequence control select @n
+					( Don't change this value, because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class CORRECTION_PROC_ORDER
+		{
+			BRIGHTNESS_CONTRAST_TO_GAMMA = 0, // Brightness, contrast -> gamma correction.
+			GAMMA_TO_BRIGHTNESS_CONTRAST = 1  // Gamma correction -> brightness, contrast.
+		};
 
 
-	/** Dithering mode select ( Don't change this value, because this value is set to the register ) */
-	enum class DITHERING_MODE
-	{
-		TRUNCATE   = 0,   // No dithering (truncate).
-		ROUND_OFF  = 1,   // Dithering with round off.
-		PATTERN2X2 = 2    // Dithering with 2x2 pattern.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Dithering mode select ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class DITHERING_MODE
+		{
+			TRUNCATE   = 0,   // No dithering (truncate).
+			ROUND_OFF  = 1,   // Dithering with round off.
+			PATTERN2X2 = 2    // Dithering with 2x2 pattern.
+		};
 
 
-	/** Dithering 2x2 pattern ( Don't change this value, because this value is set to the register ) */
-	enum class DITHERING_PATTERN
-	{
-		_00 = 0,        // 2x2 pattern '00'.
-		_01 = 1,        // 2x2 pattern '01'.
-		_10 = 2,        // 2x2 pattern '10'.
-		_11 = 3         // 2x2 pattern '11'.
-	};
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  Dithering 2x2 pattern ( Don't change this value, @n
+					because this value is set to the register )
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class DITHERING_PATTERN
+		{
+			_00 = 0,        // 2x2 pattern '00'.
+			_01 = 1,        // 2x2 pattern '01'.
+			_10 = 2,        // 2x2 pattern '10'.
+			_11 = 3         // 2x2 pattern '11'.
+		};
 
 
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
