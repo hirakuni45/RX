f6fe42cfab54bea3acbcf3bb7eeaa8ac7f193c70
hira <hira@rvf-rc45.net>
2018-08-14 13:29:37 +0900

    add: new project

---
+++ RTK5_RAYTRACER/main.cpp
@@ -0,0 +1,293 @@
+//=====================================================================//
+/*! @file
+    @brief  RX65N RayTrace
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+#include "common/fixed_fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/format.hpp"
+#include "common/command.hpp"
+#include "common/spi_io2.hpp"
+#include "common/sdc_man.hpp"
+#include "graphics/font8x16.hpp"
+#include "graphics/graphics.hpp"
+#define CASH_KFONT
+#include "graphics/kfont.hpp"
+
+#include "raytracer.hpp"
+
+namespace {
+
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+// オーディオの D/A として使用
+//	typedef device::PORT<device::PORT0, device::bitpos::B5> SW2;
+
+	typedef device::system_io<12000000> SYSTEM_IO;
+
+	typedef utils::fixed_fifo<char, 512>  RECV_BUFF;
+	typedef utils::fixed_fifo<char, 1024> SEND_BUFF;
+	typedef device::sci_io<device::SCI9, RECV_BUFF, SEND_BUFF> SCI;
+	SCI		sci_;
+
+	typedef device::PORT<device::PORT6, device::bitpos::B3> LCD_DISP;
+	typedef device::PORT<device::PORT6, device::bitpos::B6> LCD_LIGHT;
+	typedef device::glcdc_io<device::GLCDC, 480, 272,
+		device::glcdc_def::PIX_TYPE::RGB565> GLCDC_IO;
+	GLCDC_IO	glcdc_io_;
+
+	typedef graphics::font8x16 AFONT;
+#ifdef CASH_KFONT
+	typedef graphics::kfont<16, 16, 64> KFONT;
+#else
+	typedef graphics::kfont<16, 16> KFONT;
+#endif
+	KFONT		kfont_;
+
+	typedef graphics::render<uint16_t, 480, 272, AFONT, KFONT> RENDER;
+	RENDER		render_(reinterpret_cast<uint16_t*>(0x00000000), kfont_);
+
+	// カード電源制御は使わない場合、「device::NULL_PORT」を指定する。
+//	typedef device::PORT<device::PORT6, device::bitpos::B4> SDC_POWER;
+	typedef device::NULL_PORT SDC_POWER;
+
+#ifdef SDHI_IF
+	// RX65N Envision Kit の SDHI ポートは、候補３になっている
+	typedef fatfs::sdhi_io<device::SDHI, SDC_POWER, device::port_map::option::THIRD> SDHI;
+	SDHI	sdh_;
+#else
+	// Soft SDC 用　SPI 定義（SPI）
+	typedef device::PORT<device::PORT2, device::bitpos::B2> MISO;  // DAT0
+	typedef device::PORT<device::PORT2, device::bitpos::B0> MOSI;  // CMD
+	typedef device::PORT<device::PORT2, device::bitpos::B1> SPCK;  // CLK
+
+	typedef device::spi_io2<MISO, MOSI, SPCK> SPI;  ///< Soft SPI 定義
+
+	SPI		spi_;
+
+	typedef device::PORT<device::PORT1, device::bitpos::B7> SDC_SELECT;  // DAT3 カード選択信号
+	typedef device::PORT<device::PORT2, device::bitpos::B5> SDC_DETECT;  // CD   カード検出
+
+	typedef fatfs::mmc_io<SPI, SDC_SELECT, SDC_POWER, SDC_DETECT> MMC;   // ハードウェアー定義
+
+	MMC		sdh_(spi_, 20000000);
+#endif
+	typedef utils::sdc_man SDC;
+	SDC		sdc_;
+
+	utils::command<256> cmd_;
+
+
+	bool check_mount_() {
+		auto f = sdc_.get_mount();
+		if(!f) {
+			utils::format("SD card not mount.\n");
+		}
+		return f;
+	}
+
+
+	void command_()
+	{
+		if(!cmd_.service()) {
+			return;
+		}
+
+		uint8_t cmdn = cmd_.get_words();
+		if(cmdn >= 1) {
+			bool f = false;
+			if(cmd_.cmp_word(0, "dir")) {  // dir [xxx]
+				if(check_mount_()) {
+					if(cmdn >= 2) {
+						char tmp[128];
+						cmd_.get_word(1, sizeof(tmp), tmp);
+						sdc_.dir(tmp);
+					} else {
+						sdc_.dir("");
+					}
+				}
+				f = true;
+			} else if(cmd_.cmp_word(0, "cd")) {  // cd [xxx]
+				if(check_mount_()) {
+					if(cmdn >= 2) {
+						char tmp[128];
+						cmd_.get_word(1, sizeof(tmp), tmp);
+						sdc_.cd(tmp);						
+					} else {
+						sdc_.cd("/");
+					}
+				}
+				f = true;
+			} else if(cmd_.cmp_word(0, "pwd")) { // pwd
+				utils::format("%s\n") % sdc_.get_current();
+				f = true;
+			} else if(cmd_.cmp_word(0, "help")) {
+				utils::format("    dir [path]\n");
+				utils::format("    cd [path]\n");
+				utils::format("    pwd\n");
+				f = true;
+			}
+			if(!f) {
+				char tmp[128];
+				if(cmd_.get_word(0, sizeof(tmp), tmp)) {
+					utils::format("Command error: '%s'\n") % tmp;
+				}
+			}
+		}
+	}
+}
+
+extern "C" {
+
+	void draw_pixel(int x, int y, int r, int g, int b)
+	{
+		uint16_t c = (static_cast<uint16_t>(r & 0xf8) << 8)
+				   | (static_cast<uint16_t>(g & 0xfc) << 3)
+				   | (static_cast<uint16_t>(b & 0xf8) >> 3);
+		render_.plot(x, y, c);
+	}
+
+	void sci_putch(char ch)
+	{
+		sci_.putch(ch);
+	}
+
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+
+
+	DSTATUS disk_initialize(BYTE drv) {
+		return sdh_.disk_initialize(drv);
+	}
+
+
+	DSTATUS disk_status(BYTE drv) {
+		return sdh_.disk_status(drv);
+	}
+
+
+	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_read(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_write(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
+		return sdh_.disk_ioctl(drv, ctrl, buff);
+	}
+
+
+	DWORD get_fattime(void) {
+		time_t t = 0;
+///		rtc_.get_time(t);
+		return utils::str::get_fattime(t);
+	}
+
+
+	void utf8_to_sjis(const char* src, char* dst, uint32_t len) {
+		utils::str::utf8_to_sjis(src, dst, len);
+	}
+
+
+	int fatfs_get_mount() {
+		return sdc_.get_mount();
+	}
+
+
+	int make_full_path(const char* src, char* dst, uint16_t len)
+	{
+		return sdc_.make_full_path(src, dst, len);
+	}
+}
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	SYSTEM_IO::setup_system_clock();
+
+	{  // SCI 設定
+		static const uint8_t sci_level = 2;
+		sci_.start(115200, sci_level);
+	}
+
+	{  // SD カード・クラスの初期化
+		sdh_.start();
+		sdc_.start();
+	}
+
+	utils::format("\rRTK5RX65N Start for Ray Trace\n");
+
+	cmd_.set_prompt("# ");
+
+	{  // GLCDC 初期化
+		LCD_DISP::DIR  = 1;
+		LCD_LIGHT::DIR = 1;
+		LCD_DISP::P  = 0;  // DISP Disable
+		LCD_LIGHT::P = 0;  // BackLight Disable (No PWM)
+		if(glcdc_io_.start()) {
+			utils::format("Start GLCDC\n");
+			LCD_DISP::P  = 1;  // DISP Enable
+			LCD_LIGHT::P = 1;  // BackLight Enable (No PWM)
+			if(!glcdc_io_.control(GLCDC_IO::CONTROL_CMD::START_DISPLAY)) {
+				utils::format("GLCDC ctrl fail...\n");
+			}
+		} else {
+			utils::format("Fail GLCDC\n");
+		}
+	}
+
+	LED::DIR = 1;
+
+	uint8_t n = 0;
+	bool run = false;
+	while(1) {
+		glcdc_io_.sync_vpos();
+
+		sdc_.service(sdh_.service());
+
+		command_();
+		if(!run) {
+			uint32_t org = glcdc_io_.get_vpos();
+			doRaytrace();
+			uint32_t t = glcdc_io_.get_vpos() - org;
+			char tmp[32];
+			utils::sformat("%d.%d [sec]", tmp, sizeof(tmp)) % (t / 60) % ((t / 6) % 10);
+			render_.draw_text(320, 0, tmp);
+			run = true;
+		}
+
+		++n;
+		if(n >= 30) {
+			n = 0;
+		}
+		if(n < 10) {
+			LED::P = 0;
+		} else {
+			LED::P = 1;
+		}
+	}
+}
---
+++ RTK5_RAYTRACER/raytracer.hpp
@@ -0,0 +1,338 @@
+#pragma once
+/*------------------------------------------------------------------------
+  This is based on Paul Heckbert's "business card raytracer".
+  His site is: http://www.cs.cmu.edu/~ph/
+
+  I modified it a lot adding colors/materials, arbitrary sphere
+  positions, etc.
+  
+  I also added some comments and made it readable. The original
+  code was designed to be small enough to print on the back of
+  a business card (hence the name) so it was very hard to read.
+  
+  FTB.
+------------------------------------------------------------------------*/
+#include <cmath>
+#include <cstdint>
+
+extern "C" {
+	void draw_pixel(int x, int y, int r, int g, int b);
+};
+
+/*------------------------------------------------------------------------
+  (For non-arduino platforms)
+------------------------------------------------------------------------*/
+#ifndef PROGMEM
+#define PROGMEM
+float pgm_read_float(const float *f) {  return *f;  }
+#endif
+
+/*------------------------------------------------------------------------
+  Values you can play with...
+------------------------------------------------------------------------*/
+
+// Position of the camera (nb. 'Z' is up/down)
+static const float cameraX = 0.0;
+static const float cameraY = 0.0;
+static const float cameraZ = 3.0;
+
+// What the camera is pointing at
+static const float targetX = 1.0;
+static const float targetY = 8.0;
+static const float targetZ = 4.0;
+
+// We cast this many rays per pixel for stochastic antialiasing and soft-shadows. 
+//
+// Large numbers produce a nicer image but it runs a lot slower
+//static const int raysPerPixel = 4;
+
+// The camera's field of view, smaller=>zoom, larger=>wide angle
+static const float fov = 0.45f;
+
+// The size of the soft shadow, larger=>wider area
+static const float shadowRegion = 0.125f;
+
+/*------------------------------------------------------------------------
+  Materials
+------------------------------------------------------------------------*/
+static const float ambient = 0.05f;
+static const float materials[] PROGMEM = {
+// R,    G,    B,   REFLECTIVITY
+  0.8f, 0.8f, 0.8f,   0.5f,    // Mirror
+//  1.0f, 0.0f, 0.0f,   0.3f,    // Red
+  0.0f, 1.0f, 0.0f,   0.2f,    // Green
+//  0.0f, 0.0f, 0.1f,   0.3f     // Dark blue
+  0.0f, 0.8f, 0.8f,   0.3f     // Cyan
+};
+
+/*------------------------------------------------------------------------
+  The spheres in the world
+------------------------------------------------------------------------*/
+#define NUM_SPHERES 4
+static const float spheres[] PROGMEM = {
+// center  radius material
+   5,15,8,   5,     0,
+  -6,12,4,   3,     0,
+   1,10,2,   2,     1,
+   0, 9,5,   1,     2
+};
+
+/*------------------------------------------------------------------------
+  A 3D vector class
+------------------------------------------------------------------------*/
+struct vec3 {
+  float x,y,z;  // Vector has three float attributes.
+  vec3(){}
+  vec3(float a, float b, float c){x=a;y=b;z=c;}
+  vec3 operator+(const vec3& v) const { return vec3(x+v.x,y+v.y,z+v.z);  }    // Vector add
+  vec3 operator-(const vec3& v) const { return (*this)+(v*-1);           }    // Vector subtract
+  vec3 operator*(float s)       const { return vec3(x*s,y*s,z*s);        }    // Vector scale
+  float operator%(const vec3& v)const { return x*v.x+y*v.y+z*v.z;        }    // Scalar product
+  vec3 operator^(const vec3& v) const { return vec3(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x);  } // Vector product
+  vec3 operator!()              const { return *this*(1.0/sqrt(*this%*this));  }  // Normalized vector
+  void operator+=(const vec3& v)      { x+=v.x;  y+=v.y;  z+=v.z;        }
+  void operator*=(float s)            { x*=s;    y*=s;    z*=s;          }
+};
+
+// A ray...
+struct ray {
+  // This occupies 24 bytes - you could only fit 20 of these into
+  // a Tiny85 even if you could use the entire RAM (which you can't...)
+  vec3 o;  // Origin
+  vec3 d;  // Direction
+};
+
+/*------------------------------------------------------------------------
+  Intersect a ray with the world
+  Return 'SKY' if no hit was found but ray goes upward
+  Return 'FLOOR' if no hit was found but ray goes downward towards the floor
+  Return a material index if a hit was found
+
+  Distance to the hit is returned in 'distance'. 
+  The surface normal at the hit is returned in 'normal'
+------------------------------------------------------------------------*/
+// Values for 'SKY' and 'FLOOR'
+static const uint8_t SKY=255;
+static const uint8_t FLOOR=254;
+
+uint8_t trace(const ray& r, float& distance, vec3& normal)
+{
+  // Assume we didn't hit anything
+  uint8_t result = SKY;
+
+  // Does the ray go downwards?
+  float d = -r.o.z/r.d.z;
+  if (d > 0.01) {
+    // Yes, assume it hits the floor
+    result = true;
+    distance = d;
+    result = FLOOR;
+    normal = vec3(0,0,1);
+  }
+
+  // Test the objects in the scene to see if there's anything in the way
+  for (uint8_t i=0; i<NUM_SPHERES; ++i) {
+    vec3 oc = r.o;
+    // Read a sphere from progmem, calculate vector 'oc'
+    const float *n = spheres+(i*5);
+    oc.x -= pgm_read_float(n++);
+    oc.y -= pgm_read_float(n++);
+    oc.z -= pgm_read_float(n++);
+    d = pgm_read_float(n++);  // Radius
+
+    // Ray-sphere intersection test
+    // Math is here: http://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+    const float b = r.d%oc;        // I.(o-c)
+    const float c = (oc%oc)-(d*d); // (o-c).(o-c) - r^2
+
+    // Does the ray hit the sphere?
+    d = (b*b)-c;
+    if (d > 0) {
+      // Yes, compute the distance to the hit
+      d = (-b)-sqrt(d);
+
+      // Is it the closest hit so far?
+      if ((d > 0.01) and ((result==SKY) or (d<distance))) {
+        // Yes, save results
+        distance = d;
+        normal = !(oc+r.d*d);
+        result = uint8_t(pgm_read_float(n));  // The sphere's material
+      }
+    }
+  }
+  return result;
+}
+float raise(float p, uint8_t n)
+{
+  while (n--) {
+    p = p*p;
+  }
+  return p;
+}
+
+/*----------------------------------------------------------
+  Small, fast pseudo-random number generator
+  
+  I found this in a forum and I'm not sure who originally
+  wrote it. It works very well though....
+ 
+  If you wrote this then get in touch and I'll put
+  your name here. :-)                              FTB.
+----------------------------------------------------------*/
+uint8_t randomByte()
+{
+  static uint8_t rngA, rngB, rngC, rngX;
+  ++rngX;                        // X is incremented every round and is not affected by any other variable
+  rngA = (rngA ^ rngC ^ rngX);       // note the mix of addition and XOR
+  rngB = (rngB + rngA);            // And the use of very few instructions
+  rngC = ((rngC + (rngB >> 1)) ^ rngA);  // the right shift is to ensure that high-order bits from B can affect  
+  return rngC;
+}
+
+// A random float in the range [-0.5 ... 0.5]  (more or less)
+float randomFloat()
+{
+  char r = char(randomByte());
+  return float(r)/256.0;
+}
+#define RF randomFloat()
+#define SH (RF*shadowRegion)
+
+/*------------------------------------------------------------------------
+  Sample the world and return the pixel color for a ray
+------------------------------------------------------------------------*/
+float sample(ray& r, vec3& color)
+{
+  // See if the ray hits anything in the world
+  float t;  vec3& n = color;      // RAM is tight, use 'color' as temp workspace
+  const uint8_t hit = trace(r,t,n);
+
+  // Did we hit anything
+  if (hit == SKY) {
+    // Generate a sky color if the ray goes upwards without hitting anything
+    color = vec3(0.1f,0.0f,0.3f) + vec3(.7f,.2f,0.5f)*raise(1.0-r.d.z,2);
+    return 0.0;
+  }
+
+  // New ray origin
+  r.o += r.d*t;
+
+  // Half vector
+  const vec3 half = !(r.d+n*((n%r.d)*-2));
+
+// Vector that points towards the light
+  r.d = vec3(9+SH, 6+SH,16); // Where the light is
+  r.d = !(r.d-r.o);          // Normalized light vector
+
+  // Lambertian factor
+  float d = r.d%n;    // Light vector % surface normal
+
+  // See if we're in shadow
+  if ((d<0) or (trace(r,t,n)!=SKY)) {
+    d = 0;
+  }
+
+  // Did we hit the floor?
+  if (hit == FLOOR) {
+    // Yes, generate a floor color
+    d=(d*0.2)+0.1;   t=d*3.0;  // d=dark, t=light
+    color = vec3(t,t,t);       // Assume grey color
+    t = 1.0f/5.0f;     // Floor tiles are 5m across
+//    int fx = int(ceil(r.o.x*t));
+//    int fy = int(ceil(r.o.y*t));
+//    bool dark = ((fx+fy)&1)!=0;  // Light or dark color?
+    bool dark = (((int)(ceil(r.o.x*t)+ceil(r.o.y*t)))&1);  // Light or dark color? -> fix for AVR compiler
+    if (dark) { color.y = color.z = d; }        // g+b => dark => 'red'
+    return 0;
+  }
+
+  // No, we hit the scene, read material color from progmem
+  const float *mat = materials+(hit*4);
+  color.x = pgm_read_float(mat++);
+  color.y = pgm_read_float(mat++);
+  color.z = pgm_read_float(mat++);
+ 
+  // Specular light in 't'
+  t = d;
+  if (t > 0) {
+    t = raise(r.d%half,5);
+  }
+
+  // Calculate total color using diffuse and specular components
+  color *= d*d+ambient;  // Ambient+diffuse
+  color += vec3(t,t,t);  // Specular
+
+  // We need to trace a reflection ray...need to modify 'r' for the recursion
+  r.d = half;
+  return pgm_read_float(mat);    // Reflectivity of this material
+}
+
+/*------------------------------------------------------------------------
+  Raytrace the entire image
+------------------------------------------------------------------------*/
+void doRaytrace(int raysPerPixel = 4, int dw = 320, int dh = 240, int q = 1)
+{
+  // Trace it
+  int dw2=dw/2;;
+  int dh2=dh/2;
+  const float pixel =  fov/float(dh2);    // Size of one pixel on screen
+
+  // Position/target of camera
+  const vec3 camera = vec3(cameraX,cameraY,cameraZ);
+  const vec3 target = vec3(targetX,targetY,targetZ);
+  
+///  unsigned long t = millis();
+  unsigned long t = 0;
+
+  for (int y=0; y<dh; y+=q) {
+    for (int x=0; x<dw; x+=q) {
+      vec3 acc(0,0,0);     // Color accumulator
+      for (int p=raysPerPixel; p--;) {
+        ray r;  vec3 temp;
+        float xpos = float(x-dw2), ypos=float(dh2-y);
+        if (raysPerPixel>1) { xpos+=RF; ypos+=RF; }       // Stochastic antialiasing when RPP > 1
+
+        // Calculate a ray through this pixel
+        temp = !(target-camera);
+        vec3& right = r.o;   right = !(temp^vec3(0,0,1));
+        vec3& up = r.d;      up = !(right^temp);
+        r.d = !(temp + ((right*xpos)+(up*ypos))*pixel);  // Ray direction
+        r.o = camera;                                    // Ray starts at the camera
+        
+        // Sample the world, accumulate the color returned
+        vec3& color = temp;
+        float reflect1 = sample(r,color);
+        acc += color;
+        // 'sample()' would normally be recursive but there's not enough RAM to do that on a Tiny85...
+        if (reflect1 > 0) {
+          // ...so we do the 'recursion' manually
+          float reflect2 = sample(r,color);
+          acc += color*reflect1;
+          if (reflect2 > 0) {
+            // ...3 levels deep
+            sample(r,color);
+            acc += color*(reflect1*reflect2);
+          }
+        }
+      }
+      
+      // Output the pixel
+      acc = acc*(255.0/float(raysPerPixel));
+      int r = acc.x;    if (r>255) { r=255; }
+      int g = acc.y;    if (g>255) { g=255; }
+      int b = acc.z;    if (b>255) { b=255; }
+	draw_pixel(x, y, r, g, b);
+///      if(q==1) display.drawPixel(x, y, RGBTO565(r,g,b));
+///      else display.fillRect(x, y, q, q, RGBTO565(r,g,b));
+    }
+// workaround for ESP8266 which reboots after staying too long in the loops (WTF-watchdog?)
+#ifdef ESP8266
+    delay(1);
+#endif
+///    char buf[100];
+///    snprintf(buf,100,"%3d%% %3ds", (y+q)*100/dh, (millis()-t)/1000);
+///    display.setCursor(8,0);
+///    display.println(buf);
+  }
+}
+
