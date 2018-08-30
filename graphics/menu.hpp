2fd941621dcb6b54ed03f97d468d3b036610e2ee
hira <hira@rvf-rc45.net>
2018-08-30 17:22:55 +0900

    update: manage menu system

--- graphics/menu.hpp
+++ graphics/menu.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	メニュー・クラス
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -15,11 +15,11 @@ namespace graphics {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief	メニュー・クラス
-		@param[in]	GR		描画クラス
+		@param[in]	REND	描画クラス
 		@param[in]	MAX		最大メニュー数
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class GR, uint16_t MAX>
+	template <class REND, uint16_t MAX>
 	class menu {
 
 	public:
@@ -35,47 +35,70 @@ namespace graphics {
 		};
 
 	private:
-		GR&	gr_;
+		REND&		rend_;
 
 		uint16_t	size_;
 
 		struct obj_t {
 			type		type_;
-			const void* src_;
 			uint8_t		w_;
 			uint8_t		h_;
+			const void* src_;
 
-			obj_t() : type_(type::TEXT), src_(nullptr), w_(0), h_(0) { }
+			obj_t() : type_(type::TEXT), w_(0), h_(0), src_(nullptr) { }
 		};
 		obj_t	obj_[MAX];
 
 		int16_t		mx_;
 		int16_t		my_;
 
-		int16_t		spc_;
+		int8_t		gap_;
+		int8_t		space_w_;
+		int8_t		space_h_;
 
 		uint16_t	pos_;
 
+		typename REND::value_type	fc_;
+		typename REND::value_type	bc_;
+
+		bool		frame_;
 		bool		focus_;
 
 	public:
 		//-----------------------------------------------------------------//
 		/*!
 			@brief	コンストラクター
+			@param[in]	rend	描画クラス
 		*/
 		//-----------------------------------------------------------------//
-		menu(GR& gr) : gr_(gr), size_(0), mx_(0), my_(0),
-					   spc_(0), pos_(0), focus_(true)
+		menu(REND& rend) noexcept : rend_(rend), size_(0), mx_(0), my_(0),
+			gap_(0), space_w_(0), space_h_(0), pos_(0),
+			fc_(REND::COLOR::Black), bc_(REND::COLOR::White),
+			frame_(true), focus_(true)
 		{ }
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief	隙間の設定
-			@param[in]	spc	隙間
+			@brief	スペースの設定
+			@param[in]	w	横幅
+			@param[in]	h	高さ
 		*/
 		//-----------------------------------------------------------------//
-		void set_space(int16_t spc) { spc_ = spc; }
+		void set_space(int16_t w, int16_t h) noexcept
+		{
+			space_w_ = w;
+			space_h_ = h;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	ギャップの設定
+			@param[in]	gap	ギャップ
+		*/
+		//-----------------------------------------------------------------//
+		void set_gap(int16_t gap) noexcept { gap_ = gap; }
 
 
 		//-----------------------------------------------------------------//
@@ -83,17 +106,30 @@ namespace graphics {
 			@brief	サイズを返す（登録されているメニュー数）
 		*/
 		//-----------------------------------------------------------------//
-		uint16_t size() const { return size_; }
+		uint16_t size() const noexcept { return size_; }
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief	フォーカスを許可
+			@param[in]	f	不許可の場合「false」
 		*/
 		//-----------------------------------------------------------------//
-		void enable_focus(bool f = true)
+		void enable_focus(bool f = true) noexcept
 		{
 			focus_ = f;
+		} 
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	フレームの描画
+			@param[in]	f	不許可の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		void draw_frame(bool f = true) noexcept
+		{
+			frame_ = f;
 		} 
 
 
@@ -102,7 +138,7 @@ namespace graphics {
 			@brief	フォーカスを戻す
 		*/
 		//-----------------------------------------------------------------//
-		void focus_prev()
+		void focus_prev() noexcept
 		{
 			if(!focus_) return;
 
@@ -117,7 +153,7 @@ namespace graphics {
 			@brief	フォーカスを進める
 		*/
 		//-----------------------------------------------------------------//
-		void focus_next()
+		void focus_next() noexcept
 		{
 			if(!focus_) return;
 
@@ -134,7 +170,7 @@ namespace graphics {
 			@return メニュー位置
 		*/
 		//-----------------------------------------------------------------//
-		uint16_t get_pos() const { return pos_; }
+		uint16_t get_pos() const noexcept { return pos_; }
 
 
 		//-----------------------------------------------------------------//
@@ -142,7 +178,7 @@ namespace graphics {
 			@brief	クリア
 		*/
 		//-----------------------------------------------------------------//
-		void clear()
+		void clear() noexcept
 		{
 			size_ = 0;
 			mx_ = 0;
@@ -158,19 +194,19 @@ namespace graphics {
 			@param[in]	src	オブジェクト・ソース
 		*/
 		//-----------------------------------------------------------------//
-		void add(type t, const void* src) {
+		void add(type t, const void* src) noexcept {
 			if(size_ >= MAX) return;
 
 			if(t == type::TEXT) {
-				obj_[size_].w_ = gr_.draw_text_length(static_cast<const char*>(src), false);
-				obj_[size_].h_ = gr_.get_afont_height();
+				obj_[size_].w_ = rend_.get_text_length(static_cast<const char*>(src), false);
+				obj_[size_].h_ = REND::font_height;
 			} else if(t == type::PROP) {
-				obj_[size_].w_ = gr_.draw_text_length(static_cast<const char*>(src), true);
-				obj_[size_].h_ = gr_.get_afont_height();
+				obj_[size_].w_ = rend_.get_text_length(static_cast<const char*>(src), true);
+				obj_[size_].h_ = REND::font_height;
 			} else if(t == type::MOBJ) {
 				uint8_t w;
 				uint8_t h;
-				gr_.get_mobj_size(src, w, h);
+				rend_.get_mobj_size(src, w, h);
 				obj_[size_].w_ = w;
 				obj_[size_].h_ = h;
 			} else {
@@ -185,7 +221,8 @@ namespace graphics {
 			for(auto i = 0; i < size_; ++i) {
 				if(x < obj_[i].w_) x = obj_[i].w_;
 				y += obj_[i].h_;
-				y += spc_;
+				y += gap_;
+				y += space_h_ * 2;
 			}
 			mx_ = x;
 			my_ = y;
@@ -199,39 +236,52 @@ namespace graphics {
 			@param[in]	oy	描画オフセットＹ
 		*/
 		//-----------------------------------------------------------------//
-		void render(int16_t ox = 0, int16_t oy = 0) const
+		void render(int16_t ox = 0, int16_t oy = 0) const noexcept
 		{
 			if(size_ == 0) return;
 
-			int16_t x = (gr_.get_width()  - ox - mx_) / 2;
-			int16_t y = (gr_.get_height() - oy - my_) / 2;
+			int16_t x = (REND::width  - ox - mx_) / 2;
+			int16_t y = (REND::height - oy - my_) / 2;
 			x += ox;
 			y += oy;
+			auto tmp_fc = rend_.get_fore_color();
+			rend_.set_fore_color(fc_);
 			for(auto i = 0; i < size_; ++i) {
-				y += spc_ / 2;
+				y += gap_ / 2;
 				uint8_t w;
 				uint8_t h;
 				if(obj_[i].type_ == type::TEXT) {
-					gr_.draw_text(x, y, static_cast<const char*>(obj_[i].src_), false);
+					rend_.fill_r(x, y, mx_ + space_w_ * 2, obj_[i].h_ + space_h_ * 2, bc_);
+					y += space_h_;
+					auto t = static_cast<const char*>(obj_[i].src_);
+					rend_.draw_text(x + space_w_, y, t);
+						
 				} else if(obj_[i].type_ == type::PROP) {
-					gr_.draw_text(x, y, static_cast<const char*>(obj_[i].src_), true);
+					rend_.fill_r(x, y, mx_ + space_w_ * 2, obj_[i].h_ + space_h_ * 2, bc_);
+					y += space_h_;
+					auto t = static_cast<const char*>(obj_[i].src_);
+					rend_.draw_text(x + space_w_, y, t, true);
 				} else if(obj_[i].type_ == type::MOBJ) {
-					gr_.draw_mobj(x, y, obj_[i].src_);
+					y += space_h_;
+					rend_.draw_mobj(x, y, obj_[i].src_);
 				}
-				if(focus_ && i == pos_) {
-					gr_.reverse(x - 1, y, obj_[i].w_ + 2, obj_[i].h_);
-				}
-				y += gr_.get_afont_height();
-				y += spc_ / 2;
+//				if(focus_ && i == pos_) {
+//					rend_.reverse(x - 1, y, obj_[i].w_ + 2, obj_[i].h_);
+//				}
+				y += REND::font_height;
+				y += space_h_;
+				y += gap_ - (gap_ / 2);
 			}
+			rend_.set_fore_color(tmp_fc);
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief	GR インスタンスへの参照
+			@brief	REND クラスの参照
+			@return REND クラス
 		*/
 		//-----------------------------------------------------------------//
-		GR& at() { return gr_; }
+		REND& at() noexcept { return rend_; }
 	};
 }
