7479a520be8aa6b849ea442af52085357ef21ffe
hira <hira@rvf-rc45.net>
2018-09-17 14:38:12 +0900

    add: the new project (CNC)

---
+++ rx24t_CNC/main.cpp
@@ -0,0 +1,114 @@
+//=====================================================================//
+/*! @file
+    @brief  RX24T CNC ドライバー @n
+			・ステッピングモーターパルス出力 @n
+			・リミットスイッチによる制御 @n
+			・P00(4) ピンに赤色LED（VF:1.9V）を吸い込みで接続する
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/cmt_io.hpp"
+#include "common/fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/format.hpp"
+#include "common/command.hpp"
+
+namespace {
+
+	device::cmt_io<device::CMT0, utils::null_task>  cmt_;
+
+	typedef utils::fifo<uint8_t, 1024> RBF;
+	typedef utils::fifo<uint8_t, 128> SBF;
+	typedef device::sci_io<device::SCI1, RBF, SBF> SCI;
+	SCI		sci_;
+
+	utils::command<256> command_;
+}
+
+extern "C" {
+
+	void sci_putch(char ch)
+	{
+		sci_.putch(ch);
+	}
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+
+}
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
+
+	device::SYSTEM::MEMWAIT = 0b10; // 80MHz 動作 wait 設定
+
+	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
+	device::SYSTEM::OPCCR = 0;  // 高速モード選択
+	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
+
+	// clock osc 10MHz
+	device::SYSTEM::MOSCWTCR = 9;	// 4ms wait
+	// メインクロック・ドライブ能力設定、内部発信
+	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV21.b(1);
+	device::SYSTEM::MOSCCR.MOSTP = 0;  // メインクロック発振器動作
+	while(device::SYSTEM::OSCOVFSR.MOOVF() == 0) asm("nop");
+
+	device::SYSTEM::PLLCR.STC = 0b001111;		// PLL input: 1, PLL 8 倍(80MHz)
+	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
+	while(device::SYSTEM::OSCOVFSR.PLOVF() == 0) asm("nop");
+
+	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(2)		// 1/4 (80/4=20)
+						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (80/1=80)
+						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (80/1=80)
+						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (80/2=40)
+						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (80/2=40)
+	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
+
+	// タイマー設定（６０Ｈｚ）
+	uint8_t cmt_irq_level = 4;
+	cmt_.start(60, cmt_irq_level);
+
+	// SCI 設定
+	static const uint8_t sci_level = 2;
+	sci_.start(115200, sci_level);
+
+	utils::format("RX24T CNC Driver\n");
+
+	command_.set_prompt("# ");
+
+	device::PORT0::PDR.B0 = 1; // output
+
+	uint32_t cnt = 0;
+	while(1) {
+		cmt_.sync();
+
+		// コマンド入力と、コマンド解析
+		if(command_.service()) {
+
+		}
+
+		++cnt;
+		if(cnt >= 30) {
+			cnt = 0;
+		}
+		device::PORT0::PODR.B0 = (cnt < 10) ? 0 : 1;
+	}
+}
