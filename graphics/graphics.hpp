f738222eb0bb6490361c91f51d406bd87f944a2d
hira <hira@rvf-rc45.net>
2018-09-25 10:30:33 +0900

    update: change API name

--- graphics/graphics.hpp
+++ graphics/graphics.hpp
@@ -723,7 +723,7 @@ namespace graphics {
 			int16_t x = (width  - w) / 2;
 			int16_t y = (height - h) / 2;
 			frame(x, y, w, h, COLOR::White);
-			fill(x + 1, y + 1, w - 2, h - 2, COLOR::Black);
+			fill_box(x + 1, y + 1, w - 2, h - 2, COLOR::Black);
 			auto l = get_text_length(text);
 			x += (w - l) / 2;
 			y += (h - font_height) / 2;
