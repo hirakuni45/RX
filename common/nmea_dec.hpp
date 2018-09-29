794bd80cbf0f1ea8f6f0d43f5ad04af20d5fd2b9
hira <hira@rvf-rc45.net>
2018-09-30 07:06:09 +0900

    update: cleanup

--- common/nmea_dec.hpp
+++ common/nmea_dec.hpp
@@ -2,7 +2,8 @@
 //=====================================================================//
 /*!	@file
 	@brief	NMEA デコード・クラス（GPS 測位コードパース）@n
-			http://www.hiramine.com/physicalcomputing/general/gps_nmeaformat.html
+			for GTPA013 @n
+			初期ボーレートは９６００で行う。
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2016, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
@@ -464,54 +465,80 @@ namespace utils {
 		void set_baudrate(BAUDRATE bpsno) noexcept
 		{
 //			"$PMTK251,38400*27\r\n"
-// for SUP500 GPS
-#if 0
-			sci_.putch(0xA0);
-			sci_.putch(0xA1);
-			sci_.putch(0x00);
-
-			sci_.putch(0x04);	// メッセージ数
-
-			sci_.putch(0x05);	// メッセージID
-			sci_.putch(0x00);	// COM port 0  以外はない
-			sci_.putch(bpsno); 	// 0: 4800, 1: 9600, 2: 19200, 3: 38400, 4: 57600, 5: 115200
-			sci_.putch(0x00);	// 0: update to SRAM, 1: update to both SRAM & FLASH
-			uint8_t sum = 0x05 ^ bpsno;
-			sci_.putch(sum);
-
-			sci_.putch(0x0D);
-			sci_.putch(0x0A);
-#endif
+#define PMTK_SET_BAUD_57600 "$PMTK251,57600*2C"
+#define PMTK_SET_BAUD_9600 "$PMTK251,9600*17"
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@breif	G.P.S. の１０Ｈｚ測位の設定@n
-					※事前にボーレートを高く設定する必要がある
+			@breif	位置更新レートの設定 @n
+					※高いレートを使う場合、事前にボーレートを高く設定する必要がある。
+			@param[in]	rate	更新レート（１～１０）
+			@return 成功なら「true」
 		 */
 		//-----------------------------------------------------------------//
-		void set_rate10()
+		bool set_update_rate(uint32_t rate)
 		{
-// $PMTK220,1000*1F<CR><LF> 1Hz
-// $PMTK220, 200*2C<CR><LF> 5Hz
-// $PMTK220,100*2F<CR><LF> 10Hz
-// SUP500 GPS
 #if 0
-			sci_.putch(0xA0);
-			sci_.putch(0xA1);
-			sci_.putch(0x00);
-
-			sci_.putch(0x03);
-
-			sci_.putch(0x0E);
-			sci_.putch(0x0A);
-			sci_.putch(0x00);
-			sci_.putch(0x04);
-
-			sci_.putch(0x0D);
-			sci_.putch(0x0A);
+// different commands to set the update rate from once a second (1 Hz) to 10 times a second (10Hz)
+// Note that these only control the rate at which the position is echoed, to actually speed up the
+// position fix you must also send one of the position fix rate commands below too.
+#define PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ  "$PMTK220,10000*2F" // Once every 10 seconds, 100 millihertz.
+#define PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ  "$PMTK220,5000*1B"  // Once every 5 seconds, 200 millihertz.
+#define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F"
+#define PMTK_SET_NMEA_UPDATE_2HZ  "$PMTK220,500*2B"
+#define PMTK_SET_NMEA_UPDATE_5HZ  "$PMTK220,200*2C"
+#define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F"
+// Position fix update rate commands.
+#define PMTK_API_SET_FIX_CTL_100_MILLIHERTZ  "$PMTK300,10000,0,0,0,0*2C" // Once every 10 seconds, 100 millihertz.
+#define PMTK_API_SET_FIX_CTL_200_MILLIHERTZ  "$PMTK300,5000,0,0,0,0*18"  // Once every 5 seconds, 200 millihertz.
+#define PMTK_API_SET_FIX_CTL_1HZ  "$PMTK300,1000,0,0,0,0*1C"
+#define PMTK_API_SET_FIX_CTL_5HZ  "$PMTK300,200,0,0,0,0*2F"
+// Can't fix position faster than 5 times a second!
 #endif
+
+// $PMTK220,1000*1F<CR><LF> 1Hz
+// $PMTK220,200*2C<CR><LF> 5Hz
+// $PMTK220,100*2F<CR><LF> 10Hz
 		}
+
+
+#if 0
+// turn on only the second sentence (GPRMC)
+#define PMTK_SET_NMEA_OUTPUT_RMCONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
+// turn on GPRMC and GGA
+#define PMTK_SET_NMEA_OUTPUT_RMCGGA "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+// turn on ALL THE DATA
+#define PMTK_SET_NMEA_OUTPUT_ALLDATA "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+// turn off output
+#define PMTK_SET_NMEA_OUTPUT_OFF "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+
+// to generate your own sentences, check out the MTK command datasheet and use a checksum calculator
+// such as the awesome http://www.hhhh.org/wiml/proj/nmeaxor.html
+
+#define PMTK_LOCUS_STARTLOG  "$PMTK185,0*22"
+#define PMTK_LOCUS_STOPLOG "$PMTK185,1*23"
+#define PMTK_LOCUS_STARTSTOPACK "$PMTK001,185,3*3C"
+#define PMTK_LOCUS_QUERY_STATUS "$PMTK183*38"
+#define PMTK_LOCUS_ERASE_FLASH "$PMTK184,1*22"
+#define LOCUS_OVERLAP 0
+#define LOCUS_FULLSTOP 1
+
+#define PMTK_ENABLE_SBAS "$PMTK313,1*2E"
+#define PMTK_ENABLE_WAAS "$PMTK301,2*2E"
+
+// standby command & boot successful message
+#define PMTK_STANDBY "$PMTK161,0*28"
+#define PMTK_STANDBY_SUCCESS "$PMTK001,161,3*36"  // Not needed currently
+#define PMTK_AWAKE "$PMTK010,002*2D"
+
+// ask for the release and version
+#define PMTK_Q_RELEASE "$PMTK605*31"
+
+// request for updates on antenna status 
+#define PGCMD_ANTENNA "$PGCMD,33,1*6C" 
+#define PGCMD_NOANTENNA "$PGCMD,33,0*6D" 
+#endif
 	};
 }
