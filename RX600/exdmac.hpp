75105befe8e4af4ca593998489c88d6dc7b67b8c
hira <hira@rvf-rc45.net>
2018-10-25 02:50:32 +0900

    add: a new file

---
+++ RX600/exdmac.hpp
@@ -0,0 +1,358 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX600 グループ　EXDMAC 定義 (a)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/io_utils.hpp"
+#include "RX600/peripheral.hpp"
+#include "RX600/icu.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  exdmac core 定義
+		@param[in]	base	ベース・アドレス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <uint32_t base>
+	struct exdmac_core_t {
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 転送元アドレスレジスタ（EDMSAR）
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		static rw32_t<base + 0x00> EDMSAR;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 転送先アドレスレジスタ（EDMDAR）
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		static rw32_t<base + 0x04> EDMDAR;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 転送カウントレジスタ（EDMCRA）
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		static rw32_t<base + 0x08> EDMCRA;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA ブロック転送カウントレジスタ（EDMCRB）
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		static rw16_t<base + 0x0C> EDMCRB;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 転送モードレジスタ（EDMTMD）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmtmd_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 2>  DCTG;
+
+			bits_rw_t<io_, bitpos::B8, 2>  SZ;
+
+			bits_rw_t<io_, bitpos::B12, 2> DTS;
+			bits_rw_t<io_, bitpos::B14, 2> MD;
+		};
+		static edmtmd_t<base + 0x10> EDMTMD;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 出力設定レジスタ（EDMOMD）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmomd_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> DACKSEL;
+			bit_rw_t<io_, bitpos::B1> DACKW;
+			bit_rw_t<io_, bitpos::B2> DACKE;
+			bit_rw_t<io_, bitpos::B3> DACKS;
+		};
+		static edmomd_t<base + 0x12> EDMOMD;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 割り込み設定レジスタ（EDMINT）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmint_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> DARIE;
+			bit_rw_t<io_, bitpos::B1> SARIE;
+			bit_rw_t<io_, bitpos::B2> RPTIE;
+			bit_rw_t<io_, bitpos::B3> ESIE;
+			bit_rw_t<io_, bitpos::B4> DTIE;
+		};
+		static edmint_t<base + 0x13> EDMINT;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA アドレスモードレジスタ（EDMAMD）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmamd_t : public rw32_t<ofs> {
+			typedef rw32_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0,  4>  DARA;
+
+			bits_rw_t<io_, bitpos::B6,  2>  DM;
+			bits_rw_t<io_, bitpos::B8,  4>  SARA;
+
+			bits_rw_t<io_, bitpos::B14, 2>  SM;
+			bit_rw_t <io_, bitpos::B16>     DIR;
+			bit_rw_t <io_, bitpos::B17>     AMS;
+		};
+		static edmamd_t<base + 0x14> EDMAMD;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 転送許可レジスタ（EDMCNT）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmcnt_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> DTE;
+		};
+		static edmcnt_t<base + 0x1C> EDMCNT;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA ソフトウェア起動レジスタ（EDMREQ）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmreq_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> SWREQ;
+			bit_rw_t<io_, bitpos::B4> CLRS;
+		};
+		static edmreq_t<base + 0x1D> EDMREQ;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA ステータスレジスタ（EDMSTS）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmsts_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> ESIF;
+			bit_rw_t<io_, bitpos::B4> DTIF;
+
+			bit_ro_t<io_, bitpos::B7> ACT;
+		};
+		static edmsts_t<base + 0x1E> EDMSTS;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 外部要求センスモードレジスタ（EDMRMD）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmrmd_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bits_rw_t<io_, bitpos::B0, 2> DREQS;
+		};
+		static edmrmd_t<base + 0x20> EDMRMD;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 外部要求フラグレジスタ（EDMERF）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmerf_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> EREQ;
+		};
+		static edmerf_t<base + 0x21> EDMERF;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA 周辺要求フラグレジスタ（EDMPRF）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmprf_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> PREQ;
+		};
+		static edmprf_t<base + 0x22> EDMPRF;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA モジュール起動レジスタ（EDMAST）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		template <uint32_t ofs>
+		struct edmast_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B0> DMST;
+		};
+		static edmast_t<0x00082A00> EDMAST;
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  クラスタバッファレジスタ y（CLSBRy）（y = 0 ～ 7）
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		static rw32_t<0x00082BE0> CLSBR0;
+		static rw32_t<0x00082BE4> CLSBR1;
+		static rw32_t<0x00082BE8> CLSBR2;
+		static rw32_t<0x00082BEC> CLSBR3;
+		static rw32_t<0x00082BF0> CLSBR4;
+		static rw32_t<0x00082BF4> CLSBR5;
+		static rw32_t<0x00082BF8> CLSBR6;
+		static rw32_t<0x00082BFC> CLSBR7;
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  edmac 定義
+		@param[in]	base	ベース・アドレス
+		@param[in]	t		ペリフェラル型
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <uint32_t base, peripheral t>
+	struct exdmac0_t : public exdmac_core_t<base> {
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  EXDMA オフセットレジスタ（EDMOFR）
+			@param[in]	ofs	オフセット
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		static rw32_t<base + 0x18> EDMOFR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ペリフェラル型を返す
+			@return ペリフェラル型
+		*/
+		//-----------------------------------------------------------------//
+		static peripheral get_peripheral() { return t; }
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  edmac 定義
+		@param[in]	base	ベース・アドレス
+		@param[in]	t		ペリフェラル型
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <uint32_t base, peripheral t>
+	struct exdmac1_t : public exdmac_core_t<base> {
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ペリフェラル型を返す
+			@return ペリフェラル型
+		*/
+		//-----------------------------------------------------------------//
+		static peripheral get_peripheral() { return t; }
+	};
+
+	typedef exdmac0_t<0x00082800, peripheral::EXDMAC0>  EXDMAC0;
+	typedef exdmac1_t<0x00082840, peripheral::EXDMAC1>  EXDMAC1;
+}
