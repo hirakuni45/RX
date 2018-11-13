f739e08132e0e84066774bb0e09b75bcbe7b373a
hira <hira@rvf-rc45.net>
2018-11-14 02:13:02 +0900

    update: RX63T

--- FIRST_sample/main.cpp
+++ FIRST_sample/main.cpp
@@ -7,6 +7,9 @@
 			RX65N (Renesas Envision kit RX65N): @n
 					12MHz のベースクロックを使用する @n
 			　　　　P70 に接続された LED を利用する @n
+			RX63T @n
+					12MHz のベースクロックを使用する @n
+					PB7 に接続された LED を利用する @n
 			RX24T: @n
 					10MHz のベースクロックを使用する @n
 			　　　　P00 ピンにLEDを接続する
@@ -31,6 +34,9 @@ namespace {
 #elif defined(SIG_RX65N)
 	typedef device::system_io<12000000> SYSTEM_IO;
 	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+#elif defined(SIG_RX63T)
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORTB, device::bitpos::B7> LED;
 #elif defined(SIG_RX24T)
 	typedef device::system_io<10000000> SYSTEM_IO;
 	typedef device::PORT<device::PORT0, device::bitpos::B0> LED;
