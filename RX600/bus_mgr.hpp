a457f424a22c5be3cb51e248c35469cef6de70bd
hira <hira@rvf-rc45.net>
2018-11-14 08:22:07 +0900

    add: a new file

---
+++ RX600/bus_mgr.hpp
@@ -0,0 +1,26 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX64M グループ・バス・マネージャー
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "RX600/bus.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  バス定義基底クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct bus_mgr {
+
+
+
+
+	};
+}
