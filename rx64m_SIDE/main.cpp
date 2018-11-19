219999650d641aedea572a494904c8e73063178f
hira <hira@rvf-rc45.net>
2018-11-19 13:10:27 +0900

    update: cleanup

--- rx64m_SIDE/main.cpp
+++ rx64m_SIDE/main.cpp
@@ -15,32 +15,71 @@
 
 #include "side/arcade.h"
 
+#include "../RTK5_RAYTRACER/raytracer.hpp"
+
 namespace {
 
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B7> LED;
+	typedef device::SCI1 SCI_CH;
+
+	typedef utils::fixed_fifo<char, 512> RXB;  // RX (RECV) バッファの定義
+	typedef utils::fixed_fifo<char, 256> TXB;  // TX (SEND) バッファの定義
+
+	typedef device::sci_io<SCI_CH, RXB, TXB> SCI;
+// SCI ポートの第二候補を選択する場合
+//	typedef device::sci_io<SCI_CH, RXB, TXB, device::port_map::option::SECOND> SCI;
+	SCI		sci_;
+
+	typedef device::cmt_io<device::CMT0> CMT;
+	CMT		cmt_;
+
 	InvadersMachine side_;
-
-	class cmt_task {
-	public:
-		void operator() () {
-		}
-	};
-
-	device::cmt_io<device::CMT0, cmt_task>  cmt_;
-
-	typedef utils::fixed_fifo<char, 128> buffer;
-	device::sci_io<device::SCI1, buffer, buffer> sci_;
-
 }
 
 extern "C" {
+
+	void draw_pixel(int x, int y, int r, int g, int b)
+	{
+		volatile uint16_t c = (static_cast<uint16_t>(r & 0xf8) << 8)
+				   | (static_cast<uint16_t>(g & 0xfc) << 3)
+				   | (static_cast<uint16_t>(b & 0xf8) >> 3);
+//		tft_.plot(x, y, c);
+	}
+
+	void draw_text(int x, int y, const char* t)
+	{
+//		render_.fill_box(x, y, strlen(t) * 8, 16, render_.get_back_color());
+//		render_.draw_text(x, y, t);
+	}
+
+	uint32_t millis(void)
+	{
+		// x 16.6667
+//		return (glcdc_io_.get_vpos_count() * 4267) >> 8;
+		return cmt_.get_counter() * 10;
+	}
+
+	// syscalls.c から呼ばれる、標準出力（stdout, stderr）
 	void sci_putch(char ch)
 	{
 		sci_.putch(ch);
 	}
 
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+	// syscalls.c から呼ばれる、標準入力（stdin）
 	char sci_getch(void)
 	{
 		return sci_.getch();
+	}
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
 	}
 }
 
@@ -48,33 +87,46 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
+	SYSTEM_IO::setup_system_clock();
 
-	device::SYSTEM::MOSCWTCR = 9;	// 1ms wait
-	// メインクロック強制発振とドライブ能力設定
-	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV2.b(0b10)
-						  | device::SYSTEM::MOFCR.MOFXIN.b(1);			
-	device::SYSTEM::MOSCCR.MOSTP = 0;		// メインクロック発振器動作
-	while(device::SYSTEM::OSCOVFSR.MOOVF() == 0) asm("nop");
+	{  // タイマー設定（100Hz）
+		uint8_t intr = 4;
+		cmt_.start(100, intr);
+	}
 
-	device::SYSTEM::PLLCR.STC = 0b010011;		// PLL 10 倍(120MHz)
-	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
-	while(device::SYSTEM::OSCOVFSR.PLOVF() == 0) asm("nop");
+	{  // SCI の開始
+		uint8_t intr = 2;        // 割り込みレベル
+		uint32_t baud = 115200;  // ボーレート
+		sci_.start(baud, intr);
+	}
 
-	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (120/1=120)
-						  | device::SYSTEM::SCKCR.BCK.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (120/1=120)
-						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKC.b(1)		// 1/2 (120/2=60)
-						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
-	device::SYSTEM::SCKCR2.UCK = 0b0100;  // USB Clock: 1/5 (120/5=24)
-	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
+	utils::format("Start SIDE sample\n");	
 
-	device::PORT0::PDR.B7 = 1; // output
+	utils::format("PLLCR.STC: %d\n") % static_cast<uint16_t>(device::SYSTEM::PLLCR.STC());
+	utils::format("ICK: %d\n") % static_cast<uint16_t>(device::SYSTEM::SCKCR.ICK());
+
+	doRaytrace(4, 320, 240);
+
+
+	LED::DIR = 1;
+	uint8_t cnt = 0;
 	while(1) {
+		cmt_.sync();
 
+		++cnt;
+		if(cnt >= 50) {
+			cnt = 0;
+		}
+		if(cnt < 25) {
+			LED::P = 0;
+		} else {
+			LED::P = 1;
+		}
 
-
+		// エコーバック処理
+		while(sci_length() > 0) {
+			auto ch = sci_getch();
+			sci_putch(ch);
+		}
 	}
 }
