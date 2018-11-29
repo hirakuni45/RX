3e94e7962bd3fe0b9f1b43b018481ed65c53f0e3
hira <hira@rvf-rc45.net>
2018-11-29 23:45:37 +0900

    update: cleanup include path

--- RX65x/s12adf.hpp
+++ RX65x/s12adf.hpp
@@ -3,16 +3,12 @@
 /*!	@file
 	@brief	RX65X グループ・S12ADFa 定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
 //=====================================================================//
-#include "common/io_utils.hpp"
-#include "RX600/peripheral.hpp"
-#include "RX600/port.hpp"
-#include "RX600/mpc.hpp"
-#include "RX600/icu.hpp"
+#include "common/device.hpp"
 
 namespace device {
 
