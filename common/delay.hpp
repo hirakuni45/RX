4ec37c114444ab7fe063c9677f76015e0be37918
hira <hira@rvf-rc45.net>
2018-12-02 18:02:52 +0900

    update: delay count for RX24T

--- common/delay.hpp
+++ common/delay.hpp
@@ -57,11 +57,11 @@ namespace utils {
 					asm("nop");
 				}
 #elif defined(SIG_RX24T)
-				for(uint32_t n = 0; n < (F_ICLK / 5333333); ++n) {
+				for(uint32_t n = 0; n < (F_ICLK / 4444444); ++n) {
 					asm("nop");
 				}
 #elif defined(SIG_RX66T)
-				for(uint32_t n = 0; n < (F_ICLK / 5333333); ++n) {
+				for(uint32_t n = 0; n < (F_ICLK / 4444444); ++n) {
 					asm("nop");
 				}
 #else
