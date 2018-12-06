d485b31d4e497a5cad3cf5b2f26a3d6704ce3175
hira <hira@rvf-rc45.net>
2018-12-07 01:37:01 +0900

    add: a new file

---
+++ RTK5_GAMEEMU/main.cpp
@@ -0,0 +1,476 @@
+//=====================================================================//
+/*! @file
+    @brief  RX65N GAMEEMU @n
+			・GAME Emulator @n
+			・SD カードを使えるようにする必要がある。@n
+			・オーディオとして、DA0、DA1 出力を繋ぐ必要がある。
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+#include "common/fixed_fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/format.hpp"
+#include "common/command.hpp"
+#include "common/spi_io2.hpp"
+#include "common/sdc_man.hpp"
+#include "common/tpu_io.hpp"
+#include "sound/sound_out.hpp"
+#include "graphics/font8x16.hpp"
+#include "graphics/kfont.hpp"
+#include "graphics/graphics.hpp"
+#include "graphics/filer.hpp"
+
+#include "chip/FAMIPAD.hpp"
+
+// #include "nesemu.hpp"
+
+namespace {
+
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+// オーディオの D/A として使用
+//	typedef device::PORT<device::PORT0, device::bitpos::B5> SW2;
+
+	// Famicon PAD (CMOS 4021B Shift Register)
+	// 電源は、微小なので、接続を簡単に行う為、ポートを使う
+	typedef device::PORT<device::PORT6, device::bitpos::B0> PAD_VCC;
+	typedef device::PORT<device::PORT6, device::bitpos::B1> PAD_GND;
+	typedef device::PORT<device::PORT6, device::bitpos::B2> PAD_P_S;
+	typedef device::PORT<device::PORT6, device::bitpos::B5> PAD_CLK;
+	typedef device::PORT<device::PORT7, device::bitpos::B3> PAD_OUT;
+	typedef chip::FAMIPAD<PAD_P_S, PAD_CLK, PAD_OUT> FAMIPAD;
+	FAMIPAD		famipad_;
+
+	typedef device::system_io<12000000> SYSTEM_IO;
+
+	typedef utils::fixed_fifo<char, 256> RECV_BUFF;
+	typedef utils::fixed_fifo<char, 256> SEND_BUFF;
+	typedef device::sci_io<device::SCI9, RECV_BUFF, SEND_BUFF> SCI;
+	SCI		sci_;
+
+	typedef device::PORT<device::PORT6, device::bitpos::B3> LCD_DISP;
+	typedef device::PORT<device::PORT6, device::bitpos::B6> LCD_LIGHT;
+	typedef device::glcdc_io<device::GLCDC, 480, 272,
+		device::glcdc_def::PIX_TYPE::RGB565> GLCDC_IO;
+	GLCDC_IO	glcdc_io_;
+
+	// カード電源制御は使わない場合、「device::NULL_PORT」を指定する。
+//	typedef device::PORT<device::PORT6, device::bitpos::B4> SDC_POWER;
+	typedef device::NULL_PORT SDC_POWER;
+
+#ifdef SDHI_IF
+	// RX65N Envision Kit の SDHI ポートは、候補３になっている
+	typedef fatfs::sdhi_io<device::SDHI, SDC_POWER, device::port_map::option::THIRD> SDHI;
+	SDHI	sdh_;
+#else
+	// Soft SDC 用　SPI 定義（SPI）
+	typedef device::PORT<device::PORT2, device::bitpos::B2> MISO;  // DAT0
+	typedef device::PORT<device::PORT2, device::bitpos::B0> MOSI;  // CMD
+	typedef device::PORT<device::PORT2, device::bitpos::B1> SPCK;  // CLK
+
+	typedef device::spi_io2<MISO, MOSI, SPCK> SPI;  ///< Soft SPI 定義
+
+	SPI		spi_;
+
+	typedef device::PORT<device::PORT1, device::bitpos::B7> SDC_SELECT;  // DAT3 カード選択信号
+	typedef device::PORT<device::PORT2, device::bitpos::B5> SDC_DETECT;  // CD   カード検出
+
+	typedef fatfs::mmc_io<SPI, SDC_SELECT, SDC_POWER, SDC_DETECT> MMC;   // ハードウェアー定義
+
+	MMC		sdh_(spi_, 20000000);
+#endif
+	typedef utils::sdc_man SDC;
+	SDC		sdc_;
+
+	volatile uint32_t	wpos_;
+
+	/// DMAC 終了割り込み
+	class dmac_term_task {
+	public:
+		void operator() () {
+			device::DMAC0::DMCNT.DTE = 1;  // DMA を再スタート
+			wpos_ = 0;
+		}
+	};
+
+	typedef device::dmac_mgr<device::DMAC0, dmac_term_task> DMAC_MGR;
+	DMAC_MGR	dmac_mgr_;
+
+	uint32_t get_wave_pos_() { return (dmac_mgr_.get_count() & 0x3ff) ^ 0x3ff; }
+
+	typedef device::R12DA DAC;
+	typedef device::dac_out<DAC> DAC_OUT;
+	DAC_OUT		dac_out_;
+
+	typedef utils::sound_out<1024, 512> SOUND_OUT;
+	SOUND_OUT	sound_out_;
+
+	class tpu_task {
+	public:
+		void operator() () {
+			uint32_t tmp = wpos_;
+			++wpos_;
+			if((tmp ^ wpos_) & 64) {
+				sound_out_.service(64);
+			}
+		}
+	};
+
+	typedef device::tpu_io<device::TPU0, tpu_task> TPU0;
+	TPU0		tpu0_;
+
+	typedef graphics::font8x16 AFONT;
+	typedef graphics::kfont<16, 16> KFONT;
+	KFONT		kfont_;
+
+	typedef graphics::render<uint16_t, 480, 272, AFONT, KFONT> RENDER;
+	RENDER		render_(reinterpret_cast<uint16_t*>(0x00000000), kfont_);
+
+	typedef graphics::filer<SDC, RENDER> FILER;
+	FILER		filer_(sdc_, render_);
+
+//	emu::nesemu		nesemu_;
+
+//	utils::command<256> cmd_;
+
+	uint8_t			fami_pad_data_;
+
+	bool check_mount_() {
+		auto f = sdc_.get_mount();
+		return f;
+	}
+
+#if 0
+	void command_()
+	{
+		if(!cmd_.service()) {
+			return;
+		}
+
+		uint8_t cmdn = cmd_.get_words();
+		if(cmdn >= 1) {
+			bool f = false;
+			if(cmd_.cmp_word(0, "dir")) {  // dir [xxx]
+				if(check_mount_()) {
+					if(cmdn >= 2) {
+						char tmp[128];
+						cmd_.get_word(1, sizeof(tmp), tmp);
+						sdc_.dir(tmp);
+					} else {
+						sdc_.dir("");
+					}
+				}
+				f = true;
+			} else if(cmd_.cmp_word(0, "cd")) {  // cd [xxx]
+				if(check_mount_()) {
+					if(cmdn >= 2) {
+						char tmp[128];
+						cmd_.get_word(1, sizeof(tmp), tmp);
+						sdc_.cd(tmp);						
+					} else {
+						sdc_.cd("/");
+					}
+				}
+				f = true;
+			} else if(cmd_.cmp_word(0, "pwd")) { // pwd
+				utils::format("%s\n") % sdc_.get_current();
+				f = true;
+			} else if(cmd_.cmp_word(0, "nes")) {
+				if(check_mount_()) {
+					if(cmdn >= 2) {
+						char tmp[128];
+						cmd_.get_word(1, sizeof(tmp), tmp);
+///						f = nesemu_.open(tmp);						
+					}
+				}
+			} else if(cmd_.cmp_word(0, "help")) {
+				utils::format("    dir [path]\n");
+				utils::format("    cd [path]\n");
+				utils::format("    pwd\n");
+				utils::format("    nes path\n");
+				f = true;
+			}
+			if(!f) {
+				char tmp[128];
+				if(cmd_.get_word(0, sizeof(tmp), tmp)) {
+					utils::format("Command error: '%s'\n") % tmp;
+				}
+			}
+		}
+	}
+#endif
+
+#if 0
+	void update_nesemu_()
+	{
+		void* org = reinterpret_cast<void*>(0x00000000);
+		nesemu_.service(org, glcdc_io_.get_xsize(), glcdc_io_.get_ysize());
+
+		uint32_t len = nesemu_.get_audio_len();
+		const uint16_t* wav = nesemu_.get_audio_buf();
+		for(uint32_t i = 0; i < len; ++i) {
+			while((sound_out_.at_fifo().size() - sound_out_.at_fifo().length()) < 8) {
+			}
+			sound::wave_t t;
+			t.l_ch = t.r_ch = *wav++;
+			sound_out_.at_fifo().put(t);
+		}
+	}
+#endif
+}
+
+extern "C" {
+
+	uint8_t get_fami_pad()
+	{
+		return fami_pad_data_;
+	}
+
+
+	void set_sample_rate(uint32_t freq)
+	{
+		uint8_t intr_level = 5;
+		if(!tpu0_.start(freq, intr_level)) {
+			utils::format("TPU0 start error...\n");
+		}
+	}
+
+
+    int emu_log(const char* text)
+    {
+//        emu::tools::put(text);
+		sci_puts(text);
+        return 0;
+    }
+
+
+	void sci_putch(char ch)
+	{
+		sci_.putch(ch);
+	}
+
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+
+
+	DSTATUS disk_initialize(BYTE drv) {
+		return sdh_.disk_initialize(drv);
+	}
+
+
+	DSTATUS disk_status(BYTE drv) {
+		return sdh_.disk_status(drv);
+	}
+
+
+	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_read(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_write(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
+		return sdh_.disk_ioctl(drv, ctrl, buff);
+	}
+
+
+	DWORD get_fattime(void) {
+		time_t t = 0;
+///		rtc_.get_time(t);
+		return utils::str::get_fattime(t);
+	}
+
+
+	void utf8_to_sjis(const char* src, char* dst, uint32_t len) {
+		utils::str::utf8_to_sjis(src, dst, len);
+	}
+
+
+	int fatfs_get_mount() {
+		return sdc_.get_mount();
+	}
+
+
+	int make_full_path(const char* src, char* dst, uint16_t len)
+	{
+		return sdc_.make_full_path(src, dst, len);
+	}
+}
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	SYSTEM_IO::setup_system_clock();
+
+	{  // 内臓１２ビット D/A の設定
+		bool amp_ena = true;
+		dac_out_.start(DAC_OUT::output::CH0_CH1, amp_ena);
+		dac_out_.out0(0x8000);
+		dac_out_.out1(0x8000);
+	}
+
+	{  // SCI 設定
+		static const uint8_t sci_level = 2;
+		sci_.start(115200, sci_level);
+	}
+
+	{  // SD カード・クラスの初期化
+		sdh_.start();
+		sdc_.start();
+	}
+
+	// 波形メモリーの無音状態初期化
+	sound_out_.mute();
+
+	{  // サンプリング・タイマー設定（初期 44.1KHz ）
+//		set_sample_rate(44100);
+		set_sample_rate(22050);
+	}
+
+	{  // DMAC マネージャー開始
+		uint8_t intr_level = 4;
+		auto ret = dmac_mgr_.start(tpu0_.get_intr_vec(), DMAC_MGR::trans_type::SP_DN_32,
+			reinterpret_cast<uint32_t>(sound_out_.get_wave()), DAC::DADR0.address(),
+			sound_out_.size(), intr_level);
+		if(!ret) {
+			utils::format("DMAC Not start...\n");
+		}
+	}
+
+	utils::format("\rRTK5RX65N Start for NES Emulator sample\n");
+//	cmd_.set_prompt("# ");
+
+	{  // GLCDC 初期化
+		LCD_DISP::DIR  = 1;
+		LCD_LIGHT::DIR = 1;
+		LCD_DISP::P  = 0;  // DISP Disable
+		LCD_LIGHT::P = 0;  // BackLight Disable (No PWM)
+		if(glcdc_io_.start()) {
+			utils::format("Start GLCDC\n");
+			LCD_DISP::P  = 1;  // DISP Enable
+			LCD_LIGHT::P = 1;  // BackLight Enable (No PWM)
+			if(!glcdc_io_.control(GLCDC_IO::CONTROL_CMD::START_DISPLAY)) {
+				utils::format("GLCDC Ctrl Fail\n");
+			}
+		} else {
+			utils::format("GLCDC Fail\n");
+		}
+	}
+
+	LED::DIR = 1;
+
+	{
+		PAD_VCC::DIR = 1;
+		PAD_VCC::P = 1;
+		PAD_GND::DIR = 1;
+		PAD_GND::P = 0;
+		bool pullup = true;
+		famipad_.start(pullup);
+	}
+
+//	nesemu_.start();
+
+	uint8_t n = 0;
+	uint8_t flt = 0;
+	bool filer = false;
+	bool mount = check_mount_();
+	while(1) {
+		glcdc_io_.sync_vpos();
+		fami_pad_data_ = famipad_.update();
+
+		uint8_t data = fami_pad_data_;
+		if(chip::on(data, chip::FAMIPAD_ST::SELECT) && chip::on(data, chip::FAMIPAD_ST::START)) {
+			++flt;
+		} else {
+			flt = 0;
+		}
+
+		uint32_t ctrl = 0;
+		if(flt >= 120) {
+			sound_out_.mute();
+			graphics::set(graphics::filer_ctrl::OPEN, ctrl);
+			filer = true;
+			flt = 0;
+		}
+
+		{
+			bool m = check_mount_();
+			if(!mount && m) {
+				render_.fill_box(480-24, 0, 24, 16, 0x0000);
+			}
+			if(mount && !m) {
+				render_.draw_text(480-24, 0, "?SD");
+				filer = false;
+			}
+			mount = m;
+		}
+
+		if(filer) {
+//			if(chip::on(data, chip::FAMIPAD_ST::SELECT)) {
+//				graphics::set(graphics::filer_ctrl::OPEN, ctrl);
+//			}
+			if(chip::on(data, chip::FAMIPAD_ST::UP)) {
+				graphics::set(graphics::filer_ctrl::UP, ctrl);
+			}
+			if(chip::on(data, chip::FAMIPAD_ST::DOWN)) {
+				graphics::set(graphics::filer_ctrl::DOWN, ctrl);
+			}
+			if(chip::on(data, chip::FAMIPAD_ST::LEFT)) {
+				graphics::set(graphics::filer_ctrl::BACK, ctrl);
+			}
+			if(chip::on(data, chip::FAMIPAD_ST::RIGHT)) {
+				graphics::set(graphics::filer_ctrl::SELECT, ctrl);
+			}
+			char path[256];
+			if(filer_.update(ctrl, path, sizeof(path))) {
+				char tmp[256];
+				sdc_.make_full_path(path, tmp, sizeof(tmp));
+///				nesemu_.close();
+///				if(nesemu_.open(tmp)) {
+///					filer = false;
+///				} else {
+///					render_.draw_dialog(400, 80, "Not NES file");
+///				}
+			}
+		} else {
+///			update_nesemu_();
+		}
+
+		sdc_.service(sdh_.service());
+
+//		command_();
+
+		++n;
+		if(n >= 30) {
+			n = 0;
+		}
+		if(n < 10) {
+			LED::P = 0;
+		} else {
+			LED::P = 1;
+		}
+	}
+}
