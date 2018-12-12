f348fc6a63e5f87d4df741e0d1ea3de76e581f3d
hira <hira@rvf-rc45.net>
2018-12-12 13:03:36 +0900

    update: interrupt vector, RX66T

--- RX600/doc.hpp
+++ RX600/doc.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX24T グループ・DOC 定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
