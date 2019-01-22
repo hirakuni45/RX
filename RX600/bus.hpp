695467d8104f6f6bdeee8cc36f71e8c1f89ac3ec
hira <hira@rvf-rc45.net>
2019-01-23 01:26:19 +0900

    update: cleanup

--- RX600/bus.hpp
+++ RX600/bus.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX64M/RX71M/RX65N/RX651/RX66T バス定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2016 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2016, 2018 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
