bf3f8cad0db66ae3b21f9c21cc25886052651685
hira <hira@rvf-rc45.net>
2019-04-03 03:14:21 +0900

    update: cleanup

--- graphics/tgl.hpp
+++ graphics/tgl.hpp
@@ -3,34 +3,54 @@
 /*!	@file
 	@brief	Tiny 3D Glaphics Library (Tiny OpenGL)
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2018, 2019 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
 //=====================================================================//
 #include "common/vtx.hpp"
+#include "graphics/color.hpp"
+#include "graphics/glmatrix.hpp"
 
 namespace graphics {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief	TinyGL class
-		@param[in]	VNUM	頂点バッファ数
+		@param[in]	RDR		レンダークラス
+		@param[in]	VNUM	最大頂点数
+		@param[in]	PNUM	最大プリミティブ数
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template<uint32_t VNUM>
+	template<class RDR, uint32_t VNUM, uint32_t PNUM>
 	class tgl {
+	public:
 
-		union vtx_t {
-			vtx::spos	s2;
-			vtx::ipos	i2;
-			vtx::fpos	f2;
-			vtx::svtx	s3;
-			vtx::ivtx	i3;
-			vtx::fvtx	f3;
+		enum class PTYPE {
+			NONE,
+			POINTS,
+			LINES,
+			LINE_STRIP,
+			LINE_LOOP,
 		};
-		vtx_t		vtx_[VNUM];
-		uint32_t	vidx_;
+
+	private:
+		RDR&		rdr_;
+
+		uint32_t	vtx_idx_;
+		vtx::ivtx4	vtxs_[VNUM];
+
+		struct dt_t {
+			PTYPE		pt_;
+			uint32_t	org_;
+			uint32_t	len_;
+			dt_t() : pt_(PTYPE::NONE), org_(0), len_(0) { }
+		};
+
+		uint32_t	dt_idx_;
+		dt_t		dts_[PNUM];
+
+		share_color	color_;
 
 	public:
 		//-----------------------------------------------------------------//
@@ -38,7 +58,53 @@ namespace graphics {
 			@brief	コンストラクター
 		*/
 		//-----------------------------------------------------------------//
-		tgl() : vidx_(0) { }
+		tgl(RDR& rdr) : rdr_(rdr),
+			vtx_idx_(0), vtxs_{ 0.0f },
+			dt_idx_(0), dts_{},
+			color_()
+		{ }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	描画開始
+			@param[in]	pt	描画タイプ
+		*/
+		//-----------------------------------------------------------------//
+		void begin(PTYPE pt)
+		{
+			if(dt_idx_ >= PNUM) return;
+
+			dts_[dt_idx_].pt_ = pt;
+			dts_[dt_idx_].org_ = vtx_idx_;
+			dts_[dt_idx_].len_ = 0;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	描画終了
+		*/
+		//-----------------------------------------------------------------//
+		void end()
+		{
+			if(dts_[dt_idx_].org_ == vtx_idx_) {
+				return;
+			}
+			dts_[dt_idx_].len = vtx_idx_ - dts_[dt_idx_].org_;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	色設定
+			@param[in]	c	カラー
+		*/
+		//-----------------------------------------------------------------//
+		void color(const share_color& c)
+		{
+			color_ = c;
+		}
 
 
 		//-----------------------------------------------------------------//
@@ -49,8 +115,12 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void vertex(const vtx::spos& v)
 		{
-			if(vidx_ >= VNUM) return;
-			vtx_[vidx_].s2 = v;
+			if(vtx_idx_ >= VNUM) return;
+			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
+			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
+			vtxs_[vtx_idx_].z = 0.0f;
+			vtxs_[vtx_idx_].w = 1.0f;
+			++vtx_idx_;
 		}
 
 
@@ -62,6 +132,12 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void vertex(const vtx::ipos& v)
 		{
+			if(vtx_idx_ >= VNUM) return;
+			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
+			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
+			vtxs_[vtx_idx_].z = 0.0f;
+			vtxs_[vtx_idx_].w = 1.0f;
+			++vtx_idx_;
 		}
 
 
@@ -73,6 +149,12 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void vertex(const vtx::fpos& v)
 		{
+			if(vtx_idx_ >= VNUM) return;
+			vtxs_[vtx_idx_].x = v.x;
+			vtxs_[vtx_idx_].y = v.y;
+			vtxs_[vtx_idx_].z = 0.0f;
+			vtxs_[vtx_idx_].w = 1.0f;
+			++vtx_idx_;
 		}
 
 
@@ -84,6 +166,12 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void vertex(const vtx::svtx& v)
 		{
+			if(vtx_idx_ >= VNUM) return;
+			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
+			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
+			vtxs_[vtx_idx_].z = static_cast<float>(v.z);
+			vtxs_[vtx_idx_].w = 1.0f;
+			++vtx_idx_;
 		}
 
 
@@ -95,6 +183,12 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void vertex(const vtx::ivtx& v)
 		{
+			if(vtx_idx_ >= VNUM) return;
+			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
+			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
+			vtxs_[vtx_idx_].z = static_cast<float>(v.z);
+			vtxs_[vtx_idx_].w = 1.0f;
+			++vtx_idx_;
 		}
 
 
@@ -106,9 +200,42 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void vertex(const vtx::fvtx& v)
 		{
+			if(vtx_idx_ >= VNUM) return;
+			vtxs_[vtx_idx_].x = v.x;
+			vtxs_[vtx_idx_].y = v.y;
+			vtxs_[vtx_idx_].z = v.z;
+			vtxs_[vtx_idx_].w = 1.0f;
+			++vtx_idx_;
 		}
 
 
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	レンダリング
+		*/
+		//-----------------------------------------------------------------//
+		void renderring()
+		{
+			for(uint32_t i = 0; i < dt_idx_; ++i) {
+				const auto& t = dts_[i];
+				auto org = t.org_;
+				auto len = t.len_;
+				switch(t.pt_) {
+				case PTYPE::POINTS:
+					break;
+				case PTYPE::LINES:
+					break;
+				case PTYPE::LINE_STRIP:
+					break;
+				case PTYPE::LINE_LOOP:
+					break;
+				default:
+					break;
+				}
+			}
 
+			dt_idx_ = 0;
+			vtx_idx_ = 0;
+		}
 	};
 }
