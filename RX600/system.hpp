83eef41f3f477d3fc69ccbec9eac4a2bc534209b
hira <hira@rvf-rc45.net>
2019-04-12 11:44:23 +0900

    update: cleanup

--- RX600/system.hpp
+++ RX600/system.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX64M/RX71M/RX651/RX65N/RX66T グループ・システム定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2016, 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2016, 2019 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
