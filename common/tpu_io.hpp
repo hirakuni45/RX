0ecbd1d20fd33e16dd01a0747ad25e6c2b261cab
hira <hira@rvf-rc45.net>
2019-04-15 15:20:12 +0900

    update: cleanup

--- common/tpu_io.hpp
+++ common/tpu_io.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX600 グループ・TPU I/O 制御
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2017 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2017, 2019 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
