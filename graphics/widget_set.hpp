c40af532fdb61be3aa4ab0e2a564ea3fb2e7c187
hira <hira@rvf-rc45.net>
2019-04-22 05:32:25 +0900

    update: widget management

---
+++ graphics/widget_set.hpp
@@ -0,0 +1,27 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	widget 集合関係
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2019 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include <cstdint>
+
+namespace gui {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief	widget 集合
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct widget_set {
+
+		enum class ID {
+			BUTTON,
+		};
+
+	};
+}
