dedc1f05222891a631176df4db559f50c32e91e1
hira <hira@rvf-rc45.net>
2019-05-03 11:44:58 +0900

    add: a new file

---
+++ graphics/spinbox.hpp
@@ -0,0 +1,186 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	スピンボックス表示と制御
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2019 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include <functional>
+#include "graphics/widget.hpp"
+
+namespace gui {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief	スピンボックス・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct spinbox : public widget {
+
+		typedef spinbox value_type;
+
+		typedef std::function<void(uint32_t)> SELECT_FUNC_TYPE;
+
+		static const int16_t round_radius = 6;
+		static const int16_t frame_width  = 3;
+		static const int16_t box_size     = 30;		///< サイズが省略された場合の標準的なサイズ
+		static const int16_t edge_to_title = 4;
+
+	private:
+
+		SELECT_FUNC_TYPE	select_func_;
+		uint32_t			select_id_;
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	コンストラクター
+			@param[in]	loc		ロケーション
+			@param[in]	str		ボタン文字列
+		*/
+		//-----------------------------------------------------------------//
+		spinbox(const vtx::srect& loc = vtx::srect(0), const char* str = "") noexcept :
+			widget(loc, str), select_func_(), select_id_(0)
+		{
+			if(get_location().size.x <= 0) {
+				auto tlen = 0;
+				if(str != nullptr) {
+					tlen = strlen(str) * 8;
+				}
+				at_location().size.x = (frame_width + edge_to_title) * 2 + tlen + box_size * 2;
+			}
+			if(get_location().size.y <= 0) {
+				at_location().size.y = box_size;
+			}
+			insert_widget(this);
+		}
+
+
+		spinbox(const spinbox& th) = delete;
+		spinbox& operator = (const spinbox& th) = delete;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	デストラクタ
+		*/
+		//-----------------------------------------------------------------//
+		virtual ~spinbox() { remove_widget(this); }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	型整数を取得
+			@return 型整数
+		*/
+		//-----------------------------------------------------------------//
+		const char* get_name() const noexcept override { return "SpinBox"; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	ID を取得
+			@return ID
+		*/
+		//-----------------------------------------------------------------//
+		ID get_id() const noexcept override { return ID::SPINBOX; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	初期化
+		*/
+		//-----------------------------------------------------------------//
+		void init() noexcept override { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	タッチ判定を更新
+			@param[in]	pos		判定位置
+			@param[in]	num		タッチ数
+			@param[in]	slt		スライド・タイプの場合「true」
+		*/
+		//-----------------------------------------------------------------//
+		void update_touch(const vtx::spos& pos, uint16_t num) noexcept override
+		{
+			update_touch_def(pos, num);
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	選択推移
+			@param[in]	ena		無効状態にする場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		void exec_select(bool ena = true) noexcept override
+		{
+			++select_id_;
+			if(select_func_) {
+				select_func_(select_id_);
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	許可・不許可
+			@param[in]	ena		不許可の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		void enable(bool ena = true) noexcept override
+		{
+			if(ena) set_state(STATE::ENABLE);
+			else set_state(STATE::DISABLE);
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	セレクト ID の取得
+			@return	セレクト ID
+		*/
+		//-----------------------------------------------------------------//
+		uint32_t get_select_id() const noexcept { return select_id_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	セレクト関数への参照
+			@return	セレクト関数
+		*/
+		//-----------------------------------------------------------------//
+		SELECT_FUNC_TYPE& at_select_func() noexcept { return select_func_; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	描画
+			@param[in]	rdr		描画インスタンス
+		*/
+		//-----------------------------------------------------------------//
+		template<class RDR>
+		void draw(RDR& rdr) noexcept
+		{
+			auto r = vtx::srect(get_final_position(), get_location().size);
+			rdr.set_fore_color(graphics::def_color::White);
+			rdr.round_box(r, round_radius);
+			if(get_touch_state().level_) {
+				rdr.set_fore_color(graphics::def_color::Silver);
+			} else {
+				rdr.set_fore_color(graphics::def_color::Darkgray);
+			}
+			r.org  += frame_width;
+			r.size -= frame_width * 2;
+			rdr.round_box(r, round_radius - frame_width);
+
+			rdr.set_fore_color(graphics::def_color::White);
+			auto sz = rdr.at_font().get_text_size(get_title());
+			rdr.draw_text(r.org + (r.size - sz) / 2, get_title());
+		}
+	};
+}
