b10ae4a2c954a41862cb67c11fd44a074c76be74
hira <hira@rvf-rc45.net>
2019-08-11 11:51:10 +0900

    Add: a new file

---
+++ common/files.hpp
@@ -0,0 +1,11 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	ファイルス・クラス @n
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2019 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+
