03e3ccb8318247dc2fda4d8dd56c1145f335a8a0
hira <hira@rvf-rc45.net>
2019-10-26 10:42:05 +0900

    Update: Safe port mode switch

--- RX24T/port_map.hpp
+++ RX24T/port_map.hpp
@@ -83,6 +83,8 @@ namespace device {
 			case peripheral::RIIC0:
 				{
 					uint8_t sel = enable ? 0b01111 : 0;
+					PORTB::PMR.B1 = 0;
+					PORTB::PMR.B2 = 0;
 					MPC::PB1PFS.PSEL = sel;  // PB1/SCL0 (34/100)
 					MPC::PB2PFS.PSEL = sel;  // PB2/SDA0 (33/100)
 					PORTB::PMR.B1 = enable;
@@ -93,6 +95,9 @@ namespace device {
 			case peripheral::RSPI0:
 				{
 					uint8_t sel = enable ? 0b01101 : 0;
+					PORT2::PMR.B2 = 0;
+					PORT2::PMR.B3 = 0;
+					PORT2::PMR.B4 = 0;
 					MPC::P22PFS.PSEL = sel;  // P22/MISOA  (66/100)
 					MPC::P23PFS.PSEL = sel;  // P23/MOSIA  (65/100)
 					MPC::P24PFS.PSEL = sel;  // P24/RSPCKA (64/100)
@@ -105,12 +110,15 @@ namespace device {
 			case peripheral::SCI1C:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTD::PMR.B4 = 0;
 					MPC::PD4PFS.PSEL = sel;  // PD4/SCK1 (21/100)
 					PORTD::PMR.B4 = enable;
 				}
 			case peripheral::SCI1:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTD::PMR.B3 = 0;
+					PORTD::PMR.B5 = 0;
 					MPC::PD3PFS.PSEL = sel;  // PD3/TXD1 (22/100)
 					MPC::PD5PFS.PSEL = sel;  // PD5/RXD1 (20/100)
 					PORTD::PMR.B3 = enable;
@@ -121,12 +129,15 @@ namespace device {
 			case peripheral::SCI5C:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTB::PMR.B7 = 0;
 					MPC::PB7PFS.PSEL = sel;  // PB7/SCK5 (26/100)
 					PORTB::PMR.B7 = enable;
 				}
 			case peripheral::SCI5:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTB::PMR.B5 = 0;
+					PORTB::PMR.B6 = 0;
 					MPC::PB5PFS.PSEL = sel;  // PB5/TXD5 (28/100)
 					MPC::PB6PFS.PSEL = sel;  // PB6/RXD5 (27/100)
 					PORTB::PMR.B5 = enable;
@@ -137,12 +148,15 @@ namespace device {
 			case peripheral::SCI6C:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTA::PMR.B4 = 0;
 					MPC::PA4PFS.PSEL = sel;  // PA4/SCK6 (37/100)
 					PORTA::PMR.B4 = enable;
 				}
 			case peripheral::SCI6:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTB::PMR.B0 = 0;
+					PORTA::PMR.B5 = 0;
 					MPC::PB0PFS.PSEL = sel;  // PB0/TXD6 (35/100)
 					MPC::PA5PFS.PSEL = sel;  // PA5/RXD6 (36/100)
 					PORTB::PMR.B0 = enable;
@@ -170,6 +184,8 @@ namespace device {
 			case peripheral::SCI5:  // Version B device only
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTE::PMR.B0 = 0;
+					PORTD::PMR.B7 = 0;
 					MPC::PE0PFS.PSEL = sel;  // PE0/TXD5 (17/100)
 					MPC::PD7PFS.PSEL = sel;  // PD7/RXD5 (18/100)
 					PORTE::PMR.B0 = enable;
@@ -180,12 +196,15 @@ namespace device {
 			case peripheral::SCI6C:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTB::PMR.B3 = 0;
 					MPC::PB3PFS.PSEL = sel;  // PB3/SCK6 (32/100)
 					PORTB::PMR.B3 = enable;
 				}
 			case peripheral::SCI6:
 				{
 					uint8_t sel = enable ? 0b01010 : 0;
+					PORTB::PMR.B2 = 0;
+					PORTB::PMR.B1 = 0;
 					MPC::PB2PFS.PSEL = sel;  // PB2/TXD6 (33/100)
 					MPC::PB1PFS.PSEL = sel;  // PB1/RXD6 (34/100)
 					PORTB::PMR.B2 = enable;
@@ -251,18 +270,22 @@ namespace device {
 				sel = ena ? 0b00001 : 0;
 				switch(ch) {
 				case channel::A:
+					PORTB::PMR.B3 = 0;
 					MPC::PB3PFS.PSEL = sel;  // MTIOC0A (32/100)
 					PORTB::PMR.B3 = ena;
 					break;
 				case channel::B:
+					PORTB::PMR.B2 = 0;
 					MPC::PB2PFS.PSEL = sel;  // MTIOC0B (33/100)
 					PORTB::PMR.B2 = ena;
 					break;
 				case channel::C:
+					PORTB::PMR.B1 = 0;
 					MPC::PB1PFS.PSEL = sel;  // MTIOC0C (34/100)
 					PORTB::PMR.B1 = ena;
 					break;
 				case channel::D:
+					PORTB::PMR.B0 = 0;
 					MPC::PB0PFS.PSEL = sel;  // MTIOC0D (35/100)
 					PORTB::PMR.B0 = ena;
 					break;
@@ -276,24 +299,30 @@ namespace device {
 				switch(ch) {
 				case channel::A:
 					sel = ena ? 0b00001 : 0;
+					PORTA::PMR.B5 = 0;
 					MPC::PA5PFS.PSEL = sel;  // MTIOC1A (36/100)
 					PORTA::PMR.B5 = ena;
 					break;
 				case channel::B:
 					sel = ena ? 0b00001 : 0;
+					PORTA::PMR.B4 = 0;
 					MPC::PA4PFS.PSEL = sel;  // MTIOC1B (37/100)
 					PORTA::PMR.B4 = ena;
 					break;
 				case channel::CLK_AB:
 					sel = ena ? 0b00010 : 0;
 					if(opt == option::FIRST) {
+						PORT3::PMR.B3 = 0;
 						MPC::P33PFS.PSEL = sel;
 						PORT3::PMR.B3 = ena;
+						PORT3::PMR.B2 = 0;
 						MPC::P32PFS.PSEL = sel;
 						PORT3::PMR.B2 = ena;
 					} else if(opt == option::SECOND) {
+						PORT2::PMR.B1 = 0;
 						MPC::P21PFS.PSEL = sel;
 						PORT2::PMR.B1 = ena;
+						PORT2::PMR.B0 = 0;
 						MPC::P20PFS.PSEL = sel;
 						PORT2::PMR.B0 = ena;
 					} else {
@@ -308,24 +337,30 @@ namespace device {
 
 			case peripheral::MTU2:
 				switch(ch) {
-				case channel::A:					
+				case channel::A:
+					PORTA::PMR.B3 = 0;
 					MPC::PA3PFS.PSEL = sel;  // MTIOC2A (38/100)
 					PORTA::PMR.B3 = ena;
 					break;
 				case channel::B:
+					PORTA::PMR.B2 = 0;
 					MPC::PA2PFS.PSEL = sel;  // MTIOC2B (39/100)
 					PORTA::PMR.B2 = ena;
 					break;
 				case channel::CLK_AB:
 					sel = ena ? 0b00010 : 0;
 					if(opt == option::FIRST) {
+						PORT3::PMR.B3 = 0;
 						MPC::P33PFS.PSEL = sel;  // 
 						PORT3::PMR.B3 = ena;
+						PORT3::PMR.B2 = 0;
 						MPC::P32PFS.PSEL = sel;
 						PORT3::PMR.B2 = ena;
 					} else if(opt == option::SECOND) {
+						PORT2::PMR.B1 = 0;
 						MPC::P21PFS.PSEL = sel;
 						PORT2::PMR.B1 = ena;
+						PORT2::PMR.B0 = 0;
 						MPC::P20PFS.PSEL = sel;
 						PORT2::PMR.B0 = ena;
 					} else {
@@ -335,18 +370,24 @@ namespace device {
 				case channel::CLK_CD:
 					sel = ena ? 0b00010 : 0;
 					if(opt == option::FIRST) {
+						PORT3::PMR.B1 = 0;
 						MPC::P31PFS.PSEL = sel;
 						PORT3::PMR.B1 = ena;
+						PORT3::PMR.B0 = 0;
 						MPC::P30PFS.PSEL = sel;
 						PORT3::PMR.B0 = ena;
 					} else if(opt == option::SECOND) {
+						PORT1::PMR.B1 = 0;
 						MPC::P11PFS.PSEL = sel;
 						PORT1::PMR.B1 = ena;
+						PORT1::PMR.B0 = 0;
 						MPC::P10PFS.PSEL = sel;
 						PORT1::PMR.B0 = ena;
 					} else if(opt == option::THIRD) {
+						PORTE::PMR.B4 = 0;
 						MPC::PE4PFS.PSEL = sel;
 						PORTE::PMR.B4 = ena;
+						PORTE::PMR.B3 = 0;
 						MPC::PE3PFS.PSEL = sel;
 						PORTE::PMR.B3 = ena;
 					} else {
@@ -361,19 +402,23 @@ namespace device {
 
 			case peripheral::MTU3:
 				switch(ch) {
-				case channel::A:					
+				case channel::A:
+					PORT3::PMR.B3 = 0;
 					MPC::P33PFS.PSEL = sel;  // MTIOC3A (58/100)
 					PORT3::PMR.B3 = ena;
 					break;
 				case channel::B:
+					PORT7::PMR.B1 = 0;
 					MPC::P71PFS.PSEL = sel;  // MTIOC3B (56/100)
 					PORT7::PMR.B1 = ena;
 					break;
 				case channel::C:
+					PORT3::PMR.B2 = 0;
 					MPC::P32PFS.PSEL = sel;  // MTIOC3C (59/100)
 					PORT3::PMR.B2 = ena;
 					break;
 				case channel::D:
+					PORT7::PMR.B4 = 0;
 					MPC::P74PFS.PSEL = sel;  // MTIOC3D (53/100)
 					PORT7::PMR.B4 = ena;
 					break;
@@ -385,19 +430,23 @@ namespace device {
 
 			case peripheral::MTU4:
 				switch(ch) {
-				case channel::A:					
+				case channel::A:
+					PORT7::PMR.B2 = 0;
 					MPC::P72PFS.PSEL = sel;  // MTIOC4A (55/100)
 					PORT7::PMR.B2 = ena;
 					break;
 				case channel::B:
+					PORT7::PMR.B3 = 0;
 					MPC::P73PFS.PSEL = sel;  // MTIOC4B (54/100)
 					PORT7::PMR.B3 = ena;
 					break;
 				case channel::C:
+					PORT7::PMR.B5 = 0;
 					MPC::P75PFS.PSEL = sel;  // MTIOC4C (52/100)
 					PORT7::PMR.B5 = ena;
 					break;
 				case channel::D:
+					PORT7::PMR.B6 = 0;
 					MPC::P76PFS.PSEL = sel;  // MTIOC4D (51/100)
 					PORT7::PMR.B6 = ena;
 					break;
@@ -410,26 +459,32 @@ namespace device {
 			case peripheral::MTU5:
 				switch(ch) {
 				case channel::U:
+					PORT2::PMR.B4 = 0;
 					MPC::P24PFS.PSEL = sel;  // MTIOC5U (64/100)
 					PORT2::PMR.B4 = ena;
 					break;
 				case channel::V:
+					PORT2::PMR.B3 = 0;
 					MPC::P23PFS.PSEL = sel;  // MTIOC5V (65/100)
 					PORT2::PMR.B3 = ena;
 					break;
 				case channel::W:
+					PORT2::PMR.B2 = 0;
 					MPC::P22PFS.PSEL = sel;  // MTIOC5W (66/100)
 					PORT2::PMR.B2 = ena;
 					break;
 				case channel::U2:
+					PORT8::PMR.B2 = 0;
 					MPC::P82PFS.PSEL = sel;  // MTIOC5U (96/100)
 					PORT8::PMR.B2 = ena;
 					break;
 				case channel::V2:
+					PORT8::PMR.B1 = 0;
 					MPC::P81PFS.PSEL = sel;  // MTIOC5V (97/100)
 					PORT8::PMR.B1 = ena;
 					break;
 				case channel::W2:
+					PORT8::PMR.B0 = 0;
 					MPC::P80PFS.PSEL = sel;  // MTIOC5W (98/100)
 					PORT8::PMR.B0 = ena;
 					break;
@@ -442,18 +497,22 @@ namespace device {
 			case peripheral::MTU6:
 				switch(ch) {
 				case channel::A:
+					PORTA::PMR.B1 = 0;
 					MPC::PA1PFS.PSEL = sel;  // MTIOC6A (40/100)
 					PORTA::PMR.B1 = ena;
 					break;
 				case channel::B:
+					PORT9::PMR.B5 = 0;
 					MPC::P95PFS.PSEL = sel;  // MTIOC6B (45/100)
 					PORT9::PMR.B5 = ena;
 					break;
 				case channel::C:
+					PORTA::PMR.B0 = 0;
 					MPC::PA0PFS.PSEL = sel;  // MTIOC6C (41/100)
 					PORTA::PMR.B0 = ena;
 					break;
 				case channel::D:
+					PORT9::PMR.B2 = 0;
 					MPC::P92PFS.PSEL = sel;  // MTIOC6D (48/100)
 					PORT9::PMR.B2 = ena;
 					break;
@@ -466,18 +525,22 @@ namespace device {
 			case peripheral::MTU7:
 				switch(ch) {
 				case channel::A:
+					PORT9::PMR.B4 = 0;
 					MPC::P94PFS.PSEL = sel;  // MTIOC7A (46/100)
 					PORT9::PMR.B4 = ena;
 					break;
 				case channel::B:
+					PORT9::PMR.B3 = 0;
 					MPC::P93PFS.PSEL = sel;  // MTIOC7B (47/100)
 					PORT9::PMR.B3 = ena;
 					break;
 				case channel::C:
+					PORT9::PMR.B1 = 0;
 					MPC::P91PFS.PSEL = sel;  // MTIOC7C (49/100)
 					PORT9::PMR.B1 = ena;
 					break;
 				case channel::D:
+					PORT9::PMR.B0 = 0;
 					MPC::P90PFS.PSEL = sel;  // MTIOC7D (50/100)
 					PORT9::PMR.B0 = ena;
 					break;
@@ -490,18 +553,22 @@ namespace device {
 			case peripheral::MTU9:
 				switch(ch) {
 				case channel::A:
+					PORTD::PMR.B7 = 0;
 					MPC::PD7PFS.PSEL = sel;  // MTIOC9A (18/100)
 					PORTD::PMR.B7 = ena;
 					break;
 				case channel::B:
+					PORTE::PMR.B0 = 0;
 					MPC::PE0PFS.PSEL = sel;  // MTIOC9B (17/100)
 					PORTE::PMR.B0 = ena;
 					break;
 				case channel::C:
+					PORTD::PMR.B6 = 0;
 					MPC::PD6PFS.PSEL = sel;  // MTIOC9C (19/100)
 					PORTD::PMR.B6 = ena;
 					break;
 				case channel::D:
+					PORTE::PMR.B1 = 0;
 					MPC::PE1PFS.PSEL = sel;  // MTIOC9D (16/100)
 					PORTE::PMR.B1 = ena;
 					break;
