7e31669ca96d988fb314d680e3f6096eb4e18737
hira <hira@rvf-rc45.net>
2019-10-31 10:33:20 +0900

    Add: a new file

---
+++ RTK5_AUDIO_sample/audio_gui.hpp
@@ -0,0 +1,30 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	オーディオ GUI クラス
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2019 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "graphics/widget_director.hpp"
+
+namespace app {
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+    /*!
+        @brief  AUDIO GUI クラス
+    */
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+    class audio_gui {
+
+
+
+
+
+
+	};
+}
+
+
