e62c885f918d11623118a300c7e8c5d136c90b8a
hira <hira@rvf-rc45.net>
2019-11-08 15:30:20 +0900

    Add: a new file

---
+++ CAN_sample/main.cpp
@@ -0,0 +1,146 @@
+//=====================================================================//
+/*! @file
+    @brief  CAN サンプル @n
+			RX64M, RX71M: @n
+					12MHz のベースクロックを使用する @n
+			　　　　P07 ピンにLEDを接続する @n
+			RX65N (Renesas Envision kit RX65N): @n
+					12MHz のベースクロックを使用する @n
+			　　　　P70 に接続された LED を利用する @n
+			RX24T: @n
+					10MHz のベースクロックを使用する @n
+			　　　　P00 ピンにLEDを接続する @n
+			RX66T: @n
+					10MHz のベースクロックを使用する @n
+			　　　　P00 ピンにLEDを接続する
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2019 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/renesas.hpp"
+
+#include "common/fixed_fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/cmt_io.hpp"
+
+#include "common/format.hpp"
+#include "common/input.hpp"
+
+namespace {
+
+#if defined(SIG_RX71M)
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B7> LED;
+	typedef device::SCI1 SCI_CH;
+	static const char* system_str_ = { "RX71M" };
+#elif defined(SIG_RX64M)
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B7> LED;
+	typedef device::SCI1 SCI_CH;
+	static const char* system_str_ = { "RX64M" };
+#elif defined(SIG_RX65N)
+	typedef device::system_io<12000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+	typedef device::SCI9 SCI_CH;
+	static const char* system_str_ = { "RX65N" };
+#elif defined(SIG_RX24T)
+	typedef device::system_io<10000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B0> LED;
+	typedef device::SCI1 SCI_CH;
+	static const char* system_str_ = { "RX24T" };
+#elif defined(SIG_RX66T)
+	typedef device::system_io<10000000, 160000000> SYSTEM_IO;
+	typedef device::PORT<device::PORT0, device::bitpos::B0> LED;
+	typedef device::SCI1 SCI_CH;
+	static const char* system_str_ = { "RX66T" };
+#endif
+
+	typedef utils::fixed_fifo<char, 512> RXB;  // RX (RECV) バッファの定義
+	typedef utils::fixed_fifo<char, 256> TXB;  // TX (SEND) バッファの定義
+
+	typedef device::sci_io<SCI_CH, RXB, TXB> SCI;
+// SCI ポートの第二候補を選択する場合
+//	typedef device::sci_io<SCI_CH, RXB, TXB, device::port_map::option::SECOND> SCI;
+	SCI		sci_;
+
+	typedef device::cmt_io<device::CMT0> CMT;
+	CMT			cmt_;
+}
+
+
+extern "C" {
+
+	// syscalls.c から呼ばれる、標準出力（stdout, stderr）
+	void sci_putch(char ch)
+	{
+		sci_.putch(ch);
+	}
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+	// syscalls.c から呼ばれる、標準入力（stdin）
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+}
+
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	SYSTEM_IO::setup_system_clock();
+
+	{  // タイマー設定（100Hz）
+		uint8_t intr = 4;
+		cmt_.start(100, intr);
+	}
+
+	{  // SCI の開始
+		uint8_t intr = 2;        // 割り込みレベル
+		uint32_t baud = 115200;  // ボーレート
+		sci_.start(baud, intr);
+	}
+
+	auto clk = F_ICLK / 1000000;
+	utils::format("Start CAN sample for '%s' %d[MHz]\n") % system_str_ % clk;
+
+	LED::DIR = 1;
+	LED::P = 0;
+	{  // utils::format 
+		utils::format("");
+	}
+	LED::P = 1;
+
+	uint8_t cnt = 0;
+	while(1) {
+		cmt_.sync();
+
+		// エコーバック処理
+		while(sci_length() > 0) {
+			auto ch = sci_getch();
+			sci_putch(ch);
+		}
+
+		++cnt;
+		if(cnt >= 50) {
+			cnt = 0;
+		}
+		if(cnt < 25) {
+			LED::P = 0;
+		} else {
+			LED::P = 1;
+		}
+	}
+}
