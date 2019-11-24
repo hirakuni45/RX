b41331ceb97816df0c843745ab2a631259dc0531
hirakuni45 <hira@rvf-rc45.net>
2019-11-25 04:50:11 +0900

    Update: change path for drw2d_mgr

--- RTK5_DDS/main.cpp
+++ RTK5_DDS/main.cpp
@@ -77,8 +77,8 @@ namespace {
 	static const int16_t LCD_Y = 272;
 	static void* LCD_ORG = reinterpret_cast<void*>(0x00000100);
 	static const auto PIXT = graphics::pixel::TYPE::RGB565;
-	typedef device::glcdc_io<device::GLCDC, LCD_X, LCD_Y, PIXT> GLCDC_IO;
-	GLCDC_IO	glcdc_io_(nullptr, LCD_ORG);
+	typedef device::glcdc_mgr<device::GLCDC, LCD_X, LCD_Y, PIXT> GLCDC_MGR;
+	GLCDC_MGR	glcdc_mgr_(nullptr, LCD_ORG);
 
 	typedef graphics::font8x16 AFONT;
 	AFONT		afont_;
@@ -91,9 +91,9 @@ namespace {
 	typedef graphics::font<AFONT, KFONT> FONT;
 	FONT		font_(afont_, kfont_);
 
-//	typedef device::drw2d_mgr<GLCDC_IO, FONT> RENDER;
-	typedef graphics::render<GLCDC_IO, FONT> RENDER;
-	RENDER		render_(glcdc_io_, font_);
+//	typedef device::drw2d_mgr<GLCDC_MGR, FONT> RENDER;
+	typedef graphics::render<GLCDC_MGR, FONT> RENDER;
+	RENDER		render_(glcdc_mgr_, font_);
 
 	// FT5206, SCI6 簡易 I2C 定義
 	typedef device::PORT<device::PORT0, device::bitpos::B7> FT5206_RESET;
@@ -224,11 +224,11 @@ int main(int argc, char** argv)
 		LCD_LIGHT::DIR = 1;
 		LCD_DISP::P  = 0;  // DISP Disable
 		LCD_LIGHT::P = 0;  // BackLight Disable (No PWM)
-		if(glcdc_io_.start()) {
+		if(glcdc_mgr_.start()) {
 			utils::format("Start GLCDC\n");
 			LCD_DISP::P  = 1;  // DISP Enable
 			LCD_LIGHT::P = 1;  // BackLight Enable (No PWM)
-			if(!glcdc_io_.control(GLCDC_IO::CONTROL_CMD::START_DISPLAY)) {
+			if(!glcdc_mgr_.control(GLCDC_MGR::CONTROL_CMD::START_DISPLAY)) {
 				utils::format("GLCDC ctrl fail...\n");
 			}
 		} else {
