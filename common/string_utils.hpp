9b60e1c312fe082046e6a7284a618c096fe7f289
hirakuni45 <hira@rvf-rc45.net>
2020-02-13 07:32:58 +0900

    Update: cleanup

--- common/string_utils.hpp
+++ common/string_utils.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	文字列操作ユーティリティー
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2016, 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2016, 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
