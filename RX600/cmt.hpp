5dca1664f7275b3887135309443ae80890693393
hirakuni45 <hira@rvf-rc45.net>
2020-02-13 05:23:44 +0900

    Update: cleanup

--- RX600/cmt.hpp
+++ RX600/cmt.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX600 グループ・CMT 定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2013, 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2013, 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -17,20 +17,12 @@ namespace device {
 		@brief  CMT 定義基底クラス
 		@param[in]	base	ベース・アドレス
 		@param[in]	per		ペリフェラル
-		@param[in]	INT		割り込みベクター型
+		@param[in]	VEC		ベクター型
 		@param[in]	ivec	割り込みベクター
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <uint32_t base, peripheral per, typename INT, INT ivec>
+	template <uint32_t base, peripheral per, typename VEC, VEC ivec>
 	struct cmt_t {
-
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-		/*!
-			@brief  割り込みベクターの型
-		*/
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-		typedef INT vector_type;
-
 
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 		/*!
@@ -105,7 +97,7 @@ namespace device {
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  許可
+			@brief  許可・不許可
 			@param[in]	ena		不許可の場合「false」
 		*/
 		//-----------------------------------------------------------------//
@@ -130,21 +122,22 @@ namespace device {
 
 		//-----------------------------------------------------------------//
 		/*!
+			@brief  割り込みベクターを返す
+			@return ベクター型
+		*/
+		//-----------------------------------------------------------------//
+		static auto get_ivec() noexcept { return ivec; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
 			@brief  ペリフェラル型を返す
 			@return ペリフェラル型
 		*/
 		//-----------------------------------------------------------------//
 		static peripheral get_peripheral() noexcept { return per; }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  割り込みベクターを返す
-			@return ベクター型
-		*/
-		//-----------------------------------------------------------------//
-		static INT get_ivec() noexcept { return ivec; }
 	};
+
 
 #if defined(SIG_RX24T) || defined(SIG_RX66T)
 	typedef cmt_t<0x00088002, peripheral::CMT0, ICU::VECTOR, ICU::VECTOR::CMI0> CMT0;
