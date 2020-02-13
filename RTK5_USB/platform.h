8ae4ead155cffa73d7d88b8045d848e21b92e4ff
hirakuni45 <hira@rvf-rc45.net>
2020-02-13 17:38:46 +0900

    Add: a new project

---
+++ RTK5_USB/platform.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdint.h>
+
+#define BSP_MCU_RX65N
+
+#ifndef __cplusplus
+#define bool _Bool
+#define false 0
+#define true 1
+#endif
+
+#define __evenaccess
+
+#include "r_usb/iodefine.h"
+
+// #define BSP_CFG_RTOS_USED 1
+
+
