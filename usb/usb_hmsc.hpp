9d9d16678dd135571a0e3116063dd319f2f4879e
hirakuni45 <hira@rvf-rc45.net>
2020-02-13 17:41:29 +0900

    Update: HMSC manage

--- usb/usb_hmsc.hpp
+++ usb/usb_hmsc.hpp
@@ -3,26 +3,49 @@
 /*!	@file
 	@brief	USB Host Mass Strage Class
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
 //=====================================================================//
 #include "common/renesas.hpp"
+#include "ff13c/source/ff.h"
+#include "ff13c/source/diskio.h"
 
-namespace device {
+#include "r_usb_hmsc.h"
+#include "r_usb_extern.h"
+
+namespace fatfs {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  HMSC
+		@brief  HMSC class
 		@param[in]	USB_IO	USB 制御クラス
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	template <class USB_IO>
 	class usb_hmsc {
 
+		static const uint32_t SECTOR_SIZE = 512;
+
+		typedef utils::format format;
+
 		USB_IO&		usb_;
 
+		FATFS		fatfs_;
+
+		bool		mount_;
+
+		void wait_loop_()
+		{
+			if(usb_cstd_check_schedule() == USB_FLGSET) {
+				usb_hstd_hcd_task((usb_vp_int_t) 0);
+				usb_hstd_mgr_task((usb_vp_int_t) 0);
+				usb_hhub_task((usb_vp_int_t) 0);
+				R_USB_HmscTask(); /* HMSC Task */
+			}
+			usb_cstd_scheduler();
+		}
 
 	public:
 		//-----------------------------------------------------------------//
@@ -30,7 +53,7 @@ namespace device {
 			@brief  コンストラクター
 		*/
 		//-----------------------------------------------------------------//
-		usb_hmsc(USB_IO& usb) : usb_(usb) { }
+		usb_hmsc(USB_IO& usb) : usb_(usb), fatfs_(), mount_(false) { }
 
 
 		//-----------------------------------------------------------------//
@@ -41,93 +64,186 @@ namespace device {
 		//-----------------------------------------------------------------//
 		bool start() noexcept
 		{
-
-
-
-
-
-
 			return true;
 		}
 
-#if 0
+
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  廃棄（割り込みを停止して、ユニットを停止）
-		*/
+			@brief	ステータス
+			@param[in]	drv		Physical drive nmuber (0)
+		 */
 		//-----------------------------------------------------------------//
-		void destroy() noexcept
+		DSTATUS disk_status(BYTE drv) const noexcept
 		{
-		    CMT::CMCR.CMIE = 0;
-			CMT::enable(false);
+			if (drv) return STA_NOINIT;
+
+			return mount_ ? RES_OK : RES_NOTRDY;
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  割り込みと同期
-		*/
+			@brief	初期化
+			@param[in]	drv		Physical drive nmuber (0)
+			@return SD のステータスを返す。
+		 */
 		//-----------------------------------------------------------------//
-		void sync() const noexcept
+		DSTATUS disk_initialize(BYTE drv) noexcept
 		{
-			if(level_ > 0) {
-				volatile uint32_t cnt = counter_;
-				while(cnt == counter_) sleep_();
+			return RES_OK;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	リード・セクター
+			@param[in]	drv		Physical drive nmuber (0)
+			@param[out]	buff	Pointer to the data buffer to store read data
+			@param[in]	sector	Start sector number (LBA)
+			@param[in]	count	Sector count (1..128)
+		 */
+		//-----------------------------------------------------------------//
+		DRESULT disk_read(BYTE drv, void* buff, DWORD sector, UINT count) noexcept
+		{
+			uint32_t len = count * SECTOR_SIZE;
+
+			usb_utr_t t;
+			usb_hmsc_smp_drive2_addr(drv, &t);
+
+			if(USB_FALSE == R_USB_HmscGetDevSts(drv)) {
+				return RES_ERROR;
+			}
+
+			auto err = R_USB_HmscStrgReadSector(&t, drv, static_cast<uint8_t*>(buff), sector, count, len);
+			if(USB_OK != err) {
+        		return RES_ERROR;
+    		}
+
+			uint16_t res = 0;
+			usb_utr_t* mess = nullptr;
+			do {
+				res = R_USB_HmscGetDevSts(drv);
+				wait_loop_();
+				err = USB_TRCV_MSG(USB_HSTRG_MBX, (usb_msg_t** ) &mess, (uint16_t )0);
+			} while((err != USB_OK) && (res != USB_FALSE)) ;
+
+			if(err == USB_OK) { // Complete R_USB_HmscStrgReadSector()
+				err = mess->result;
+				USB_REL_BLK(USB_HSTRG_MPL, (usb_mh_t )mess);
+			} else { // Device detach
+				wait_loop_();
+				err = USB_TRCV_MSG(USB_HSTRG_MBX, (usb_msg_t** ) & mess, (uint16_t )0);
+				if(USB_OK == err) {
+					USB_REL_BLK(USB_HSTRG_MPL, (usb_mh_t )mess);
+				}
+				err = USB_ERROR;
+			}
+
+			if(err != USB_OK) {
+				return RES_ERROR;
+			}
+
+			return RES_OK;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	ライト・セクター
+			@param[in]	drv		Physical drive nmuber (0)
+			@param[in]	buff	Pointer to the data to be written	
+			@param[in]	sector	Start sector number (LBA)
+			@param[in]	count	Sector count (1..128)
+		 */
+		//-----------------------------------------------------------------//
+		DRESULT disk_write(BYTE drv, const void* buff, DWORD sector, UINT count) noexcept
+		{
+			uint32_t len = count * SECTOR_SIZE;
+
+			usb_utr_t t;
+			usb_hmsc_smp_drive2_addr(drv, &t);
+
+			if(USB_FALSE == R_USB_HmscGetDevSts(drv)) {
+				return RES_ERROR;
+			}
+
+			auto err = R_USB_HmscStrgWriteSector(&t, drv, static_cast<const uint8_t*>(buff), sector, count, len);
+			if(USB_OK != err) {
+				return RES_ERROR;
+			}
+
+			uint16_t res = 0;
+			usb_utr_t* mess = nullptr;
+			do {
+				res = R_USB_HmscGetDevSts(drv);
+
+				wait_loop_();
+
+				err = USB_TRCV_MSG(USB_HSTRG_MBX, (usb_msg_t**) &mess, (uint16_t )0);
+			} while ((err != USB_OK) && (res != USB_FALSE));
+
+			if(USB_OK == err) {
+				err = mess->result;
+				USB_REL_BLK(USB_HSTRG_MPL, (usb_mh_t)mess);
 			} else {
-				auto ref = CMT::CMCNT();
-				while(ref <= CMT::CMCNT()) sleep_();
-				task_();
-				++counter_;
+				wait_loop_();
+				err = USB_TRCV_MSG(USB_HSTRG_MBX, (usb_msg_t**) &mess, (uint16_t )0);
+				if(err == USB_OK) {
+					USB_REL_BLK(USB_HSTRG_MPL, (usb_mh_t)mess);
+				}
+				err = USB_ERROR;
+			}
+
+			if(err != USB_OK) {
+				return RES_ERROR;
+			}
+
+			return RES_OK;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	I/O コントロール
+			@param[in]	drv		Physical drive nmuber (0)
+			@param[in]	ctrl	Control code
+			@param[in]	buff	Buffer to send/receive control data
+		 */
+		//-----------------------------------------------------------------//
+		DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) noexcept
+		{
+			return RES_OK;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	サービス
+		 */
+		//-----------------------------------------------------------------//
+		void service() noexcept
+		{
+			auto event = R_USB_GetEvent(&usb_.at_ctrl()); /* Get event code */
+			switch (event) {
+			case USB_STS_CONFIGURED:
+				format("USB: CONFIGURED\n");
+				{
+					auto st = f_mount(&fatfs_, "", 1);
+					if(st == FR_OK) mount_ = true;
+					else mount_ = false;
+				}
+				break;
+
+			case USB_STS_DETACH:
+				format("USB: DETACH\n");
+				f_mount(&fatfs_, "", 0);
+				mount_ = false;
+				break;
+
+			default :
+				break;
 			}
 		}
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  割り込みカウンターの値を設定
-			@param[in]	n	割り込みカウンターの値
-		*/
-		//-----------------------------------------------------------------//
-		static void set_counter(uint32_t n) noexcept { counter_ = n; }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  割り込みカウンターの値を取得
-			@return 割り込みカウンターの値
-		*/
-		//-----------------------------------------------------------------//
-		static uint32_t get_counter() noexcept { return counter_; }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  CMCNT レジスターの値を取得
-			@return CMCNT レジスター
-		*/
-		//-----------------------------------------------------------------//
-		uint16_t get_cmt_count() const noexcept { return CMT::CMCNT(); }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  CMCOR レジスターの値を取得
-			@return CMCOR レジスター
-		*/
-		//-----------------------------------------------------------------//
-		uint16_t get_cmp_count() const noexcept { return CMT::CMCOR(); }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  TASK クラスの参照
-			@return TASK クラス
-		*/
-		//-----------------------------------------------------------------//
-		static TASK& at_task() noexcept { return task_; }
-#endif
 	};
-
-//	template <class CMT, class TASK> volatile uint32_t cmt_io<CMT, TASK>::counter_ = 0;
-//	template <class CMT, class TASK> TASK cmt_io<CMT, TASK>::task_;
 }
--- usb/usb_io.hpp
+++ usb/usb_io.hpp
@@ -3,25 +3,70 @@
 /*!	@file
 	@brief	USB I/O 制御クラス
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
 //=====================================================================//
 #include "common/renesas.hpp"
-// #include "common/intr_utils.hpp"
-// #include "common/vect.h"
+
+#include "r_usb_basic_if.h"
+#include "r_usb_hmsc_if.h"
+
+extern "C" {
+	void usb_hstd_usb_handler(void);
+	void hw_usb_pclear_sts_resm(void);
+};
 
 namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief  USB 基本Ｉ／Ｏ制御クラス
-		@param[in]	USB	USB チャネル
+		@param[in]	USB_CH	USB チャネル
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class USB>
+	template <class USB_CH>
 	class usb_io {
+
+		typedef utils::format format;
+
+		enum class STATE : uint16_t {
+			ATTACH,
+			DATA_READY,
+			DATA_WRITE,
+			FILE_READ,
+			DETACH,
+			ERROR,
+		};
+
+		uint8_t			ilvl_;
+
+		ICU::VECTOR		ivec_;
+
+		usb_ctrl_t		ctrl_;
+
+		STATE			state_;
+
+		int32_t			event_;
+
+		uint32_t		cnt_;
+
+		static volatile uint32_t	int_cnt_;
+
+		static INTERRUPT_FUNC void i_task_()
+		{
+			usb_hstd_usb_handler(); /* Call interrupt routine */
+//			++int_cnt_;
+		}
+
+
+		static INTERRUPT_FUNC void r_task_()
+		{
+///			hw_usb_pclear_sts_resm();
+///			ICU.IPR[ilvl_] = 0x00; /* Priority Resume1=0 */
+///			ICU.IR[ilvl_] = 0; /* Interrupt Request USB_resume USBR0 Clear */
+		}
 
 	public:
 		//-----------------------------------------------------------------//
@@ -29,101 +74,67 @@ namespace device {
 			@brief  コンストラクター
 		*/
 		//-----------------------------------------------------------------//
-		usb_io() { }
+		usb_io() : ilvl_(0), ivec_(ICU::VECTOR::NONE),
+			ctrl_(), state_(STATE::DETACH),
+			event_(-1)
+		{ }
+
+
+
+		usb_ctrl_t& at_ctrl() { return ctrl_; }
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  開始
+			@param[in]	ilvl	割り込みレベル
 			@return 正常なら「true」
 		*/
 		//-----------------------------------------------------------------//
-		bool start() noexcept
+		bool start(uint8_t ilvl) noexcept
 		{
-			power_mgr::turn(USB::get_peripheral());
+			ilvl_ = ilvl;
 
+			// VBUSEN, OVERCURA ピンの設定
+			if(!port_map::turn(USB_CH::get_peripheral())) {
+				return false;
+			}
+
+			ctrl_.module    = USB_IP0;
+			ctrl_.type      = USB_HMSC;
+
+			usb_cfg_t cfg;
+			cfg.usb_speed   = USB_FS;
+			cfg.usb_mode    = USB_HOST;
+			auto ret = R_USB_Open(&ctrl_, &cfg);
+			format("R_USB_Open: (%d)\n") % static_cast<int>(ret);
 
 			return true;
 		}
 
-#if 0
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  廃棄（割り込みを停止して、ユニットを停止）
-		*/
-		//-----------------------------------------------------------------//
-		void destroy() noexcept
+
+		void enable_mod(bool ena = true) noexcept
 		{
-		    CMT::CMCR.CMIE = 0;
-			CMT::enable(false);
+			format("USB0: enable_mod (%d)...\n") % static_cast<int>(ena);
+			power_mgr::turn(USB_CH::get_peripheral(), ena);
 		}
 
 
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  割り込みと同期
-		*/
-		//-----------------------------------------------------------------//
-		void sync() const noexcept
+		void init_intr() noexcept
 		{
-			if(level_ > 0) {
-				volatile uint32_t cnt = counter_;
-				while(cnt == counter_) sleep_();
-			} else {
-				auto ref = CMT::CMCNT();
-				while(ref <= CMT::CMCNT()) sleep_();
-				task_();
-				++counter_;
-			}
+			auto no = USB_CH::get_i_vec();
+			ivec_ = icu_mgr::set_interrupt(no, i_task_, ilvl_);
+			format("USB0: init_intr(Vec: %d, No: %d, Lvl: %d)...\n")
+				% static_cast<int>(ivec_) % static_cast<int>(no) % static_cast<int>(ilvl_);
 		}
 
 
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  割り込みカウンターの値を設定
-			@param[in]	n	割り込みカウンターの値
-		*/
-		//-----------------------------------------------------------------//
-		static void set_counter(uint32_t n) noexcept { counter_ = n; }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  割り込みカウンターの値を取得
-			@return 割り込みカウンターの値
-		*/
-		//-----------------------------------------------------------------//
-		static uint32_t get_counter() noexcept { return counter_; }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  CMCNT レジスターの値を取得
-			@return CMCNT レジスター
-		*/
-		//-----------------------------------------------------------------//
-		uint16_t get_cmt_count() const noexcept { return CMT::CMCNT(); }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  CMCOR レジスターの値を取得
-			@return CMCOR レジスター
-		*/
-		//-----------------------------------------------------------------//
-		uint16_t get_cmp_count() const noexcept { return CMT::CMCOR(); }
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief  TASK クラスの参照
-			@return TASK クラス
-		*/
-		//-----------------------------------------------------------------//
-		static TASK& at_task() noexcept { return task_; }
-#endif
+		void enable_intr(bool ena = true) noexcept
+		{
+///			format("USB0: enable_intr(%d)...\n") % static_cast<int>(ena);
+			ICU::IER.enable(static_cast<uint8_t>(ivec_), ena);
+		}
 	};
 
-//	template <class CMT, class TASK> volatile uint32_t cmt_io<CMT, TASK>::counter_ = 0;
-//	template <class CMT, class TASK> TASK cmt_io<CMT, TASK>::task_;
+	template <class USB_CH> volatile uint32_t usb_io<USB_CH>::int_cnt_ = 0;
 }
