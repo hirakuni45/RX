41be2cf9820346667b1bd4741b4c39fecf7bfa2d
hirakuni45 <hira@rvf-rc45.net>
2020-04-18 01:26:19 +0900

    Update: audio manage

--- AUDIO_sample/audio_codec.hpp
+++ AUDIO_sample/audio_codec.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	オーディオ・コーデック・クラス
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018, 2019 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2018, 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
