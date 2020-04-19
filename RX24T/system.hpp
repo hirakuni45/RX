b7165c79fcbd46f04f01e3d86984ccebf12e46bf
hirakuni45 <hira@rvf-rc45.net>
2020-04-19 09:16:47 +0900

    Update: The reality of the template when it is not optimized

--- RX24T/system.hpp
+++ RX24T/system.hpp
@@ -17,11 +17,13 @@ namespace device {
 		@brief  システム定義基底クラス
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template<class _>
 	struct system_t {
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  システムクロックコントロールレジスタ（SCKCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -38,12 +40,14 @@ namespace device {
 			bits_rw_t<io_, bitpos::B24, 4> ICK;
 			bits_rw_t<io_, bitpos::B28, 4> FCK;
 		};
-		static sckcr_t<0x00080020> SCKCR;
+		typedef sckcr_t<0x00080020> SCKCR_t;
+		static SCKCR_t SCKCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  システムクロックコントロールレジスタ 3（SCKCR3）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -56,12 +60,14 @@ namespace device {
 
 			bits_rw_t<io_, bitpos::B8, 3> CKSEL;
 		};
-		static sckcr3_t<0x00080026> SCKCR3;
+		typedef sckcr3_t<0x00080026> SCKCR3_t;
+		static SCKCR3_t SCKCR3;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  PLL コントロールレジスタ（PLLCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -75,12 +81,14 @@ namespace device {
 			bits_rw_t<io_, bitpos::B0, 2> PLIDIV;
 			bits_rw_t<io_, bitpos::B8, 6> STC;
 		};
-		static pllcr_t<0x00080028> PLLCR;
+		typedef pllcr_t<0x00080028> PLLCR_t;
+		static PLLCR_t PLLCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  PLL コントロールレジスタ 2（PLLCR2）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -93,12 +101,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B0> PLLEN;
 		};
-		static pllcr2_t<0x0008002A> PLLCR2;
+		typedef pllcr2_t<0x0008002A> PLLCR2_t;
+		static PLLCR2_t PLLCR2;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  メインクロック発振器コントロールレジスタ（MOSCCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -111,12 +121,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B0> MOSTP;
 		};
-		static mosccr_t<0x00080032> MOSCCR;
+		typedef mosccr_t<0x00080032> MOSCCR_t;
+		static MOSCCR_t MOSCCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  低速オンチップオシレータコントロールレジスタ（LOCOCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -129,12 +141,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B0> LCSTP;
 		};
-		static lococr_t<0x00080034> LOCOCR;
+		typedef lococr_t<0x00080034> LOCOCR_t;
+		static LOCOCR_t LOCOCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  IWDT 専用オンチップオシレータコントロールレジスタ（ILOCOCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -147,12 +161,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B0> ILCSTP;
 		};
-		static ilococr_t<0x00080035> ILOCOCR;
+		typedef ilococr_t<0x00080035> ILOCOCR_t;
+		static ILOCOCR_t ILOCOCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  発振安定フラグレジスタ（OSCOVFSR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -166,12 +182,14 @@ namespace device {
 			bit_ro_t<ro8_t<base>, bitpos::B0> MOOVF;
 			bit_ro_t<ro8_t<base>, bitpos::B2> PLOVF;
 		};
-		static oscovfsr_t<0x0008003C> OSCOVFSR;
+		typedef oscovfsr_t<0x0008003C> OSCOVFSR_t;
+		static OSCOVFSR_t OSCOVFSR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  発振停止検出コントロールレジスタ（OSTDCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -185,12 +203,14 @@ namespace device {
 			bit_rw_t<io_, bitpos::B0> OSTDIE;
 			bit_rw_t<io_, bitpos::B7> OSTDE;
 		};
-		static ostdcr_t<0x00080040> OSTDCR;
+		typedef ostdcr_t<0x00080040> OSTDCR_t;
+		static OSTDCR_t OSTDCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  発振停止検出ステータスレジスタ（OSTDSR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -203,12 +223,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B0> OSTDF;
 		};
-		static ostdsr_t<0x00080041> OSTDSR;
+		typedef ostdsr_t<0x00080041> OSTDSR_t;
+		static OSTDSR_t OSTDSR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  メインクロック発振器ウェイトコントロールレジスタ（MOSCWTCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -221,12 +243,14 @@ namespace device {
 
 			bits_rw_t<io_, bitpos::B0, 5> MSTS;
 		};
-		static moscwtcr_t<0x000800A2> MOSCWTCR;
+		typedef moscwtcr_t<0x000800A2> MOSCWTCR_t;
+		static MOSCWTCR_t MOSCWTCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief	メインクロック発振器強制発振コントロールレジスタ（MOFCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -240,7 +264,8 @@ namespace device {
 			bit_rw_t<io_, bitpos::B5> MODRV21;
 			bit_rw_t<io_, bitpos::B6> MOSEL;
 		};
-		static mofcr_t<0x0008C293> MOFCR;
+		typedef mofcr_t<0x0008C293> MOFCR_t;
+		static MOFCR_t MOFCR;
 
 
 		//-----------------------------------------------------------------//
@@ -248,12 +273,14 @@ namespace device {
 			@brief  メモリウェイトサイクル設定レジスタ（MEMWAIT）
 		*/
 		//-----------------------------------------------------------------//
-		static rw8_t<0x00080031> MEMWAIT;
+		typedef rw8_t<0x00080031> MEMWAIT_t;
+		static MEMWAIT_t MEMWAIT;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  スタンバイコントロールレジスタ（SBYCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -266,12 +293,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B15>	SSBY;
 		};
-		static sbycr_t<0x0008000C> SBYCR;
+		typedef sbycr_t<0x0008000C> SBYCR_t;
+		static SBYCR_t SBYCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  モジュールストップコントロールレジスタ A（MSTPCRA）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -302,12 +331,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B28> MSTPA28;
 		};
-		static mstpcra_t<0x00080010> MSTPCRA;
+		typedef mstpcra_t<0x00080010> MSTPCRA_t;
+		static MSTPCRA_t MSTPCRA;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  モジュールストップコントロールレジスタ B（MSTPCRB）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -335,12 +366,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B30> MSTPB30;
 		};
-		static mstpcrb_t<0x00080014> MSTPCRB;
+		typedef mstpcrb_t<0x00080014> MSTPCRB_t;
+		static MSTPCRB_t MSTPCRB;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  モジュールストップコントロールレジスタ C（MSTPCRC）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -357,12 +390,14 @@ namespace device {
 
 			bit_rw_t<io_, bitpos::B31> DSLPE;
 		};
-		static mstpcrb_t<0x00080018> MSTPCRC;
+		typedef mstpcrb_t<0x00080018> MSTPCRC_t;
+		static MSTPCRC_t MSTPCRC;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  動作電力コントロールレジスタ（OPCCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -377,12 +412,14 @@ namespace device {
 
 			bit_rw_t <io_, bitpos::B4>    OPCMTSF;
 		};
-		static opccr_t<0x000800A0> OPCCR;
+		typedef opccr_t<0x000800A0> OPCCR_t;
+		static OPCCR_t OPCCR;
 
 
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  プロテクトレジスタ（PRCR）
+			@param[in]	base	ベースアドレス
 		*/
 		//-----------------------------------------------------------------//
 		template <uint32_t base>
@@ -398,8 +435,28 @@ namespace device {
 			bit_rw_t <io_, bitpos::B3>    PRC3;
 			bits_rw_t<io_, bitpos::B8, 8> PRKEY;
 		};
-		static prcr_t<0x000803FE> PRCR;
+		typedef prcr_t<0x000803FE> PRCR_t;
+		static PRCR_t PRCR;
 	};
+	typedef system_t<void> SYSTEM;
 
-	typedef system_t SYSTEM;
+	template<class _> typename system_t<_>::SCKCR_t system_t<_>::SCKCR;
+	template<class _> typename system_t<_>::SCKCR3_t system_t<_>::SCKCR3;
+	template<class _> typename system_t<_>::PLLCR_t system_t<_>::PLLCR;
+	template<class _> typename system_t<_>::PLLCR2_t system_t<_>::PLLCR2;
+	template<class _> typename system_t<_>::MOSCCR_t system_t<_>::MOSCCR;
+	template<class _> typename system_t<_>::LOCOCR_t system_t<_>::LOCOCR;
+	template<class _> typename system_t<_>::ILOCOCR_t system_t<_>::ILOCOCR;
+	template<class _> typename system_t<_>::OSCOVFSR_t system_t<_>::OSCOVFSR;
+	template<class _> typename system_t<_>::OSTDCR_t system_t<_>::OSTDCR;
+	template<class _> typename system_t<_>::OSTDSR_t system_t<_>::OSTDSR;
+	template<class _> typename system_t<_>::MOSCWTCR_t system_t<_>::MOSCWTCR;
+	template<class _> typename system_t<_>::MOFCR_t system_t<_>::MOFCR;
+	template<class _> typename system_t<_>::MEMWAIT_t system_t<_>::MEMWAIT;
+	template<class _> typename system_t<_>::SBYCR_t system_t<_>::SBYCR;
+	template<class _> typename system_t<_>::MSTPCRA_t system_t<_>::MSTPCRA;
+	template<class _> typename system_t<_>::MSTPCRB_t system_t<_>::MSTPCRB;
+	template<class _> typename system_t<_>::MSTPCRC_t system_t<_>::MSTPCRC;
+	template<class _> typename system_t<_>::OPCCR_t system_t<_>::OPCCR;
+	template<class _> typename system_t<_>::PRCR_t system_t<_>::PRCR;
 }
