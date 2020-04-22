641c6e77247e6ed3fd4fac73a793d5ac74b380b3
hirakuni45 <hira@rvf-rc45.net>
2020-04-22 20:54:22 +0900

    Update: The reality of the template when it is not optimized

--- RX600/lvda.hpp
+++ RX600/lvda.hpp
@@ -1,9 +1,9 @@
 #pragma once
 //=====================================================================//
 /*!	@file
-	@brief	RX600 グループ LVDA 定義
+	@brief	RX24T/RX64M/RX71M/RX65N/RX66T/RX72M/RX72N グループ LVDA 定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2018, 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -17,6 +17,7 @@ namespace device {
 		@brief  電圧検出回路（LVDA）
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class _>
 	struct lvda_t {
 
 		//-----------------------------------------------------------------//
@@ -36,7 +37,8 @@ namespace device {
 			bits_rw_t<io_, bitpos::B0, 2>  LVD1IDTSEL;
 			bit_rw_t <io_, bitpos::B2>     LVD1IRQSEL;
 		};
-		static lvd1cr1_t<0x000800E0>  LVD1CR1;
+		typedef lvd1cr1_t<0x000800E0>  LVD1CR1_;
+		static  LVD1CR1_ LVD1CR1;
 
 
 		//-----------------------------------------------------------------//
@@ -56,7 +58,8 @@ namespace device {
 			bit_rw_t <io_, bitpos::B0>   LVD1DET;
 			bit_ro_t <io_, bitpos::B1>   LVD1MON;
 		};
-		static lvd1sr_t<0x000800E1>  LVD1SR;
+		typedef lvd1sr_t<0x000800E1>  LVD1SR_;
+		static  LVD1SR_ LVD1SR;
 
 
 		//-----------------------------------------------------------------//
@@ -76,7 +79,8 @@ namespace device {
 			bits_rw_t<io_, bitpos::B0, 2>  LVD2IDTSEL;
 			bit_rw_t <io_, bitpos::B2>     LVD2IRQSEL;
 		};
-		static lvd2cr1_t<0x000800E2>  LVD2CR1;
+		typedef lvd2cr1_t<0x000800E2>  LVD2CR1_;
+		static  LVD2CR1_ LVD2CR1;
 
 
 		//-----------------------------------------------------------------//
@@ -96,7 +100,8 @@ namespace device {
 			bit_rw_t <io_, bitpos::B0>   LVD2DET;
 			bit_ro_t <io_, bitpos::B1>   LVD2MON;
 		};
-		static lvd2sr_t<0x000800E3>  LVD2SR;
+		typedef lvd2sr_t<0x000800E3>  LVD2SR_;
+		static  LVD2SR_ LVD2SR;
 
 
 		//-----------------------------------------------------------------//
@@ -116,7 +121,8 @@ namespace device {
 			bit_rw_t<io_, bitpos::B5>   LVD1E;
 			bit_rw_t<io_, bitpos::B6>   LVD2E;
 		};
-		static lvcmpcr_t<0x0008C297>  LVCMPCR;
+		typedef lvcmpcr_t<0x0008C297>  LVCMPCR_;
+		static  LVCMPCR_ LVCMPCR;
 
 
 		//-----------------------------------------------------------------//
@@ -140,7 +146,8 @@ namespace device {
 			bits_rw_t<io_, bitpos::B4, 4>   LVD2LVL;
 #endif
 		};
-		static lvdlvlr_t<0x0008C298>  LVDLVLR;
+		typedef lvdlvlr_t<0x0008C298>  LVDLVLR_;
+		static  LVDLVLR_ LVDLVLR;
 
 
 		//-----------------------------------------------------------------//
@@ -158,18 +165,19 @@ namespace device {
 			using io_::operator &=;
 
 			bit_rw_t <io_, bitpos::B0>     LVD1RIE;
-#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T)
+#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T) || defined(SIG_RX72M) || defined(SIG_RX72N)
 			bit_rw_t <io_, bitpos::B1>     LVD1DFDIS;
 #endif
 			bit_rw_t <io_, bitpos::B2>     LVD1CMPE;
 
-#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T)
+#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T) || defined(SIG_RX72M) || defined(SIG_RX72N)
 			bits_rw_t<io_, bitpos::B4, 2>  LVD1FSAMP;
 #endif
 			bit_rw_t <io_, bitpos::B6>     LVD1RI;
 			bit_rw_t <io_, bitpos::B7>     LVD1RN;
 		};
-		static lvd1cr0_t<0x0008C29A>  LVD1CR0;
+		typedef lvd1cr0_t<0x0008C29A>  LVD1CR0_;
+		static  LVD1CR0_ LVD1CR0;
 
 
 		//-----------------------------------------------------------------//
@@ -187,18 +195,29 @@ namespace device {
 			using io_::operator &=;
 
 			bit_rw_t <io_, bitpos::B0>     LVD2RIE;
-#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T)
+#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T) || defined(SIG_RX72M) || defined(SIG_RX72N)
 			bit_rw_t <io_, bitpos::B1>     LVD2DFDIS;
 #endif
 			bit_rw_t <io_, bitpos::B2>     LVD2CMPE;
 
-#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T)
+#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX66T) || defined(SIG_RX72M) || defined(SIG_RX72N)
 			bits_rw_t<io_, bitpos::B4, 2>  LVD2FSAMP;
 #endif
 			bit_rw_t <io_, bitpos::B6>     LVD2RI;
 			bit_rw_t <io_, bitpos::B7>     LVD2RN;
 		};
-		static lvd2cr0_t<0x0008C29B>  LVD2CR0;
+		typedef lvd2cr0_t<0x0008C29B>  LVD2CR0_;
+		static  LVD2CR0_ LVD2CR0;
 	};
-	typedef lvda_t LVDA;
+	template <class _> typename lvda_t<_>::LVD1CR1_ lvda_t<_>::LVD1CR1;
+	template <class _> typename lvda_t<_>::LVD1SR_  lvda_t<_>::LVD1SR;
+	template <class _> typename lvda_t<_>::LVD2CR1_ lvda_t<_>::LVD2CR1;
+	template <class _> typename lvda_t<_>::LVD2SR_  lvda_t<_>::LVD2SR;
+	template <class _> typename lvda_t<_>::LVCMPCR_ lvda_t<_>::LVCMPCR;
+	template <class _> typename lvda_t<_>::LVDLVLR_ lvda_t<_>::LVDLVLR;
+	template <class _> typename lvda_t<_>::LVD1CR0_ lvda_t<_>::LVD1CR0;
+	template <class _> typename lvda_t<_>::LVD2CR0_ lvda_t<_>::LVD2CR0;
+
+
+	typedef lvda_t<void> LVDA;
 }
