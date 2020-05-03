14b9a3445e43b9682b9629d6eeb1e6be80a968b4
hirakuni45 <hira@rvf-rc45.net>
2020-05-04 05:24:10 +0900

    Add: The new project

---
+++ DSOS_sample/main.cpp
@@ -0,0 +1,341 @@
+//=====================================================================//
+/*! @file
+    @brief  RX65N/RX72N Envision Kit デジタル・ストレージ・オシロスコープ @n
+			マイコン内臓１２ビットＡ／Ｄ変換を使って、波形を観測するガジェット
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2018, 2020 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#define CASH_KFONT
+
+#include "common/renesas.hpp"
+#include "common/cmt_io.hpp"
+#include "common/fixed_fifo.hpp"
+#include "common/sci_io.hpp"
+#include "common/sci_i2c_io.hpp"
+#include "common/format.hpp"
+#include "common/command.hpp"
+#include "common/shell.hpp"
+#include "common/spi_io2.hpp"
+#include "common/tpu_io.hpp"
+#include "common/qspi_io.hpp"
+#include "graphics/font8x16.hpp"
+#include "graphics/graphics.hpp"
+#include "graphics/filer.hpp"
+#include "graphics/kfont.hpp"
+#include "graphics/font.hpp"
+
+#include "chip/FT5206.hpp"
+
+#include "capture.hpp"
+#include "render_wave.hpp"
+
+namespace {
+
+	static const int16_t LCD_X = 480;
+	static const int16_t LCD_Y = 272;
+	static const auto PIX = graphics::pixel::TYPE::RGB565;
+
+	typedef utils::fixed_fifo<uint8_t, 64> RB64;
+	typedef utils::fixed_fifo<uint8_t, 64> SB64;
+
+#if defined(SIG_RX65N)
+
+	static const char* sys_msg_ = { "RX65N Envision Kit" };
+
+	typedef device::system_io<12'000'000> SYSTEM_IO;
+	typedef device::PORT<device::PORT7, device::bitpos::B0> LED;
+	typedef device::SCI9 SCI_CH;
+
+	typedef device::PORT<device::PORT6, device::bitpos::B3> LCD_DISP;
+	typedef device::PORT<device::PORT6, device::bitpos::B6> LCD_LIGHT;
+	// フレームバッファ開始アドレスは、100 番地から開始とする。
+	// ※０～ＦＦは未使用領域
+	static void* LCD_ORG = reinterpret_cast<void*>(0x00000100);
+
+	// カード電源制御を使わない場合、「device::NULL_PORT」を指定する。
+	typedef device::PORT<device::PORT6, device::bitpos::B4> SDC_POWER;
+	// 書き込み禁止は使わない
+	typedef device::NULL_PORT SDC_WP;
+
+	typedef device::PORT<device::PORT0, device::bitpos::B7> FT5206_RESET;
+	typedef device::sci_i2c_io<device::SCI6, RB64, SB64, device::port_map::option::FIRST_I2C> FT5206_I2C;
+
+#elif defined(SIG_RX72N)
+
+	static const char* sys_msg_ = { "RX72N Envision Kit" };
+
+	typedef device::system_io<16'000'000> SYSTEM_IO;
+	typedef device::PORT<device::PORT4, device::bitpos::B0> LED;
+	typedef device::SCI2 SCI_CH;
+
+	// GLCDC の制御関係
+	typedef device::PORT<device::PORTB, device::bitpos::B3> LCD_DISP;
+	typedef device::PORT<device::PORT6, device::bitpos::B7> LCD_LIGHT;
+	static void* LCD_ORG = reinterpret_cast<void*>(0x0080'0000);
+
+	// SD-CARD の制御関係
+	typedef device::PORT<device::PORT4, device::bitpos::B2> SDC_POWER;
+	// 書き込み禁止は使わない
+	typedef device::NULL_PORT SDC_WP;
+
+	typedef device::PORT<device::PORT6, device::bitpos::B6> FT5206_RESET;
+	typedef device::sci_i2c_io<device::SCI6, RB64, SB64, device::port_map::option::THIRD_I2C> FT5206_I2C;
+
+#endif
+
+	typedef device::cmt_io<device::CMT0, utils::null_task> CMT;
+	CMT			cmt_;
+
+	typedef utils::fixed_fifo<char, 512>  RECV_BUFF;
+	typedef utils::fixed_fifo<char, 1024> SEND_BUFF;
+	typedef device::sci_io<SCI_CH, RECV_BUFF, SEND_BUFF> SCI;
+	SCI			sci_;
+
+	typedef device::glcdc_mgr<device::GLCDC, LCD_X, LCD_Y, PIX> GLCDC_MGR;
+	GLCDC_MGR	glcdc_mgr_(nullptr, LCD_ORG);
+
+	// RX65N/RX72N Envision Kit の SDHI は、候補３になっている
+	typedef fatfs::sdhi_io<device::SDHI, SDC_POWER, SDC_WP, device::port_map::option::THIRD> SDHI;
+	SDHI		sdh_;
+
+	typedef graphics::font8x16 AFONT;
+	AFONT		afont_;
+#ifdef CASH_KFONT
+	typedef graphics::kfont<16, 16, 64> KFONT;
+#else
+	typedef graphics::kfont<16, 16> KFONT;
+#endif
+	KFONT		kfont_;
+	typedef graphics::font<AFONT, KFONT> FONT;
+	FONT		font_(afont_, kfont_);
+
+	typedef graphics::render<GLCDC_MGR, FONT> RENDER;
+	RENDER		render_(glcdc_mgr_, font_);
+
+	typedef utils::capture<2048> CAPTURE;
+	CAPTURE		capture_;
+
+	FT5206_I2C	ft5206_i2c_;
+	typedef chip::FT5206<FT5206_I2C> FT5206;
+	FT5206		ft5206_(ft5206_i2c_);
+
+	typedef utils::render_wave<RENDER, CAPTURE, FT5206> RENDER_WAVE;
+	RENDER_WAVE	render_wave_(render_, capture_, ft5206_);
+
+	typedef utils::command<256> CMD;
+	CMD			cmd_;
+	typedef utils::shell<CMD> SHELL;
+	SHELL		shell_(cmd_);
+
+	utils::capture_trigger	trigger_ = utils::capture_trigger::NONE;
+
+
+	void update_led_()
+	{
+		static uint8_t n = 0;
+		++n;
+		if(n >= 30) {
+			n = 0;
+		}
+		if(n < 10) {
+			LED::P = 0;
+		} else {
+			LED::P = 1;
+		}
+	}
+
+
+	void command_()
+	{
+		if(!cmd_.service()) {
+			return;
+		}
+		if(shell_.analize()) {
+			return;
+		}
+		if(cmd_.cmp_word(0, "cap")) { // capture
+			trigger_ = utils::capture_trigger::SINGLE;
+			capture_.set_trigger(trigger_);			
+		} else if(cmd_.cmp_word(0, "help")) {
+			shell_.help();
+			utils::format("    cap        single trigger\n");
+		} else {
+			utils::format("Command error: '%s'\n") % cmd_.get_command();
+		}
+	}
+}
+
+
+extern "C" {
+
+	void sci_putch(char ch)
+	{
+		sci_.putch(ch);
+	}
+
+
+	void sci_puts(const char* str)
+	{
+		sci_.puts(str);
+	}
+
+
+	char sci_getch(void)
+	{
+		return sci_.getch();
+	}
+
+
+	uint16_t sci_length()
+	{
+		return sci_.recv_length();
+	}
+
+
+	DSTATUS disk_initialize(BYTE drv) {
+		return sdh_.disk_initialize(drv);
+	}
+
+
+	DSTATUS disk_status(BYTE drv) {
+		return sdh_.disk_status(drv);
+	}
+
+
+	DRESULT disk_read(BYTE drv, BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_read(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_write(BYTE drv, const BYTE* buff, DWORD sector, UINT count) {
+		return sdh_.disk_write(drv, buff, sector, count);
+	}
+
+
+	DRESULT disk_ioctl(BYTE drv, BYTE ctrl, void* buff) {
+		return sdh_.disk_ioctl(drv, ctrl, buff);
+	}
+
+
+	DWORD get_fattime(void) {
+		time_t t = 0;
+///		rtc_.get_time(t);
+		return utils::str::get_fattime(t);
+	}
+}
+
+int main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	SYSTEM_IO::setup_system_clock();
+
+	{  // SCI 設定
+		static const uint8_t sci_level = 2;
+		sci_.start(115200, sci_level);
+	}
+
+	{  // SD カード・クラスの初期化
+		sdh_.start();
+	}
+
+	{  // キャプチャー開始
+//		uint32_t freq = 2000000;  // 2 MHz
+		uint32_t freq = 100000;  // 100 KHz
+		if(!capture_.start(freq)) {
+			utils::format("Capture not start...\n");
+		}
+	}
+
+	utils::format("RTK5RX65N Start for Digital Storage Oscilloscope\n");
+	cmd_.set_prompt("# ");
+
+	{  // GLCDC の初期化
+		LCD_DISP::DIR  = 1;
+		LCD_LIGHT::DIR = 1;
+		LCD_DISP::P  = 0;  // DISP Disable
+		LCD_LIGHT::P = 0;  // BackLight Disable (No PWM)
+		if(glcdc_mgr_.start()) {
+			utils::format("Start GLCDC\n");
+			LCD_DISP::P  = 1;  // DISP Enable
+			LCD_LIGHT::P = 1;  // BackLight Enable (No PWM)
+			if(!glcdc_mgr_.control(GLCDC_MGR::CONTROL_CMD::START_DISPLAY)) {
+				utils::format("GLCDC ctrl fail...\n");
+			}
+		} else {
+			utils::format("GLCDC Fail\n");
+		}
+	}
+
+	{  // DRW2D 初期化
+		auto ver = render_.get_version();
+		utils::format("DRW2D Version: %04X\n") % ver;
+
+		if(render_.start()) {
+			utils:: format("Start DRW2D\n");
+		} else {
+			utils:: format("DRW2D Fail\n");
+		}
+	}
+
+	{  // FT5206 touch screen controller
+		FT5206::reset<FT5206_RESET>();
+		uint8_t intr_lvl = 1;
+		if(!ft5206_i2c_.start(FT5206_I2C::SPEED::STANDARD, intr_lvl)) {
+			utils::format("FT5206 I2C Start Fail...\n");
+		}
+		if(!ft5206_.start()) {
+			utils::format("FT5206 Start Fail...\n");
+		}
+	}
+
+	LED::DIR = 1;
+
+	{  // startup trigger...
+		trigger_ = utils::capture_trigger::SINGLE;
+		capture_.set_trigger(trigger_);			
+	}
+
+	// タッチパネルの安定待ち
+#if 0
+	{
+		uint8_t nnn = 0;
+		while(1) {
+			glcdc_io_.sync_vpos();
+			ft5206_.update();
+//			if(ft5206_.get_touch_num() == 0) {
+//				++nnn;
+///				if(nnn >= 60) break;
+//			} else {
+// utils::format("%d\n") % static_cast<uint16_t>(ft5206_.get_touch_num());
+//				nnn = 0;
+//			}
+		}
+	}
+#endif
+
+	while(1) {
+		glcdc_mgr_.sync_vpos();
+
+		ft5206_.update();
+
+		sdh_.service();
+
+		command_();
+
+		// タッチ操作による画面更新が必要か？
+		bool f = render_wave_.ui_service();
+
+		// 波形をキャプチャーしたら描画
+		if(f || (trigger_ != utils::capture_trigger::NONE
+			&& capture_.get_trigger() == utils::capture_trigger::NONE)) {
+			trigger_ = utils::capture_trigger::NONE;
+			render_wave_.update();
+		}
+
+		update_led_();
+	}
+}
---
+++ DSOS_sample/render_wave.hpp
@@ -0,0 +1,474 @@
+#pragma once
+//=====================================================================//
+/*! @file
+    @brief  波形描画クラス
+    @author 平松邦仁 (hira@rvf-rc45.net)
+    @copyright  Copyright (C) 2018 Kunihito Hiramatsu @n
+                Released under the MIT license @n
+                https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include <cstdint>
+#include "common/enum_utils.hpp"
+#include "common/intmath.hpp"
+#include "graphics/color.hpp"
+#include "graphics/dialog.hpp"
+
+namespace utils {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  波形描画クラス
+		@param[in]	RENDER	描画クラス
+		@param[in]	CAPTURE	キャプチャー・クラス
+		@param[in]	TOUCH	タッチ・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class RENDER, class CAPTURE, class TOUCH>
+	class render_wave {
+
+		static const int16_t GRID             = 40;
+		static constexpr float   GRID_SCALE   = 1.0f / static_cast<float>(GRID);
+		static const int16_t MENU_SIZE        = 40;
+		static const int16_t TIME_SCROLL_AREA = RENDER::font_type::height + GRID;
+		static const int16_t CH0_MOVE_AREA    = (RENDER::glc_type::width - MENU_SIZE) / 2;
+		static const int16_t CH1_MOVE_AREA    = RENDER::glc_type::width - MENU_SIZE;
+		static const int16_t TIME_BEGIN_POS   = RENDER::font_type::height;
+		static const int16_t TIME_LIMIT_POS   = RENDER::glc_type::height - RENDER::font_type::height - 1;
+		static const int16_t VOLT_BEGIN_POS   = 0;
+		static const int16_t VOLT_LIMIT_POS   = RENDER::glc_type::width - MENU_SIZE;
+
+		typedef gui::dialog<RENDER, TOUCH> DIALOG;
+
+		RENDER&		render_;
+		CAPTURE&	capture_;
+		TOUCH&		touch_;
+		DIALOG		dialog_;
+
+		int16_t		time_pos_;
+		int16_t		time_org_;
+
+		int16_t		ch0_vpos_;
+		int16_t		ch0_vorg_;
+		int16_t		ch1_vpos_;
+		int16_t		ch1_vorg_;
+
+		// 100Hz, 250Hz, 500Hz,
+		// 1KHz, 5KHz, 10KHz, 25KHz, 50KHz,
+		// 100KHz, 250KHz, 500KHz, 1MHz, 2MHz
+		uint16_t	rate_div_;
+
+		//	Volt/div "0.05", "0.1", "0.25", "0.5", "1.0", "2.5", "5.0", "10.0" 
+		uint16_t	ch0_div_;
+		uint16_t	ch1_div_;
+
+		enum class MENU : uint8_t {
+			NONE,
+			CH0,
+			CH1,
+			TRG,
+			SMP,
+			MES,
+			OPT
+		};
+
+		MENU		menu_;
+		MENU		menu_run_;
+
+		enum class MEASERE : uint8_t {
+			NONE,
+			TIME,
+			VOLT,
+		};
+		MEASERE		measere_;
+		int16_t		mes_time_begin_;
+		int16_t		mes_time_size_;
+		int16_t		mes_time_org_;
+		int16_t		mes_volt_begin_;
+		int16_t		mes_volt_size_;
+		int16_t		mes_volt_org_;
+		int16_t		mes_ref_size_;
+		int16_t		mes_ref_len_;
+
+		bool		touch_down_;
+		uint8_t		touch_num_;
+
+		typedef graphics::def_color DEF_COLOR;
+
+
+		void update_measere_() noexcept
+		{
+			render_.set_fore_color(DEF_COLOR::White);
+			if(measere_ == MEASERE::TIME) {
+				auto org = mes_time_begin_;
+				render_.line(vtx::spos(org, TIME_BEGIN_POS), vtx::spos(org, TIME_LIMIT_POS));
+				auto end = mes_time_begin_ + mes_time_size_;
+				if(0 < end && end < VOLT_LIMIT_POS) {
+					render_.line(vtx::spos(end, TIME_BEGIN_POS), vtx::spos(end, TIME_LIMIT_POS));
+				}
+			} else if(measere_ == MEASERE::VOLT) {
+				auto org = mes_volt_begin_;
+				render_.line(vtx::spos(VOLT_BEGIN_POS, org), vtx::spos(VOLT_LIMIT_POS, org));
+				auto end = mes_volt_begin_ + mes_volt_size_;
+				if(0 < end && end < VOLT_LIMIT_POS) {
+					
+					render_.line(vtx::spos(VOLT_BEGIN_POS, end), vtx::spos(VOLT_LIMIT_POS, end)); 
+				}
+			}
+		}
+
+
+		void auto_scale_(float a, char unit, char* out, uint32_t len)
+		{
+			char sc = 0;
+			if(a < 1e-6) {
+				a *= 1e9;
+				sc = 'n';
+			} else if(a < 1e-3) {
+				a *= 1e6;
+				sc = 'u';
+			} else if(a < 1.0f) {
+				a *= 1e3;
+				sc = 'm';
+			}
+			if(sc != 0) {
+				utils::sformat("%4.3f [%c%c]", out, len) % a % sc % unit;
+			} else {
+				utils::sformat("%4.3f [%c]", out, len) % a % unit;
+			}
+		}
+
+
+		void freq_scale_(float a, char* out, uint32_t len)
+		{
+			char sc = 0;
+			if(a >= 1e6) {
+				a /= 1e6;
+				sc = 'M';
+			} else if(a >= 1e3) {
+				a /= 1e3;
+				sc = 'K';
+			}
+			if(sc != 0) {
+				utils::sformat("%3.2f [%cHz]", out, len) % a % sc;
+			} else {
+				utils::sformat("%3.2f [Hz]", out, len) % a;
+			}
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクタ
+			@param[in]	render	描画クラス
+			@param[in]	capture	キャプチャー・クラス
+		*/
+		//-----------------------------------------------------------------//
+		render_wave(RENDER& render, CAPTURE& capture, TOUCH& touch) noexcept :
+			render_(render), capture_(capture), touch_(touch), dialog_(render, touch),
+			time_pos_(0), time_org_(0),
+			ch0_vpos_(0), ch0_vorg_(0), ch1_vpos_(0), ch1_vorg_(0),
+			rate_div_(11), ch0_div_(3), ch1_div_(3),
+			menu_(MENU::NONE), menu_run_(MENU::NONE),
+			measere_(MEASERE::NONE),
+			mes_time_begin_(0), mes_time_size_(40), mes_time_org_(0),
+			mes_volt_begin_(0), mes_volt_size_(40), mes_volt_org_(0),
+			mes_ref_size_(0), mes_ref_len_(0),
+			touch_down_(false), touch_num_(0)
+		{ }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  グリッドの描画
+			@param[in]	x		開始Ｘ
+			@param[in]	y		開始Ｙ
+			@param[in]	w		横幅
+			@param[in]	h		高さ
+			@param[in]	unit	ユニット・ピクセル
+		*/
+		//-----------------------------------------------------------------//
+		void draw_grid(int16_t x, int16_t y, int16_t w, int16_t h, int16_t unit) noexcept
+		{
+			render_.set_fore_color(DEF_COLOR::Aqua);
+			for(int16_t i = x; i <= (x + w); i += unit) {
+				uint32_t mask;
+				if(i == x || i == (x + w)) {
+					mask = -1;
+				} else {
+					mask = 0b11000000110000001100000011000000;
+				}
+				render_.set_stipple(mask);
+				render_.line(vtx::spos(i, y), vtx::spos(i, y + h));
+			}
+			for(int16_t i = y; i <= (y + h); i += unit) {
+				uint32_t mask;
+				if(i == y || i == (y + h)) {
+					mask = -1;
+				} else {
+					mask = 0b11000000110000001100000011000000;
+				}
+				render_.set_stipple(mask);
+				render_.line(vtx::spos(x, i), vtx::spos(x + w, i));
+			}
+			render_.set_stipple();
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  サンプリング情報描画
+			@param[in]	ch	チャネル
+		*/
+		//-----------------------------------------------------------------//
+		void draw_sampling_info() noexcept
+		{
+			static const char* freq[] = {
+				"100Hz", "250Hz", "500Hz",
+				"1KHz", "5KHz", "10KHz", "25KHz", "50KHz",
+				"100KHz", "250KHz", "500KHz", "1MHz", "2MHz"
+			};
+			static const char* rate[] = {
+				"10mS", "4mS", "2mS",
+				"1mS", "200uS", "100uS", "40uS", "20uS",
+				"10uS", "4uS", "2uS", "1uS", "500nS"
+			};
+			static const float rate_f[] = {
+				10e-3, 4e-3, 2e-3,
+				1e-3, 200e-6, 100e-6, 40e-6, 20e-6,
+				10e-6, 4e-6, 2e-6, 1e-6, 500e-9
+			};
+
+			char tmp[64];
+			utils::sformat("%s (%s)", tmp, sizeof(tmp)) % freq[rate_div_] % rate[rate_div_];
+			render_.set_fore_color(DEF_COLOR::Black);
+			render_.fill_box(vtx::srect(0, 0, 480, 16));
+			render_.set_fore_color(DEF_COLOR::White);
+			auto x = render_.draw_text(vtx::spos(0, 0), tmp);
+
+			// 計測モード時結果
+			if(measere_ == MEASERE::TIME) {
+				float a = static_cast<float>(mes_time_size_) * GRID_SCALE * rate_f[rate_div_];
+				auto_scale_(a, 'S', tmp, sizeof(tmp));
+				x = render_.draw_text(vtx::spos(x + 8, 0), tmp);
+				freq_scale_(1.0f / a, tmp, sizeof(tmp));
+				render_.draw_text(vtx::spos(x + 8, 0), tmp);
+			} else if(measere_ == MEASERE::VOLT) {
+//				float a = static_cast<float>(mes_time_size_) * GRID_SCALE * rate_f[rate_div_];
+//				utils::sformat("%4.3f", tmp, sizeof(tmp)) % a;
+//				render_.draw_text(x + 8, 0, tmp);
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  チャネル情報描画
+			@param[in]	ch	チャネル
+		*/
+		//-----------------------------------------------------------------//
+		void draw_channel_info(uint32_t ch) noexcept
+		{
+			static const char* divs[] = {
+				"0.05", "0.1", "0.25", "0.5", "1.0", "2.5", "5.0", "10.0" 
+			};
+
+			char tmp[32];
+			if(ch == 0) {
+				render_.set_fore_color(DEF_COLOR::Lime);
+				render_.fill_box(vtx::srect(  0, 272 - 16 + 1, 15, 15));
+				render_.set_fore_color(DEF_COLOR::Black);
+				render_.fill_box(vtx::srect( 16, 272 - 16 + 1, 240 - 16, 15));
+				render_.set_fore_color(DEF_COLOR::White);
+				utils::sformat("CH0: %s [V]", tmp, sizeof(tmp)) % divs[ch0_div_];
+				render_.draw_text(vtx::spos(  16, 272 - 16 + 1), tmp);
+			} else {
+				render_.set_fore_color(DEF_COLOR::Fuchsi);
+				render_.fill_box(vtx::srect(240, 272 - 16 + 1, 15, 15));
+				utils::sformat("CH1: %s [V]", tmp, sizeof(tmp)) % divs[ch1_div_];
+				render_.set_fore_color(DEF_COLOR::Black);
+				render_.fill_box(vtx::srect(240 + 16, 272 - 16 + 1, 240 - 16, 15));
+				render_.set_fore_color(DEF_COLOR::White);
+				render_.draw_text(vtx::spos(240 + 16, 272 - 16 + 1), tmp);
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  UI 操作サービス
+			@return 更新が必要なら「true」
+		*/
+		//-----------------------------------------------------------------//
+		bool ui_service() noexcept
+		{
+			auto num = touch_.get_touch_num();
+			auto num_back = touch_num_;
+			touch_num_ = num;
+			const auto& p = touch_.get_touch_pos(0);
+
+			if(num == 0) {
+				touch_down_ = false;
+				if(p.before == TOUCH::EVENT::CONTACT && p.event == TOUCH::EVENT::UP) {
+					menu_run_ = menu_;
+					menu_ = MENU::NONE;
+					if(menu_run_ == MENU::TRG) {
+						// とりあえず、シングル・トリガー
+						capture_.set_trigger(utils::capture_trigger::SINGLE);
+					} else if(menu_run_ == MENU::MES) {
+						measere_ = enum_utils::inc(measere_, MEASERE::NONE, MEASERE::VOLT);
+					}
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			if(num == 1) {
+				if(p.event == TOUCH::EVENT::DOWN) {
+					time_org_ = time_pos_;
+					ch0_vorg_ = ch0_vpos_;
+					ch1_vorg_ = ch1_vpos_;
+					mes_time_org_ = mes_time_begin_;
+					mes_volt_org_ = mes_volt_begin_;
+				} else if(p.event == TOUCH::EVENT::CONTACT) {
+					auto d = p.pos - p.org;
+					if(CH1_MOVE_AREA <= p.pos.x && p.pos.x < RENDER::glc_type::width) {
+						menu_ = static_cast<MENU>((p.pos.y - 16) / MENU_SIZE + 1);
+					} else if(measere_ == MEASERE::TIME) {
+						if(TIME_BEGIN_POS <= p.pos.y && p.pos.y < TIME_LIMIT_POS
+							&& VOLT_BEGIN_POS <= p.pos.x && p.pos.x < VOLT_LIMIT_POS) {
+							mes_time_begin_ = mes_time_org_ + d.x;
+							if(mes_time_begin_ < 0) mes_time_begin_ = 0;
+							else if(mes_time_begin_ >= 440) mes_time_begin_ = 440;
+						}
+					} else if(measere_ == MEASERE::VOLT) {
+						if(TIME_BEGIN_POS <= p.pos.y && p.pos.y < TIME_LIMIT_POS
+							&& VOLT_BEGIN_POS <= p.pos.x && p.pos.x < VOLT_LIMIT_POS) {
+							mes_volt_begin_ = mes_volt_org_ + d.y;
+							if(mes_volt_begin_ < 16) mes_volt_begin_ = 16;
+							else if(mes_volt_begin_ >= (272 - 16)) mes_volt_begin_ = 272 - 16 - 1;
+						}
+					} else {
+						if(0 <= p.pos.y && p.pos.y < TIME_SCROLL_AREA) {
+							time_pos_ = time_org_ + d.x;
+							touch_down_ = true;
+						} else if(0 <= p.pos.x && p.pos.x < CH0_MOVE_AREA) {
+							ch0_vpos_ = ch0_vorg_ + d.y;
+							touch_down_ = true;
+						} else if(CH0_MOVE_AREA <= p.pos.x && p.pos.x < CH1_MOVE_AREA) {
+							ch1_vpos_ = ch1_vorg_ + d.y;
+							touch_down_ = true;
+						}
+					}
+				}
+			} else if(num == 2) {
+				const auto& p2 = touch_.get_touch_pos(1);
+				if(measere_ == MEASERE::TIME) {
+					auto d = p.pos - p2.pos;
+					auto len = intmath::sqrt16(d.x * d.x + d.y * d.y);
+					if(p2.event == TOUCH::EVENT::DOWN) {
+						mes_ref_size_ = mes_time_size_;
+						mes_ref_len_  = len.val;
+					} else {
+						mes_time_size_ = mes_ref_size_
+							+ static_cast<int16_t>(len.val) - mes_ref_len_;
+						if(mes_time_size_ < 0) mes_time_size_ = 0;
+					}
+				} else if(measere_ == MEASERE::VOLT) {
+					auto d = p.pos - p2.pos;
+					auto len = intmath::sqrt16(d.x * d.x + d.y * d.y);
+					if(p2.event == TOUCH::EVENT::DOWN) {
+						mes_ref_size_ = mes_volt_size_;
+						mes_ref_len_  = len.val;
+					} else {
+						mes_volt_size_ = mes_ref_size_
+							+ static_cast<int16_t>(len.val) - mes_ref_len_;
+						if(mes_volt_size_ < 0) mes_volt_size_ = 0;
+					}
+				} else {
+
+					return false;
+				}
+			} else if(num == 3) {
+
+				return false;
+			}
+
+			return true;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  アップ・デート（再描画）
+		*/
+		//-----------------------------------------------------------------//
+		void update() noexcept
+		{
+			render_.set_fore_color(DEF_COLOR::Black);
+			render_.fill_box(vtx::srect(0, 16, 440, 240));
+			draw_grid(0, 16, 440, 240, GRID);
+
+			if(measere_ != MEASERE::NONE) {
+				update_measere_();
+			} else if(touch_down_) {
+				render_.set_fore_color(DEF_COLOR::Red);
+				render_.line(vtx::spos(0, TIME_SCROLL_AREA),
+							 vtx::spos(CH1_MOVE_AREA, TIME_SCROLL_AREA));				 
+				render_.line(vtx::spos(CH0_MOVE_AREA, TIME_SCROLL_AREA),
+							 vtx::spos(CH0_MOVE_AREA, 272 - 16));
+				render_.line(vtx::spos(CH1_MOVE_AREA, TIME_SCROLL_AREA),
+							 vtx::spos(CH1_MOVE_AREA, 272 - 16));
+			}
+
+			// メニュー・ボタンの描画
+			{
+				static const char* menu[] = {
+					"CH0", "CH1", "Trg", "Smp", "Mes", "Opt" };
+				for(int16_t i = 0; i < 6; ++i) {
+					auto c = DEF_COLOR::Olive;
+					if(static_cast<MENU>(i + 1) == menu_) c = DEF_COLOR::Yellow;
+					render_.set_back_color(c);
+					dialog_.square_button(vtx::srect(441, 16 + GRID * i + 1, GRID - 1, GRID - 1), menu[i]);
+				}
+			}
+
+			for(int16_t x = 0; x < (440 - 1); ++x) {
+				int16_t p0 = time_pos_ + x;
+				int16_t p1 = time_pos_ + x + 1;
+				if(p1 >= static_cast<int16_t>(CAPTURE::CAP_NUM)) {
+					break;
+				}
+				const auto& d0 = capture_.get(p0);
+				const auto& d1 = capture_.get(p1);
+				{
+					int16_t ofs = ch0_vpos_ + 272 / 2;
+					int16_t y0 = d0.ch0_;
+					y0 -= 2048;
+					y0 /= -17;
+					int16_t y1 = d1.ch0_;
+					y1 -= 2048;
+					y1 /= -17;
+					render_.set_fore_color(DEF_COLOR::Lime);
+					render_.line(vtx::spos(x, ofs + y0), vtx::spos(x + 1, ofs + y1));
+				}
+				{
+					int16_t ofs = ch1_vpos_ + 272 / 2;
+					int16_t y0 = d0.ch1_;
+					y0 -= 2048;
+					y0 /= -17;
+					int16_t y1 = d1.ch1_;
+					y1 -= 2048;
+					y1 /= -17;
+					render_.set_fore_color(DEF_COLOR::Fuchsi);
+					render_.line(vtx::spos(x, ofs + y0), vtx::spos(x + 1, ofs + y1));
+				}
+			}
+
+			draw_sampling_info();
+			draw_channel_info(0);
+			draw_channel_info(1);
+		}
+	};
+}
