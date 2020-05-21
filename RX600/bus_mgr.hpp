b8b75b7b4df75e738ef9f8af4cc2c14dde632140
hirakuni45 <hira@rvf-rc45.net>
2020-05-22 06:35:14 +0900

    Update: cleanup

--- RX600/bus_mgr.hpp
+++ RX600/bus_mgr.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	RX64M グループ・バス・マネージャー
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2018, 2020 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -15,11 +15,12 @@ namespace device {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
 		@brief  バス制御クラス
+		@param[in]	CSA		CS 空間クラス
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	struct bus_mgr {
-
-
+	template <class CSA>
+	class bus_mgr {
+	public:
 
 
 
