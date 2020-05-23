29dfe294599511b8de75b13edf0699d6cc698314
hirakuni45 <hira@rvf-rc45.net>
2020-05-24 01:28:21 +0900

    Rename: cmt_io to cmt_mgr

--- rx24t_PWM_sample/main.cpp
+++ rx24t_PWM_sample/main.cpp
@@ -9,7 +9,7 @@
 */
 //=====================================================================//
 #include "common/renesas.hpp"
-#include "common/cmt_io.hpp"
+#include "common/cmt_mgr.hpp"
 #include "common/sci_io.hpp"
 #include "common/fixed_fifo.hpp"
 #include "common/format.hpp"
@@ -17,7 +17,7 @@
 
 namespace {
 
-	device::cmt_io<device::CMT0>  cmt_;
+	device::cmt_mgr<device::CMT0>  cmt_;
 
 	typedef utils::fixed_fifo<char, 128> BUFFER;
 	device::sci_io<device::SCI1, BUFFER, BUFFER> sci_;
