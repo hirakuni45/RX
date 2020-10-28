2bb2b8ca1f1d1989fad93623032cce181377be9f
hirakuni45 <hira@rvf-rc45.net>
2020-10-28 16:58:50 +0900

    Add: a new file

---
+++ graphics/dialog.hpp
@@ -0,0 +1,157 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	ダイアログ表示と制御
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2020 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "graphics/widget.hpp"
+
+namespace gui {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief	ダイアログ・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct dialog : public widget {
+
+		typedef dialog value_type;
+
+		static const int16_t round_radius = 6;  // round radius
+
+	private:
+
+		int16_t		caption_height_;
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	コンストラクター
+			@param[in]	loc		ロケーション
+			@param[in]	str		フレーム・タイトル
+		*/
+		//-----------------------------------------------------------------//
+		dialog(const vtx::srect& loc = vtx::srect(0), const char* str = "") noexcept :
+			widget(loc, str), caption_height_(0)
+		{
+			insert_widget(this);
+		}
+
+
+		dialog(const dialog& th) = delete;
+		dialog& operator = (const dialog& th) = delete;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	デストラクタ
+		*/
+		//-----------------------------------------------------------------//
+		virtual ~dialog() noexcept { remove_widget(this); }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	型整数を取得
+			@return 型整数
+		*/
+		//-----------------------------------------------------------------//
+		const char* get_name() const noexcept override { return "Dialog"; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	ID を取得
+			@return ID
+		*/
+		//-----------------------------------------------------------------//
+		ID get_id() const noexcept override { return ID::DIALOG; }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	初期化
+		*/
+		//-----------------------------------------------------------------//
+		void init() noexcept override { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	タッチ判定を更新
+			@param[in]	pos		判定位置
+			@param[in]	num		タッチ数
+			@param[in]	slt		スライド・タイプの場合「true」
+		*/
+		//-----------------------------------------------------------------//
+		void update_touch(const vtx::spos& pos, uint16_t num) noexcept override { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	選択推移
+			@param[in]	inva	無効状態にする場合「true」
+		*/
+		//-----------------------------------------------------------------//
+		void exec_select(bool inva) noexcept override { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	許可・不許可
+			@param[in]	ena		不許可の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		void enable(bool ena = true) override
+		{
+			if(ena) set_state(STATE::ENABLE);
+			else set_state(STATE::DISABLE);
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	描画
+		*/
+		//-----------------------------------------------------------------//
+		template<class RDR>
+		void draw(RDR& rdr) noexcept
+		{
+			auto r = get_location();
+			rdr.set_fore_color(graphics::def_color::White);
+			rdr.round_box(r, round_radius);
+			if(get_touch_state().level_) {
+				rdr.set_fore_color(graphics::def_color::Silver);
+			} else {
+				rdr.set_fore_color(graphics::def_color::Darkgray);
+			}
+			r.org += 2;
+			r.size -= 4;
+			rdr.round_box(r, round_radius - 2);
+
+			rdr.set_fore_color(graphics::def_color::White);
+			auto sz = rdr.at_font().get_text_size(get_title());
+			rdr.draw_text(r.org + (r.size - sz) / 2, get_title());
+		}
+
+
+		template <class T>
+		dialog& operator + (T& th)
+		{
+			th.set_parents(this);
+			return *this;
+		}
+
+
+		template <class T>
+		dialog& operator += (T& th)
+		{
+			th.set_parents(this);
+			return *this;
+		}
+	};
+}
