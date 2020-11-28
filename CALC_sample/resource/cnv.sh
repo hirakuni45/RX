21ecc63ebe901e3879ce312f16c793bffeb63021
hirakuni45 <hira@rvf-rc45.net>
2020-11-28 10:33:18 +0900

    Add: resource

---
+++ CALC_sample/resource/cnv.sh
@@ -0,0 +1,3 @@
+bmc MathFont.png -text -c_style x_2  -header 8 -offset  0,0 -size 22,14 x_2.h
+bmc MathFont.png -text -c_style x_y  -header 8 -offset 24,0 -size 22,14 x_y.h
+bmc MathFont.png -text -c_style x_m1 -header 8 -offset 48,0 -size 22,14 x_m1.h
