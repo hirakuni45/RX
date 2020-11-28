4c78296c3cda19ff11637166d50cfdaa83ff6d91
hirakuni45 <hira@rvf-rc45.net>
2020-11-28 10:32:57 +0900

    Update: add resource

---
+++ CALC_sample/resource.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+
+#include "resource/x_2.h"
+#include "resource/x_y.h"
+#include "resource/x_m1.h"
+
+#include "resource.hpp"
+
+namespace resource {
+
+	const uint8_t* bitmap::x_2_  = x_2;
+	const uint8_t* bitmap::x_y_  = x_y;
+	const uint8_t* bitmap::x_m1_ = x_m1;
+
+}
