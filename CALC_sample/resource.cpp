b7d4085ea2fa3e7503f242ea827b7cd9a0c8e9e7
hirakuni45 <hira@rvf-rc45.net>
2020-11-30 09:47:15 +0900

    Update: cleanup

--- CALC_sample/resource.cpp
+++ CALC_sample/resource.cpp
@@ -4,6 +4,19 @@
 #include "resource/x_y.h"
 #include "resource/x_m1.h"
 
+#include "resource/s_0.h"
+#include "resource/s_1.h"
+#include "resource/s_2.h"
+#include "resource/s_3.h"
+#include "resource/s_4.h"
+#include "resource/s_5.h"
+#include "resource/s_6.h"
+#include "resource/s_7.h"
+#include "resource/s_8.h"
+#include "resource/s_9.h"
+#include "resource/s_p.h"
+#include "resource/s_m.h"
+
 #include "resource.hpp"
 
 namespace resource {
@@ -12,4 +25,16 @@ namespace resource {
 	const uint8_t* bitmap::x_y_  = x_y;
 	const uint8_t* bitmap::x_m1_ = x_m1;
 
+	const uint8_t* bitmap::s_0_  = s_0;
+	const uint8_t* bitmap::s_1_  = s_1;
+	const uint8_t* bitmap::s_2_  = s_2;
+	const uint8_t* bitmap::s_3_  = s_3;
+	const uint8_t* bitmap::s_4_  = s_4;
+	const uint8_t* bitmap::s_5_  = s_5;
+	const uint8_t* bitmap::s_6_  = s_6;
+	const uint8_t* bitmap::s_7_  = s_7;
+	const uint8_t* bitmap::s_8_  = s_8;
+	const uint8_t* bitmap::s_9_  = s_9;
+	const uint8_t* bitmap::s_p_  = s_p;
+	const uint8_t* bitmap::s_m_  = s_m;
 }
