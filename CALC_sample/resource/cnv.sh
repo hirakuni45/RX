266ec49fb9710a326be8eb47158790b7c2606c83
hirakuni45 <hira@rvf-rc45.net>
2020-11-30 09:46:52 +0900

    Update: cleanup

--- CALC_sample/resource/cnv.sh
+++ CALC_sample/resource/cnv.sh
@@ -1,3 +1,15 @@
 bmc MathFont.png -text -c_style x_2  -header 8 -offset  0,0 -size 22,14 x_2.h
 bmc MathFont.png -text -c_style x_y  -header 8 -offset 24,0 -size 22,14 x_y.h
 bmc MathFont.png -text -c_style x_m1 -header 8 -offset 48,0 -size 22,14 x_m1.h
+bmc MathFont.png -text -c_style s_0  -header 8 -offset 0*0,16 -size 5,7 s_0.h
+bmc MathFont.png -text -c_style s_1  -header 8 -offset 0*0,16 -size 5,7 s_1.h
+bmc MathFont.png -text -c_style s_2  -header 8 -offset 0*0,16 -size 5,7 s_2.h
+bmc MathFont.png -text -c_style s_3  -header 8 -offset 0*0,16 -size 5,7 s_3.h
+bmc MathFont.png -text -c_style s_4  -header 8 -offset 0*0,16 -size 5,7 s_4.h
+bmc MathFont.png -text -c_style s_5  -header 8 -offset 0*0,16 -size 5,7 s_5.h
+bmc MathFont.png -text -c_style s_6  -header 8 -offset 0*0,16 -size 5,7 s_6.h
+bmc MathFont.png -text -c_style s_7  -header 8 -offset 0*0,16 -size 5,7 s_7.h
+bmc MathFont.png -text -c_style s_8  -header 8 -offset 0*0,16 -size 5,7 s_8.h
+bmc MathFont.png -text -c_style s_9  -header 8 -offset 0*0,16 -size 5,7 s_9.h
+bmc MathFont.png -text -c_style s_p  -header 8 -offset 0*0,16 -size 5,7 s_p.h
+bmc MathFont.png -text -c_style s_m  -header 8 -offset 0*0,16 -size 5,7 s_m.h
