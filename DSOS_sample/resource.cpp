309f1f099223c888d1ba77c106c7b4b859c370f4
hirakuni45 <hira@rvf-rc45.net>
2021-01-16 00:44:19 +0900

    Add: a new file

---
+++ DSOS_sample/resource.cpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+
+#include "resource/res_dir_0.h"
+#include "resource/res_dir_1.h"
+#include "resource/res_dir_2.h"
+#include "resource/res_dir_3.h"
+
+#include "resource.hpp"
+
+namespace resource {
+
+	const uint8_t* bitmap::dir_0_  = dir_0;
+	const uint8_t* bitmap::dir_1_  = dir_1;
+	const uint8_t* bitmap::dir_2_  = dir_2;
+	const uint8_t* bitmap::dir_3_  = dir_3;
+
+}
