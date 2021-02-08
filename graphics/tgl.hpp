e1f8b0d8671641d714f8659afce178b16dea3b25
hirakuni45 <hira@rvf-rc45.net>
2021-02-08 23:46:32 +0900

    Update: cleanup

--- graphics/tgl.hpp
+++ graphics/tgl.hpp
@@ -3,7 +3,7 @@
 /*!	@file
 	@brief	Tiny 3D Glaphics Library (Tiny OpenGL)
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018, 2019 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2018, 2021 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
@@ -16,6 +16,31 @@ namespace graphics {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
+		@brief	TinyGL base class
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class tgl_base {
+	public:
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief	TinyGL Primitive Type
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class PTYPE {
+			NONE,
+			POINTS,			///< 点の描画
+			LINES,			///< 単線の描画
+			LINE_STRIP,		///< 複数線の描画
+			LINE_LOOP,		///< 閉じた線の描画
+			QUAD,			///< ４点ポリゴンの描画
+		};
+
+		typedef gl::matrixf	MATRIX;
+	};
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
 		@brief	TinyGL class
 		@param[in]	RDR		レンダークラス
 		@param[in]	VNUM	最大頂点数
@@ -23,30 +48,19 @@ namespace graphics {
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	template<class RDR, uint32_t VNUM, uint32_t PNUM>
-	class tgl {
-	public:
+	class tgl : public tgl_base {
 
-		enum class PTYPE {
-			NONE,
-			POINTS,
-			LINES,
-			LINE_STRIP,
-			LINE_LOOP,
-		};
-
-		typedef gl::matrixf	MATRIX;
-
-	private:
 		RDR&		rdr_;
 
 		uint32_t	vtx_idx_;
-		vtx::ivtx4	vtxs_[VNUM];
+		vtx::fvtx4	vtxs_[VNUM];
 
 		struct dt_t {
 			PTYPE		pt_;
+			share_color	col_;
 			uint32_t	org_;
-			uint32_t	len_;
-			dt_t() : pt_(PTYPE::NONE), org_(0), len_(0) { }
+			uint32_t	end_;
+			dt_t() : pt_(PTYPE::NONE), col_(0, 0, 0), org_(0), end_(0) { }
 		};
 
 		uint32_t	dt_idx_;
@@ -62,7 +76,7 @@ namespace graphics {
 			@brief	コンストラクター
 		*/
 		//-----------------------------------------------------------------//
-		tgl(RDR& rdr) : rdr_(rdr),
+		tgl(RDR& rdr) noexcept : rdr_(rdr),
 			vtx_idx_(0), vtxs_{},
 			dt_idx_(0), dts_{},
 			color_(0, 0, 0),
@@ -72,17 +86,39 @@ namespace graphics {
 
 		//-----------------------------------------------------------------//
 		/*!
+			@brief	開始
+		*/
+		//-----------------------------------------------------------------//
+		bool start() noexcept
+		{
+#ifdef SIG_RX72N
+			utils::format("TinyGL Start\n");
+			__init_tfu();
+			utils::format("  TFU initializations\n");
+#endif
+//	void __builtin_rx_atan2hypotf(float, float, float*, float*);
+//	float __builtin_rx_sinf(float);
+//	float __builtin_rx_cosf(float);
+//	float __builtin_rx_atan2f(float, float);
+//	float __builtin_rx_hypotf(float, float);
+			return true;
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
 			@brief	描画開始
 			@param[in]	pt	描画タイプ
 		*/
 		//-----------------------------------------------------------------//
-		void begin(PTYPE pt)
+		void begin(PTYPE pt) noexcept
 		{
 			if(dt_idx_ >= PNUM) return;
 
 			dts_[dt_idx_].pt_ = pt;
+			dts_[dt_idx_].col_ = color_;
 			dts_[dt_idx_].org_ = vtx_idx_;
-			dts_[dt_idx_].len_ = 0;
+			dts_[dt_idx_].end_ = vtx_idx_;
 		}
 
 
@@ -91,12 +127,13 @@ namespace graphics {
 			@brief	描画終了
 		*/
 		//-----------------------------------------------------------------//
-		void end()
+		void end() noexcept
 		{
 			if(dts_[dt_idx_].org_ == vtx_idx_) {
 				return;
 			}
-			dts_[dt_idx_].len_ = vtx_idx_ - dts_[dt_idx_].org_;
+			dts_[dt_idx_].end_ = vtx_idx_;
+			++dt_idx_;
 		}
 
 
@@ -106,7 +143,7 @@ namespace graphics {
 			@param[in]	c	カラー
 		*/
 		//-----------------------------------------------------------------//
-		void color(const share_color& c)
+		void color(const share_color& c) noexcept
 		{
 			color_ = c;
 		}
@@ -118,49 +155,18 @@ namespace graphics {
 			@param[in]	v	頂点
 		*/
 		//-----------------------------------------------------------------//
-		void vertex(const vtx::spos& v)
+		void vertex(float x, float y) noexcept
 		{
 			if(vtx_idx_ >= VNUM) return;
-			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
-			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
+			vtxs_[vtx_idx_].x = x;
+			vtxs_[vtx_idx_].y = y;
 			vtxs_[vtx_idx_].z = 0.0f;
 			vtxs_[vtx_idx_].w = 1.0f;
 			++vtx_idx_;
 		}
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief	頂点の登録（ipos）
-			@param[in]	v	頂点
-		*/
-		//-----------------------------------------------------------------//
-		void vertex(const vtx::ipos& v)
-		{
-			if(vtx_idx_ >= VNUM) return;
-			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
-			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
-			vtxs_[vtx_idx_].z = 0.0f;
-			vtxs_[vtx_idx_].w = 1.0f;
-			++vtx_idx_;
-		}
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief	頂点の登録（fpos）
-			@param[in]	v	頂点
-		*/
-		//-----------------------------------------------------------------//
-		void vertex(const vtx::fpos& v)
-		{
-			if(vtx_idx_ >= VNUM) return;
-			vtxs_[vtx_idx_].x = v.x;
-			vtxs_[vtx_idx_].y = v.y;
-			vtxs_[vtx_idx_].z = 0.0f;
-			vtxs_[vtx_idx_].w = 1.0f;
-			++vtx_idx_;
-		}
+		void vertex(const vtx::spos& v) noexcept { vertex(v.x, v.y); }
+		void vertex(const vtx::ipos& v) noexcept { vertex(v.x, v.y); }
+		void vertex(const vtx::fpos& v) noexcept { vertex(v.x, v.y); }
 
 
 		//-----------------------------------------------------------------//
@@ -169,49 +175,18 @@ namespace graphics {
 			@param[in]	v	頂点
 		*/
 		//-----------------------------------------------------------------//
-		void vertex(const vtx::svtx& v)
+		void vertex(float x, float y, float z) noexcept
 		{
 			if(vtx_idx_ >= VNUM) return;
-			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
-			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
-			vtxs_[vtx_idx_].z = static_cast<float>(v.z);
+			vtxs_[vtx_idx_].x = x;
+			vtxs_[vtx_idx_].y = y;
+			vtxs_[vtx_idx_].z = z;
 			vtxs_[vtx_idx_].w = 1.0f;
 			++vtx_idx_;
 		}
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief	頂点の登録（ivtx）
-			@param[in]	v	頂点
-		*/
-		//-----------------------------------------------------------------//
-		void vertex(const vtx::ivtx& v)
-		{
-			if(vtx_idx_ >= VNUM) return;
-			vtxs_[vtx_idx_].x = static_cast<float>(v.x);
-			vtxs_[vtx_idx_].y = static_cast<float>(v.y);
-			vtxs_[vtx_idx_].z = static_cast<float>(v.z);
-			vtxs_[vtx_idx_].w = 1.0f;
-			++vtx_idx_;
-		}
-
-
-		//-----------------------------------------------------------------//
-		/*!
-			@brief	頂点の登録（fvtx）
-			@param[in]	v	頂点
-		*/
-		//-----------------------------------------------------------------//
-		void vertex(const vtx::fvtx& v)
-		{
-			if(vtx_idx_ >= VNUM) return;
-			vtxs_[vtx_idx_].x = v.x;
-			vtxs_[vtx_idx_].y = v.y;
-			vtxs_[vtx_idx_].z = v.z;
-			vtxs_[vtx_idx_].w = 1.0f;
-			++vtx_idx_;
-		}
+		void vertex(const vtx::svtx& v) noexcept { vertex(v.x, v.y, v.z); }
+		void vertex(const vtx::ivtx& v) noexcept { vertex(v.x, v.y, v.z); }
+		void vertex(const vtx::fvtx& v) noexcept { vertex(v.x, v.y, v.z); }
 
 
 		//-----------------------------------------------------------------//
@@ -230,10 +205,33 @@ namespace graphics {
 		//-----------------------------------------------------------------//
 		void renderring() noexcept
 		{
+			MATRIX::matrix_type wm;
+			matrix_.world_matrix(wm);
+
+			int ox;
+			int oy;
+			int w;
+			int h;
+			matrix_.get_viewport(ox, oy, w, h);  // drw2d for fixed point
+			ox <<= 4;
+			oy <<= 4;
+			w <<= 4;
+			h <<= 4;
+
 			for(uint32_t i = 0; i < dt_idx_; ++i) {
 				const auto& t = dts_[i];
-				auto org = t.org_;
-				auto len = t.len_;
+				rdr_.set_fore_color(t.col_);
+
+				uint32_t k = 0;
+				vtx::spos tmp[16];
+				for(uint32_t j = t.org_; j < t.end_; ++j) {
+					vtx::fvtx src(vtxs_[j].x, vtxs_[j].y, vtxs_[j].z);
+					vtx::fvtx dst;
+					matrix_.vertex_screen(wm, src, dst);
+					tmp[k].x = static_cast<int16_t>(dst.x * w) + (w / 2) + ox;
+					tmp[k].y = static_cast<int16_t>(dst.y * h) + (h / 2) + oy;
+					++k;
+				}
 				switch(t.pt_) {
 				case PTYPE::POINTS:
 					break;
@@ -242,6 +240,16 @@ namespace graphics {
 				case PTYPE::LINE_STRIP:
 					break;
 				case PTYPE::LINE_LOOP:
+					for(uint32_t j = 0; j < k; ++j) {
+						auto l = j + 1;
+						if(l >= k) l = 0;
+						rdr_.line_d(tmp[j], tmp[l]);
+					}
+					break;
+				case PTYPE::QUAD:
+					for(uint32_t j = 0; j < k; j += 4) {
+						rdr_.quad_d(tmp[j+0], tmp[j+1], tmp[j+2], tmp[j+3]);
+					}
 					break;
 				default:
 					break;
