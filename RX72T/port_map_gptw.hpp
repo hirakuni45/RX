9627fa7e11778c35efd61bee7ec48c3ba9d049cc
hirakuni45 <hira@rvf-rc45.net>
2021-05-02 23:29:25 +0900

    Update: cleanup

--- RX72T/port_map_gptw.hpp
+++ RX72T/port_map_gptw.hpp
@@ -50,19 +50,19 @@ namespace device {
 		};
 
 	private:
-		static bool gptw0_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw0_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 0A:
+			/// GTIOC0A:
 			/// P12[0] ( 3/144) (--/100)
 			/// P71[0] (78/144) (56/100)
 			/// PD2[1] (28/144) (23/100)
 			/// PD7[0] (23/144) (18/100)
 			/// PG1[1] (81/144) (--/100)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B2 = 0;
@@ -98,14 +98,14 @@ namespace device {
 					break;
 				}
 				break;
-			/// 0B:
+			/// GTIOC0B:
 			/// P15[0] (144/144) (--/100)
 			/// P74[0] ( 75/144) (53/100)
 			/// PD1[1] ( 29/144) (--/100) (24/100)
 			/// PD6[0] ( 24/144) (19/100)
 			/// PG2[1] ( 81/144) (--/100)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B5 = 0;
@@ -149,19 +149,19 @@ namespace device {
 		}
 
 
-		static bool gptw1_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw1_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 1A:
+			/// GTIOC1A:
 			/// P13[0] ( 2/144) (--/100)
 			/// P72[0] (77/144) (55/100)
 			/// PD0[1] (30/144) (--/100) (25/100) 
 			/// PD5[0] (25/144) (20/100)
 			/// PK2[1] (83/144) (--/100)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B3 = 0;
@@ -197,14 +197,14 @@ namespace device {
 					break;
 				}
 				break;
-			/// 1B:
-			/// P16[0] (143/144)
+			/// GTIOC1B:
+			/// P16[0] (143/144) (111/112)
 			/// P75[0] ( 74/144)
 			/// PB7[0] ( 39/144)
 			/// PD4[0] ( 26/144)
 			/// PG0[1] ( 82/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B6 = 0;
@@ -248,19 +248,19 @@ namespace device {
 		}
 
 
-		static bool gptw2_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw2_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 2A:
+			/// GTIOC2A:
 			/// P14[0] ( 1/144)
 			/// P73[0] (76/144)
 			/// PB6[0] (40/144)
 			/// PD3[0] (27/144)
 			/// PK0[1] (85/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B4 = 0;
@@ -296,14 +296,14 @@ namespace device {
 					break;
 				}
 				break;
-			/// 2B:
+			/// GTIOC2B:
 			/// P17[0] (142/144)
 			/// P76[0] ( 73/144)
 			/// PB5[0] ( 41/144)
 			/// PD2[0] ( 28/144)
 			/// PK1[1] ( 84/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B7 = 0;
@@ -347,18 +347,18 @@ namespace device {
 		}
 
 
-		static bool gptw3_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw3_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 3A:
+			/// GTIOC3A:
 			/// P32[0] (87/144)
 			/// PD1[0] (29/144)
 			/// PD7[1] (23/144)
 			/// PE5[0] ( 5/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT3::PMR.B2 = 0;
@@ -388,13 +388,13 @@ namespace device {
 					break;
 				}
 				break;
-			/// 3B:
+			/// GTIOC3B:
 			/// P11[0] (140/144)
 			/// P33[0] ( 86/144)
 			/// PD0[0] ( 30/144)
 			/// PD6[1] ( 24/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010110 : 0b010100) : 0;
 					PORT1::PMR.B1 = 0;
@@ -432,16 +432,16 @@ namespace device {
 		}
 
 
-		static bool gptw4_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw4_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 4A:
+			/// GTIOC4A:
 			/// P71[1] (78/144)
 			/// P95[0] (67/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 					PORT7::PMR.B1 = 0;
@@ -459,11 +459,11 @@ namespace device {
 					break;
 				}
 				break;
-			/// 4B:
+			/// GTIOC4B:
 			/// P74[1] (75/144)
 			/// P92[0] (70/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 					PORT7::PMR.B4 = 0;
@@ -489,16 +489,16 @@ namespace device {
 		}
 
 
-		static bool gptw5_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw5_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 5A:
+			/// GTIOC5A:
 			/// P72[1] (77/144)
 			/// P94[0] (68/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 					PORT7::PMR.B2 = 0;
@@ -516,11 +516,11 @@ namespace device {
 					break;
 				}
 				break;
-			/// 5B:
+			/// GTIOC5B:
 			/// P75[1] (74/144)
 			/// P91[0] (71/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 					PORT7::PMR.B5 = 0;
@@ -546,16 +546,16 @@ namespace device {
 		}
 
 
-		static bool gptw6_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw6_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel;
 			switch(ch) {
-			/// 6A:
+			/// GTIOC6A:
 			/// P73[1] (76/144)
 			/// P93[0] (69/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 					PORT7::PMR.B3 = 0;
@@ -573,11 +573,11 @@ namespace device {
 					break;
 				}
 				break;
-			/// 6B:
+			/// GTIOC6B:
 			/// P76[1] (73/144)
 			/// P90[0] (72/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 					PORT7::PMR.B6 = 0;
@@ -603,16 +603,16 @@ namespace device {
 		}
 
 
-		static bool gptw7_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw7_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 			switch(ch) {
-			/// 7A:
+			/// GTIOC7A:
 			/// P12[1] ( 3/144)
 			/// P95[1] (67/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					PORT1::PMR.B2 = 0;
 					MPC::P12PFS.PSEL = sel;
@@ -628,20 +628,20 @@ namespace device {
 					break;
 				}
 				break;
-			/// 7B:
-			/// P76[1] (73/144)
-			/// P90[1] (72/144)
+			/// GTIOC7B:
+			/// P15[1]
+			/// P92[1]
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
-					PORT7::PMR.B6 = 0;
-					MPC::P76PFS.PSEL = sel;
-					PORT7::PMR.B6 = ena;
+					PORT1::PMR.B5 = 0;
+					MPC::P15PFS.PSEL = sel;
+					PORT1::PMR.B5 = ena;
 					break;
 				case ORDER::SECOND:
-					PORT9::PMR.B0 = 0;
-					MPC::P90PFS.PSEL = sel;
-					PORT9::PMR.B0 = ena;
+					PORT9::PMR.B2 = 0;
+					MPC::P92PFS.PSEL = sel;
+					PORT9::PMR.B2 = ena;
 					break;
 				default:
 					ret = false;
@@ -656,16 +656,16 @@ namespace device {
 		}
 
 
-		static bool gptw8_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw8_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 			switch(ch) {
-			/// 8A:
+			/// GTIOC8A:
 			/// P13[1] ( 2/144)
 			/// P94[1] (68/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					PORT1::PMR.B3 = 0;
 					MPC::P13PFS.PSEL = sel;
@@ -681,11 +681,11 @@ namespace device {
 					break;
 				}
 				break;
-			/// 8B:
+			/// GTIOC8B:
 			/// P16[1] (143/144)
 			/// P91[1] ( 71/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					PORT1::PMR.B6 = 0;
 					MPC::P16PFS.PSEL = sel;
@@ -708,16 +708,16 @@ namespace device {
 		}
 
 
-		static bool gptw9_(CHANNEL ch, bool ena, bool neg, ORDER opt) noexcept
+		static bool gptw9_(CHANNEL ch, bool ena, bool neg, ORDER order) noexcept
 		{
 			bool ret = true;
 			uint8_t sel = ena ? (neg ? 0b010111 : 0b010101) : 0;
 			switch(ch) {
-			/// 9A:
+			/// GTIOC9A:
 			/// P14[1] ( 1/144)
 			/// P93[1] (69/144)
 			case CHANNEL::A:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					PORT1::PMR.B4 = 0;
 					MPC::P14PFS.PSEL = sel;
@@ -733,11 +733,11 @@ namespace device {
 					break;
 				}
 				break;
-			/// 9B:
+			/// GTIOC9B:
 			/// P17[1] (142/144)
 			/// P90[1] ( 72/144)
 			case CHANNEL::B:
-				switch(opt) {
+				switch(order) {
 				case ORDER::FIRST:
 					PORT1::PMR.B7 = 0;
 					MPC::P17PFS.PSEL = sel;
@@ -768,13 +768,13 @@ namespace device {
 			@param[in]	ch	チャネル
 			@param[in]	ena	無効にする場合「false」
 			@param[in]	neg	反転入出力の場合「true」
-			@param[in]	opt	候補を選択する場合
+			@param[in]	order	候補を選択する場合
 			@return 無効な周辺機器の場合「false」
 		*/
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-		static bool turn(peripheral per, CHANNEL ch, bool ena = true, bool neg = false, ORDER opt = ORDER::FIRST) noexcept
+		static bool turn(peripheral per, CHANNEL ch, bool ena = true, bool neg = false, ORDER order = ORDER::FIRST) noexcept
 		{
-			if(opt == ORDER::BYPASS) return true;
+			if(order == ORDER::BYPASS) return true;
 
 			MPC::PWPR.B0WI  = 0;	// PWPR 書き込み許可
 			MPC::PWPR.PFSWE = 1;	// PxxPFS 書き込み許可
@@ -782,34 +782,34 @@ namespace device {
 			bool ret = false;
 			switch(per) {
 			case peripheral::GPTW0:
-				ret = gptw0_(ch, ena, neg, opt);
+				ret = gptw0_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW1:
-				ret = gptw1_(ch, ena, neg, opt);
+				ret = gptw1_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW2:
-				ret = gptw2_(ch, ena, neg, opt);
+				ret = gptw2_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW3:
-				ret = gptw3_(ch, ena, neg, opt);
+				ret = gptw3_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW4:
-				ret = gptw4_(ch, ena, neg, opt);
+				ret = gptw4_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW5:
-				ret = gptw5_(ch, ena, neg, opt);
+				ret = gptw5_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW6:
-				ret = gptw6_(ch, ena, neg, opt);
+				ret = gptw6_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW7:
-				ret = gptw7_(ch, ena, neg, opt);
+				ret = gptw7_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW8:
-				ret = gptw8_(ch, ena, neg, opt);
+				ret = gptw8_(ch, ena, neg, order);
 				break;
 			case peripheral::GPTW9:
-				ret = gptw9_(ch, ena, neg, opt);
+				ret = gptw9_(ch, ena, neg, order);
 				break;
 			default:
 				break;
