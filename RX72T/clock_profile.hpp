f706d48e8c7fd117d0fe85154d941659bd0b0de6
hirakuni45 <hira@rvf-rc45.net>
2021-07-03 11:30:34 +0900

    Update: cleanup

--- RX72T/clock_profile.hpp
+++ RX72T/clock_profile.hpp
@@ -20,15 +20,28 @@ namespace device {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	class clock_profile {
     public:
+#ifdef USE_USB
         static const uint32_t   BASE        =  16'000'000;      ///< 外部接続クリスタル
-        static const uint32_t   PLL_BASE    = 192'000'000;      ///< PLL ベースクロック
+        static const uint32_t   PLL_BASE    = 192'000'000;      ///< PLL ベースクロック（最大200MHz）
 
-        static const uint32_t   ICLK        = 192'000'000;      ///< ICLK 周波数
-		static const uint32_t   PCLKA       =  96'000'000;      ///< PCLKA 周波数
-        static const uint32_t   PCLKB       =  48'000'000;      ///< PCLKB 周波数
-        static const uint32_t   PCLKC       = 192'000'000;      ///< PCLKC 周波数
-        static const uint32_t   PCLKD       =  48'000'000;      ///< PCLKD 周波数
-        static const uint32_t   FCLK        =  48'000'000;      ///< FCLK 周波数
-        static const uint32_t   BCLK        =  48'000'000;      ///< BCLK 周波数
+        static const uint32_t   ICLK        = 192'000'000;      ///< ICLK 周波数（最大200MHz）
+		static const uint32_t   PCLKA       =  96'000'000;      ///< PCLKA 周波数（最大120MHz）
+        static const uint32_t   PCLKB       =  48'000'000;      ///< PCLKB 周波数（最大60MHz）
+        static const uint32_t   PCLKC       = 192'000'000;      ///< PCLKC 周波数（最大200MHz）
+        static const uint32_t   PCLKD       =  48'000'000;      ///< PCLKD 周波数（最大60MHz）
+        static const uint32_t   FCLK        =  48'000'000;      ///< FCLK 周波数（最大60MHz）
+        static const uint32_t   BCLK        =  48'000'000;      ///< BCLK 周波数（最大60MHz）
+#else
+        static const uint32_t   BASE        =  16'000'000;      ///< 外部接続クリスタル
+        static const uint32_t   PLL_BASE    = 200'000'000;      ///< PLL ベースクロック
+
+        static const uint32_t   ICLK        = 200'000'000;      ///< ICLK 周波数
+		static const uint32_t   PCLKA       = 100'000'000;      ///< PCLKA 周波数
+        static const uint32_t   PCLKB       =  50'000'000;      ///< PCLKB 周波数
+        static const uint32_t   PCLKC       = 200'000'000;      ///< PCLKC 周波数
+        static const uint32_t   PCLKD       =  50'000'000;      ///< PCLKD 周波数
+        static const uint32_t   FCLK        =  50'000'000;      ///< FCLK 周波数
+        static const uint32_t   BCLK        =  50'000'000;      ///< BCLK 周波数
+#endif
     };
 }
