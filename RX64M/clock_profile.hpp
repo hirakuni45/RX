6b49b36d428680f150e8258223fe8e3bd490a743
hirakuni45 <hira@rvf-rc45.net>
2021-11-18 07:58:56 +0900

    Update: cleanup

--- RX64M/clock_profile.hpp
+++ RX64M/clock_profile.hpp
@@ -15,20 +15,24 @@ namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  クロック・プロファイル・クラス
+		@brief  クロック・プロファイル・クラス @n
+				・分周器の仕様を超える値を指定しない事 @n
+				・PLL_BASE は、0.5 倍単位 @n
+				・他は、PLL_BASE を基数とする整数除算値 @n
+				・詳細はハードウェアーマニュアル参照の事
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	class clock_profile {
-    public:
-        static const uint32_t   BASE        =  12'000'000;      ///< 外部接続クリスタル
-        static const uint32_t   PLL_BASE    = 240'000'000;      ///< PLL ベースクロック
+	public:
+		static constexpr uint32_t	BASE		=  12'000'000;		///< 外部接続クリスタル
+		static constexpr uint32_t	PLL_BASE	= 240'000'000;		///< PLL ベースクロック（最大240MHz）
 
-        static const uint32_t   ICLK        = 120'000'000;      ///< ICLK 周波数
-		static const uint32_t   PCLKA       = 120'000'000;      ///< PCLKA 周波数
-        static const uint32_t   PCLKB       =  60'000'000;      ///< PCLKB 周波数
-        static const uint32_t   PCLKC       =  60'000'000;      ///< PCLKC 周波数
-        static const uint32_t   PCLKD       =  60'000'000;      ///< PCLKD 周波数
-        static const uint32_t   FCLK        =  60'000'000;      ///< FCLK 周波数
-        static const uint32_t   BCLK        = 120'000'000;      ///< BCLK 周波数
+		static constexpr uint32_t	ICLK		= 120'000'000;		///< ICLK 周波数（最大120MHz）
+		static constexpr uint32_t	PCLKA		= 120'000'000;		///< PCLKA 周波数（最大120MHz）
+		static constexpr uint32_t	PCLKB		=  60'000'000;		///< PCLKB 周波数（最大60MHz）
+		static constexpr uint32_t	PCLKC		=  60'000'000;		///< PCLKC 周波数（最大60MHz）
+		static constexpr uint32_t	PCLKD		=  60'000'000;		///< PCLKD 周波数（最大60MHz）
+		static constexpr uint32_t	FCLK		=  60'000'000;		///< FCLK 周波数（最大60MHz）
+		static constexpr uint32_t	BCLK		= 120'000'000;		///< BCLK 周波数（最大120MHz）
     };
 }
--- RX66T/clock_profile.hpp
+++ RX66T/clock_profile.hpp
@@ -15,20 +15,24 @@ namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  クロック・プロファイル・クラス
+		@brief  クロック・プロファイル・クラス @n
+				・分周器の仕様を超える値を指定しない事 @n
+				・PLL_BASE は、0.5 倍単位 @n
+				・他は、PLL_BASE を基数とする整数除算値 @n
+				・詳細はハードウェアーマニュアル参照の事
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	class clock_profile {
-    public:
-        static const uint32_t   BASE        =  10'000'000;      ///< 外部接続クリスタル
-        static const uint32_t   PLL_BASE    = 160'000'000;      ///< PLL ベースクロック
+	public:
+		static constexpr uint32_t	BASE		=  10'000'000;		///< 外部接続クリスタル
+		static constexpr uint32_t	PLL_BASE	= 160'000'000;		///< PLL ベースクロック（最大160MHz）
 
-        static const uint32_t   ICLK        = 160'000'000;      ///< ICLK 周波数
-		static const uint32_t   PCLKA       =  80'000'000;      ///< PCLKA 周波数
-        static const uint32_t   PCLKB       =  40'000'000;      ///< PCLKB 周波数
-        static const uint32_t   PCLKC       = 160'000'000;      ///< PCLKC 周波数
-        static const uint32_t   PCLKD       =  40'000'000;      ///< PCLKD 周波数
-        static const uint32_t   FCLK        =  40'000'000;      ///< FCLK 周波数
-        static const uint32_t   BCLK        =  40'000'000;      ///< BCLK 周波数
+		static constexpr uint32_t	ICLK		= 160'000'000;		///< ICLK 周波数（最大160MHz）
+		static constexpr uint32_t	PCLKA		=  80'000'000;		///< PCLKA 周波数（最大120MHz）
+		static constexpr uint32_t	PCLKB		=  40'000'000;		///< PCLKB 周波数（最大60MHz）
+		static constexpr uint32_t	PCLKC		= 160'000'000;		///< PCLKC 周波数（最大160MHz）
+		static constexpr uint32_t	PCLKD		=  40'000'000;		///< PCLKD 周波数（最大60MHz）
+		static constexpr uint32_t	FCLK		=  40'000'000;		///< FCLK 周波数（最大60MHz）
+		static constexpr uint32_t	BCLK		=  40'000'000;		///< BCLK 周波数（最大60MHz）
     };
 }
