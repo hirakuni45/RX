00b489295c57714ea1c674f1d2c9b83b3761719b
hirakuni45 <hira@rvf-rc45.net>
2021-11-29 22:26:10 +0900

    Update: Add port order enum class 'RENESAS'

--- RX600/port_map_order.hpp
+++ RX600/port_map_order.hpp
@@ -26,34 +26,45 @@ namespace device {
 		*/
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 		enum class ORDER : uint8_t {
-			BYPASS,		///< ポートマップの設定をバイパスする場合
-			FIRST,		///< 第１候補
-			SECOND,		///< 第２候補
-			THIRD,		///< 第３候補
-			FOURTH,		///< 第４候補
-			FIFTH,		///< 第５候補
-			SIXTH,		///< 第６候補
-			SEVENTH,	///< 第７候補
-			EIGHTH,		///< 第８候補
-			NINTH,		///< 第９候補
-			TENTH,		///< 第１０候補
+			BYPASS,			///< ポートマップの設定をバイパスする場合
+			FIRST,			///< 第１候補
+			SECOND,			///< 第２候補
+			THIRD,			///< 第３候補
+			FOURTH,			///< 第４候補
+			FIFTH,			///< 第５候補
+			SIXTH,			///< 第６候補
+			SEVENTH,		///< 第７候補
+			EIGHTH,			///< 第８候補
+			NINTH,			///< 第９候補
+			TENTH,			///< 第１０候補
 
-			LOCAL0,		///< 独自の特殊な設定０
-			LOCAL1,		///< 独自の特殊な設定１
+			LOCAL0,			///< 独自の特殊な設定０
+			LOCAL1,			///< 独自の特殊な設定１
 
-			FIRST_I2C,	///< SCI ポートを簡易 I2C として使う場合、第１候補
-			SECOND_I2C,	///< SCI ポートを簡易 I2C として使う場合、第２候補
-			THIRD_I2C,	///< SCI ポートを簡易 I2C として使う場合、第３候補
+			FIRST_I2C,		///< SCI ポートを簡易 I2C として使う場合、第１候補
+			SECOND_I2C,		///< SCI ポートを簡易 I2C として使う場合、第２候補
+			THIRD_I2C,		///< SCI ポートを簡易 I2C として使う場合、第３候補
 
-			FIRST_SPI,	///< SCI ポートを簡易 SPI として使う場合、第１候補
-			SECOND_SPI,	///< SCI ポートを簡易 SPI として使う場合、第２候補
-			THIRD_SPI,	///< SCI ポートを簡易 SPI として使う場合、第３候補
+			FIRST_SPI,		///< SCI ポートを簡易 SPI として使う場合、第１候補
+			SECOND_SPI,		///< SCI ポートを簡易 SPI として使う場合、第２候補
+			THIRD_SPI,		///< SCI ポートを簡易 SPI として使う場合、第３候補
 
-			FIRST_MII,	///< ETHERC MII 接続、第１候補
-			SECOND_MII,	///< ETHERC MII 接続、第２候補
+			FIRST_MII,		///< ETHERC MII 接続、第１候補
+			SECOND_MII,		///< ETHERC MII 接続、第２候補
 
-			FIRST_RMII,	///< ETHERC RMII 接続、第１候補
-			SECOND_RMII,///< ETHERC RMII 接続、第２候補
+			FIRST_RMII,		///< ETHERC RMII 接続、第１候補
+			SECOND_RMII,	///< ETHERC RMII 接続、第２候補
+		};
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  ルネサス型 @n
+					ルネサス社が提供するボードに対する型
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class RENESAS : uint8_t {
+			RX72N_ENVISION_KIT,			///< RX72N Envision Kit に対する設定
 		};
     };
 }
