d76931dc68721cf1642ec8355138a6c1c1c3cf3e
hirakuni45 <hira@rvf-rc45.net>
2022-07-29 04:26:55 +0900

    Update: add overclock profile

--- RX66T/clock_profile.hpp
+++ RX66T/clock_profile.hpp
@@ -39,6 +39,7 @@ namespace device {
 	#else
 		static constexpr bool       TURN_USB    = false;			///< USB を使う場合「true」
 		static constexpr uint32_t	BASE		=  10'000'000;		///< 外部接続クリスタル
+#if 0
 		static constexpr uint32_t	PLL_BASE	= 160'000'000;		///< PLL ベースクロック（最大160MHz）
 
 		static constexpr uint32_t	ICLK		= 160'000'000;		///< ICLK 周波数（最大160MHz）
@@ -48,6 +49,18 @@ namespace device {
 		static constexpr uint32_t	PCLKD		=  40'000'000;		///< PCLKD 周波数（最大60MHz）
 		static constexpr uint32_t	FCLK		=  40'000'000;		///< FCLK 周波数（最大60MHz）
 		static constexpr uint32_t	BCLK		=  40'000'000;		///< BCLK 周波数（最大60MHz）
+#else
+		// Over clock... 動く、動くぞ・・・
+		static constexpr uint32_t	PLL_BASE	= 240'000'000;		///< PLL ベースクロック（最大160MHz）
+
+		static constexpr uint32_t	ICLK		= 240'000'000;		///< ICLK 周波数（最大160MHz）
+		static constexpr uint32_t	PCLKA		= 120'000'000;		///< PCLKA 周波数（最大120MHz）
+		static constexpr uint32_t	PCLKB		=  60'000'000;		///< PCLKB 周波数（最大60MHz）
+		static constexpr uint32_t	PCLKC		= 120'000'000;		///< PCLKC 周波数（最大160MHz）
+		static constexpr uint32_t	PCLKD		=  60'000'000;		///< PCLKD 周波数（最大60MHz）
+		static constexpr uint32_t	FCLK		=  60'000'000;		///< FCLK 周波数（最大60MHz）
+		static constexpr uint32_t	BCLK		=  60'000'000;		///< BCLK 周波数（最大60MHz）
+#endif
 #endif
     };
 }
--- RX72T/clock_profile.hpp
+++ RX72T/clock_profile.hpp
@@ -24,6 +24,7 @@ namespace device {
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	class clock_profile {
 	public:
+#if 0
 #ifdef USE_USB
 		static constexpr bool       TURN_USB    = true;				///< USB を使う場合「true」
 		static constexpr uint32_t	BASE		=  16'000'000;		///< 外部接続クリスタル
@@ -48,6 +49,19 @@ namespace device {
 		static constexpr uint32_t	PCLKD		=  50'000'000;		///< PCLKD 周波数
 		static constexpr uint32_t	FCLK		=  50'000'000;		///< FCLK 周波数
 		static constexpr uint32_t	BCLK		=  50'000'000;		///< BCLK 周波数
+#endif
+#else
+		static constexpr bool       TURN_USB    = true;				///< USB を使う場合「true」
+		static constexpr uint32_t	BASE		=  16'000'000;		///< 外部接続クリスタル
+		static constexpr uint32_t	PLL_BASE	= 240'000'000;		///< PLL ベースクロック（最大200MHz）
+
+		static constexpr uint32_t	ICLK		= 240'000'000;		///< ICLK 周波数（最大200MHz）
+		static constexpr uint32_t	PCLKA		= 120'000'000;		///< PCLKA 周波数（最大120MHz）
+		static constexpr uint32_t	PCLKB		=  60'000'000;		///< PCLKB 周波数（最大60MHz）
+		static constexpr uint32_t	PCLKC		= 240'000'000;		///< PCLKC 周波数（最大200MHz）
+		static constexpr uint32_t	PCLKD		=  60'000'000;		///< PCLKD 周波数（最大60MHz）
+		static constexpr uint32_t	FCLK		=  60'000'000;		///< FCLK 周波数（最大60MHz）
+		static constexpr uint32_t	BCLK		=  60'000'000;		///< BCLK 周波数（最大60MHz）
 #endif
 	};
 }
