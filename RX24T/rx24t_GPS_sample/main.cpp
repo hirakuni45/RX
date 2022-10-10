bef8bf98e1fbd71aa1cca674b2bd1747a073605a
hirakuni45 <hira@rvf-rc45.net>
2022-10-10 09:56:04 +0900

    Update: boost_1_74_0 path for gcc-11.3.0, system clock config

--- RX24T/rx24t_GPS_sample/main.cpp
+++ RX24T/rx24t_GPS_sample/main.cpp
@@ -63,31 +63,7 @@ int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
-	device::SYSTEM::PRCR = 0xA50B;	// クロック、低消費電力、関係書き込み許可
-
-	device::SYSTEM::MEMWAIT = 0b10; // 80MHz 動作 wait 設定
-
-	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
-	device::SYSTEM::OPCCR = 0;  // 高速モード選択
-	while(device::SYSTEM::OPCCR.OPCMTSF() != 0) asm("nop");
-
-	// clock osc 10MHz
-	device::SYSTEM::MOSCWTCR = 9;	// 4ms wait
-	// メインクロック・ドライブ能力設定、内部発信
-	device::SYSTEM::MOFCR = device::SYSTEM::MOFCR.MODRV21.b(1);
-	device::SYSTEM::MOSCCR.MOSTP = 0;  // メインクロック発振器動作
-	while(device::SYSTEM::OSCOVFSR.MOOVF() == 0) asm("nop");
-
-	device::SYSTEM::PLLCR.STC = 0b001111;		// PLL input: 1, PLL 8 倍(80MHz)
-	device::SYSTEM::PLLCR2.PLLEN = 0;			// PLL 動作
-	while(device::SYSTEM::OSCOVFSR.PLOVF() == 0) asm("nop");
-
-	device::SYSTEM::SCKCR = device::SYSTEM::SCKCR.FCK.b(2)		// 1/4 (80/4=20)
-						  | device::SYSTEM::SCKCR.ICK.b(0)		// 1/1 (80/1=80)
-						  | device::SYSTEM::SCKCR.PCKA.b(0)		// 1/1 (80/1=80)
-						  | device::SYSTEM::SCKCR.PCKB.b(1)		// 1/2 (80/2=40)
-						  | device::SYSTEM::SCKCR.PCKD.b(1);	// 1/2 (120/2=60)
-	device::SYSTEM::SCKCR3.CKSEL = 0b100;	///< PLL 選択
+	SYSTEM_IO::boost_master_clock();
 
 	{
 		// タイマー設定（６０Ｈｚ）
