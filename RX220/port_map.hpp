b5140050d927a231a68887052c9e12292ab2898d
hirakuni45 <hira@rvf-rc45.net>
2022-12-11 10:44:31 +0900

    Add: a new file (RX220)

---
+++ RX220/port_map.hpp
@@ -0,0 +1,279 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX63T グループ・ポート・マッピング
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2022 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "RX220/peripheral.hpp"
+#include "RX220/port.hpp"
+#include "RX220/mpc.hpp"
+#include "RX600/port_map_order.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  ポート・マッピング・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class port_map : public port_map_order {
+
+		static bool sub_1st_(peripheral per, bool enable) noexcept
+		{
+			switch(per) {
+#if 0
+			case peripheral::RIIC0:
+			// PB1/SCL0
+			// PB2/SDA0
+				{
+					uint8_t sel = enable ? 0b0'1111 : 0;
+					PORTB::PMR.B1 = 0;
+					PORTB::PMR.B2 = 0;
+					MPC::PB1PFS.PSEL = sel;
+					MPC::PB2PFS.PSEL = sel;
+					PORTB::PMR.B1 = enable;
+					PORTB::PMR.B2 = enable;
+				}
+				break;
+			case peripheral::RIIC1:
+			// P25/SCL1
+			// P26/SDA1
+				{
+					uint8_t sel = enable ? 0b0'1111 : 0;
+					PORT2::PMR.B5 = 0;
+					PORT2::PMR.B6 = 0;
+					MPC::P25PFS.PSEL = sel;
+					MPC::P26PFS.PSEL = sel;
+					PORT2::PMR.B5 = enable;
+					PORT2::PMR.B6 = enable;
+				}
+				break;
+
+			case peripheral::RSPI0:
+			// P24/RSPCKA
+			// P23/MOSIA
+			// P22/MISOA
+				{
+					uint8_t sel = enable ? 0b0'1101 : 0;
+					PORT2::PMR.B4 = 0;
+					PORT2::PMR.B3 = 0;
+					PORT2::PMR.B2 = 0;
+					MPC::P24PFS.PSEL = sel;
+					MPC::P22PFS.PSEL = sel;
+					MPC::P23PFS.PSEL = sel;
+					PORT2::PMR.B4 = enable;
+					PORT2::PMR.B2 = enable;
+					PORT2::PMR.B3 = enable;
+				}
+				break;
+			case peripheral::RSPI1:
+			// P24/RSPCKB
+			// P23/MOSIB
+			// P22/MISOB
+				{
+					uint8_t sel = enable ? 0b0'1110 : 0;
+					PORT2::PMR.B4 = 0;
+					PORT2::PMR.B3 = 0;
+					PORT2::PMR.B2 = 0;
+					MPC::P24PFS.PSEL = sel;
+					MPC::P23PFS.PSEL = sel;
+					MPC::P22PFS.PSEL = sel;
+					PORT2::PMR.B4 = enable;
+					PORT2::PMR.B3 = enable;
+					PORT2::PMR.B2 = enable;
+				}
+				break;
+
+			case peripheral::SCI0:
+			// P22/RXD0
+			// P23/TXD0
+				{
+					uint8_t sel = enable ? 0b0'1010 : 0;
+					PORT2::PMR.B2 = 0;
+					PORT2::PMR.B3 = 0;
+					MPC::P22PFS.PSEL = sel;
+					MPC::P23PFS.PSEL = sel;
+					PORT2::PMR.B2 = enable;
+					PORT2::PMR.B3 = enable;
+				}
+				break;
+#endif
+
+			case peripheral::SCI1:  // for BOOT serial port
+			// P30/RXD1
+			// P26/TXD1
+				{
+					uint8_t sel = enable ? 0b0'1010 : 0;
+					PORT3::PMR.B0 = 0;
+					PORT2::PMR.B6 = 0;
+					MPC::P30PFS.PSEL = sel;
+					MPC::P26PFS.PSEL = sel;
+					PORT3::PMR.B0 = enable;
+					PORT2::PMR.B6 = enable;
+				}
+				break;
+#if 0
+			case peripheral::SCI2:
+			// P03/RXD2
+			// P02/TXD2
+				{
+					uint8_t sel = enable ? 0b0'1010 : 0;
+					PORT0::PMR.B3 = 0;
+					PORT0::PMR.B2 = 0;
+					MPC::P03PFS.PSEL = sel;
+					MPC::P02PFS.PSEL = sel;
+					PORT0::PMR.B3 = enable;
+					PORT0::PMR.B2 = enable;
+				}
+				break;
+			case peripheral::SCI3:
+			// P34/RXD3
+			// P35/TXD3
+				{
+					uint8_t sel = enable ? 0b0'1010 : 0;
+					PORT3::PMR.B4 = 0;
+					PORT3::PMR.B5 = 0;
+					MPC::P34PFS.PSEL = sel;
+					MPC::P35PFS.PSEL = sel;
+					PORT3::PMR.B4 = enable;
+					PORT3::PMR.B5 = enable;
+				}
+				break;
+#endif
+			default:
+				return false;
+				break;
+			}
+			return true;
+		}
+
+
+		static bool sub_2nd_(peripheral per, bool enable) noexcept
+		{
+			switch(per) {
+#if 0
+			case peripheral::CAN1:
+			// PB6/CRX1
+			// PB5/CTX1
+				{
+					uint8_t sel = enable ? 0b1'0000 : 0;
+					PORTB::PMR.B6 = 0;
+					PORTB::PMR.B5 = 0;
+					MPC::PB6PFS.PSEL = sel;
+					MPC::PB5PFS.PSEL = sel;
+					PORTB::PMR.B6 = enable;
+					PORTB::PMR.B5 = enable;
+				}
+				break;
+
+			case peripheral::RSPI0:
+			// P24/RSPCKA
+			// P23/MOSIA
+			// P22/MISOA
+				{
+					uint8_t sel = enable ? 0b0'1101 : 0;
+					PORT2::PMR.B4 = 0;
+					PORT2::PMR.B3 = 0;
+					PORT2::PMR.B2 = 0;
+					MPC::P24PFS.PSEL = sel;
+					MPC::P22PFS.PSEL = sel;
+					MPC::P23PFS.PSEL = sel;
+					PORT2::PMR.B4 = enable;
+					PORT2::PMR.B2 = enable;
+					PORT2::PMR.B3 = enable;
+				}
+				break;
+			case peripheral::RSPI1:
+			// P24/RSPCKB
+			// P23/MOSIB
+			// P22/MISOB
+				{
+					uint8_t sel = enable ? 0b0'1110 : 0;
+					PORT2::PMR.B4 = 0;
+					PORT2::PMR.B3 = 0;
+					PORT2::PMR.B2 = 0;
+					MPC::P24PFS.PSEL = sel;
+					MPC::P23PFS.PSEL = sel;
+					MPC::P22PFS.PSEL = sel;
+					PORT2::PMR.B4 = enable;
+					PORT2::PMR.B3 = enable;
+					PORT2::PMR.B2 = enable;
+				}
+				break;
+#endif
+			default:
+				return false;
+				break;
+			}
+			return true;
+		}
+
+
+		static bool sub_3rd_(peripheral per, bool enable) noexcept
+		{
+			switch(per) {
+#if 0
+			case peripheral::SCI1:
+			// P93/RXD1
+			// P26/TXD1
+				{
+					uint8_t sel = enable ? 0b0'1010 : 0;
+					PORT9::PMR.B3 = 0;
+					PORT2::PMR.B6 = 0;
+					MPC::P93PFS.PSEL = sel;
+					MPC::P26PFS.PSEL = sel;
+					PORT9::PMR.B3 = enable;
+					PORT2::PMR.B6 = enable;
+				}
+				break;
+#endif
+			default:
+				return false;
+				break;
+			}
+			return true;
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  周辺機器別ポート切り替え
+			@param[in]	per		周辺機器タイプ
+			@param[in]	ena		無効にする場合「false」
+			@param[in]	odr		候補を選択する場合
+			@return 無効な周辺機器の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		static bool turn(peripheral per, bool ena = true, ORDER odr = ORDER::FIRST) noexcept
+		{
+			if(odr == ORDER::BYPASS) return true;
+
+			MPC::PWPR.B0WI  = 0;	// PWPR 書き込み許可
+			MPC::PWPR.PFSWE = 1;	// PxxPFS 書き込み許可
+
+			bool ret = false;
+			switch(odr) {
+			case ORDER::FIRST:
+				ret = sub_1st_(per, ena);
+				break;
+			case ORDER::SECOND:
+				ret = sub_2nd_(per, ena);
+				break;
+			case ORDER::THIRD:
+				ret = sub_3rd_(per, ena);
+				break;
+			default:
+				ret = false;
+				break;
+			}
+
+			MPC::PWPR = device::MPC::PWPR.B0WI.b();
+
+			return ret;
+		}
+	};
+}
