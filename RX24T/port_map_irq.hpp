925bcd960afe4ddb61238767beb7723555941409
hirakuni45 <hira@rvf-rc45.net>
2022-12-13 07:12:00 +0900

    Add: a new file

---
+++ RX24T/port_map_irq.hpp
@@ -0,0 +1,303 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	RX24T グループ・ポート・マッピング (IRQ)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2022 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include "RX24T/port.hpp"
+#include "RX24T/mpc.hpp"
+#include "RX24T/icu.hpp"
+#include "RX600/port_map_order.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  RX24T/IRQ ポート・マッピング・ユーティリティー
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class port_map_irq : public port_map_order {
+
+		static bool irq0_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P10
+			// P52
+			// PE5
+			case ORDER::FIRST:
+				PORT1::PMR.B0 = 0;
+				MPC::P10PFS.PSEL = 0;
+				MPC::P10PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				MPC::P52PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTE::PMR.B5 = 0;
+				MPC::PE5PFS.PSEL = 0;
+				MPC::PE5PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq1_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P11
+			// P53
+			// PA5
+			// PE4
+			case ORDER::FIRST:
+				PORT1::PMR.B1 = 0;
+				MPC::P11PFS.PSEL = 0;
+				MPC::P11PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				MPC::P53PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTA::PMR.B5 = 0;
+				MPC::PA5PFS.PSEL = 0;
+				MPC::PA5PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTE::PMR.B4 = 0;
+				MPC::PE4PFS.PSEL = 0;
+				MPC::PE4PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq2_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P00
+			// P54
+			// PD4
+			// PE3
+			case ORDER::FIRST:
+				PORT0::PMR.B0 = 0;
+				MPC::P00PFS.PSEL = 0;
+				MPC::P00PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				MPC::P54PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B4 = 0;
+				MPC::PD4PFS.PSEL = 0;
+				MPC::PD4PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTE::PMR.B3 = 0;
+				MPC::PE3PFS.PSEL = 0;
+				MPC::PE3PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq3_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P55
+			// PB4
+			// PD5
+			case ORDER::FIRST:
+				MPC::P55PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORTB::PMR.B4 = 0;
+				MPC::PB4PFS.PSEL = 0;
+				MPC::PB4PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B5 = 0;
+				MPC::PD5PFS.PSEL = 0;
+				MPC::PD5PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq4_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P01
+			// P60
+			// P96
+			case ORDER::FIRST:
+				PORT0::PMR.B1 = 0;
+				MPC::P01PFS.PSEL = 0;
+				MPC::P01PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				MPC::P60PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT9::PMR.B6 = 0;
+				MPC::P96PFS.PSEL = 0;
+				MPC::P96PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq5_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P02
+			// P61
+			// P70
+			// PB6
+			// PD6
+			case ORDER::FIRST:
+				PORT0::PMR.B2 = 0;
+				MPC::P02PFS.PSEL = 0;
+				MPC::P02PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				MPC::P61PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT7::PMR.B0 = 0;
+				MPC::P70PFS.PSEL = 0;
+				MPC::P70PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTB::PMR.B6 = 0;
+				MPC::PB6PFS.PSEL = 0;
+				MPC::PB6PFS.ISEL = ena;
+				break;
+			case ORDER::FIFTH:
+				PORTD::PMR.B6 = 0;
+				MPC::PD6PFS.PSEL = 0;
+				MPC::PD6PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq6_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P21
+			// P31
+			// P62
+			case ORDER::FIRST:
+				PORT2::PMR.B1 = 0;
+				MPC::P21PFS.PSEL = 0;
+				MPC::P21PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT3::PMR.B1 = 0;
+				MPC::P31PFS.PSEL = 0;
+				MPC::P31PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				MPC::P62PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq7_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P20
+			// P30
+			// P63
+			case ORDER::FIRST:
+				PORT2::PMR.B0 = 0;
+				MPC::P20PFS.PSEL = 0;
+				MPC::P20PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT3::PMR.B0 = 0;
+				MPC::P30PFS.PSEL = 0;
+				MPC::P30PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				MPC::P63PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  IRQ ポート切り替え
+			@param[in]	irqv	IRQ ベクター
+			@param[in]	ena		無効にする場合「false」
+			@param[in]	odr		候補選択
+			@return 無効な周辺機器の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		static bool turn(ICU::VECTOR irqv, bool ena = true, ORDER odr = ORDER::FIRST) noexcept
+		{
+			if(odr == ORDER::BYPASS) return true;
+
+			MPC::PWPR.B0WI  = 0;	// PWPR 書き込み許可
+			MPC::PWPR.PFSWE = 1;	// PxxPFS 書き込み許可
+
+			bool ret = true;
+			switch(irqv) {
+			case ICU::VECTOR::IRQ0:
+				ret = irq0_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ1:
+				ret = irq1_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ2:
+				ret = irq2_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ3:
+				ret = irq3_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ4:
+				ret = irq4_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ5:
+				ret = irq5_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ6:
+				ret = irq6_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ7:
+				ret = irq7_(ena, odr);
+				break;
+			default:
+				ret = false;
+				break;
+			}
+
+
+			MPC::PWPR = device::MPC::PWPR.B0WI.b();
+
+			return ret;
+		}
+	};
+}
---
+++ RX72N/port_map_irq.hpp
@@ -0,0 +1,552 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	RX72N グループ・ポート・マッピング (IRQ) @n
+			IRQx-DS 端子は、ディープスタンバイからの復帰が有効な端子 @n
+			  通常割り込み入力としても使え、通常「ORDER::FIRST」で選択する。 @n
+			  詳しくは、MPC の解説を参照する事。
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2022 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include "RX72N/port.hpp"
+#include "RX72N/mpc.hpp"
+#include "RX72N/icu.hpp"
+#include "RX600/port_map_order.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  RX72N/IRQ ポート・マッピング・ユーティリティー
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class port_map_irq : public port_map_order {
+
+		static bool irq0_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P30 (DS)
+			// P10
+			// PD0
+			case ORDER::FIRST:
+				PORT3::PMR.B0 = 0;
+				MPC::P30PFS.PSEL = 0;
+				MPC::P30PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B0 = 0;
+				MPC::P10PFS.PSEL = 0;
+				MPC::P10PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B0 = 0;
+				MPC::PD0PFS.PSEL = 0;
+				MPC::PD0PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq1_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P31 (DS)
+			// P11
+			// PD1
+			case ORDER::FIRST:
+				PORT3::PMR.B1 = 0;
+				MPC::P31PFS.PSEL = 0;
+				MPC::P31PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B1 = 0;
+				MPC::P11PFS.PSEL = 0;
+				MPC::P11PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B1 = 0;
+				MPC::PD1PFS.PSEL = 0;
+				MPC::PD1PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq2_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P32 (DS)
+			// P12
+			// PD2
+			case ORDER::FIRST:
+				PORT3::PMR.B2 = 0;
+				MPC::P32PFS.PSEL = 0;
+				MPC::P32PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B2 = 0;
+				MPC::P12PFS.PSEL = 0;
+				MPC::P12PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B2 = 0;
+				MPC::PD2PFS.PSEL = 0;
+				MPC::PD2PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq3_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P33 (DS)
+			// P13
+			// PD3
+			case ORDER::FIRST:
+				PORT3::PMR.B3 = 0;
+				MPC::P33PFS.PSEL = 0;
+				MPC::P33PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B3 = 0;
+				MPC::P13PFS.PSEL = 0;
+				MPC::P13PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B3 = 0;
+				MPC::PD3PFS.PSEL = 0;
+				MPC::PD3PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq4_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// PB1 (DS)
+			// P14
+			// P34
+			// PD4
+			// PF5
+			case ORDER::FIRST:
+				PORTB::PMR.B1 = 0;
+				MPC::PB1PFS.PSEL = 0;
+				MPC::PB1PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B4 = 0;
+				MPC::P14PFS.PSEL = 0;
+				MPC::P14PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT3::PMR.B4 = 0;
+				MPC::P34PFS.PSEL = 0;
+				MPC::P34PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTD::PMR.B4 = 0;
+				MPC::PD4PFS.PSEL = 0;
+				MPC::PD4PFS.ISEL = ena;
+				break;
+			case ORDER::FIFTH:
+				PORTF::PMR.B5 = 0;
+				MPC::PF5PFS.PSEL = 0;
+				MPC::PF5PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq5_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// PA4 (DS)
+			// P15
+			// PD5
+			// PE5
+			case ORDER::FIRST:
+				PORTA::PMR.B4 = 0;
+				MPC::PA4PFS.PSEL = 0;
+				MPC::PA4PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B5 = 0;
+				MPC::P15PFS.PSEL = 0;
+				MPC::P15PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B5 = 0;
+				MPC::PD5PFS.PSEL = 0;
+				MPC::PD5PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTE::PMR.B5 = 0;
+				MPC::PE5PFS.PSEL = 0;
+				MPC::PE5PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq6_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// PA3 (DS)
+			// P16
+			// PD6
+			// PE6
+			case ORDER::FIRST:
+				PORTA::PMR.B3 = 0;
+				MPC::PA3PFS.PSEL = 0;
+				MPC::PA3PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B6 = 0;
+				MPC::P16PFS.PSEL = 0;
+				MPC::P16PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B6 = 0;
+				MPC::PD6PFS.PSEL = 0;
+				MPC::PD6PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTE::PMR.B6 = 0;
+				MPC::PE6PFS.PSEL = 0;
+				MPC::PE6PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq7_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// PE2 (DS)
+			// P17
+			// PD7
+			// PE7
+			case ORDER::FIRST:
+				PORTE::PMR.B2 = 0;
+				MPC::PE2PFS.PSEL = 0;
+				MPC::PE2PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT1::PMR.B7 = 0;
+				MPC::P17PFS.PSEL = 0;
+				MPC::P17PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTD::PMR.B7 = 0;
+				MPC::PD7PFS.PSEL = 0;
+				MPC::PD7PFS.ISEL = ena;
+				break;
+			case ORDER::FOURTH:
+				PORTE::PMR.B7 = 0;
+				MPC::PE7PFS.PSEL = 0;
+				MPC::PE7PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq8_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P40 (DS)
+			// P00
+			// P20
+			case ORDER::FIRST:
+				MPC::P40PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT0::PMR.B0 = 0;
+				MPC::P00PFS.PSEL = 0;
+				MPC::P00PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT2::PMR.B0 = 0;
+				MPC::P20PFS.PSEL = 0;
+				MPC::P20PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq9_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P41 (DS)
+			// P01
+			// P21
+			case ORDER::FIRST:
+				MPC::P41PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT0::PMR.B1 = 0;
+				MPC::P01PFS.PSEL = 0;
+				MPC::P01PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT2::PMR.B1 = 0;
+				MPC::P21PFS.PSEL = 0;
+				MPC::P21PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq10_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P42 (DS)
+			// P02
+			// P55
+			case ORDER::FIRST:
+				MPC::P42PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT0::PMR.B2 = 0;
+				MPC::P02PFS.PSEL = 0;
+				MPC::P02PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT5::PMR.B5 = 0;
+				MPC::P55PFS.PSEL = 0;
+				MPC::P55PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq11_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P43 (DS)
+			// P03
+			// PA1
+			case ORDER::FIRST:
+				MPC::P43PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT0::PMR.B3 = 0;
+				MPC::P03PFS.PSEL = 0;
+				MPC::P03PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTA::PMR.B1 = 0;
+				MPC::PA1PFS.PSEL = 0;
+				MPC::PA1PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq12_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P44 (DS)
+			// PB0
+			// PC1
+			case ORDER::FIRST:
+				MPC::P44PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORTB::PMR.B0 = 0;
+				MPC::PB0PFS.PSEL = 0;
+				MPC::PB0PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTC::PMR.B1 = 0;
+				MPC::PC1PFS.PSEL = 0;
+				MPC::PC1PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq13_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P45 (DS)
+			// P05
+			// PC6
+			case ORDER::FIRST:
+				MPC::P45PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT0::PMR.B5 = 0;
+				MPC::P05PFS.PSEL = 0;
+				MPC::P05PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTC::PMR.B6 = 0;
+				MPC::PC6PFS.PSEL = 0;
+				MPC::PC6PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq14_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P46 (DS)
+			// PC0
+			// PC7
+			case ORDER::FIRST:
+				MPC::P46PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORTC::PMR.B0 = 0;
+				MPC::PC0PFS.PSEL = 0;
+				MPC::PC0PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORTC::PMR.B7 = 0;
+				MPC::PC7PFS.PSEL = 0;
+				MPC::PC7PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+		static bool irq15_(bool ena, ORDER odr) noexcept
+		{
+			switch(odr) {
+			// P47 (DS)
+			// P07
+			// P67
+			case ORDER::FIRST:
+				MPC::P47PFS.ISEL = ena;
+				break;
+			case ORDER::SECOND:
+				PORT0::PMR.B7 = 0;
+				MPC::P07PFS.PSEL = 0;
+				MPC::P07PFS.ISEL = ena;
+				break;
+			case ORDER::THIRD:
+				PORT6::PMR.B7 = 0;
+				MPC::P67PFS.PSEL = 0;
+				MPC::P67PFS.ISEL = ena;
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  IRQ ポート切り替え
+			@param[in]	irqv	IRQ ベクター
+			@param[in]	ena		無効にする場合「false」
+			@param[in]	odr		候補選択
+			@return 無効な周辺機器の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		static bool turn(ICU::VECTOR irqv, bool ena = true, ORDER odr = ORDER::FIRST) noexcept
+		{
+			if(odr == ORDER::BYPASS) return true;
+
+			MPC::PWPR.B0WI  = 0;	// PWPR 書き込み許可
+			MPC::PWPR.PFSWE = 1;	// PxxPFS 書き込み許可
+
+			bool ret = true;
+			switch(irqv) {
+			case ICU::VECTOR::IRQ0:
+				ret = irq0_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ1:
+				ret = irq1_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ2:
+				ret = irq2_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ3:
+				ret = irq3_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ4:
+				ret = irq4_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ5:
+				ret = irq5_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ6:
+				ret = irq6_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ7:
+				ret = irq7_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ8:
+				ret = irq8_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ9:
+				ret = irq9_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ10:
+				ret = irq10_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ11:
+				ret = irq11_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ12:
+				ret = irq12_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ13:
+				ret = irq13_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ14:
+				ret = irq14_(ena, odr);
+				break;
+			case ICU::VECTOR::IRQ15:
+				ret = irq15_(ena, odr);
+				break;
+			default:
+				ret = false;
+				break;
+			}
+
+
+			MPC::PWPR = device::MPC::PWPR.B0WI.b();
+
+			return ret;
+		}
+	};
+}
