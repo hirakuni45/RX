f117114445e1fb087040a420b9ede2c8d3a3f492
hirakuni45 <hira@rvf-rc45.net>
2023-03-06 02:19:32 +0900

    Add: a new file

---
+++ RX600/esc.hpp
@@ -0,0 +1,33 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	RX700 グループ　EtherCAT (ESC) 定義
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2023 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=====================================================================//
+#include "common/device.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  EtherCAT 定義
+		@param[in]	base	ベース・アドレス
+		@param[in]	per		ペリフェラル型
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <uint32_t base, peripheral per>
+	struct ethercat_t {
+
+		static constexpr auto PERIPHERAL = per;	///< ペリフェラル型
+
+
+
+
+
+	};
+
+}
