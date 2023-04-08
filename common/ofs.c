3a1343778a6c1ff8d082f2b59edb3cd1c7cb4e02
hirakuni45 <hira@rvf-rc45.net>
2023-04-09 01:47:06 +0900

    Update: OFS0, OFS1 option set memory management

--- common/ofs.c
+++ common/ofs.c
@@ -1,20 +1,23 @@
-//=====================================================================//
+//=========================================================================//
 /*! @file
-    @brief  ハードウェアー OFS 設定
+    @brief  ハードウェアー OFS 設定 @n
+			RX63[1N], RX64M, RX71M, RX65N @n
+			オプション機能選択レジスタのアドレスは、リンカーファイルに記述してある。 @n
+			通常マイコンによりアドレスは異なる。
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2018 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2018, 2023 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
-//=====================================================================//
+//=========================================================================//
 #include <stdint.h>
 #include "common/byte_order.h"
 
 // オプション機能選択レジスタ 0（OFS0）
-#define SECT_SPCC __attribute__ ((section (".spcc"))) /* 0x00120040 */
-#define SECT_OSIC __attribute__ ((section (".osic"))) /* 0x00120050 */
-#define SECT_OFS0 __attribute__ ((section (".ofs0"))) /* 0x00120068 */
-#define SECT_OFS1 __attribute__ ((section (".ofs1"))) /* 0x0012006C */
+// #define SECT_SPCC __attribute__ ((section (".spcc")))
+// #define SECT_OSIC __attribute__ ((section (".osic")))
+#define SECT_OFS0 __attribute__ ((section (".ofs0")))
+#define SECT_OFS1 __attribute__ ((section (".ofs1")))
 
 // オプション機能選択レジスタ 1（OFS1）
 // b1-b0: VDSEL[1:0]        電圧検出0レベル選択ビット
@@ -28,7 +31,11 @@
 // b8   : HOCOEN            HOCO発振有効ビット
 //                          0:  リセット後、HOCO発振が有効
 //                          1:  リセット後、HOCO発振が無効
+#if defined(SIG_RX64M) || defined(SIG_RX71M) || defined(SIG_RX65N) || defined(SIG_RX72N) || defined(SIG_RX72M)
 const uint32_t OFS1_value SECT_OFS1 = 0xfffffff9;  // 2.97V, LVDAS 有効
+#elif defined(SIG_RX631) || defined(SIG_RX63N)
+const uint32_t OFS1_value SECT_OFS1 = 0xfffffffB;  // LVDAS 有効
+#endif
 
 #if 0
 const unsigned long __SPCCreg SECT_SPCC = 0xffffffff;
@@ -55,4 +62,3 @@ const unsigned long __OFS0reg OFS_REG = 0xffffffff;
 // OFS1 register
 const unsigned long __OFS1reg OFS_REG = 0xffffffff;
 #endif
-
