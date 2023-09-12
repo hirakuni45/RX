4d9e400badbfcad99f8efc443de49da4c9bf216e
hirakuni45 <hira@rvf-rc45.net>
2023-09-13 04:12:55 +0900

    Update: cleanup

--- RX26T/port_map.hpp
+++ RX26T/port_map.hpp
@@ -54,102 +54,132 @@ namespace device {
 			return true;
 		}
 
-#if 0
+
 		static bool sci5_(ORDER opt, bool enable)
 		{
 			uint8_t sel = enable ? 0b00'1010 : 0;
 			switch(opt) {
+			case ORDER::FIRST_I2C:
+			case ORDER::FIRST_SPI:
+			// P70/SCK5
+				PORT7::PMR.B0 = 0;
+				MPC::P70PFS.PSEL = sel;
+				PORT7::PMR.B0 = enable;
 			case ORDER::FIRST:
-			// PE0/RXD5
-			// PD7/TXD5
-			// PD2/SCK5
-				if(clock) {
-					PORTD::PMR.B2 = 0;
-					MPC::PD2PFS.PSEL = sel;
-					PORTD::PMR.B2 = enable;
-				}
-				PORTE::PMR.B0 = 0;
-				PORTD::PMR.B7 = 0;
-				MPC::PE0PFS.PSEL = sel;
-				MPC::PD7PFS.PSEL = sel;
-				PORTE::PMR.B0 = enable;
-				PORTD::PMR.B7 = enable;
+			// P37/RXD5
+				PORT3::PMR.B7 = 0;
+				MPC::P37PFS.PSEL = sel;
+				PORT3::PMR.B7 = enable;
+			// P36/TXD5
+				PORT3::PMR.B6 = 0;
+				MPC::P36PFS.PSEL = sel;
+				PORT3::PMR.B6 = enable;
 				break;
+			case ORDER::SECOND_I2C:
+			case ORDER::SECOND_SPI:
+			// P70/SCK5
+				PORT7::PMR.B0 = 0;
+				MPC::P70PFS.PSEL = sel;
+				PORT7::PMR.B0 = enable;
 			case ORDER::SECOND:
-			// PD2/SCK5 (23/100) (28/144)
-			// PB6/RXD5 (27/100) (40/144)
-			// PB5/TXD5 (28/100) (41/144)
-				if(clock) {
-					PORTD::PMR.B2 = 0;
-					MPC::PD2PFS.PSEL = sel;
-					PORTD::PMR.B2 = enable;
-				}
+			// P91/RXD5
+				PORT9::PMR.B1 = 0;
+				MPC::P91PFS.PSEL = sel;
+				PORT9::PMR.B1 = enable;
+			// P90/TXD5
+				PORT9::PMR.B0 = 0;
+				MPC::P90PFS.PSEL = sel;
+				PORT9::PMR.B0 = enable;
+				break;
+			case ORDER::THIRD_I2C:
+			case ORDER::THIRD_SPI:
+			// PB7/SCK5
+				PORTB::PMR.B7 = 0;
+				MPC::PB7PFS.PSEL = sel;
+				PORTB::PMR.B7 = enable;
+			case ORDER::THIRD:
+			// PB6/RXD5
 				PORTB::PMR.B6 = 0;
-				PORTB::PMR.B5 = 0;
 				MPC::PB6PFS.PSEL = sel;
-				MPC::PB5PFS.PSEL = sel;
 				PORTB::PMR.B6 = enable;
+			// PB5/TXD5
+				PORTB::PMR.B5 = 0;
+				MPC::PB5PFS.PSEL = sel;
 				PORTB::PMR.B5 = enable;
 				break;
-
+			case ORDER::FOURTH_I2C:
+			case ORDER::FOURTH_SPI:
+			// PD2/SCK5
+				PORTD::PMR.B2 = 0;
+				MPC::PD2PFS.PSEL = sel;
+				PORTD::PMR.B2 = enable;
+			case ORDER::FOURTH:
+			// PE0/RXD5
+				PORTE::PMR.B0 = 0;
+				MPC::PE0PFS.PSEL = sel;
+				PORTE::PMR.B0 = enable;
+			// PD7/TXD5
+				PORTD::PMR.B7 = 0;
+				MPC::PD7PFS.PSEL = sel;
+				PORTD::PMR.B7 = enable;
+				break;
 			default:
 				return false;
 			}
 			return true;
 		}
-
 
 		static bool sci6_(ORDER opt, bool enable)
 		{
-			uint8_t sel = enable ? 0b001010 : 0;
+			uint8_t sel = enable ? 0b00'1010 : 0;
 			switch(opt) {
+			case ORDER::FIRST_I2C:
+			case ORDER::FIRST_SPI:
+			// P82/SCK6
+				PORT8::PMR.B2 = 0;
+				MPC::P82PFS.PSEL = sel;
+				PORT8::PMR.B2 = enable;
 			case ORDER::FIRST:
-			// PB3/SCK6 (32/100) (48/144)
-			// PB2/TXD6 (33/100) (49/144)
-			// PB1/RXD6 (34/100) (50/144)
-				if(clock) {
-					PORTB::PMR.B3 = 0;
-					MPC::PB3PFS.PSEL = sel;
-					PORTB::PMR.B3 = enable;
-				}
-				PORTB::PMR.B2 = 0;
-				PORTB::PMR.B1 = 0;
-				MPC::PB2PFS.PSEL = sel;
-				MPC::PB1PFS.PSEL = sel;
-				PORTB::PMR.B2 = enable;
-				PORTB::PMR.B1 = enable;
-				break;
-			case ORDER::SECOND:
-			// PB0/TXD6 (35/100) (51/144)
-			// PA5/RXD6 (36/100) (54/144)
-			// PA4/SCK6 (37/100) (55/144)
-				if(clock) {
-					PORTA::PMR.B4 = 0;
-					MPC::PA3PFS.PSEL = sel;
-					PORTA::PMR.B4 = enable;
-				}
-				PORTB::PMR.B0 = 0;
-				PORTA::PMR.B5 = 0;
-				MPC::PB0PFS.PSEL = sel;
-				MPC::PA5PFS.PSEL = sel;
-				PORTB::PMR.B0 = enable;
-				PORTA::PMR.B5 = enable;
-				break;
-			case ORDER::THIRD:
-			// P82/SCK6 (96/100) (137/144)
-			// P81/TXD6 (97/100) (138/144)
-			// P80/RXD6 (98/100) (139/144)
-				if(clock) {
-					PORT8::PMR.B2 = 0;
-					MPC::P82PFS.PSEL = sel;
-					PORT8::PMR.B2 = enable;
-				}
-				PORT8::PMR.B1 = 0;
+			// P80/RXD6
 				PORT8::PMR.B0 = 0;
-				MPC::P81PFS.PSEL = sel;
 				MPC::P80PFS.PSEL = sel;
-				PORT8::PMR.B1 = enable;
 				PORT8::PMR.B0 = enable;
+			// P81/TXD6
+				PORT8::PMR.B1 = 0;
+				MPC::P81PFS.PSEL = sel;
+				PORT8::PMR.B1 = enable;
+				break;
+			case ORDER::SECOND_I2C:
+			case ORDER::SECOND_SPI:
+			// PA4/SCK5
+				PORTA::PMR.B4 = 0;
+				MPC::PA4PFS.PSEL = sel;
+				PORTA::PMR.B4 = enable;
+			case ORDER::SECOND:
+			// P95/RXD5
+				PORT9::PMR.B5 = 0;
+				MPC::P95PFS.PSEL = sel;
+				PORT9::PMR.B5 = enable;
+			// PB0/TXD5
+				PORTB::PMR.B0 = 0;
+				MPC::PB0PFS.PSEL = sel;
+				PORTB::PMR.B0 = enable;
+				break;
+			case ORDER::THIRD_I2C:
+			case ORDER::THIRD_SPI:
+			// PB3/SCK5
+				PORTB::PMR.B3 = 0;
+				MPC::PB3PFS.PSEL = sel;
+				PORTB::PMR.B3 = enable;
+			case ORDER::THIRD:
+			// PB1/RXD5
+				PORTB::PMR.B1 = 0;
+				MPC::PB1PFS.PSEL = sel;
+				PORTB::PMR.B1 = enable;
+			// PB2/TXD5
+				PORTB::PMR.B2 = 0;
+				MPC::PB2PFS.PSEL = sel;
+				PORTB::PMR.B2 = enable;
 				break;
 			default:
 				return false;
@@ -157,7 +187,7 @@ namespace device {
 			return true;
 		}
 
-
+#if 0
 		static bool rsci8_(ORDER opt, bool enable)
 		{
 			uint8_t sel;
@@ -704,13 +734,13 @@ namespace device {
 			case peripheral::SCI1:
 				ret = sci1_(odr, ena);
 				break;
-#if 0
 			case peripheral::SCI5:
 				ret = sci5_(odr, ena);
 				break;
 			case peripheral::SCI6:
 				ret = sci6_(odr, ena);
 				break;
+#if 0
 			case peripheral::RSCI8:
 				ret = rsci8_(odr, ena);
 				break;
