d6ced871e4db09f8aed8de68299d619242448b9f
hirakuni45 <hira@rvf-rc45.net>
2023-09-13 04:09:45 +0900

    Add: a new file

---
+++ RX600/sci_k.hpp
@@ -0,0 +1,61 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	RX グループ SCIk 定義
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2023 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include "RX600/sci_base.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  SCIk クラス
+		@param[in]	base	ベース・アドレス
+		@param[in]	per		ペリフェラル型
+		@param[in]	txi		送信割り込みベクター
+		@param[in]	rxi		受信割り込みベクター
+		@param[in]	tei		送信終了割り込みベクター
+		@param[in]	eri		受信エラー割り込みベクター
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <uint32_t base, peripheral per, ICU::VECTOR txi, ICU::VECTOR rxi,
+		typename ICUT, ICUT tei, ICUT eri>
+	struct scik_t : public sci_gj_t<base> {
+
+		static constexpr auto PERIPHERAL = per;		///< ペリフェラル型
+		static constexpr auto TXI		 = txi;		///< 受信割り込みベクター
+		static constexpr auto RXI		 = rxi;		///< 送信割り込みベクター
+		static constexpr auto TEI		 = tei;		///< 送信終了割り込みベクター
+		static constexpr auto ERI		 = eri;		///< 受信エラー割り込みベクター
+		static constexpr auto PCLK		 = clock_profile::PCLKB;	///< PCLK 周波数
+
+		//ボーレート微調整
+		static constexpr bool SEMR_BRME  = true;	///< SEMR.BRME が利用可能な場合「true」
+		// ボーレート倍速
+		static constexpr bool SEMR_BGDM  = true;	///< SEMR.BGDM が利用可能な場合「true」
+		// ノイズフィルタ
+		static constexpr bool SEMR_NFEN  = true;	///< SEMR.NFEN が利用可能な場合「true」
+		// 調歩同期基本クロックセレクト拡張
+		static constexpr bool SEMR_ABCSE = true;	///< SEMR.ABCSE が利用可能な場合「true」
+		// 受信データフル
+		static constexpr bool SSR_RDRF  = true;		///< SSR.RDRF が利用可能な場合「true」
+		// 簡易 I2C 機能の有無
+		static constexpr bool I2C_SUB   = true;		///< 簡易 I2C が利用可能な場合「true」
+		// 簡易 SPI 機能の有無
+		static constexpr bool SPI_SUB   = true;		///< 簡易 SPI が利用可能な場合「true」
+	};
+
+#if defined(SIG_RX26T)
+	typedef scik_t<0x0008'A020, peripheral::SCI1, ICU::VECTOR::TXI1, ICU::VECTOR::RXI1,
+		ICU::GROUPBL0, ICU::GROUPBL0::TEI1, ICU::GROUPBL0::ERI1> SCI1;
+	typedef scik_t<0x0008'A0A0, peripheral::SCI5, ICU::VECTOR::TXI5, ICU::VECTOR::RXI5,
+		ICU::GROUPBL0, ICU::GROUPBL0::TEI5, ICU::GROUPBL0::ERI5> SCI5;
+	typedef scik_t<0x0008'A0C0, peripheral::SCI6, ICU::VECTOR::TXI6, ICU::VECTOR::RXI6,
+		ICU::GROUPBL0, ICU::GROUPBL0::TEI6, ICU::GROUPBL0::ERI6> SCI6;
+#endif
+}
