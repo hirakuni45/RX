04bdbaae824232b3d757f81cb7d3cbaca9c7513f
hirakuni45 <hira@rvf-rc45.net>
2023-12-25 01:34:08 +0900

    Update: cleanup

--- RX220/lvda.hpp
+++ RX220/lvda.hpp
@@ -1,13 +1,13 @@
 #pragma once
-//=====================================================================//
+//=========================================================================//
 /*!	@file
 	@brief	RX220 グループ LVDAa 定義
     @author 平松邦仁 (hira@rvf-rc45.net)
-	@copyright	Copyright (C) 2022 Kunihito Hiramatsu @n
+	@copyright	Copyright (C) 2022, 2023 Kunihito Hiramatsu @n
 				Released under the MIT license @n
 				https://github.com/hirakuni45/RX/blob/master/LICENSE
 */
-//=====================================================================//
+//=========================================================================//
 #include "common/io_utils.hpp"
 
 namespace device {
