137024ec833c2eb7b64281f56fb556b3a19e4b58
hirakuni45 <hira@rvf-rc45.net>
2024-01-31 04:06:38 +0900

    Add: Support RX140

---
+++ RX140/da.hpp
@@ -0,0 +1,133 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	RX140 グループ・D/A 定義
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2024 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include "common/device.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  D/A コンバータ（DA）
+		@param[in]	per		ペリフェラル型
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <peripheral per>
+	struct da_t {
+
+		static constexpr auto PERIPHERAL = per;		///< ペリフェラル型
+		static constexpr uint32_t RESOLUTION = 8;	///< D/A 変換分解能
+		static constexpr uint32_t CHANNEL = 2;		///< チャネル数
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  アナログ入出力型
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class ANALOG : uint8_t {
+			DA0,
+			DA1,
+		};
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A ポートをアナログ出力に設定
+			@param[in]	an		アナログ型
+			@param[in]	ena		不許可の場合は「false」
+		*/
+		//-----------------------------------------------------------------//
+		static void enable(ANALOG an, bool f = true) noexcept
+		{
+			switch(an) {
+			case ANALOG::DA0:  // P03
+				break;
+			case ANALOG::DA1:  // P05
+				break;
+			default:
+				break;
+			}
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A データレジスタ 0（DADR0）
+		*/
+		//-----------------------------------------------------------------//
+		static inline rw16_t<0x0008'80C0> DADR0;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A データレジスタ 1（DADR1）
+		*/
+		//-----------------------------------------------------------------//
+		static inline rw16_t<0x0008'80C2> DADR1;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A 制御レジスタ（DACR）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct dacr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B6> DAOE0;
+			bit_rw_t<io_, bitpos::B7> DAOE1;
+		};
+		static inline dacr_t<0x0008'80C4> DACR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  DADR0 フォーマット選択レジスタ（DADPR）
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct dadpr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B7> DPSEL;
+		};
+		static inline dadpr_t<0x0008'80C5> DADPR;
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  D/A A/D 同期スタート制御レジスタ (DAADSCR)
+			@param[in]	ofs	オフセット
+		*/
+		//-----------------------------------------------------------------//
+		template <uint32_t ofs>
+		struct daadscr_t : public rw8_t<ofs> {
+			typedef rw8_t<ofs> io_;
+			using io_::operator =;
+			using io_::operator ();
+			using io_::operator |=;
+			using io_::operator &=;
+
+			bit_rw_t<io_, bitpos::B7> DAADST;
+		};
+		static inline daadscr_t<0x0008'80C6> DAADSCR;
+	};
+	typedef da_t<peripheral::DA> DA;
+}
---
+++ RX140/system_io.hpp
@@ -0,0 +1,196 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	RX140 システム制御
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2024 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include "RX140/system.hpp"
+#include "RX140/clock_profile.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  systen_io クラス
+		@param[in]	OSCT	発信器タイプを設定
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <clock_profile::OSC_TYPE OSCT>
+	class system_io {
+
+		static constexpr bool check_base_clock_() noexcept
+		{
+			bool ret = false;
+			switch(OSCT) {
+			case clock_profile::OSC_TYPE::XTAL:
+				if(clock_profile::BASE >= 1'000'000 && clock_profile::BASE <= 20'000'000) {
+					ret = true;
+				}
+				break;
+			case clock_profile::OSC_TYPE::CERA:  // 16MHz to 20MHz lead parts
+				if(clock_profile::BASE >= 16'000'000 && clock_profile::BASE <= 20'000'000) {
+					ret = true;
+				}
+				break;
+			case clock_profile::OSC_TYPE::EXT:
+				if(clock_profile::BASE > 0 && clock_profile::BASE <= 20'000'000) {
+					ret = true;
+				}
+				break;
+			case clock_profile::OSC_TYPE::HOCO:
+				// 32MHz, `54MHz
+				if(clock_profile::BASE == 32'000'000) {
+					ret = true;
+				} else if(clock_profile::BASE == 54'000'000) {
+					ret = true;
+				}
+				break;
+			default:
+				ret = true;
+				break;
+			}
+			return ret;
+		}
+
+		static constexpr uint8_t clock_div_(uint32_t clk) noexcept
+		{
+			uint8_t div = 0;
+			while(clk < clock_profile::PLL_BASE) {
+				++div;
+				clk <<= 1;
+			}
+			if(div > 0b0110) div = 0b111;
+			return div;
+		}
+
+		static constexpr bool check_clock_div_(uint32_t clk) noexcept
+		{
+			auto div = clock_div_(clk);
+			if(div > 0b0110) {
+				return false;  // overflow
+			}
+			if((clk << div) != ((clock_profile::PLL_BASE) & (0xffffffff << div))) {
+				return false;  // 割り切れない周期
+			}
+			return true;
+		}
+
+	public:
+		//---------------------------------------------------------------------//
+		/*!
+			@brief  システム・クロックの設定
+			@return エラーなら「false」
+		*/
+		//---------------------------------------------------------------------//
+		static bool boost_master_clock()
+		{
+			static_assert(check_base_clock_(), "BASE to overflow.");
+
+			// クロック関係書き込み許可
+			SYSTEM::PRCR = SYSTEM::PRCR.PRKEY.b(0xA5) | SYSTEM::PRCR.PRC0.b(1);
+
+			switch(OSCT) {
+			case clock_profile::OSC_TYPE::XTAL:
+				{
+					SYSTEM::MOSCCR.MOSTP = 0;  // enable
+					uint8_t drv21 = clock_profile::BASE >= 10'000'000;
+					SYSTEM::MOFCR = SYSTEM::MOFCR.MOSEL.b(0) | SYSTEM::MOFCR.MODRV21.b(drv21);
+					// wait
+					SYSTEM::SCKCR3.CKSEL = 0b010;
+				}
+				break;
+			case clock_profile::OSC_TYPE::CERA:
+				SYSTEM::MOSCCR.MOSTP = 0;  // enable
+				SYSTEM::MOFCR = SYSTEM::MOFCR.MOSEL.b(0);
+				// wait
+				SYSTEM::SCKCR3.CKSEL = 0b010;
+				break;
+			case clock_profile::OSC_TYPE::EXT:
+				SYSTEM::SCKCR3.CKSEL = 0b010;
+				SYSTEM::MOFCR = SYSTEM::MOFCR.MOSEL.b(1);
+				SYSTEM::MOSCWTCR.MSTS = 0;
+				SYSTEM::MOSCCR = 1;
+				// wait
+				SYSTEM::SCKCR3.CKSEL = 0b010;
+				break;
+			case clock_profile::OSC_TYPE::HOCO:
+				if(clock_profile::BASE == 24'000'000) {
+
+				} else if(clock_profile::BASE == 32'000'000) {
+
+				} else if(clock_profile::BASE == 48'000'000) {
+
+				}
+				SYSTEM::HOCOCR.HCSTP = 0;
+				// wait
+				SYSTEM::SCKCR3.CKSEL = 0b001;
+				break;
+			default:
+				SYSTEM::PRCR = SYSTEM::PRCR.PRKEY.b(0xA5);
+				return true;  // LOCO (125KHz)
+			}
+
+			static_assert(check_clock_div_(clock_profile::ICLK),  "ICLK can't divided.");
+			static_assert(check_clock_div_(clock_profile::PCLKB), "PCLKB can't divided.");
+			static_assert(check_clock_div_(clock_profile::PCLKD), "PCLKD can't divided.");
+			static_assert(check_clock_div_(clock_profile::FCLK),  "FCLK can't divided.");
+
+			SYSTEM::SCKCR = SYSTEM::SCKCR.ICK.b(clock_div_(clock_profile::ICLK))
+						  | SYSTEM::SCKCR.PCKB.b(clock_div_(clock_profile::PCLKB))
+						  | SYSTEM::SCKCR.PCKD.b(clock_div_(clock_profile::PCLKD))
+						  | SYSTEM::SCKCR.FCK.b(clock_div_(clock_profile::FCLK));
+			{
+				volatile auto tmp = device::SYSTEM::SCKCR();  // dummy read
+			}
+
+			if(clock_profile::ICLK > 32'000'000) {
+				// 動作モード切替（高速モードへ変更）
+				while(device::SYSTEM::OPCCR.OPCMTSF() != 0) { asm("nop"); }
+				device::SYSTEM::OPCCR.OPCM = 0b000;
+				while(device::SYSTEM::OPCCR.OPCMTSF() != 0) { asm("nop"); }
+
+				// Setup memory wait cycle register
+				FLASH::MEMWAITR = FLASH::MEMWAITR.MEKEY.b(0xAA) | FLASH::MEMWAITR.MEMWAIT.b(1);
+				FLASH::DFLWAITR = FLASH::DFLWAITR.DFKEY.b(0xAA) | FLASH::DFLWAITR.DFLWAIT.b(1);
+			} else {
+				FLASH::MEMWAITR = FLASH::MEMWAITR.MEKEY.b(0xAA) | FLASH::MEMWAITR.MEMWAIT.b(0);
+				FLASH::DFLWAITR = FLASH::DFLWAITR.DFKEY.b(0xAA) | FLASH::DFLWAITR.DFLWAIT.b(0);
+			}
+
+			// Master PLL settings
+			// Min: x4 (0b000111), Max: 12 (0b010111)
+			static_assert((clock_profile::PLL_BASE * 2 / clock_profile::BASE) >= 8, "PLL_BASE clock divider underflow. (x10)");
+			static_assert((clock_profile::PLL_BASE * 2 / clock_profile::BASE) <= 24, "PLL_BASE clock divider overflow. (x30)");
+			static_assert((clock_profile::PLL_BASE * 2 % clock_profile::BASE) == 0, "PLL_BASE clock can't divided.");
+			device::SYSTEM::PLLCR.STC = (clock_profile::PLL_BASE * 2 / clock_profile::BASE) - 1;
+			device::SYSTEM::PLLCR2.PLLEN = 0;  // PLL 動作
+			{
+				volatile auto tmp = device::SYSTEM::PLLCR2();
+			}
+			while(device::SYSTEM::OSCOVFSR.PLOVF() == 0) { asm("nop"); }
+
+			device::SYSTEM::SCKCR3.CKSEL = 0b100;   // PLL 選択
+			{  // dummy read register
+				volatile auto tmp = device::SYSTEM::SCKCR3();
+			}
+
+			if(OSCT == clock_profile::OSC_TYPE::XTAL || OSCT == clock_profile::OSC_TYPE::EXT) {
+				device::SYSTEM::LOCOCR.LCSTP = 1;  // 低速オンチップオシレータ停止
+				device::SYSTEM::HOCOCR.HCSTP = 1;  // 高速オンチップオシレータ停止
+			} else if(OSCT == clock_profile::OSC_TYPE::HOCO) {
+				device::SYSTEM::LOCOCR.LCSTP = 1;  // 低速オンチップオシレータ停止
+			}
+
+			device::SYSTEM::SOSCCR.SOSTP.b(!clock_profile::TURN_SBC);
+
+			device::SYSTEM::PRCR = 0xA500;	// クロック関係書き込み不許可
+
+			return true;
+		}
+	};
+}
+typedef device::system_io<device::clock_profile::OSCT> SYSTEM_IO;
