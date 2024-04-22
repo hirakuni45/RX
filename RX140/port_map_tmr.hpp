0abce60af39e0f08b881826232629c661084e0fb
hirakuni45 <hira@rvf-rc45.net>
2024-04-23 01:46:14 +0900

    Add: a new file

---
+++ RX140/port_map_tmr.hpp
@@ -0,0 +1,371 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	RX140 グループ・ポート・マッピング (TMR)
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2024 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include "RX140/peripheral.hpp"
+#include "RX140/port.hpp"
+#include "RX140/mpc.hpp"
+#include "RX600/port_map_order.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  RX140 TMR ポート・マッピング・ユーティリティー
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	class port_map_tmr : public port_map_order {
+
+		static bool tmr0_(CHANNEL ch, bool ena, ORDER odr) noexcept
+		{
+			bool ret = true;
+			uint8_t sel = ena ? 0b0'0101 : 0;
+			switch(ch) {
+			case CHANNEL::TMO:  // TMO0
+			// PB3
+			// PH1
+				switch(odr) {
+				case ORDER::FIRST:
+					PORTB::PMR.B3 = 0;
+					MPC::PB3PFS.PSEL = sel;  // ok
+					PORTB::PMR.B3 = ena;
+					break;
+				case ORDER::SECOND:
+					PORTH::PMR.B1 = 0;
+					MPC::PH1PFS.PSEL = sel;  // ok
+					PORTH::PMR.B1 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMCI:  // TMCI0 
+			// P21
+			// PB1
+			// PH3
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT2::PMR.B1 = 0;
+					MPC::P21PFS.PSEL = sel;  // ok
+					PORT2::PMR.B1 = ena;
+					break;
+				case ORDER::SECOND:
+					PORTB::PMR.B1 = 0;
+					MPC::PB1PFS.PSEL = sel;  // ok
+					PORTB::PMR.B1 = ena;
+					break;
+				case ORDER::THIRD:
+					PORTH::PMR.B3 = 0;
+					MPC::PH3PFS.PSEL = sel;  // ok
+					PORTH::PMR.B3 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMRI:  // TMRI0 
+			// P20
+			// PA4
+			// PH2
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT2::PMR.B0 = 0;
+					MPC::P20PFS.PSEL = sel;  // ok
+					PORT2::PMR.B0 = ena;
+					break;
+				case ORDER::SECOND:
+					PORTA::PMR.B4 = 0;
+					MPC::PA4PFS.PSEL = sel;  // ok
+					PORTA::PMR.B4 = ena;
+					break;
+  				case ORDER::THIRD:
+					PORTH::PMR.B2 = 0;
+					MPC::PH2PFS.PSEL = sel;  // ok
+					PORTH::PMR.B2 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			default:
+				ret = false;
+				break;
+			}
+			return ret;
+		}
+
+
+		static bool tmr1_(CHANNEL ch, bool ena, ORDER odr) noexcept
+		{
+			bool ret = true;
+			uint8_t sel = ena ? 0b0'0101 : 0;
+			switch(ch) {
+			case CHANNEL::TMO:  // TMO1
+			// P17
+			// P26
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT1::PMR.B7 = 0;
+					MPC::P17PFS.PSEL = sel;  // ok
+					PORT1::PMR.B7 = ena;
+					break;
+				case ORDER::SECOND:
+					PORT2::PMR.B6 = 0;
+					MPC::P26PFS.PSEL = sel;  // ok
+					PORT2::PMR.B6 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMCI:  // TMCI1
+			// P12
+			// P54
+			// PC4
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT1::PMR.B2 = 0;
+					MPC::P12PFS.PSEL = sel;  // ok
+					PORT1::PMR.B2 = ena;
+					break;
+				case ORDER::SECOND:
+					PORT5::PMR.B4 = 0;
+					MPC::P54PFS.PSEL = sel;  // ok
+					PORT5::PMR.B4 = ena;
+					break;
+				case ORDER::THIRD:
+					PORTC::PMR.B4 = 0;
+					MPC::PC4PFS.PSEL = sel;  // ok
+					PORTC::PMR.B4 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMRI:  // TMRI1
+			// PB5
+				switch(odr) {
+				case ORDER::FIRST:
+					PORTB::PMR.B5 = 0;
+					MPC::PB5PFS.PSEL = sel;  // ok
+					PORTB::PMR.B5 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			default:
+				ret = false;
+				break;
+			}
+			return ret;
+		}
+
+
+		static bool tmr2_(CHANNEL ch, bool ena, ORDER odr) noexcept
+		{
+			bool ret = true;
+			uint8_t sel = ena ? 0b0'0101 : 0;
+			switch(ch) {
+			case CHANNEL::TMO:  // TMO2
+			// P16
+			// PC7
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT1::PMR.B6 = 0;
+					MPC::P16PFS.PSEL = sel;  // ok
+					PORT1::PMR.B6 = ena;
+					break;
+				case ORDER::SECOND:
+					PORTC::PMR.B7 = 0;
+					MPC::PC7PFS.PSEL = sel;  // ok
+					PORTC::PMR.B7 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMCI:  // TMCI2
+			// P15
+			// P31
+			// PC6
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT1::PMR.B5 = 0;
+					MPC::P15PFS.PSEL = sel;  // ok
+					PORT1::PMR.B5 = ena;
+					break;
+				case ORDER::SECOND:
+					PORT3::PMR.B1 = 0;
+					MPC::P31PFS.PSEL = sel;  // ok
+					PORT3::PMR.B1 = ena;
+					break;
+				case ORDER::THIRD:
+					PORTC::PMR.B6 = 0;
+					MPC::PC6PFS.PSEL = sel;  // ok
+					PORTC::PMR.B6 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMRI:  // TMRI2
+			// P14
+			// PC5
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT1::PMR.B4 = 0;
+					MPC::P14PFS.PSEL = sel;  // ok
+					PORT1::PMR.B4 = ena;
+					break;
+				case ORDER::SECOND:
+					PORTC::PMR.B5 = 0;
+					MPC::PC5PFS.PSEL = sel;  // ok
+					PORTC::PMR.B5 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			default:
+				ret = false;
+				break;
+			}
+			return ret;
+		}
+
+
+		static bool tmr3_(CHANNEL ch, bool ena, ORDER odr) noexcept
+		{
+			bool ret = true;
+			uint8_t sel = ena ? 0b0'0101 : 0;
+			switch(ch) {
+			case CHANNEL::TMO:  // TMO3
+			// P13
+			// P32
+			// P55
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT1::PMR.B3 = 0;
+					MPC::P13PFS.PSEL = sel;  // ok
+					PORT1::PMR.B3 = ena;
+					break;
+				case ORDER::SECOND:
+					PORT3::PMR.B2 = 0;
+					MPC::P32PFS.PSEL = sel;  // ok
+					PORT3::PMR.B2 = ena;
+					break;
+				case ORDER::THIRD:
+					PORT5::PMR.B5 = 0;
+					MPC::P55PFS.PSEL = sel;  // ok
+					PORT5::PMR.B5 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMCI:  // TMCI3 
+			// P27
+			// P34
+			// PA6
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT2::PMR.B7 = 0;
+					MPC::P27PFS.PSEL = sel;  // ok
+					PORT2::PMR.B7 = ena;
+					break;
+				case ORDER::SECOND:
+					PORT3::PMR.B4 = 0;
+					MPC::P34PFS.PSEL = sel;  // ok
+					PORT3::PMR.B4 = ena;
+					break;
+				case ORDER::THIRD:
+					PORTA::PMR.B6 = 0;
+					MPC::PA6PFS.PSEL = sel;  // ok
+					PORTA::PMR.B6 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			case CHANNEL::TMRI:  // TMRI3 
+			// P30
+				switch(odr) {
+				case ORDER::FIRST:
+					PORT3::PMR.B0 = 0;
+					MPC::P30PFS.PSEL = sel;  // ok
+					PORT3::PMR.B0 = ena;
+					break;
+				default:
+					ret = false;
+					break;
+				}
+				break;
+			default:
+				ret = false;
+				break;
+			}
+			return ret;
+		}
+
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  TMR 関係、チャネル別ポート切り替え
+			@param[in]	per	周辺機器タイプ
+			@param[in]	ch	チャネル
+			@param[in]	ena	無効にする場合「false」
+			@param[in]	odr	候補選択
+			@return 無効な周辺機器の場合「false」
+		*/
+		//-----------------------------------------------------------------//
+		static bool turn(peripheral per, CHANNEL ch, bool ena = true, ORDER odr = ORDER::FIRST) noexcept
+		{
+			if(odr == ORDER::BYPASS) return true;
+
+			MPC::PWPR.B0WI  = 0;	// PWPR 書き込み許可
+			MPC::PWPR.PFSWE = 1;	// PxxPFS 書き込み許可
+
+			bool ret = true;
+			switch(per) {
+			case peripheral::TMR0:
+				ret = tmr0_(ch, ena, odr);
+				break;
+			case peripheral::TMR1:
+				ret = tmr1_(ch, ena, odr);
+				break;
+			case peripheral::TMR2:
+				ret = tmr2_(ch, ena, odr);
+				break;
+			case peripheral::TMR3:
+				ret = tmr3_(ch, ena, odr);
+				break;
+			default:
+				ret = false;
+				break;
+			}
+
+			MPC::PWPR = device::MPC::PWPR.B0WI.b();
+
+			return ret;
+		}
+	};
+}
