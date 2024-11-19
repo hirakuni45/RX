19ed43a17f11ea07692f93f312651f54642fb379
hirakuni45 <hira@rvf-rc45.net>
2024-11-19 22:27:18 +0900

    Update: cleanup

--- RX231/cmpb.hpp
+++ RX231/cmpb.hpp
@@ -398,8 +398,146 @@ namespace device {
 	struct cmpb_t : public cmpb01_t, cmpb23_t {
 
 		static constexpr auto PERIPHERAL = peripheral::CMPB;	///< ペリフェラル型
-		static constexpr auto LVD1 = ICU::VECTOR::LVD1;			///< 割り込みベクター
-		static constexpr auto LVD2 = ICU::VECTOR::LVD2;			///< 割り込みベクター
+		static constexpr auto LVD1  = ICU::VECTOR::LVD1;		///< LVD1 割り込みベクター
+		static constexpr auto LVD2  = ICU::VECTOR::LVD2;		///< LVD2 割り込みベクター
+		static constexpr auto CMPB0 = ICU::VECTOR::CMPB0;		///< CMPB0 割り込みベクター
+		static constexpr auto CMPB1 = ICU::VECTOR::CMPB1;		///< CMPB1 割り込みベクター
+		static constexpr auto CMPB2 = ICU::VECTOR::CMPB2;		///< CMPB2 割り込みベクター
+		static constexpr auto CMPB3 = ICU::VECTOR::CMPB3;		///< CMPB3 割り込みベクター
+
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  アナログ入力型
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class ANALOG : uint8_t {
+			CMPA2,		///< PE4/CMPA2 入力
+			CMPB0,		///< PE1/CMPB0 入力
+			CVREFB0,	///< PE2/CVREFB0 入力
+			CMPB1,		///< PA3/CMPB1 入力
+			CVREFB1,	///< PA4/CVREFB1 入力
+			CMPB2,		///< P15/CMPB2 入力
+			CVREFB2,	///< P14/CVREFB2 入力
+			CMPB3,		///< P26/CMPB3 入力
+			CVREFB3,	///< P27/CVREFB3 入力
+			CMPOB0,		///< PE5/CMPOB0 出力
+			CMPOB1,		///< PB1/CMPOB1 出力
+			CMPOB2,		///< P17/CMPOB2 出力
+			CMPOB3,		///< P30/CMPOB3 出力
+		};
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief	ポート設定と解除
+			@param[in]	an	アナログ型
+			@param[in]	ena	ポート無効の場合「false」
+		*/
+		//-----------------------------------------------------------------//		
+		static void enable(ANALOG an, bool ena = true)
+		{
+			MPC::PWPR.B0WI  = 0;
+			MPC::PWPR.PFSWE = 1;
+
+			switch(an) {
+			case ANALOG::CMPA2:
+				if(ena) {
+					PORTE::PDR.B4 = 0;
+					PORTE::PMR.B4 = 0;
+				}
+				MPC::PE4PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPB0:
+				if(ena) {
+					PORTE::PDR.B1 = 0;
+					PORTE::PMR.B1 = 0;
+				}
+				MPC::PE1PFS.ASEL = ena;
+				break;
+			case ANALOG::CVREFB0:
+				if(ena) {
+					PORTE::PDR.B2 = 0;
+					PORTE::PMR.B2 = 0;
+				}
+				MPC::PE2PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPB1:
+				if(ena) {
+					PORTA::PDR.B3 = 0;
+					PORTA::PMR.B3 = 0;
+				}
+				MPC::PA3PFS.ASEL = ena;
+				break;
+			case ANALOG::CVREFB1:
+				if(ena) {
+					PORTA::PDR.B4 = 0;
+					PORTA::PMR.B4 = 0;
+				}
+				MPC::PA4PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPB2:
+				if(ena) {
+					PORT1::PDR.B5 = 0;
+					PORT1::PMR.B5 = 0;
+				}
+				MPC::P15PFS.ASEL = ena;
+				break;
+			case ANALOG::CVREFB2:
+				if(ena) {
+					PORT1::PDR.B4 = 0;
+					PORT1::PMR.B4 = 0;
+				}
+				MPC::P14PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPB3:
+				if(ena) {
+					PORT2::PDR.B6 = 0;
+					PORT2::PMR.B6 = 0;
+				}
+				MPC::P26PFS.ASEL = ena;
+				break;
+			case ANALOG::CVREFB3:
+				if(ena) {
+					PORT2::PDR.B7 = 0;
+					PORT2::PMR.B7 = 0;
+				}
+				MPC::P27PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPOB0:
+				if(ena) {
+					PORTE::PDR.B5 = 1;
+					PORTE::PMR.B5 = 0;
+				}
+//				MPC::PE5PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPOB1:
+				if(ena) {
+					PORTB::PDR.B1 = 1;
+					PORTB::PMR.B1 = 0;
+				}
+//				MPC::PB1PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPOB2:
+				if(ena) {
+					PORT1::PDR.B7 = 1;
+					PORT1::PMR.B7 = 0;
+				}
+//				MPC::P17PFS.ASEL = ena;
+				break;
+			case ANALOG::CMPOB3:
+				if(ena) {
+					PORT3::PDR.B0 = 1;
+					PORT3::PMR.B0 = 0;
+				}
+//				MPC::P30PFS.ASEL = ena;
+				break;
+			default:
+				break;
+			}
+
+			MPC::PWPR = device::MPC::PWPR.B0WI.b();
+		}
 	};
 #endif
 	typedef cmpb_t CMPB;
