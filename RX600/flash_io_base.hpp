01d26bf0a558d11a5e5bb0fa661fff9f8681fd0a
hirakuni45 <hira@rvf-rc45.net>
2024-11-26 10:21:05 +0900

    Add: a new file

---
+++ RX600/flash_io_base.hpp
@@ -0,0 +1,42 @@
+#pragma once
+//=========================================================================//
+/*!	@file
+	@brief	flash_io ベース・クラス
+    @author 平松邦仁 (hira@rvf-rc45.net)
+	@copyright	Copyright (C) 2024 Kunihito Hiramatsu @n
+				Released under the MIT license @n
+				https://github.com/hirakuni45/RX/blob/master/LICENSE
+*/
+//=========================================================================//
+#include <cstdint>
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  flash_io ベース・クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	struct flash_io_base {
+
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		/*!
+			@brief  エラー型
+		*/
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+		enum class ERROR : uint8_t {
+			NONE,		///< エラー無し
+			START,		///< 開始不良
+			CLOCK,		///< クロック設定不良
+			ADDRESS,	///< アドレス不良
+			LENGTH,		///< 長さ不良
+			BANK,		///< バンク数不良
+			TIMEOUT,	///< タイムアウト
+			WRITE,		///< 書き込みエラー
+			LOCK,		///< Lock エラー
+			ST_ILGL,	///< ILGL ステータス検出
+			ST_ERS,		///< ERS ステータス検出
+			ST_PRG,		///< PRG ステータス検出
+		};
+	};
+}
