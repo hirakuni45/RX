716cc8bf201568db23cff507c995c8197e364084
hirakuni45 <hira@rvf-rc45.net>
2024-12-22 04:37:09 +0900

    Update: cleanup

--- RX600/sci_base.hpp
+++ RX600/sci_base.hpp
@@ -1,7 +1,7 @@
 #pragma once
 //=========================================================================//
 /*!	@file
-	@brief	RX グループ SCI ベース定義
+	@brief	Serial Communications Interface base / シリアルコミュニケーションインタフェースベース
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2022, 2024 Kunihito Hiramatsu @n
 				Released under the MIT license @n
@@ -14,7 +14,7 @@ namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  SCIa クラス
+		@brief  SCI_a class
 		@param[in]	base	ベース・アドレス
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
