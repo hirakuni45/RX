74f602aad226ac54155733e2eaa0d3cfd6b72a06
hirakuni45 <hira@rvf-rc45.net>
2024-12-23 03:43:01 +0900

    Update: cleanup

--- RX140/mpc.hpp
+++ RX140/mpc.hpp
@@ -1,7 +1,7 @@
 #pragma once
 //=========================================================================//
 /*!	@file
-	@brief	RX140 グループ MPC 定義
+	@brief	Multi-Function Pin Controller / マルチファンクションピンコントローラ (RX140)
     @author 平松邦仁 (hira@rvf-rc45.net)
 	@copyright	Copyright (C) 2024 Kunihito Hiramatsu @n
 				Released under the MIT license @n
@@ -14,7 +14,7 @@ namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  マルチファンクションピンコントローラークラス
+		@brief  MPC class
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	struct mpc_t {
